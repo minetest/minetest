5e0c4266774a0acb73d4ade74ab811378c7b1131
Perttu Ahola <celeron55@gmail.com>
2010-12-25 03:34:27 +0200

    missing files

---
+++ src/guiMessageMenu.cpp
@@ -0,0 +1,152 @@
+/*
+Minetest-c55
+Copyright (C) 2010 celeron55, Perttu Ahola <celeron55@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "guiMessageMenu.h"
+#include "debug.h"
+#include "serialization.h"
+#include <string>
+
+GUIMessageMenu::GUIMessageMenu(gui::IGUIEnvironment* env,
+		gui::IGUIElement* parent, s32 id,
+		int *active_menu_count,
+		std::wstring message_text
+):
+	GUIModalMenu(env, parent, id, active_menu_count),
+	m_message_text(message_text),
+	m_status(false)
+{
+}
+
+GUIMessageMenu::~GUIMessageMenu()
+{
+	removeChildren();
+}
+
+void GUIMessageMenu::removeChildren()
+{
+	{
+		gui::IGUIElement *e = getElementFromId(256);
+		if(e != NULL)
+			e->remove();
+	}
+	{
+		gui::IGUIElement *e = getElementFromId(257);
+		if(e != NULL)
+			e->remove();
+	}
+}
+
+void GUIMessageMenu::regenerateGui(v2u32 screensize)
+{
+	/*
+		Remove stuff
+	*/
+	removeChildren();
+	
+	/*
+		Calculate new sizes and positions
+	*/
+	core::rect<s32> rect(
+			screensize.X/2 - 580/2,
+			screensize.Y/2 - 300/2,
+			screensize.X/2 + 580/2,
+			screensize.Y/2 + 300/2
+	);
+	
+	DesiredRect = rect;
+	recalculateAbsolutePosition(false);
+
+	v2s32 size = rect.getSize();
+
+	/*
+		Add stuff
+	*/
+	{
+		core::rect<s32> rect(0, 0, 400, 50);
+		rect = rect + v2s32(size.X/2-400/2, size.Y/2-50/2-25);
+		Environment->addStaticText(m_message_text.c_str(), rect, false,
+				true, this, 256);
+	}
+	{
+		core::rect<s32> rect(0, 0, 140, 30);
+		rect = rect + v2s32(size.X/2-140/2, size.Y/2-30/2+25);
+		gui::IGUIElement *e = 
+		Environment->addButton(rect, this, 257, L"Proceed");
+		Environment->setFocus(e);
+	}
+}
+
+void GUIMessageMenu::drawMenu()
+{
+	gui::IGUISkin* skin = Environment->getSkin();
+	if (!skin)
+		return;
+	video::IVideoDriver* driver = Environment->getVideoDriver();
+	
+	video::SColor bgcolor(140,0,0,0);
+	driver->draw2DRectangle(bgcolor, AbsoluteRect, &AbsoluteClippingRect);
+
+	gui::IGUIElement::draw();
+}
+
+bool GUIMessageMenu::OnEvent(const SEvent& event)
+{
+	if(event.EventType==EET_KEY_INPUT_EVENT)
+	{
+		if(event.KeyInput.Key==KEY_ESCAPE && event.KeyInput.PressedDown)
+		{
+			m_status = true;
+			quitMenu();
+			return true;
+		}
+		if(event.KeyInput.Key==KEY_RETURN && event.KeyInput.PressedDown)
+		{
+			m_status = true;
+			quitMenu();
+			return true;
+		}
+	}
+	if(event.EventType==EET_GUI_EVENT)
+	{
+		if(event.GUIEvent.EventType==gui::EGET_ELEMENT_FOCUS_LOST
+				&& isVisible())
+		{
+			if(!canTakeFocus(event.GUIEvent.Element))
+			{
+				dstream<<"GUIMessageMenu: Not allowing focus change."
+						<<std::endl;
+				// Returning true disables focus change
+				return true;
+			}
+		}
+		if(event.GUIEvent.EventType==gui::EGET_BUTTON_CLICKED)
+		{
+			switch(event.GUIEvent.Caller->getID())
+			{
+			case 257:
+				m_status = true;
+				quitMenu();
+				break;
+			}
+		}
+	}
+
+	return Parent ? Parent->OnEvent(event) : false;
+}
+
---
+++ src/guiMessageMenu.h
@@ -0,0 +1,61 @@
+/*
+Minetest-c55
+Copyright (C) 2010 celeron55, Perttu Ahola <celeron55@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef GUIMESSAGEMENU_HEADER
+#define GUIMESSAGEMENU_HEADER
+
+#include "common_irrlicht.h"
+#include "modalMenu.h"
+#include "utility.h"
+#include <string>
+
+class GUIMessageMenu : public GUIModalMenu
+{
+public:
+	GUIMessageMenu(gui::IGUIEnvironment* env,
+			gui::IGUIElement* parent, s32 id,
+			int *active_menu_count,
+			std::wstring message_text);
+	~GUIMessageMenu();
+	
+	void removeChildren();
+	/*
+		Remove and re-add (or reposition) stuff
+	*/
+	void regenerateGui(v2u32 screensize);
+
+	void drawMenu();
+
+	bool OnEvent(const SEvent& event);
+
+	/*
+		true = ok'd
+	*/
+	bool getStatus()
+	{
+		return m_status;
+	}
+	
+private:
+	std::wstring m_message_text;
+	bool m_status;
+};
+
+#endif
+
