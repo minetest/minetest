1c1cd90ab79dd5b4c31be183dff870b29886271d
donaldr3 <donald@fsf.org>
2011-09-27 13:31:39 -0400

    added bucket image listing

--- src/inventory.h
+++ src/inventory.h
@@ -367,6 +367,8 @@ public:
 			return "tool_mesepick.png";
 		else if(m_toolname == "WShovel")
 			return "tool_woodshovel.png";
+		else if(m_toolname == "WBucket")
+			basename = "bucket.png";
 		else if(m_toolname == "STShovel")
 			return "tool_stoneshovel.png";
 		else if(m_toolname == "SteelShovel")
