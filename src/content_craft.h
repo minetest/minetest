4f021fc7ddd175888a3bac2fcc413231a4ef5c3d
Perttu Ahola <celeron55@gmail.com>
2011-11-17 10:19:31 +0200

    Remove crafting definitions from content_craft.cpp

--- src/content_craft.h
+++ src/content_craft.h
@@ -24,12 +24,6 @@ class InventoryItem;
 class Player;
 class IGameDef;
 
-/*
-	items: actually *items[9]
-	return value: allocates a new item, or returns NULL.
-*/
-InventoryItem *craft_get_result(InventoryItem **items, IGameDef *gamedef);
-
 void craft_set_creative_inventory(Player *player, IGameDef *gamedef);
 
 // Called when give_initial_stuff setting is used
