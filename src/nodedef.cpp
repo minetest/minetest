cb589b1f997a3017908ed6b9b1b641732b303de8
Perttu Ahola <celeron55@gmail.com>
2011-12-01 14:12:47 +0200

    Make non-fancy leaf texture to go in texture atlas too

--- src/nodedef.cpp
+++ src/nodedef.cpp
@@ -532,8 +532,8 @@ public:
 					f->drawtype = NDT_NORMAL;
 					f->solidness = 1;
 					for(u32 i=0; i<6; i++){
-						f->tname_tiles[i] = f->tname_tiles[i]
-								+ std::string("^[noalpha");
+						f->setTexture(i, f->tname_tiles[i]
+								+ std::string("^[noalpha"));
 					}
 				}
 				break;
