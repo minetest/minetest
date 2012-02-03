202658678dbe7ed0e8782465d3622ec83904ea64
Perttu Ahola <celeron55@gmail.com>
2012-02-03 02:24:36 +0200

    Set default maximum viewing range to a bit more than default server send range to make fog work well

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -70,8 +70,8 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("wanted_fps", "30");
 	settings->setDefault("fps_max", "60");
 	// A bit more than the server will send around the player, to make fog blend well
-	settings->setDefault("viewing_range_nodes_min", "35");
 	settings->setDefault("viewing_range_nodes_max", "160");
+	settings->setDefault("viewing_range_nodes_min", "15");
 	settings->setDefault("screenW", "800");
 	settings->setDefault("screenH", "600");
 	settings->setDefault("address", "");
@@ -123,7 +123,7 @@ void set_default_settings(Settings *settings)
 	// This causes frametime jitter on client side, or does it?
 	settings->setDefault("max_simultaneous_block_sends_per_client", "4");
 	settings->setDefault("max_simultaneous_block_sends_server_total", "20");
-	settings->setDefault("max_block_send_distance", "10");
+	settings->setDefault("max_block_send_distance", "9");
 	settings->setDefault("max_block_generate_distance", "7");
 	settings->setDefault("time_send_interval", "5");
 	settings->setDefault("time_speed", "96");
