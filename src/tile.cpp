abab3db23d3d6e6fd3d51555ab355110a6e0540f
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2012-02-08 11:49:22 +0100

    tile: don't duplicate std::string::find_last_of

--- src/tile.cpp
+++ src/tile.cpp
@@ -1023,15 +1023,9 @@ video::IImage* generate_image_from_scratch(std::string name,
 	char separator = '^';
 
 	// Find last meta separator in name
-	s32 last_separator_position = -1;
-	for(s32 i=name.size()-1; i>=0; i--)
-	{
-		if(name[i] == separator)
-		{
-			last_separator_position = i;
-			break;
-		}
-	}
+	s32 last_separator_position = name.find_last_of(separator);
+	//if(last_separator_position == std::npos)
+	//	last_separator_position = -1;
 
 	/*infostream<<"generate_image_from_scratch(): "
 			<<"last_separator_position="<<last_separator_position
