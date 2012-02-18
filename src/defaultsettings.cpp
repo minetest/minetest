8b16e5f27d7cebc836eaf0513a39f915a902aa0b
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2012-02-18 18:51:19 +0200

    Anaglyph stereo support, created by xyz

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -52,7 +52,10 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("keymap_toggle_profiler", "KEY_F6");
 	settings->setDefault("keymap_increase_viewing_range_min", "+");
 	settings->setDefault("keymap_decrease_viewing_range_min", "-");
+	settings->setDefault("anaglyph", "false");
 	settings->setDefault("aux1_descends", "false");
+	settings->setDefault("anaglyph_strength", "0.01");
+
 	// Some (temporary) keys for debugging
 	settings->setDefault("keymap_print_debug_stacks", "KEY_KEY_P");
 	settings->setDefault("keymap_quicktune_prev", "KEY_HOME");
