10591d20806cffeb5d0989624ab9c7b376c0ea0c
Perttu Ahola <celeron55@gmail.com>
2012-03-27 19:57:24 +0300

    Set default minimum viewing range of 35 nodes

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -71,7 +71,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("fps_max", "60");
 	// A bit more than the server will send around the player, to make fog blend well
 	settings->setDefault("viewing_range_nodes_max", "160");
-	settings->setDefault("viewing_range_nodes_min", "15");
+	settings->setDefault("viewing_range_nodes_min", "35");
 	settings->setDefault("screenW", "800");
 	settings->setDefault("screenH", "600");
 	settings->setDefault("address", "");
