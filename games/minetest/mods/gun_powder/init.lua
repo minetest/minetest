957c1d92222492861dd95e4780e4e0ffa71f1cba
Joel Leclerc <lkjoel@ubuntu.com>
2012-04-13 18:27:30 -0600

    Added Obsidian, Timber, modified item and item_entity, modified gun_powder

--- games/minetest/mods/gun_powder/init.lua
+++ games/minetest/mods/gun_powder/init.lua
@@ -1,13 +1,13 @@
 minetest.register_craftitem("gun_powder:gun_powder", {
+	description = "Gun Powder"
 	inventory_image = "gun_powder.png",
-	stack_max = 64,
 })
 	
 minetest.register_node("gun_powder:gun_stone", {
-	description = "Stone",
-	tile_images = {"gun_stone.png"},
+	description = "Stone with gun powder",
+	tile_images = {"default_stone.png^gun_powder_mineral.png"},
 	is_ground_content = true,
 	groups = {cracky=3},
 	drop = 'gun_powder:gun_powder',
-	legacy_mineral = true,
+	sounds = default.node_sound_stone_defaults(),
 })
