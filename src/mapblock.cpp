e74668ef7f68156ef9c24ecc97d130ca53f8c624
Perttu Ahola <celeron55@gmail.com>
2012-06-08 14:57:02 +0300

    Update and fix doc/mapformat.txt; make current format more solid in mapblock.cpp

--- src/mapblock.cpp
+++ src/mapblock.cpp
@@ -616,9 +616,13 @@ void MapBlock::serialize(std::ostream &os, u8 version, bool disk)
 	*/
 	if(disk)
 	{
-		// Node timers
-		if(version >= 23)
-			m_node_timers.serialize(os);
+		// Version 23 doesn't actually contain node timers
+		// (this field should have not been added)
+		if(version == 23)
+			writeU8(os, 0);
+		// Node timers (uncomment when node timers are taken into use)
+		/*if(version >= 24)
+			m_node_timers.serialize(os);*/
 
 		// Static objects
 		m_static_objects.serialize(os);
@@ -698,11 +702,15 @@ void MapBlock::deSerialize(std::istream &is, u8 version, bool disk)
 	if(disk)
 	{
 		// Node timers
-		if(version >= 23){
+		if(version == 23)
+			// Read unused zero
+			readU8(is);
+		// Uncomment when node timers are taken into use
+		/*else if(version >= 24){
 			TRACESTREAM(<<"MapBlock::deSerialize "<<PP(getPos())
 					<<": Node timers"<<std::endl);
 			m_node_timers.deSerialize(is);
-		}
+		}*/
 
 		// Static objects
 		TRACESTREAM(<<"MapBlock::deSerialize "<<PP(getPos())
