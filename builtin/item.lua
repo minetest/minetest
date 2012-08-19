0b1ab41a954e2a2b035888e200d775372a3b394e
Perttu Ahola <celeron55@gmail.com>
2012-08-19 10:55:04 +0300

    Fix github issue #213: Dropping unknown items crashes the game
    The items will now just disappear when dropped.

--- builtin/item.lua
+++ builtin/item.lua
@@ -227,10 +227,12 @@ function minetest.item_drop(itemstack, dropper, pos)
 		local v = dropper:get_look_dir()
 		local p = {x=pos.x+v.x, y=pos.y+1.5+v.y, z=pos.z+v.z}
 		local obj = minetest.env:add_item(p, itemstack)
-		v.x = v.x*2
-		v.y = v.y*2 + 1
-		v.z = v.z*2
-		obj:setvelocity(v)
+		if obj then
+			v.x = v.x*2
+			v.y = v.y*2 + 1
+			v.z = v.z*2
+			obj:setvelocity(v)
+		end
 	else
 		minetest.env:add_item(pos, itemstack)
 	end
