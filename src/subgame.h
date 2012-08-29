3441b8d51a7136736e9f277806f4647ad6c56f70
Matthew I <matttpt@gmail.com>
2012-08-29 19:22:01 -0400

    Enforce stricter world names (fix #211)

--- src/subgame.h
+++ src/subgame.h
@@ -24,6 +24,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <set>
 #include <vector>
 
+#define WORLDNAME_ALLOWED_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+
 struct SubgameSpec
 {
 	std::string id; // "" = game does not exist
