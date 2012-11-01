7fcf153adab121bfd50b246f32e074adab3a3b6d
PilzAdam <adam-k@outlook.com>
2012-11-01 18:49:12 +0100

    Add functions to the default mod of minimal game to support old code

--- games/minimal/mods/default/init.lua
+++ games/minimal/mods/default/init.lua
@@ -1586,6 +1586,22 @@ minetest.register_alias("mapgen_stone_with_coal", "default:stone_with_coal")
 minetest.register_alias("mapgen_stone_with_iron", "default:stone_with_iron")
 minetest.register_alias("mapgen_mese", "default:mese")
 
+-- Support old code
+function default.spawn_falling_node(p, nodename)
+	spawn_falling_node(p, nodename)
+end
+
+-- Horrible crap to support old code
+-- Don't use this and never do what this does, it's completely wrong!
+-- (More specifically, the client and the C++ code doesn't get the group)
+function default.register_falling_node(nodename, texture)
+	minetest.log("error", debug.traceback())
+	minetest.log('error', "WARNING: default.register_falling_node is deprecated")
+	if minetest.registered_nodes[nodename] then
+		minetest.registered_nodes[nodename].groups.falling_node = 1
+	end
+end
+
 --
 -- Global callbacks
 --
