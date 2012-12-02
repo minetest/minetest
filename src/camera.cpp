566f7f638a00a698a25b73adc2e619fe63e1a802
Perttu Ahola <celeron55@gmail.com>
2012-12-02 03:10:23 +0200

    Get rid of jordan4ibanez's insane digging animation speed

--- src/camera.cpp
+++ src/camera.cpp
@@ -189,7 +189,7 @@ void Camera::step(f32 dtime)
 
 	if (m_digging_button != -1)
 	{
-		f32 offset = dtime * 4.5;
+		f32 offset = dtime * 3.5;
 		float m_digging_anim_was = m_digging_anim;
 		m_digging_anim += offset;
 		if (m_digging_anim >= 1)
