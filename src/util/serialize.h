02a3626e12ac51321d669b2aefe22e9d87365ac5
DannyDark <the_skeleton_of_a_child@yahoo.co.uk>
2012-12-10 04:16:43 +0000

    Fixed Irrlichttypes.h being included twice.

--- src/util/serialize.h
+++ src/util/serialize.h
@@ -20,7 +20,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #ifndef UTIL_SERIALIZE_HEADER
 #define UTIL_SERIALIZE_HEADER
 
-#include "../irrlichttypes.h"
 #include "../irrlichttypes_bloated.h"
 #include "../irr_v2d.h"
 #include "../irr_v3d.h"
