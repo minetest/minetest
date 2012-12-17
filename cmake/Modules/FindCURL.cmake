ce5f73760ef001c5cfd33e9174d4589222c18dbb
sfan5 <sfan5@live.de>
2012-12-17 20:47:35 +0100

    Tweak FindCURL.cmake

--- cmake/Modules/FindCURL.cmake
+++ cmake/Modules/FindCURL.cmake
@@ -5,13 +5,25 @@
 #  CURL_LIBRARY    - List of libraries when using curl.
 #  CURL_FOUND        - True if curl found.
 
-# Look for the header file.
-FIND_PATH(CURL_INCLUDE_DIR NAMES curl/curl.h)
+if( UNIX )
+  FIND_PATH(CURL_INCLUDE_DIR NAMES curl.h
+		PATHS
+		/usr/local/include/curl
+		/usr/include/curl
+	)
 
-# Look for the library.
-FIND_LIBRARY(CURL_LIBRARY NAMES curl)
+	FIND_LIBRARY(CURL_LIBRARY NAMES libcurl.a curl
+		PATHS
+		/usr/local/lib
+		/usr/lib
+	)
+else( UNIX )
+	FIND_PATH(CURL_INCLUDE_DIR NAMES curl/curl.h) # Look for the header file.
+  FIND_LIBRARY(CURL_LIBRARY NAMES curl) # Look for the library.
+  INCLUDE(FindPackageHandleStandardArgs) # handle the QUIETLY and REQUIRED arguments and set CURL_FOUND to TRUE if
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL DEFAULT_MSG CURL_LIBRARY CURL_INCLUDE_DIR) # all listed variables are TRUE
+endif( UNIX )
 
-# handle the QUIETLY and REQUIRED arguments and set CURL_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL DEFAULT_MSG CURL_LIBRARY CURL_INCLUDE_DIR)
+MESSAGE(STATUS "CURL_SOURCE_DIR = ${CURL_SOURCE_DIR}")
+MESSAGE(STATUS "CURL_INCLUDE_DIR = ${CURL_INCLUDE_DIR}")
+MESSAGE(STATUS "CURL_LIBRARY = ${CURL_LIBRARY}")
