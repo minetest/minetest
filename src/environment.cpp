03f205f5a36ae726bcfb63103cc9042f9c1f05f6
Ilya <TTChangeTheWorld@gmail.com>
2013-04-08 21:41:12 +0400

    Deleted list, which is unnecessary.
    I think, it's so. Correct me pls, if i make mistake.

--- src/environment.cpp
+++ src/environment.cpp
@@ -901,7 +901,6 @@ void ServerEnvironment::clearAllObjects()
 {
 	infostream<<"ServerEnvironment::clearAllObjects(): "
 			<<"Removing all active objects"<<std::endl;
-	std::list<u16> objects_to_remove;
 	for(std::map<u16, ServerActiveObject*>::iterator
 			i = m_active_objects.begin();
 			i != m_active_objects.end(); ++i)
@@ -936,14 +935,8 @@ void ServerEnvironment::clearAllObjects()
 		// Delete active object
 		if(obj->environmentDeletes())
 			delete obj;
-		// Id to be removed from m_active_objects
-		objects_to_remove.push_back(id);
-	}
-	// Remove references from m_active_objects
-	for(std::list<u16>::iterator i = objects_to_remove.begin();
-			i != objects_to_remove.end(); ++i)
-	{
-		m_active_objects.erase(*i);
+	        //Why must we create new list, if we can do everything here....
+		m_active_objects.erase(id);
 	}
 
 	std::list<v3s16> loadable_blocks;
