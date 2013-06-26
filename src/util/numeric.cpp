0f1693a31577d65472bd3c6a1649de51e55cbc2e
Vincent Heuken <vincent@vincentheuken.com>
2013-06-25 17:44:17 -0700

    include mathconstants.h in numerics.cpp

--- src/util/numeric.cpp
+++ src/util/numeric.cpp
@@ -18,6 +18,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #include "numeric.h"
+#include "mathconstants.h"
 
 #include "../log.h"
 #include "../constants.h" // BS, MAP_BLOCKSIZE
