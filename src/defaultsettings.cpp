7e7672e52147eb21206d90412fec530597db6093
sapier <Sapier at GMX dot net>
2013-07-22 22:07:12 +0200

    Switch modstore to https

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -266,9 +266,9 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("main_menu_mod_mgr","1");
 	settings->setDefault("old_style_mod_selection","true");
 	settings->setDefault("main_menu_game_mgr","0");
-	settings->setDefault("modstore_download_url", "http://forum.minetest.net/media/");
-	settings->setDefault("modstore_listmods_url", "http://forum.minetest.net/mmdb/mods/");
-	settings->setDefault("modstore_details_url", "http://forum.minetest.net/mmdb/mod/*/");
+	settings->setDefault("modstore_download_url", "https://forum.minetest.net/media/");
+	settings->setDefault("modstore_listmods_url", "https://forum.minetest.net/mmdb/mods/");
+	settings->setDefault("modstore_details_url", "https://forum.minetest.net/mmdb/mod/*/");
 
 }
 
