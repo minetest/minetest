02bda456b078cd18be353f5816eb1df7e8753524
ShadowNinja <noreply@gmail.com>
2013-09-17 15:28:21 -0400

    Lower the default max_users from 100 to 15

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -159,7 +159,7 @@ void set_default_settings(Settings *settings)
 	// "map-dir" doesn't exist by default.
 	settings->setDefault("default_game", "minetest");
 	settings->setDefault("motd", "");
-	settings->setDefault("max_users", "100");
+	settings->setDefault("max_users", "15");
 	settings->setDefault("strict_protocol_version_checking", "false");
 	settings->setDefault("creative_mode", "false");
 	settings->setDefault("enable_damage", "true");
