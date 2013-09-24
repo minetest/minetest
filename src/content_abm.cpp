afb27329fc05076876c19e90f8c4343efa540207
0gb.us <us_0gb@laptop-0gb-us.0gb.us>
2013-09-23 23:15:38 -0700

    Remove mapgen_air alias (#935)

--- src/content_abm.cpp
+++ src/content_abm.cpp
@@ -209,7 +209,7 @@ class LiquidDropABM : public ActiveBlockModifier {
 		{ return contents; }
 		virtual std::set<std::string> getRequiredNeighbors() {
 			std::set<std::string> neighbors;
-			neighbors.insert("mapgen_air");
+			neighbors.insert("air");
 			return neighbors;
 		}
 		virtual float getTriggerInterval()
@@ -241,7 +241,7 @@ class LiquidFreeze : public ActiveBlockModifier {
 		}
 		virtual std::set<std::string> getRequiredNeighbors() {
 			std::set<std::string> s;
-			s.insert("mapgen_air");
+			s.insert("air");
 			s.insert("group:melts");
 			return s;
 		}
@@ -303,7 +303,7 @@ class LiquidMeltWeather : public ActiveBlockModifier {
 		}
 		virtual std::set<std::string> getRequiredNeighbors() {
 			std::set<std::string> s;
-			s.insert("mapgen_air");
+			s.insert("air");
 			s.insert("group:freezes");
 			return s;
 		}
