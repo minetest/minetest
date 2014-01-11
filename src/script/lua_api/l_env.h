4774952b54d074ee869676150aad57c6170c90e5
Novatux <nathanael.courant@laposte.net>
2014-01-11 17:31:50 +0100

    Add forceloading

--- src/script/lua_api/l_env.h
+++ src/script/lua_api/l_env.h
@@ -151,6 +151,14 @@ private:
 	static int l_get_heat(lua_State *L);
 	static int l_get_humidity(lua_State *L);
 	
+	// minetest.forceload_block(blockpos)
+	// forceloads a block
+	static int l_forceload_block(lua_State *L);
+	
+	// minetest.forceload_free_block(blockpos)
+	// stops forceloading a position
+	static int l_forceload_free_block(lua_State *L);
+	
 public:
 	static void Initialize(lua_State *L, int top);
 };
