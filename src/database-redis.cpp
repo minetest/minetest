b08c2ce89e0d37bd299d8eba6e84f0fe03f81ce2
Sfan5 <sfan5@live.de>
2014-04-10 16:13:23 +0200

    meow

--- src/database-redis.cpp
+++ src/database-redis.cpp
@@ -1,6 +1,6 @@
 /*
 Minetest
-Copyright (C) 2013 sfan5 <sfan5@live.de>
+Copyright (C) 2014 celeron55, Perttu Ahola <celeron55@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
