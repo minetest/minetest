a1d718f38cb34491de300cddef08046af3bd7a83
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2014-04-13 19:02:30 +0300

    Change layout and use text lists instead

--- builtin/playerinfo.lua
+++ builtin/playerinfo.lua
@@ -2,10 +2,6 @@
 
 playerinfo = {}
 
-local MAX_COLUMNS = 6
-local MAX_ROWS = 8
-local COLUMN_WIDTH = 3
-
 --
 -- Player icon handler
 --
@@ -39,48 +35,95 @@ end
 -- Player list handler
 --
 
-function get_playerlist_formspec()
-	local total_players = #minetest.get_connected_players()
-	local total_columns = math.min(total_players, MAX_COLUMNS)
-	local total_rows = math.min(math.ceil(total_players / MAX_COLUMNS), MAX_ROWS)
-	local formspec="size["..(total_columns * COLUMN_WIDTH)..","..(total_rows + 1).."]"
+-- Stores each player's player list, to avoid changes while the formspec is refreshed
+local formspec_players = {}
 
-	local current_column = 0
-	local current_row = 0
+local function formspec_players_set(name)
+	formspec_players[name] = {}
 	for i, player in pairs(minetest.get_connected_players()) do
-		if i > total_columns * total_rows then break end
-		if current_column >= (MAX_COLUMNS * COLUMN_WIDTH) then
-			current_column = 0
-			current_row = current_row + 1
+		-- Add player name
+		local pname = player:get_player_name()
+		-- Add player ping
+		local pping = minetest.get_player_information(pname).avg_rtt / 2
+		pping = math.floor(pping * 1000) -- Milliseconds
+
+		table.insert(formspec_players[name], {name = pname, ping = pping})
+	end
+end
+
+local function formspec_string(name, index)
+	local formspec="size[8,6]"
+
+	-- Turn the player table into a string
+	local players = ""
+	for i, entry in pairs(formspec_players[name]) do
+		local ping_color = "#AAFFAA"
+		if entry.ping > 500 then
+			ping_color = "#FFAAAA"
+		elseif entry.ping > 100 then
+			ping_color = "#FFFF0AA"
 		end
 
+		players = players..ping_color..entry.name.." ("..entry.ping.."ms)"
+		if i < #formspec_players[name] then
+			players = players..","
+		end
+	end
+	formspec = formspec.."label[4,0;Players: "..#formspec_players[name].." / "..minetest.setting_get("max_users").."]"
+	formspec = formspec.."button[6,0;2,1;player_list_refresh;Refresh]"
+	formspec = formspec.."textlist[4,1;4,4;player_list;"..players..";"..index..";true]"
+
+	local player = minetest.get_player_by_name(formspec_players[name][index].name)
+	if player then
 		local name = player:get_player_name()
-		local ping = minetest.get_player_information(name).avg_rtt / 2
-		ping = math.floor(ping * 1000) -- Seconds to miliseconds
-
-		formspec = formspec.."image["..current_column..","..current_row..";1,1;"..playerinfo.get_icon(player).."]"
-		formspec = formspec.."label["..(current_column + 1)..","..current_row..";"..name.."\nPing: "..ping.."]"
-
-		current_column = current_column + COLUMN_WIDTH
+		formspec = formspec.."label[0,0;"..name.."]"
+		formspec = formspec.."image[1,1;2,2;"..playerinfo.get_icon(name).."]"
+	else
+		-- The player we selected left while the window was open
+		formspec = formspec.."label[0,0;Player disconnected]"
 	end
 
-	formspec = formspec.."button_exit[0,"..total_rows..";"..(total_columns * COLUMN_WIDTH)..",1;exit;Close]"
+	formspec = formspec.."button_exit[0,5;8,1;player_list_exit;Close]"
 	return formspec
 end
 
+local function formspec(name, index)
+	-- No index means we must set the player list
+	local pindex = index
+	if pindex < 1 then
+		formspec_players_set(name)
+		pindex = 1
+	end
+
+	minetest.show_formspec(name, "playerinfo", formspec_string(name, pindex))
+end
+
 --
--- Chat command
+-- Commands
 --
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "playerinfo" then return end
+	local name = player:get_player_name()
+	local event = minetest.explode_textlist_event(fields["player_list"])
+	if fields["quit"] then
+		formspec_players[name] = nil
+	elseif fields["player_list_refresh"] then
+		formspec(name, 0)
+	elseif event.type == "CHG" then
+		formspec(name, event.index)
+	end
+end)
 
 minetest.register_chatcommand("players", {
 	params = "",
-	description = "show a list of connected players",
+	description = "show connected player information",
 	privs = {},
 	func = function(name, param)
 		if minetest.is_singleplayer() then
 			print("This command only works in multiplayer!")
 		else
-			minetest.show_formspec(name, "players", get_playerlist_formspec())
+			formspec(name, 0)
 		end
 	end,
 })
