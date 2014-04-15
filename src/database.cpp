54ffe2e5de9ce44129f84f4748743f893b75fda7
ShadowNinja <shadowninja@minetest.net>
2014-04-15 16:07:53 -0400

    Use binary operators rather than "Python modulo" in decoding block positions

--- src/database.cpp
+++ src/database.cpp
@@ -20,33 +20,29 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "database.h"
 #include "irrlichttypes.h"
 
-static s32 unsignedToSigned(s32 i, s32 max_positive)
+static inline s16 unsigned_to_signed(u16 i, u16 max_positive)
 {
-	if(i < max_positive)
+	if (i < max_positive) {
 		return i;
-	else
-		return i - 2*max_positive;
+	} else {
+		return i - (max_positive * 2);
+	}
 }
 
-// modulo of a negative number does not work consistently in C
-static s64 pythonmodulo(s64 i, s64 mod)
+
+s64 Database::getBlockAsInteger(const v3s16 pos) const
 {
-	if(i >= 0)
-		return i % mod;
-	return mod - ((-i) % mod);
-}
-
-s64 Database::getBlockAsInteger(const v3s16 pos) {
-	return (u64) pos.Z * 16777216 +
-		(u64) pos.Y * 4096 +
+	return (u64) pos.Z * 0x1000000 +
+		(u64) pos.Y * 0x1000 +
 		(u64) pos.X;
 }
 
-v3s16 Database::getIntegerAsBlock(s64 i) {
-	s32 x = unsignedToSigned(pythonmodulo(i, 4096), 2048);
-	i = (i - x) / 4096;
-	s32 y = unsignedToSigned(pythonmodulo(i, 4096), 2048);
-	i = (i - y) / 4096;
-	s32 z = unsignedToSigned(pythonmodulo(i, 4096), 2048);
-	return v3s16(x,y,z);
+v3s16 Database::getIntegerAsBlock(const s64 i) const
+{
+        v3s16 pos;
+        pos.Z = unsigned_to_signed((i >> 24) & 0xFFF, 0x1000 / 2);
+        pos.Y = unsigned_to_signed((i >> 12) & 0xFFF, 0x1000 / 2);
+        pos.X = unsigned_to_signed((i      ) & 0xFFF, 0x1000 / 2);
+        return pos;
 }
+
