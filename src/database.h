41bc24477b732b74bd18839a9742d32bd85d1b44
ShadowNinja <shadowninja@minetest.net>
2014-04-22 12:52:48 -0400

    Revert binary database block position encoding
    This reverts commits a2003b0d553c7223a61c75e5dad79ea68e058ba2 and 54ffe2e5de9ce44129f84f4748743f893b75fda7.
    
    These weren't correct.  Add a black magic warning instead.

--- src/database.h
+++ src/database.h
@@ -35,7 +35,7 @@ public:
 	virtual void saveBlock(MapBlock *block)=0;
 	virtual MapBlock* loadBlock(v3s16 blockpos)=0;
 	s64 getBlockAsInteger(const v3s16 pos) const;
-	v3s16 getIntegerAsBlock(const s64 i) const;
+	v3s16 getIntegerAsBlock(s64 i) const;
 	virtual void listAllLoadableBlocks(std::list<v3s16> &dst)=0;
 	virtual int Initialized(void)=0;
 	virtual ~Database() {};
