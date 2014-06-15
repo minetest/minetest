ce9f1e1d3cacb8e81436a63254cb1d70cad7a0ed
sfan5 <sfan5@live.de>
2014-06-15 15:34:22 +0200

    Update buildbot scripts and add 64-bit buildbot

--- util/buildbot/toolchain_mingw64.cmake
+++ util/buildbot/toolchain_mingw64.cmake
@@ -2,12 +2,12 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
+SET(CMAKE_C_COMPILER x86_64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc)
+SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
