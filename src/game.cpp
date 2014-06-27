ed2c8ba9c5d1a80cefe20bd75660536d9a0dd0f5
RealBadAngel <maciej.kasatkin@o2.pl>
2014-06-27 20:16:37 +0200

    Fix wrong status text rectangle. Fix for #1412

--- src/game.cpp
+++ src/game.cpp
@@ -3227,7 +3227,7 @@ void the_game(bool &kill, bool random_input, InputHandler *input,
 				core::rect<s32> rect(
 						10,
 						status_y - guitext_status->getTextHeight(),
-						10 + guitext_chat->getTextWidth(),
+						10 + guitext_status->getTextWidth(),
 						status_y
 				);
 				guitext_status->setRelativePosition(rect);
