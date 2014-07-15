892b16d0431107c8e504afe9116e361f11b31d59
4Evergreen4 <eljohnson@frontier.com>
2014-07-15 17:19:27 -0400

    Add screen width/height option in the settings tab.

--- builtin/mainmenu/tab_settings.lua
+++ builtin/mainmenu/tab_settings.lua
@@ -134,6 +134,9 @@ local function formspec(tabview, name, tabdata)
 				.. dump(core.setting_getbool("bilinear_filter")) .. "]"..
 		"checkbox[4.5,1.5;cb_trilinear;".. fgettext("Tri-Linear Filtering") .. ";"
 				.. dump(core.setting_getbool("trilinear_filter")) .. "]"..
+		"label[1,3.3;" .. fgettext("W/H") .. "]" .. 
+		"field[2.1,3.7;1.2,0.5;te_screenw;;" .. current_width .. "]" .. 
+		"field[3.2,3.7;1.2,0.5;te_screenh;;" .. current_height .. "]" ..
 		"box[7.75,0;4,4;#999999]" ..
 		"checkbox[8,0;cb_shaders;".. fgettext("Shaders") .. ";"
 				.. dump(core.setting_getbool("enable_shaders")) .. "]"
