8d6bfbf709bd575cb2c0e2dfcbb689aab81061fa
RealBadAngel <maciej.kasatkin@o2.pl>
2014-07-26 12:31:11 +0200

    Water surface shader.

--- client/shaders/water_surface_shader/opengl_fragment.glsl
+++ client/shaders/water_surface_shader/opengl_fragment.glsl
@@ -5,112 +5,59 @@ uniform sampler2D useNormalmap;
 uniform vec4 skyBgColor;
 uniform float fogDistance;
 uniform vec3 eyePosition;
+uniform float animationTimer;
+uniform float timeOfDay;
 
 varying vec3 vPosition;
+varying vec3 eyeVec;
 varying vec3 worldPosition;
 
-varying vec3 eyeVec;
-varying vec3 tsEyeVec;
-varying vec3 lightVec;
-varying vec3 tsLightVec;
-
-bool normalTexturePresent = false; 
-
-const float e = 2.718281828459;
-const float BS = 10.0;
- 
-float intensity (vec3 color){
-	return (color.r + color.g + color.b) / 3.0;
-}
-
-float get_rgb_height (vec2 uv){
-	return intensity(texture2D(baseTexture,uv).rgb);
-}
-
-vec4 get_normal_map(vec2 uv){
-	vec4 bump = texture2D(normalTexture, uv).rgba;
-	bump.xyz = normalize(bump.xyz * 2.0 -1.0);
-	bump.y = -bump.y;
-	return bump;
-}
-
+vec2 windDir = vec2(0.5, -0.8); //wind direction XY
+float windSpeed = 0.5; //wind speed
+float scale = 1.0; //overall wave scale
+vec2 bigWaves = vec2(0.3, 0.3); //strength of big waves
+vec2 midWaves = vec2(0.3, 0.15); //strength of middle sized waves
+vec2 smallWaves = vec2(0.15, 0.1); //strength of small waves
+float choppy = 0.01; //wave choppyness
+vec4 tangent = vec4(1.0, 0.0, 0.0, 0.0);
+vec4 normal = vec4(0.0, 1.0, 0.0, 0.0);
+vec4 binormal = vec4(0.0, 0.0, 1.0, 0.0);
+	
 void main (void)
 {
-	vec3 color;
-	vec4 bump;
-	vec2 uv = gl_TexCoord[0].st;
-	bool use_normalmap = false;
-	
-#ifdef USE_NORMALMAPS
-	if (texture2D(useNormalmap,vec2(1.0,1.0)).r > 0.0){
-		normalTexturePresent = true;
-	}
-#endif
+	vec2 uv = worldPosition.xz;
+	float timer = animationTimer * 75.0;
+   	//normal map
+	vec2 nCoord = vec2(0.0, 0.0);
+	nCoord = uv * (scale * 0.05) + windDir * timer * (windSpeed*0.04);
+	vec3 normal0 = 2.0 * texture2D(normalTexture, nCoord + vec2(-timer*0.015,-timer*0.005)).rgb - 1.0;
+	nCoord = uv * (scale * 0.1) + windDir * timer * (windSpeed*0.08)-(normal0.xy/normal0.zz)*choppy;
+	vec3 normal1 = 2.0 * texture2D(normalTexture, nCoord + vec2(+timer*0.020,+timer*0.015)).rgb - 1.0;
+	nCoord = uv * (scale * 0.25) + windDir * timer * (windSpeed*0.07)-(normal1.xy/normal1.zz)*choppy;
+	vec3 normal2 = 2.0 * texture2D(normalTexture, nCoord + vec2(-timer*0.04,-timer*0.03)).rgb - 1.0;
+	nCoord = uv * (scale * 0.5) + windDir * timer * (windSpeed*0.09)-(normal2.xy/normal2.z)*choppy;
+	vec3 normal3 = 2.0 * texture2D(normalTexture, nCoord + vec2(+timer*0.03,+timer*0.04)).rgb - 1.0;
+	nCoord = uv * (scale* 1.0) + windDir * timer * (windSpeed*0.4)-(normal3.xy/normal3.zz)*choppy;
+	vec3 normal4 = 2.0 * texture2D(normalTexture, nCoord + vec2(-timer*0.02,+timer*0.1)).rgb - 1.0;  
+	nCoord = uv * (scale * 2.0) + windDir * timer * (windSpeed*0.7)-(normal4.xy/normal4.zz)*choppy;
+	vec3 normal5 = 2.0 * texture2D(normalTexture, nCoord + vec2(+timer*0.1,-timer*0.06)).rgb - 1.0;
 
-#ifdef ENABLE_PARALLAX_OCCLUSION
-	if (normalTexturePresent){
-		vec3 tsEye = normalize(tsEyeVec);
-		float height = PARALLAX_OCCLUSION_SCALE * texture2D(normalTexture, uv).a - PARALLAX_OCCLUSION_BIAS;
-		uv = uv + texture2D(normalTexture, uv).z * height * vec2(tsEye.x,-tsEye.y);
-	}
-#endif
+	vec4 nVec = vec4(normalize(normal0 * bigWaves.x + normal1 * bigWaves.y +
+							normal2 * midWaves.x + normal3 * midWaves.y +
+							normal4 * smallWaves.x + normal5 * smallWaves.y), 0.9);
+	vec4 vDir = normalize(vec4(eyeVec, 1.0));
+    vec4 vVec = normalize(vec4(dot(vDir, tangent),	dot(vDir, binormal), dot(vDir, normal), 1.0));
+	float fresnelTerm = dot(nVec, normalize(reflect(-vVec, nVec)));
+	vec3 reflectiveColor = vec3(0.7, 0.7, 0.7);
+	vec3 refractiveColor = vec3(0.1, 0.3, 0.5);
+	vec3 color = mix (reflectiveColor, refractiveColor, fresnelTerm);
+	float alpha = clamp((color.r + color.b + color.g)*1.5, 0.3, 0.5);
 
-#ifdef USE_NORMALMAPS
-	if (normalTexturePresent){
-		bump = get_normal_map(uv);
-		use_normalmap = true;
-	} 
-#endif
-		
-#ifdef GENERATE_NORMALMAPS
-	if (use_normalmap == false){
-		float tl = get_rgb_height (vec2(uv.x-SAMPLE_STEP,uv.y+SAMPLE_STEP));
-		float t  = get_rgb_height (vec2(uv.x-SAMPLE_STEP,uv.y-SAMPLE_STEP));
-		float tr = get_rgb_height (vec2(uv.x+SAMPLE_STEP,uv.y+SAMPLE_STEP));
-		float r  = get_rgb_height (vec2(uv.x+SAMPLE_STEP,uv.y));
-		float br = get_rgb_height (vec2(uv.x+SAMPLE_STEP,uv.y-SAMPLE_STEP));
-		float b  = get_rgb_height (vec2(uv.x,uv.y-SAMPLE_STEP));
-		float bl = get_rgb_height (vec2(uv.x-SAMPLE_STEP,uv.y-SAMPLE_STEP));
-		float l  = get_rgb_height (vec2(uv.x-SAMPLE_STEP,uv.y));
-		float dX = (tr + 2.0 * r + br) - (tl + 2.0 * l + bl);
-		float dY = (bl + 2.0 * b + br) - (tl + 2.0 * t + tr);
-		bump = vec4 (normalize(vec3 (dX, -dY, NORMALMAPS_STRENGTH)),1.0);
-		use_normalmap = true;
-	}
-#endif
-
-vec4 base = texture2D(baseTexture, uv).rgba;
-	
-#ifdef ENABLE_BUMPMAPPING
-	if (use_normalmap){
-		vec3 L = normalize(lightVec);
-		vec3 E = normalize(eyeVec);
-		float specular = pow(clamp(dot(reflect(L, bump.xyz), E), 0.0, 1.0),0.5); 
-		float diffuse = dot(E,bump.xyz);		
-		color = 0.05*base.rgb + diffuse*base.rgb + 0.2*specular*base.rgb;
-	} else {
-		color = base.rgb;
-	}
-#else
-	color = base.rgb;
-#endif
-
-#if MATERIAL_TYPE == TILE_MATERIAL_LIQUID_TRANSPARENT || MATERIAL_TYPE == TILE_MATERIAL_LIQUID_OPAQUE
-	float alpha = gl_Color.a;
 	vec4 col = vec4(color.rgb, alpha);
 	col *= gl_Color;
 	if(fogDistance != 0.0){
 		float d = max(0.0, min(vPosition.z / fogDistance * 1.5 - 0.6, 1.0));
 		alpha = mix(alpha, 0.0, d);
 	}
-	gl_FragColor = vec4(col.rgb, alpha);
-#else
-	vec4 col = vec4(color.rgb, base.a);
-	col *= gl_Color;
-	if(fogDistance != 0.0){
-		float d = max(0.0, min(vPosition.z / fogDistance * 1.5 - 0.6, 1.0));
-		col = mix(col, skyBgColor, d);
-	}
-	gl_FragColor = vec4(col.rgb, base.a);
-#endif
+    gl_FragColor = vec4(col.rgb, alpha);
 }
