f0b92a8553cac05f37c54746a6f12d4ca5c9bac9
TriBlade9 <triblade9@mail.com>
2014-09-18 07:18:07 +0800

    Extracted the selectionbox_width variable so it only gets set once per game load

--- src/drawscene.cpp
+++ src/drawscene.cpp
@@ -30,15 +30,21 @@ typedef enum {
 	EYECOUNT = 2
 } paralax_sign;
 
+static s16 selectionbox_width;
+
 void draw_selectionbox(video::IVideoDriver* driver, Hud& hud,
 		std::vector<aabb3f>& hilightboxes, bool show_hud)
 {
 	if (!show_hud)
 		return;
 
+	if(!selectionbox_width) {
+		 selectionbox_width = rangelim(g_settings->getS16("selectionbox_width"), 1, 5);
+	}
+
 	video::SMaterial oldmaterial = driver->getMaterial2D();
 	video::SMaterial m;
-	m.Thickness = rangelim(g_settings->getS16("selectionbox_width"), 1, 5);
+	m.Thickness = selectionbox_width;
 	m.Lighting = false;
 	driver->setMaterial(m);
 	hud.drawSelectionBoxes(hilightboxes);
