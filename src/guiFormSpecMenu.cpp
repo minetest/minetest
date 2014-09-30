aeede9c978f1a387b58e12a12a924d22f822919e
Craig Robbins <kde.psych@gmail.com>
2014-09-30 21:39:21 +1000

    Fixed undesiarable right-click and move mouse behaviour introduced by f6321e2 Not sure about the rest of the changes, but this one change (by f6321e2) is NOT correct and makes using the craft grid almost impossible

--- src/guiFormSpecMenu.cpp
+++ src/guiFormSpecMenu.cpp
@@ -2948,7 +2948,7 @@ bool GUIFormSpecMenu::OnEvent(const SEvent& event)
 	if((event.EventType==EET_MOUSE_INPUT_EVENT &&
 			event.MouseInput.Event != EMIE_MOUSE_MOVED) ||
 			(event.MouseInput.Event == EMIE_MOUSE_MOVED &&
-			event.MouseInput.isRightPressed() && getItemAtPos(m_pointer).i != getItemAtPos(m_old_pointer).i)){
+			!event.MouseInput.isRightPressed() && getItemAtPos(m_pointer).i != getItemAtPos(m_old_pointer).i)) {
 		// Mouse event other than movement or crossing the border of inventory field while holding rmb
 
 		// Get selected item and hovered/clicked item (s)
