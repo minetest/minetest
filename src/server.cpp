35d8beb14367fe7f999a3d5a45e5ca46982afe83
RealBadAngel <maciej.kasatkin@o2.pl>
2014-10-21 05:12:15 +0200

    Recalculate normals for cached meshes.
    Check if mesh is here before adding to meshcollector.
    
    Fix deleting the meshes.

--- src/server.cpp
+++ src/server.cpp
@@ -4890,6 +4890,11 @@ IShaderSource* Server::getShaderSource()
 {
 	return NULL;
 }
+scene::ISceneManager* Server::getSceneManager()
+{
+	return NULL;
+}
+
 u16 Server::allocateUnknownNodeId(const std::string &name)
 {
 	return m_nodedef->allocateDummy(name);
