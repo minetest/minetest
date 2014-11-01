1e5ebaf825304f53c87c167708463cebebce8dc5
Calinou <calinou@opmbx.org>
2014-11-01 21:03:11 +0100

    Increase maximum view bobbing speed (to the double of the default walking speed)

--- src/camera.cpp
+++ src/camera.cpp
@@ -503,7 +503,7 @@ void Camera::update(LocalPlayer* player, f32 frametime, f32 busytime,
 	{
 		// Start animation
 		m_view_bobbing_state = 1;
-		m_view_bobbing_speed = MYMIN(speed.getLength(), 40);
+		m_view_bobbing_speed = MYMIN(speed.getLength(), 80);
 	}
 	else if (m_view_bobbing_state == 1)
 	{
