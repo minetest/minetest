5f452322cc348fb67867c10eda3df863edfd0463
RealBadAngel <maciej.kasatkin@o2.pl>
2014-12-06 22:13:31 +0100

    Restore finalColorBlend implementation in shaders.

--- client/shaders/water_surface_shader/opengl_vertex.glsl
+++ client/shaders/water_surface_shader/opengl_vertex.glsl
@@ -49,8 +49,8 @@ void main(void)
 	vec4 pos = gl_Vertex;
 	vec4 pos2 = mWorld * gl_Vertex;
 	if (gl_TexCoord[0].y < 0.05) {
-	pos.x += (smoothTriangleWave(animationTimer * 20.0 + pos2.x * 0.1 + pos2.z * 0.1) * 2.0 - 1.0) * 0.8;
-			pos.y -= (smoothTriangleWave(animationTimer * 10.0 + pos2.x * -0.5 + pos2.z * -0.5) * 2.0 - 1.0) * 0.4;
+		pos.x += (smoothTriangleWave(animationTimer * 20.0 + pos2.x * 0.1 + pos2.z * 0.1) * 2.0 - 1.0) * 0.8;
+		pos.y -= (smoothTriangleWave(animationTimer * 10.0 + pos2.x * -0.5 + pos2.z * -0.5) * 2.0 - 1.0) * 0.4;	
 	}
 	gl_Position = mWorldViewProj * pos;
 #else
@@ -97,5 +97,31 @@ void main(void)
 	eyeVec = (gl_ModelViewMatrix * gl_Vertex).xyz;
 	tsEyeVec = eyeVec * tbnMatrix;
 
-	gl_FrontColor = gl_BackColor = gl_Color;
+	vec4 color;
+	float day = gl_Color.r;
+	float night = gl_Color.g;
+	float light_source = gl_Color.b;
+
+	float rg = mix(night, day, dayNightRatio);
+	rg += light_source * 2.5; // Make light sources brighter
+	float b = rg;
+
+	// Moonlight is blue
+	b += (day - night) / 13.0;
+	rg -= (day - night) / 23.0;
+
+	// Emphase blue a bit in darker places
+	// See C++ implementation in mapblock_mesh.cpp finalColorBlend()
+	b += max(0.0, (1.0 - abs(b - 0.13)/0.17) * 0.025);
+
+	// Artificial light is yellow-ish
+	// See C++ implementation in mapblock_mesh.cpp finalColorBlend()
+	rg += max(0.0, (1.0 - abs(rg - 0.85)/0.15) * 0.065);
+
+	color.r = rg;
+	color.g = rg;
+	color.b = b;
+
+	color.a = gl_Color.a;
+	gl_FrontColor = gl_BackColor = clamp(color,0.0,1.0);
 }
