5a5ad0c3082fd4f653f03b9570c21441947d2205
MinerDad <minerdad7@gmail.com>
2014-12-14 19:41:55 -0700

    Add class doxygen comments for itemdef.h

--- src/itemdef.h
+++ src/itemdef.h
@@ -30,10 +30,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 class IGameDef;
 struct ToolCapabilities;
 
-/*
-	Base item definition
-*/
-
 enum ItemType
 {
 	ITEM_NONE,
@@ -42,42 +38,58 @@ enum ItemType
 	ITEM_TOOL,
 };
 
+/**
+ *  Defines items that can be used by players and added to their inventories.
+ *
+ *  The hand item is defined with name = "" and is guarenteed to always have tool_capabilities.
+ *  This is enforced by CItemDefManager::clear() and CItemDefManager::registerItem().
+ *
+ *  tool_capabilities may be NULL but if it is non-NULL, this struct owns it and will delete it in the destructor.
+ *
+ *  node_placement_prediction is the name of the node that will be placed immediately by the client. The server will
+ *  update the precise end result a moment later. "" = no prediction.
+ *  If an ITEM_NODE item is defined in a mod, it defaults to the name of the item. This is done in ModApiItemMod::l_register_item_raw().
+ */
 struct ItemDefinition
 {
-	/*
-		Basic item properties
-	*/
+	///@name Basic item properties
+	///@{
+
 	ItemType type;
-	std::string name; // "" = hand
-	std::string description; // Shown in tooltip.
+	/// "" = hand
+	std::string name;
+	/// Shown in the tooltip.
+	std::string description;
+	///@}
 
-	/*
-		Visual properties
-	*/
-	std::string inventory_image; // Optional for nodes, mandatory for tools/craftitems
-	std::string wield_image; // If empty, inventory_image or mesh (only nodes) is used
+	///@name Visual properties
+	///@{
+
+	/// Optional for ITEM_NODE, Mandatory for ITEM_CRAFT and ITEM_TOOL.
+	std::string inventory_image;
+	/// If empty, the inventory_image or mesh (only ITEM_NODE) is used.
+	std::string wield_image;
 	v3f wield_scale;
+	///@}
 
-	/*
-		Item stack and interaction properties
-	*/
+	///@name Item stack and interaction properties
+	///@{
+
 	s16 stack_max;
 	bool usable;
 	bool liquids_pointable;
-	// May be NULL. If non-NULL, deleted by destructor
+	/// May be NULL. If non-NULL, deleted by destructor.
 	ToolCapabilities *tool_capabilities;
 	ItemGroupList groups;
 	SimpleSoundSpec sound_place;
 	f32 range;
+	///@}
 
-	// Client shall immediately place this node when player places the item.
-	// Server will update the precise end result a moment later.
-	// "" = no prediction
 	std::string node_placement_prediction;
 
-	/*
-		Some helpful methods
-	*/
+	///@name Some helpful methods
+	///@{
+
 	ItemDefinition();
 	ItemDefinition(const ItemDefinition &def);
 	ItemDefinition& operator=(const ItemDefinition &def);
@@ -85,29 +97,68 @@ struct ItemDefinition
 	void reset();
 	void serialize(std::ostream &os, u16 protocol_version) const;
 	void deSerialize(std::istream &is);
+	///@}
 private:
 	void resetInitial();
 };
 
+/**
+ *  @class CItemDefManager
+ *
+ *  This stores ItemDefinitions for all known items in the game.
+ *
+ *  Use the IItemDefManager and IWritableItemDefManager interfaces,
+ *  do not use this class directly.
+ *  Use createItemDefManager() to instantiate this class.
+ *
+ *  All functions that take a name also accept an alias.
+ *
+ *  getInventoryTexture() and getWieldMesh() act differently depending on
+ *  if called from the main thread or called from another thread.
+ *  If called from another thread, it queues up a job to get the inventory
+ *  texture and wield mesh and wait up to 1 second for it to finish.  See
+ *  CItemDefManager::processQueue and CItemDefManager::getClientCached.
+ *
+ *  Every job is cached (including the main thread jobs), so subsequent calls
+ *  will just read from the cache.
+ *  The cache key is the passed in parameter, so an alias will have a separate
+ *  cached result from the actual name. Adding aliases or item definitions will
+ *  not clear the cache.
+ *
+ *  getAll() returns all names and aliases.
+ *
+ *  get() returns the "unknown" item if the requested item isn't found.
+ *
+ *  registerItem() overwrites any existing item with the same name and
+ *  removes an alias if it is equal to the new item's name.
+ *
+ *  registerAlias() will not overwrite an existing alias if it already exists.
+ *
+ *  clear() removes all registered items and aliases. It re-adds the
+ *  built-in items, which are: hand, unknown, air, and ignore.
+ *
+ *  processQueue() handles the job requests from getInventoryTexture() and
+ *  getWieldMesh(). It should only be run from the main thread.
+ */
+
+/**
+ * @class IItemDefManager
+ *
+ * See CItemDefManager
+ */
 class IItemDefManager
 {
 public:
 	IItemDefManager(){}
 	virtual ~IItemDefManager(){}
 
-	// Get item definition
 	virtual const ItemDefinition& get(const std::string &name) const=0;
-	// Get alias definition
 	virtual std::string getAlias(const std::string &name) const=0;
-	// Get set of all defined item names and aliases
 	virtual std::set<std::string> getAll() const=0;
-	// Check if item is known
 	virtual bool isKnown(const std::string &name) const=0;
 #ifndef SERVER
-	// Get item inventory texture
 	virtual video::ITexture* getInventoryTexture(const std::string &name,
 			IGameDef *gamedef) const=0;
-	// Get item wield mesh
 	virtual scene::IMesh* getWieldMesh(const std::string &name,
 		IGameDef *gamedef) const=0;
 #endif
@@ -115,27 +166,25 @@ public:
 	virtual void serialize(std::ostream &os, u16 protocol_version)=0;
 };
 
+/**
+ * @class IWritableItemDefManager
+ *
+ * See CItemDefManager
+ */
 class IWritableItemDefManager : public IItemDefManager
 {
 public:
 	IWritableItemDefManager(){}
 	virtual ~IWritableItemDefManager(){}
 
-	// Remove all registered item and node definitions and aliases
-	// Then re-add the builtin item definitions
 	virtual void clear()=0;
-	// Register item definition
 	virtual void registerItem(const ItemDefinition &def)=0;
-	// Set an alias so that items named <name> will load as <convert_to>.
-	// Alias is not set if <name> has already been defined.
-	// Alias will be removed if <name> is defined at a later point of time.
 	virtual void registerAlias(const std::string &name,
 			const std::string &convert_to)=0;
 
 	virtual void serialize(std::ostream &os, u16 protocol_version)=0;
 	virtual void deSerialize(std::istream &is)=0;
 
-	// Do stuff asked by threads that can only be done in the main thread
 	virtual void processQueue(IGameDef *gamedef)=0;
 };
 
