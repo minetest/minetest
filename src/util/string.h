55319b9363dc9938fd27d350197b7ab2010f16ee
ShadowNinja <shadowninja@minetest.net>
2014-12-30 02:21:01 -0500

    Tweak string docs

--- src/util/string.h
+++ src/util/string.h
@@ -50,40 +50,40 @@ bool parseColorString(const std::string &value, video::SColor &color, bool quiet
 
 
 /**
- * Returns a copy of s with spaces inserted at the right hand side to ensure
- *			that the string is len characters in length. If s is <= len then the
- *			returned string will be identical to s.
+ * Returns a copy of \p str with spaces inserted at the right hand side to ensure
+ * that the string is \p len characters in length. If \p str is <= \p len then the
+ * returned string will be identical to str.
  */
-inline std::string padStringRight(std::string s, size_t len)
+inline std::string padStringRight(std::string str, size_t len)
 {
-	if (len > s.size())
-		s.insert(s.end(), len - s.size(), ' ');
+	if (len > str.size())
+		str.insert(str.end(), len - str.size(), ' ');
 
-	return s;
+	return str;
 }
 
 /**
- * Returns a version of the string s with the first occurrence of a string
+ * Returns a version of \p str with the first occurrence of a string
  * contained within ends[] removed from the end of the string.
  *
- * @param s
+ * @param str
  * @param ends A NULL- or ""- terminated array of strings to remove from s in
- *			the copy produced. Note that once one of these strings is removed
- *			that no further postfixes contained within this array are removed.
+ *	the copy produced.  Note that once one of these strings is removed
+ *	that no further postfixes contained within this array are removed.
  *
- * @return If no end could be removed then "" is returned
+ * @return If no end could be removed then "" is returned.
  */
-inline std::string removeStringEnd(const std::string &s,
+inline std::string removeStringEnd(const std::string &str,
 		const char *ends[])
 {
 	const char **p = ends;
 
 	for (; *p && (*p)[0] != '\0'; p++) {
 		std::string end = *p;
-		if (s.size() < end.size())
+		if (str.size() < end.size())
 			continue;
-		if (s.compare(s.size() - end.size(), end.size(), end) == 0)
-			return s.substr(0, s.size() - end.size());
+		if (str.compare(str.size() - end.size(), end.size(), end) == 0)
+			return str.substr(0, str.size() - end.size());
 	}
 
 	return "";
@@ -91,7 +91,7 @@ inline std::string removeStringEnd(const std::string &s,
 
 
 /**
- * Check two strings for equivalence. If case_insensitive is true
+ * Check two strings for equivalence.  If \p case_insensitive is true
  * then the case of the strings is ignored (default is false).
  *
  * @param s1
@@ -119,14 +119,14 @@ inline bool str_equal(const std::basic_string<T> &s1,
 
 
 /**
- * Check whether str begins with the string prefix. If the argument
- *			case_insensitive == true then the check is case insensitve (default
- *			is false; i.e. case is significant).
+ * Check whether \p str begins with the string prefix. If \p case_insensitive
+ * is true then the check is case insensitve (default is false; i.e. case is
+ * significant).
  *
  * @param str
  * @param prefix
  * @param case_insensitive
- * @return	true if the str begins with prefix
+ * @return true if the str begins with prefix
  */
 template <typename T>
 inline bool str_starts_with(const std::basic_string<T> &str,
@@ -148,9 +148,9 @@ inline bool str_starts_with(const std::basic_string<T> &str,
 
 /**
  * Splits a string into its component parts separated by the character
- * delimiter.
+ * \p delimiter.
  *
- * @return an std::vector<std::basic_string<T> > of the component parts
+ * @return An std::vector<std::basic_string<T> > of the component parts
  */
 template <typename T>
 inline std::vector<std::basic_string<T> > str_split(
@@ -169,70 +169,70 @@ inline std::vector<std::basic_string<T> > str_split(
 
 
 /**
- * Return a copy of s converted to all lowercase characters
- * @param s
+ * @param str
+ * @return A copy of \p str converted to all lowercase characters.
  */
-inline std::string lowercase(const std::string &s)
+inline std::string lowercase(const std::string &str)
 {
 	std::string s2;
 
-	s2.reserve(s.size());
+	s2.reserve(str.size());
 
-	for (size_t i = 0; i < s.size(); i++)
-		s2 += tolower(s[i]);
+	for (size_t i = 0; i < str.size(); i++)
+		s2 += tolower(str[i]);
 
 	return s2;
 }
 
 
 /**
- * Returns a copy of s with leading and trailing whitespace removed.
- * @param s
+ * @param str
+ * @return A copy of \p str with leading and trailing whitespace removed.
  */
-inline std::string trim(const std::string &s)
+inline std::string trim(const std::string &str)
 {
 	size_t front = 0;
 
-	while (std::isspace(s[front]))
+	while (std::isspace(str[front]))
 		++front;
 
-	size_t back = s.size();
-	while (back > front && std::isspace(s[back - 1]))
+	size_t back = str.size();
+	while (back > front && std::isspace(str[back - 1]))
 		--back;
 
-	return s.substr(front, back - front);
+	return str.substr(front, back - front);
 }
 
 
 /**
- * Returns true if s should be regarded as (bool) true. Leading and trailing
- *			whitespace are ignored; case is ignored. Values that will return
- *			true are "y", "yes", "true" and any number that != 0.
- * @param s
+ * Returns whether \p str should be regarded as (bool) true.  Case and leading
+ * and trailing whitespace are ignored.  Values that will return
+ * true are "y", "yes", "true" and any number that is not 0.
+ * @param str
  */
-inline bool is_yes(const std::string &s)
+inline bool is_yes(const std::string &str)
 {
-	std::string s2 = lowercase(trim(s));
+	std::string s2 = lowercase(trim(str));
 
 	return s2 == "y" || s2 == "yes" || s2 == "true" || atoi(s2.c_str()) != 0;
 }
 
 
 /**
- * Converts the string s to a signed 32-bit integer. The converted value is
- * constrained so that min <= value <= max.
+ * Converts the string \p str to a signed 32-bit integer. The converted value
+ * is constrained so that min <= value <= max.
  *
  * @see atoi(3) for limitations
  *
- * @param s
+ * @param str
  * @param min Range minimum
  * @param max Range maximum
  * @return The value converted to a signed 32-bit integer and constrained
- *			within the range defined by min and max (inclusive)
+ *	within the range defined by min and max (inclusive)
  */
-inline s32 mystoi(const std::string &s, s32 min, s32 max)
+inline s32 mystoi(const std::string &str, s32 min, s32 max)
 {
-	s32 i = atoi(s.c_str());
+	s32 i = atoi(str.c_str());
 
 	if (i < min)
 		i = min;
@@ -243,12 +243,10 @@ inline s32 mystoi(const std::string &s, s32 min, s32 max)
 }
 
 
-/**
- * Returns a 64-bit value reprensented by the string s (decimal).
- */
-inline s64 stoi64(const std::string &s)
+/// Returns a 64-bit value represented by the string \p str (decimal).
+inline s64 stoi64(const std::string &str)
 {
-	std::stringstream tmp(s);
+	std::stringstream tmp(str);
 	s64 t;
 	tmp >> t;
 	return t;
@@ -259,35 +257,32 @@ inline s64 stoi64(const std::string &s)
 
 
 /**
- * Returns a 32-bit value reprensented by the string s (decimal).
- *
+ * Returns a 32-bit value reprensented by the string \p str (decimal).
  * @see atoi(3) for further limitations
  */
-inline s32 mystoi(const std::string &s)
+inline s32 mystoi(const std::string &str)
 {
-	return atoi(s.c_str());
+	return atoi(str.c_str());
 }
 
 
 /**
- * Returns a 32-bit value reprensented by the wide string s (decimal).
- *
+ * Returns s 32-bit value represented by the wide string \p str (decimal).
  * @see atoi(3) for further limitations
  */
-inline s32 mystoi(const std::wstring &s)
+inline s32 mystoi(const std::wstring &str)
 {
-	return mystoi(wide_to_narrow(s));
+	return mystoi(wide_to_narrow(str));
 }
 
 
 /**
- * Returns a float reprensented by the string s (decimal).
- *
+ * Returns a float reprensented by the string \p str (decimal).
  * @see atof(3)
  */
-inline float mystof(const std::string &s)
+inline float mystof(const std::string &str)
 {
-	return atof(s.c_str());
+	return atof(str.c_str());
 }
 
 //#endif
@@ -297,29 +292,29 @@ inline float mystof(const std::string &s)
 
 // TODO: Replace with C++11 std::to_string.
 
-/// Returns a string representing the value v
+/// Returns A string representing the value \p val.
 template <typename T>
-inline std::string to_string(T v)
+inline std::string to_string(T val)
 {
-	std::ostringstream o;
-	o << v;
-	return o.str();
+	std::ostringstream oss;
+	oss << val;
+	return oss.str();
 }
 
-/// Returns a string representing the decimal value of the 32-bit value i
+/// Returns a string representing the decimal value of the 32-bit value \p i.
 inline std::string itos(s32 i) { return to_string(i); }
-/// Returns a string representing the decimal value of the 64-bit value i
+/// Returns a string representing the decimal value of the 64-bit value \p i.
 inline std::string i64tos(s64 i) { return to_string(i); }
-/// Returns a string representing the decimal value of the float value f
+/// Returns a string representing the decimal value of the float value \p f.
 inline std::string ftos(float f) { return to_string(f); }
 
 
 /**
- * Replace all occurrences of `pattern` in `str` with `replacement`
+ * Replace all occurrences of \p pattern in \p str with \p replacement.
  *
- * @param str String to replace pattern with replacement within
- * @param pattern The pattern to replace
- * @param replacement What to replace the pattern with
+ * @param str String to replace pattern with replacement within.
+ * @param pattern The pattern to replace.
+ * @param replacement What to replace the pattern with.
  */
 inline void str_replace(std::string &str, const std::string &pattern,
 		const std::string &replacement)
@@ -333,11 +328,11 @@ inline void str_replace(std::string &str, const std::string &pattern,
 
 
 /**
- * Replace all occurrances of the character `from` in `str` with `to`.
+ * Replace all occurrences of the character \p from in \p str with \p to.
  *
- * @param str The string to (potentially) modify
- * @param from The character in str to replace
- * @param to The replacement character
+ * @param str The string to (potentially) modify.
+ * @param from The character in str to replace.
+ * @param to The replacement character.
  */
 void str_replace(std::string &str, char from, char to);
 
@@ -346,15 +341,15 @@ void str_replace(std::string &str, char from, char to);
  * Check that a string only contains whitelisted characters. This is the
  * opposite of string_allowed_blacklist().
  *
- * @param s The string to be checked.
+ * @param str The string to be checked.
  * @param allowed_chars A string containing permitted characters.
  * @return true if the string is allowed, otherwise false.
  *
  * @see string_allowed_blacklist()
  */
-inline bool string_allowed(const std::string &s, const std::string &allowed_chars)
+inline bool string_allowed(const std::string &str, const std::string &allowed_chars)
 {
-	return s.find_first_not_of(allowed_chars) == s.npos;
+	return str.find_first_not_of(allowed_chars) == str.npos;
 }
 
 
@@ -362,26 +357,26 @@ inline bool string_allowed(const std::string &s, const std::string &allowed_char
  * Check that a string contains no blacklisted characters. This is the
  * opposite of string_allowed().
  *
- * @param s The string to be checked.
+ * @param str The string to be checked.
  * @param blacklisted_chars A string containing prohibited characters.
  * @return true if the string is allowed, otherwise false.
 
  * @see string_allowed()
  */
-inline bool string_allowed_blacklist(const std::string &s,
+inline bool string_allowed_blacklist(const std::string &str,
 		const std::string &blacklisted_chars)
 {
-	return s.find_first_of(blacklisted_chars) == s.npos;
+	return str.find_first_of(blacklisted_chars) == str.npos;
 }
 
 
 /**
- * Create a string based on 'from' where a newline is forcefully inserted every
- * 'row_len' characters.
+ * Create a string based on \p from where a newline is forcefully inserted
+ * every \p row_len characters.
  *
  * @note This function does not honour word wraps and blindy inserts a newline
- *			every row_len characters whether it breaks a word or not. It is
- *			intended to be used, for example, showing paths in the GUI
+ *	every \p row_len characters whether it breaks a word or not.  It is
+ *	intended to be used for, for example, showing paths in the GUI.
  *
  * @param from The string to be wrapped into rows.
  * @param row_len The row length (in characters).
@@ -403,30 +398,29 @@ inline std::string wrap_rows(const std::string &from,
 
 
 /**
- * Removes all \\ from a string that had been escaped (FormSpec strings)
+ * Removes all backslashes from a string that had been escaped (FormSpec strings)
  *
  */
 template <typename T>
 inline std::basic_string<T> unescape_string(std::basic_string<T> &s)
 {
 	std::basic_string<T> res;
-	
+
 	for (size_t i = 0; i < s.length(); i++) {
-		if (s[i] == '\\')
-			i++;
-		res += s[i];
+		if (s[i] != '\\')
+			res += s[i];
 	}
-	
+
 	return res;
 }
 
 
 /**
- * Checks that all characters in to_check are a decimal digits
+ * Checks that all characters in \p to_check are a decimal digits.
  *
  * @param to_check
- * @return true if to_check is not empty and all characters in tocheck are
- *			decimal digits, otherwise false
+ * @return true if to_check is not empty and all characters in to_check are
+ *	decimal digits, otherwise false
  */
 inline bool is_number(const std::string &to_check)
 {
@@ -439,13 +433,13 @@ inline bool is_number(const std::string &to_check)
 
 
 /**
- * Returns a C-string, either "true" or "false", corresponding to v
+ * Returns a C-string, either "true" or "false", corresponding to \p val.
  *
- * @return If v == true, then "true" is returned, otherwise "false"
+ * @return If \p val is true, then "true" is returned, otherwise "false".
  */
-inline const char *bool_to_cstr(bool v)
+inline const char *bool_to_cstr(bool val)
 {
-	return v ? "true" : "false";
+	return val ? "true" : "false";
 }
 
 #endif
