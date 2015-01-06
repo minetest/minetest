e201620ee1051545ed7856fb18f805c67adccc85
sapier <Sapier at GMX dot net>
2015-01-07 00:10:46 +0100

    Split gui_scaling to gui_scaling + hud_scaling as those elements need different handling on some devices

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -132,6 +132,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("crosshair_color", "(255,255,255)");
 	settings->setDefault("crosshair_alpha", "255");
 	settings->setDefault("gui_scaling", "0.75");
+	settings->setDefault("hud_scaling", "1.0");
 	settings->setDefault("mouse_sensitivity", "0.2");
 	settings->setDefault("enable_sound", "true");
 	settings->setDefault("sound_volume", "0.8");
@@ -326,10 +327,10 @@ void set_default_settings(Settings *settings)
 	float x_inches = ((double) porting::getDisplaySize().X /
 			(160 * porting::getDisplayDensity()));
 	if (x_inches  < 3.5) {
-		settings->setDefault("gui_scaling", "0.6");
+		settings->setDefault("hud_scaling", "0.6");
 	}
 	else if (x_inches < 4.5) {
-		settings->setDefault("gui_scaling", "0.7");
+		settings->setDefault("hud_scaling", "0.7");
 	}
 	settings->setDefault("curl_verify_cert","false");
 #else
