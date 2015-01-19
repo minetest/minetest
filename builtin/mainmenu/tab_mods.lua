6a7d718ab76a6ab404ee7601052300dea75d60f7
ngosang <diegodelasheras@gmail.com>
2015-01-19 15:33:43 +0100

    Minor fixes in translations (reverted from commit 7dfdcc20c24a21b22de6265788217eb628f0e2f3)

--- builtin/mainmenu/tab_mods.lua
+++ builtin/mainmenu/tab_mods.lua
@@ -96,7 +96,7 @@ local function get_formspec(tabview, name, tabdata)
 		else
 			--show dependencies
 
-			retval = retval .. "," .. fgettext("Depends:") .. ","
+			retval = retval .. ",Depends:,"
 
 			local toadd = modmgr.get_dependencies(selected_mod.path)
 
