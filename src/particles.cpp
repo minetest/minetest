1e04dddb975f82d48f9cd1aa846b410c0ebfee36
obneq <obbneq@gmail.com>
2015-02-06 20:35:03 +0100

    wip particles rebased

--- src/particles.cpp
+++ src/particles.cpp
@@ -33,6 +33,222 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "mapnode.h"
 #include "client.h"
 
+#include "nodedef.h"
+class Map;
+class IGameDef;
+class Environment;
+
+// this class is from http://www.bulletbyte.de/index.php?section=8
+//#define EPET_EXPLOSION (irr::scene::E_PARTICLE_EMITTER_TYPE)(EPET_COUNT+23)
+class CParticleExplosionEmitter : public irr::scene::IParticleEmitter {
+        //protected:
+private:
+        core::array<irr::scene::SParticle> Particles;
+        core::vector3df Center;
+        f32 Radius;
+        core::vector3df Direction;
+        core::dimension2df MinStartSize, MaxStartSize;
+        u32 MinParticlesPerSecond, MaxParticlesPerSecond;
+        video::SColor MinStartColor, MaxStartColor;
+        u32 MinLifeTime, MaxLifeTime;
+        u32 Time;
+        u32 Emitted;
+public:
+        CParticleExplosionEmitter(
+                        const core::vector3df& center, f32 radius,
+                        core::vector3df vDirection, u32 minParticlesPerSecond,
+                        u32 maxParticlesPerSecond, const video::SColor& minStartColor,
+                        const video::SColor& maxStartColor, u32 lifeTimeMin, u32 lifeTimeMax,
+                        const core::dimension2df& minStartSize,
+                        const core::dimension2df& maxStartSize )
+                : Center(center), Radius(radius),
+                  MinStartSize(minStartSize), MaxStartSize(maxStartSize),
+                  MinParticlesPerSecond(minParticlesPerSecond),
+                  MaxParticlesPerSecond(maxParticlesPerSecond),
+                  MinStartColor(minStartColor), MaxStartColor(maxStartColor),
+                  MinLifeTime(lifeTimeMin), MaxLifeTime(lifeTimeMax),
+                  Time(0), Emitted(0)
+        {
+                Direction=vDirection;
+#ifdef _DEBUG
+                setDebugName("CParticleExplosionEmitter");
+#endif
+        }
+        s32 emitt(u32 now, u32 timeSinceLastCall, irr::scene::SParticle*& outArray)
+        {
+                Time += timeSinceLastCall;
+                if (Emitted>0) return 0;
+                const u32 pps = (MaxParticlesPerSecond - MinParticlesPerSecond);
+                const f32 particleCount = pps ? (f32)MinParticlesPerSecond + (rand() % pps) : MinParticlesPerSecond;
+                Particles.set_used(0);
+                Time = 0;
+                irr::scene::SParticle p;
+                for(u32 i=0; i<particleCount; ++i)
+                {
+                        // Random distance from center
+                        f32 distance =fmodf( (f32)rand(), Radius * 1000.0f ) * 0.001f;
+                        // Random direction from center
+                        p.pos.set(Center + distance);
+                        /**/p.pos.rotateXYBy( rand() % 360, Center );
+                        p.pos.rotateYZBy( rand() % 360, Center );
+                        p.pos.rotateXZBy( rand() % 360, Center );/**/
+                        p.startTime = now;
+                        p.vector = Direction;
+                        core::vector3df tgt = Direction;
+                        /**/tgt.rotateXYBy((rand()%(360*2)) - 360);
+                        tgt.rotateYZBy((rand()%(360*2)) - 360);
+                        tgt.rotateXZBy((rand()%(360*2)) - 360);/**/
+                        p.vector = tgt;
+                        if(MaxLifeTime - MinLifeTime == 0)
+                                p.endTime = now + MinLifeTime;
+                        else
+                                p.endTime = now + MinLifeTime + (rand() % (MaxLifeTime - MinLifeTime));
+                        p.color = MinStartColor.getInterpolated(
+                                                MaxStartColor, (rand() % 100) / 100.0f);
+                        p.startColor = p.color;
+                        p.startVector = p.vector;
+                        if (MinStartSize==MaxStartSize)
+                                p.startSize = MinStartSize;
+                        else
+                                p.startSize = MinStartSize.getInterpolated(
+                                                        MaxStartSize, (rand() % 100) / 100.0f);
+                        p.size = p.startSize;
+                        Particles.push_back(p);
+                }
+                outArray = Particles.pointer();
+                Emitted+=Particles.size();
+                // //hmm
+                // if(Particles.size()==0)
+                // {
+                // delete this;
+                // }
+                return Particles.size();
+        }
+        //argh
+        virtual void setDirection( const core::vector3df& newDirection ) { Direction = newDirection; }
+        virtual void setMinParticlesPerSecond( u32 minPPS ) { MinParticlesPerSecond = minPPS; }
+        virtual void setMaxParticlesPerSecond( u32 maxPPS ) { MaxParticlesPerSecond = maxPPS; }
+        virtual void setMinStartColor( const video::SColor& color ) { MinStartColor = color; }
+        virtual void setMaxStartColor( const video::SColor& color ) { MaxStartColor = color; }
+        virtual void setMaxLifeTime( const u32 t ) { MaxLifeTime = t; }
+        virtual void setMinLifeTime( const u32 t ) { MinLifeTime = t; }
+        virtual u32 getMaxLifeTime() const { return MaxLifeTime; }
+        virtual u32 getMinLifeTime() const { return MinLifeTime; }
+        virtual void setMaxAngleDegrees(const s32 t ) { MaxLifeTime = MaxLifeTime; }
+        virtual s32 getMaxAngleDegrees() const { return 0; }
+        virtual void setMaxStartSize( const core::dimension2df& size ) { MaxStartSize = size; }
+        virtual void setMinStartSize( const core::dimension2df& size ) { MinStartSize = size; }
+        virtual void setCenter( const core::vector3df& center ) { Center = center; }
+        virtual void setRadius( f32 radius ) { Radius = radius; }
+        virtual const core::vector3df& getDirection() const { return Direction; }
+        virtual u32 getMinParticlesPerSecond() const { return MinParticlesPerSecond; }
+        virtual u32 getMaxParticlesPerSecond() const { return MaxParticlesPerSecond; }
+        virtual const video::SColor& getMinStartColor() const { return MinStartColor; }
+        virtual const video::SColor& getMaxStartColor() const { return MaxStartColor; }
+        virtual const core::dimension2df& getMaxStartSize() const { return MaxStartSize; }
+        virtual const core::dimension2df& getMinStartSize() const { return MinStartSize; }
+        virtual const core::vector3df& getCenter() const { return Center; }
+        virtual f32 getRadius() const { return Radius; }
+        virtual irr::scene::E_PARTICLE_EMITTER_TYPE getType() const {
+                return (irr::scene::E_PARTICLE_EMITTER_TYPE) 666;
+        }
+        void restart() { Emitted=0; }
+};
+
+// also does gravity and light
+class CollisionAffector : public irr::scene::IParticleAffector
+{
+public:
+        CollisionAffector(IGameDef *gamedef, ClientEnvironment &env)
+        {
+                m_gamedef = gamedef;
+                m_env = &env;
+        }
+        void affect(u32 now, irr::scene::SParticle* particlearray, u32 count)
+        {
+                if( LastTime == 0 )
+                {
+                        LastTime = now;
+                        return;
+                }
+                f32 timeDelta = ( now - LastTime ) / 1000.0f;
+                LastTime = now;
+                if( !Enabled )
+                        return;
+                for(u32 i=0; i<count; ++i)
+                {
+                        //gravity done here b/c it doesnt work with a gravity affector atm.
+                        particlearray[i].vector += v3f(0.0, -0.1, 0.0) * timeDelta;
+                        irr::scene::SParticle p = particlearray[i];
+                        float size = p.size.Width;
+                        core::aabbox3d<f32> box = core::aabbox3d<f32>
+                                        (-size/2,-size/2,-size/2,size/2,size/2,size/2);
+                        //to ensure particles collide with correct position
+                        v3f pos = p.pos + intToFloat(m_env->getCameraOffset(), BS);
+                        v3f speed = p.vector;
+                        v3f acc = v3f(0.0, 0.0, 0.0);
+
+                        collisionMoveSimple(m_env, m_gamedef,
+                                BS*0.5, box,
+                                0, timeDelta,
+                                pos, speed, acc);
+
+                        particlearray[i].pos = pos;
+                        particlearray[i].vector = speed + (acc * timeDelta);
+
+                        // //useful to see whats going on
+                        // if(res == 0)
+                        // particlearray[i].color = video::SColor(255.0, 255.0, 0.0, 0.0);
+                        // if(res == 1)
+                        // particlearray[i].color = video::SColor(255.0, 0.0, 255.0, 0.0);
+                        // if(res==2)
+                        // particlearray[i].color = video::SColor(255.0, 0.0, 0.0, 255.0);
+                        // u8 light = 0;
+                        // try{
+                        // v3s16 p = v3s16(
+                        // floor(pos.X+0.5),
+                        // floor(pos.Y+0.5),
+                        // floor(pos.Z+0.5)
+                        // );
+                        // MapNode n = m_env->getClientMap().getNode(p);
+                        // light = n.getLightBlend(m_env->getDayNightRatio(), m_gamedef->ndef());
+                        // }
+                        // catch(InvalidPositionException &e){
+                        // light = blend_light(m_env->getDayNightRatio(), LIGHT_SUN, 0);
+                        // }
+                        // u8 plight = decode_light(light);
+                        // particlearray[i].color = video::SColor(255, plight, plight, plight);
+                }
+        }
+        irr::scene::E_PARTICLE_AFFECTOR_TYPE getType() const {
+                return (irr::scene::E_PARTICLE_AFFECTOR_TYPE) 666;
+        }
+        irr::u32 LastTime;
+        IGameDef *m_gamedef;
+        ClientEnvironment *m_env;
+};
+class DeleteDoneAffector : public irr::scene::IParticleAffector
+{
+public:
+        DeleteDoneAffector(irr::scene::IParticleSystemSceneNode* ps, irr::scene::ISceneManager* smgr)
+        {
+                ParticleSystem = ps;
+                SceneManager = smgr;
+        }
+        void affect(u32 now, irr::scene::SParticle* particlearray, u32 count)
+        {
+                // if the particle count every goes to zero, delete the particle system
+                if (count<=0)
+                        SceneManager->addToDeletionQueue(ParticleSystem);
+        }
+        irr::scene::E_PARTICLE_AFFECTOR_TYPE getType() const {
+                return (irr::scene::E_PARTICLE_AFFECTOR_TYPE) 667;
+        }
+private:
+        irr::scene::IParticleSystemSceneNode* ParticleSystem;
+        irr::scene::ISceneManager* SceneManager;
+};
+
 /*
 	Utility
 */
@@ -511,10 +727,10 @@ void ParticleManager::handleParticleEvent(ClientEvent *event, IGameDef *gamedef,
 void ParticleManager::addDiggingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
 		LocalPlayer *player, v3s16 pos, const TileSpec tiles[])
 {
-	for (u16 j = 0; j < 32; j++) // set the amount of particles here
-	{
+//	for (u16 j = 0; j < 32; j++) // set the amount of particles here
+//	{
 		addNodeParticle(gamedef, smgr, player, pos, tiles);
-	}
+//	}
 }
 
 void ParticleManager::addPunchingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
@@ -523,6 +739,55 @@ void ParticleManager::addPunchingParticles(IGameDef* gamedef, scene::ISceneManag
 	addNodeParticle(gamedef, smgr, player, pos, tiles);
 }
 
+void ParticleManager::addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
+		     LocalPlayer *player, /*ClientEnvironment &env,*/ v3s16 pos,
+		     const TileSpec tiles[])
+{
+	// Texture
+	u8 texid = myrand_range(0,5);
+	video::ITexture *texture = tiles[texid].texture;
+	//to ensure particles are at correct position
+	//v3s16 camera_offset = (&env)->getCameraOffset();
+	v3s16 camera_offset = m_env->getCameraOffset();
+	v3f particlepos = intToFloat(pos, BS) - intToFloat(camera_offset, BS);
+	scene::IParticleSystemSceneNode* ps =
+			smgr->addParticleSystemSceneNode(false);
+	scene::IParticleEmitter* em;
+	em = new CParticleExplosionEmitter(particlepos, // position
+					   0.5, // radius
+					   core::vector3df(0.0f,0.02f,0.0f), // initial direction
+					   20, 40, // emit rate
+					   video::SColor(0,255,255,255), // darkest color
+					   video::SColor(0,255,255,255), // brightest color
+					   800, 4000, // min and max age,
+					   core::dimension2df(0.3f,0.3f), // min size
+					   core::dimension2df(2.f,2.f)); // max size
+	//irrlicht standard
+	ps->setEmitter(em);
+	em->drop();
+	// this does not work, results in particles sticking to ground and flickering
+	// up and down. this affector sets particles startVector
+	// scene::IParticleAffector* paf = ps->createGravityAffector(v3f(0.0, -0.1, 0.0));
+	// ps->addAffector(paf);
+	// paf->drop();
+	// //for deletion after fixed time, just here for keeping the info
+	// irr::scene::ISceneNodeAnimator* sna = smgr->createDeleteAnimator(2000);
+	// ps->addAnimator(sna);
+	// sna->drop();
+	irr::scene::IParticleAffector* puf = new CollisionAffector(gamedef, *m_env);
+	ps->addAffector(puf);
+	puf->drop();
+	//need to be added after collision
+	scene::IParticleAffector* paf = new DeleteDoneAffector(ps, smgr);
+	ps->addAffector(paf);
+	paf->drop();
+	ps->setMaterialFlag(video::EMF_LIGHTING, false);
+	ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, true );
+	ps->setMaterialTexture(0, texture);
+	//ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+}
+
+/*
 void ParticleManager::addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
 		LocalPlayer *player, v3s16 pos, const TileSpec tiles[])
 {
@@ -572,6 +837,7 @@ void ParticleManager::addNodeParticle(IGameDef* gamedef, scene::ISceneManager* s
 
 	addParticle(toadd);
 }
+*/
 
 void ParticleManager::addParticle(Particle* toadd)
 {
