404323b8328c0c2eeaf03740f4af01b6fee5c432
Craig Robbins <kde.psych@gmail.com>
2015-03-05 16:25:53 +1000

    Fix mapgen using unitialised height map values

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -41,6 +41,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 const char *GenElementManager::ELEMENT_TITLE = "element";
 
+static const s16 INVALID_HEIGHT = MAP_GENERATION_LIMIT + 1;
+
 FlagDesc flagdesc_mapgen[] = {
 	{"trees",    MG_TREES},
 	{"caves",    MG_CAVES},
@@ -155,6 +157,12 @@ s16 Mapgen::findGroundLevel(v2s16 p2d, s16 ymin, s16 ymax)
 }
 
 
+void Mapgen::initHeightMap(s16 *dest, size_t len)
+{
+	for (size_t i = 0; i < len; i++)
+		dest[i] = INVALID_HEIGHT;
+}
+
 void Mapgen::updateHeightmap(v3s16 nmin, v3s16 nmax)
 {
 	if (!heightmap)
@@ -166,11 +174,13 @@ void Mapgen::updateHeightmap(v3s16 nmin, v3s16 nmax)
 		for (s16 x = nmin.X; x <= nmax.X; x++, index++) {
 			s16 y = findGroundLevel(v2s16(x, z), nmin.Y, nmax.Y);
 
-			// if the values found are out of range, trust the old heightmap
-			if (y == nmax.Y && heightmap[index] > nmax.Y)
-				continue;
-			if (y == nmin.Y - 1 && heightmap[index] < nmin.Y)
-				continue;
+			if (heightmap[index] != INVALID_HEIGHT) {
+				// if the values found are out of range, trust the old heightmap
+				if (y == nmax.Y && heightmap[index] > nmax.Y)
+					continue;
+				if (y == nmin.Y - 1 && heightmap[index] < nmin.Y)
+					continue;
+			}
 
 			heightmap[index] = y;
 		}
