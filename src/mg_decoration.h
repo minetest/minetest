444d34bd48274b62df3f8b2f3ec6deb99ed41829
paramat <mat.gregory@virginmedia.com>
2015-03-05 01:53:11 +0000

    Biome API: Enable decorations placed on water
    Add schematic decoration force placement flag

--- src/mg_decoration.h
+++ src/mg_decoration.h
@@ -35,10 +35,11 @@ enum DecorationType {
 	DECO_LSYSTEM
 };
 
-#define DECO_PLACE_CENTER_X 0x01
-#define DECO_PLACE_CENTER_Y 0x02
-#define DECO_PLACE_CENTER_Z 0x04
-#define DECO_USE_NOISE      0x08
+#define DECO_PLACE_CENTER_X  0x01
+#define DECO_PLACE_CENTER_Y  0x02
+#define DECO_PLACE_CENTER_Z  0x04
+#define DECO_USE_NOISE       0x08
+#define DECO_FORCE_PLACEMENT 0x10
 
 extern FlagDesc flagdesc_deco[];
 
