f2765c6caf3645144e18a74553b4b02eb5dc4c60
est31 <MTest31@outlook.com>
2015-03-07 04:08:40 +0100

    Let travis build android

--- util/travis/before_install.sh
+++ util/travis/before_install.sh
@@ -9,7 +9,35 @@ if [ $CC = "clang" ]; then
 	sudo apt-get install clang
 fi
 sudo apt-get install p7zip-full
-if [ $WINDOWS = "no" ]; then
+if [ $ANDROID = "yes" ]; then
+	
+	printf "Generating swap..."
+	# create approx 5 GB swap
+	sudo dd if=/dev/zero of=/swap count=10000000 
+	sudo mkswap /swap
+	sudo swapon -p1 /swap
+	echo " Done."
+	mount
+
+	# Android builds require this, otherwise they give obscure "no such file or directory" error
+	if [ `uname -m` = x86_64 ]; then
+		sudo apt-get update
+		sudo apt-get install --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
+	fi
+
+	# Download sdk and ndk
+	wget https://dl.google.com/android/android-sdk_r24.0.1-linux.tgz -O android-sdk.tgz
+	wget https://dl.google.com/android/ndk/android-ndk-r9b-linux-x86_64.tar.bz2 -O android-ndk.tar.bz2
+	printf "Extracting sdk and ndk archives..."
+	tar xf android-sdk.tgz
+	tar xf android-ndk.tar.bz2
+	echo " Done."
+	sudo mv android-sdk-linux /usr/local/android-sdk
+	sudo mv android-ndk-r9b /usr/local/android-ndk
+	( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) |\
+		/usr/local/android-sdk/tools/android update sdk --no-ui --filter \
+		platform-tool,android-10,build-tools-21.1.2
+elif [ $WINDOWS = "no" ]; then
 	sudo apt-get install libirrlicht-dev cmake libbz2-dev libpng12-dev \
 	libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libhiredis-dev \
 	libogg-dev libvorbis-dev libopenal-dev gettext
