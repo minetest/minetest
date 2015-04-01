minetest/minetest

commit ed10005d381580440558dc49277b4d350f081283

GenElementManager: Pass opaque handles to Lua and rename to ObjDefManager
Add core.clear_registered_schematics() and refactor schematics somewhat

src/mapgen.cpp
@@ -36,10 +36,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "treegen.h"
 #include "serialization.h"
 #include "util/serialize.h"
+#include "util/numeric.h"
 #include "filesys.h"
 #include "log.h"
-
-const char *GenElementManager::ELEMENT_TITLE = "element";
 
 FlagDesc flagdesc_mapgen[] = {
 	{"trees",    MG_TREES},
@@ -63,6 +62,7 @@ FlagDesc flagdesc_gennotify[] = {
 
 
 ///////////////////////////////////////////////////////////////////////////////
+
 
 Mapgen::Mapgen()
 {
@@ -431,83 +431,160 @@ void GenerateNotifier::getEvents(
 ///////////////////////////////////////////////////////////////////////////////
 
 
-GenElementManager::GenElementManager(IGameDef *gamedef)
+ObjDefManager::ObjDefManager(IGameDef *gamedef, ObjDefType type)
 {
+	m_objtype = type;
 	m_ndef = gamedef->getNodeDefManager();
 }
 
 
-GenElementManager::~GenElementManager()
+ObjDefManager::~ObjDefManager()
 {
-	for (size_t i = 0; i != m_elements.size(); i++)
-		delete m_elements[i];
+	for (size_t i = 0; i != m_objects.size(); i++)
+		delete m_objects[i];
 }
 
 
-u32 GenElementManager::add(GenElement *elem)
+ObjDefHandle ObjDefManager::add(ObjDef *obj)
 {
-	size_t nelem = m_elements.size();
+	assert(obj);
 
-	for (size_t i = 0; i != nelem; i++) {
-		if (m_elements[i] == NULL) {
-			elem->id = i;
-			m_elements[i] = elem;
-			return i;
-		}
-	}
+	if (obj->name.length() && getByName(obj->name))
+		return OBJDEF_INVALID_HANDLE;
 
-	if (nelem >= this->ELEMENT_LIMIT)
+	u32 index = addRaw(obj);
+	if (index == OBJDEF_INVALID_INDEX)
+		return OBJDEF_INVALID_HANDLE;
+
+	obj->handle = createHandle(index, m_objtype, obj->uid);
+	return obj->handle;
+}
+
+
+ObjDef *ObjDefManager::get(ObjDefHandle handle) const
+{
+	u32 index = validateHandle(handle);
+	return (index != OBJDEF_INVALID_INDEX) ? getRaw(index) : NULL;
+}
+
+
+ObjDef *ObjDefManager::set(ObjDefHandle handle, ObjDef *obj)
+{
+	u32 index = validateHandle(handle);
+	return (index != OBJDEF_INVALID_INDEX) ? setRaw(index, obj) : NULL;
+}
+
+
+u32 ObjDefManager::addRaw(ObjDef *obj)
+{
+	size_t nobjects = m_objects.size();
+	if (nobjects >= OBJDEF_MAX_ITEMS)
 		return -1;
 
-	elem->id = nelem;
-	m_elements.push_back(elem);
+	obj->index = nobjects;
 
-	verbosestream << "GenElementManager: added " << this->ELEMENT_TITLE
-		<< " element '" << elem->name << "'" << std::endl;
+	// Ensure UID is nonzero so that a valid handle == OBJDEF_INVALID_HANDLE
+	// is not possible.  The slight randomness bias isn't very significant.
+	obj->uid = myrand() & OBJDEF_UID_MASK;
+	if (obj->uid == 0)
+		obj->uid = 1;
 
-	return nelem;
+	m_objects.push_back(obj);
+
+	infostream << "ObjDefManager: added " << getObjectTitle()
+		<< ": name=\"" << obj->name
+		<< "\" index=" << obj->index
+		<< " uid="     << obj->uid
+		<< std::endl;
+
+	return nobjects;
 }
 
 
-GenElement *GenElementManager::get(u32 id)
+ObjDef *ObjDefManager::getRaw(u32 index) const
 {
-	return (id < m_elements.size()) ? m_elements[id] : NULL;
+	return m_objects[index];
 }
 
 
-GenElement *GenElementManager::getByName(const std::string &name)
+ObjDef *ObjDefManager::setRaw(u32 index, ObjDef *obj)
 {
-	for (size_t i = 0; i != m_elements.size(); i++) {
-		GenElement *elem = m_elements[i];
-		if (elem && name == elem->name)
-			return elem;
+	ObjDef *old_obj = m_objects[index];
+	m_objects[index] = obj;
+	return old_obj;
+}
+
+
+ObjDef *ObjDefManager::getByName(const std::string &name) const
+{
+	for (size_t i = 0; i != m_objects.size(); i++) {
+		ObjDef *obj = m_objects[i];
+		if (obj && !strcasecmp(name.c_str(), obj->name.c_str()))
+			return obj;
 	}
 
 	return NULL;
 }
 
 
-GenElement *GenElementManager::update(u32 id, GenElement *elem)
+void ObjDefManager::clear()
 {
-	if (id >= m_elements.size())
-		return NULL;
+	for (size_t i = 0; i != m_objects.size(); i++)
+		delete m_objects[i];
 
-	GenElement *old_elem = m_elements[id];
-	m_elements[id] = elem;
-	return old_elem;
+	m_objects.clear();
 }
 
 
-GenElement *GenElementManager::remove(u32 id)
+u32 ObjDefManager::validateHandle(ObjDefHandle handle) const
 {
-	return update(id, NULL);
+	ObjDefType type;
+	u32 index;
+	u32 uid;
+
+	bool is_valid =
+		(handle != OBJDEF_INVALID_HANDLE)         &&
+		decodeHandle(handle, &index, &type, &uid) &&
+		(type == m_objtype)                       &&
+		(index < m_objects.size())                &&
+		(m_objects[index]->uid == uid);
+
+	return is_valid ? index : -1;
 }
 
 
-void GenElementManager::clear()
+ObjDefHandle ObjDefManager::createHandle(u32 index, ObjDefType type, u32 uid)
 {
-	m_elements.clear();
+	ObjDefHandle handle = 0;
+	set_bits(&handle, 0, 18, index);
+	set_bits(&handle, 18, 6, type);
+	set_bits(&handle, 24, 7, uid);
+
+	u32 parity = calc_parity(handle);
+	set_bits(&handle, 31, 1, parity);
+
+	return handle ^ OBJDEF_HANDLE_SALT;
 }
+
+
+bool ObjDefManager::decodeHandle(ObjDefHandle handle, u32 *index,
+	ObjDefType *type, u32 *uid)
+{
+	handle ^= OBJDEF_HANDLE_SALT;
+
+	u32 parity = get_bits(handle, 31, 1);
+	set_bits(&handle, 31, 1, 0);
+	if (parity != calc_parity(handle))
+		return false;
+
+	*index = get_bits(handle, 0, 18);
+	*type  = (ObjDefType)get_bits(handle, 18, 6);
+	*uid   = get_bits(handle, 24, 7);
+	return true;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
 
 
 void MapgenParams::load(const Settings &settings)

src/mapgen.h
@@ -179,36 +179,68 @@ struct MapgenFactory {
 	virtual ~MapgenFactory() {}
 };
 
-class GenElement {
+typedef std::map<std::string, std::string> StringMap;
+typedef u32 ObjDefHandle;
+
+#define OBJDEF_INVALID_INDEX ((u32)(-1))
+#define OBJDEF_INVALID_HANDLE 0
+#define OBJDEF_HANDLE_SALT 0x00585e6fu
+#define OBJDEF_MAX_ITEMS (1 << 18)
+#define OBJDEF_UID_MASK ((1 << 7) - 1)
+
+enum ObjDefType {
+	OBJDEF_GENERIC,
+	OBJDEF_BIOME,
+	OBJDEF_ORE,
+	OBJDEF_DECORATION,
+	OBJDEF_SCHEMATIC,
+};
+
+class ObjDef {
 public:
-	virtual ~GenElement() {}
-	u32 id;
+	virtual ~ObjDef() {}
+
+	u32 index;
+	u32 uid;
+	ObjDefHandle handle;
 	std::string name;
 };
 
-class GenElementManager {
+class ObjDefManager {
 public:
-	static const char *ELEMENT_TITLE;
-	static const size_t ELEMENT_LIMIT = -1;
+	ObjDefManager(IGameDef *gamedef, ObjDefType type);
+	virtual ~ObjDefManager();
 
-	GenElementManager(IGameDef *gamedef);
-	virtual ~GenElementManager();
+	virtual const char *getObjectTitle() const = 0;
 
-	virtual GenElement *create(int type) = 0;
-
-	virtual u32 add(GenElement *elem);
-	virtual GenElement *get(u32 id);
-	virtual GenElement *update(u32 id, GenElement *elem);
-	virtual GenElement *remove(u32 id);
+	virtual ObjDef *create(int type) = 0;
 	virtual void clear();
+	virtual ObjDef *getByName(const std::string &name) const;
 
-	virtual GenElement *getByName(const std::string &name);
+	//// Add new/get/set object definitions by handle
+	virtual ObjDefHandle add(ObjDef *obj);
+	virtual ObjDef *get(ObjDefHandle handle) const;
+	virtual ObjDef *set(ObjDefHandle handle, ObjDef *obj);
 
-	INodeDefManager *getNodeDef() { return m_ndef; }
+	//// Raw variants that work on indexes
+	virtual u32 addRaw(ObjDef *obj);
+
+	// It is generally assumed that getRaw() will always return a valid object
+	// This won't be true if people do odd things such as call setRaw() with NULL
+	virtual ObjDef *getRaw(u32 index) const;
+	virtual ObjDef *setRaw(u32 index, ObjDef *obj);
+
+	INodeDefManager *getNodeDef() const { return m_ndef; }
+
+	u32 validateHandle(ObjDefHandle handle) const;
+	static ObjDefHandle createHandle(u32 index, ObjDefType type, u32 uid);
+	static bool decodeHandle(ObjDefHandle handle, u32 *index,
+		ObjDefType *type, u32 *uid);
 
 protected:
 	INodeDefManager *m_ndef;
-	std::vector<GenElement *> m_elements;
+	std::vector<ObjDef *> m_objects;
+	ObjDefType m_objtype;
 };
 
 #endif

src/mapgen_v5.cpp
@@ -479,7 +479,7 @@ void MapgenV5::generateCaves(int max_stone_y)
 		for (s16 y=node_min.Y - 1; y<=node_max.Y + 1; y++) {
 			u32 i = vm->m_area.index(node_min.X, y, z);
 			for (s16 x=node_min.X; x<=node_max.X; x++, i++, index++, index2d++) {
-				Biome *biome = (Biome *)bmgr->get(biomemap[index2d]);
+				Biome *biome = (Biome *)bmgr->getRaw(biomemap[index2d]);
 				content_t c = vm->m_data[i].getContent();
 				if (c == CONTENT_AIR
 						|| (y <= water_level
@@ -519,7 +519,7 @@ void MapgenV5::dustTopNodes()
 
 	for (s16 z = node_min.Z; z <= node_max.Z; z++)
 	for (s16 x = node_min.X; x <= node_max.X; x++, index++) {
-		Biome *biome = (Biome *)bmgr->get(biomemap[index]);
+		Biome *biome = (Biome *)bmgr->getRaw(biomemap[index]);
 
 		if (biome->c_dust == CONTENT_IGNORE)
 			continue;

src/mapgen_v7.cpp
@@ -674,7 +674,7 @@ void MapgenV7::dustTopNodes()
 
 	for (s16 z = node_min.Z; z <= node_max.Z; z++)
 	for (s16 x = node_min.X; x <= node_max.X; x++, index++) {
-		Biome *biome = (Biome *)bmgr->get(biomemap[index]);
+		Biome *biome = (Biome *)bmgr->getRaw(biomemap[index]);
 
 		if (biome->c_dust == CONTENT_IGNORE)
 			continue;
@@ -821,7 +821,7 @@ void MapgenV7::generateCaves(int max_stone_y)
 				u32 i = vm->m_area.index(node_min.X, y, z);
 				for (s16 x = node_min.X; x <= node_max.X;
 						x++, i++, index++, index2d++) {
-					Biome *biome = (Biome *)bmgr->get(biomemap[index2d]);
+					Biome *biome = (Biome *)bmgr->getRaw(biomemap[index2d]);
 					content_t c = vm->m_data[i].getContent();
 					if (c == CONTENT_AIR || (y <= water_level &&
 						c != biome->c_stone && c != c_stone))

src/mg_biome.cpp
@@ -27,18 +27,16 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "util/mathconstants.h"
 #include "porting.h"
 
-const char *BiomeManager::ELEMENT_TITLE = "biome";
-
 
 ///////////////////////////////////////////////////////////////////////////////
 
+
 BiomeManager::BiomeManager(IGameDef *gamedef) :
-	GenElementManager(gamedef)
+	ObjDefManager(gamedef, OBJDEF_BIOME)
 {
 	// Create default biome to be used in case none exist
 	Biome *b = new Biome;
 
-	b->id              = 0;
 	b->name            = "Default";
 	b->flags           = 0;
 	b->depth_top       = 0;
@@ -75,8 +73,10 @@ BiomeManager::~BiomeManager()
 void BiomeManager::calcBiomes(s16 sx, s16 sy, float *heat_map,
 	float *humidity_map, s16 *height_map, u8 *biomeid_map)
 {
-	for (s32 i = 0; i != sx * sy; i++)
-		biomeid_map[i] = getBiome(heat_map[i], humidity_map[i], height_map[i])->id;
+	for (s32 i = 0; i != sx * sy; i++) {
+		Biome *biome = getBiome(heat_map[i], humidity_map[i], height_map[i]);
+		biomeid_map[i] = biome->index;
+	}
 }
 
 
@@ -85,8 +85,8 @@ Biome *BiomeManager::getBiome(float heat, float humidity, s16 y)
 	Biome *b, *biome_closest = NULL;
 	float dist_min = FLT_MAX;
 
-	for (size_t i = 1; i < m_elements.size(); i++) {
-		b = (Biome *)m_elements[i];
+	for (size_t i = 1; i < m_objects.size(); i++) {
+		b = (Biome *)m_objects[i];
 		if (!b || y > b->y_max || y < b->y_min)
 			continue;
 
@@ -100,18 +100,18 @@ Biome *BiomeManager::getBiome(float heat, float humidity, s16 y)
 		}
 	}
 
-	return biome_closest ? biome_closest : (Biome *)m_elements[0];
+	return biome_closest ? biome_closest : (Biome *)m_objects[0];
 }
 
 void BiomeManager::clear()
 {
 
-	for (size_t i = 1; i < m_elements.size(); i++) {
-		Biome *b = (Biome *)m_elements[i];
+	for (size_t i = 1; i < m_objects.size(); i++) {
+		Biome *b = (Biome *)m_objects[i];
 		delete b;
 	}
 
-	m_elements.resize(1);
+	m_objects.resize(1);
 }
 
 

src/mg_biome.h
@@ -33,7 +33,7 @@ enum BiomeType
 	BIOME_TYPE_FLAT
 };
 
-class Biome : public GenElement, public NodeResolver {
+class Biome : public ObjDef, public NodeResolver {
 public:
 	u32 flags;
 
@@ -56,13 +56,17 @@ public:
 	virtual void resolveNodeNames(NodeResolveInfo *nri);
 };
 
-class BiomeManager : public GenElementManager {
+class BiomeManager : public ObjDefManager {
 public:
-	static const char *ELEMENT_TITLE;
-	static const size_t ELEMENT_LIMIT = 0x100;
+	static const char *OBJECT_TITLE;
 
 	BiomeManager(IGameDef *gamedef);
 	~BiomeManager();
+
+	const char *getObjectTitle() const
+	{
+		return "biome";
+	}
 
 	Biome *create(int btt)
 	{

src/mg_decoration.cpp
@@ -25,8 +25,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "log.h"
 #include "util/numeric.h"
 
-const char *DecorationManager::ELEMENT_TITLE = "decoration";
-
 FlagDesc flagdesc_deco[] = {
 	{"place_center_x", DECO_PLACE_CENTER_X},
 	{"place_center_y", DECO_PLACE_CENTER_Y},
@@ -40,7 +38,7 @@ FlagDesc flagdesc_deco[] = {
 
 
 DecorationManager::DecorationManager(IGameDef *gamedef) :
-	GenElementManager(gamedef)
+	ObjDefManager(gamedef, OBJDEF_DECORATION)
 {
 }
 
@@ -50,8 +48,8 @@ size_t DecorationManager::placeAllDecos(Mapgen *mg, u32 blockseed,
 {
 	size_t nplaced = 0;
 
-	for (size_t i = 0; i != m_elements.size(); i++) {
-		Decoration *deco = (Decoration *)m_elements[i];
+	for (size_t i = 0; i != m_objects.size(); i++) {
+		Decoration *deco = (Decoration *)m_objects[i];
 		if (!deco)
 			continue;
 
@@ -65,11 +63,11 @@ size_t DecorationManager::placeAllDecos(Mapgen *mg, u32 blockseed,
 
 void DecorationManager::clear()
 {
-	for (size_t i = 0; i < m_elements.size(); i++) {
-		Decoration *deco = (Decoration *)m_elements[i];
+	for (size_t i = 0; i < m_objects.size(); i++) {
+		Decoration *deco = (Decoration *)m_objects[i];
 		delete deco;
 	}
-	m_elements.clear();
+	m_objects.clear();
 }
 
 
@@ -169,7 +167,7 @@ size_t Decoration::placeDeco(Mapgen *mg, u32 blockseed, v3s16 nmin, v3s16 nmax)
 
 			v3s16 pos(x, y, z);
 			if (generate(mg->vm, &ps, pos))
-				mg->gennotify.addEvent(GENNOTIFY_DECORATION, pos, id);
+				mg->gennotify.addEvent(GENNOTIFY_DECORATION, pos, index);
 		}
 	}
 

src/mg_decoration.h
@@ -59,7 +59,7 @@ struct CutoffData {
 };
 #endif
 
-class Decoration : public GenElement, public NodeResolver {
+class Decoration : public ObjDef, public NodeResolver {
 public:
 	INodeDefManager *ndef;
 
@@ -121,13 +121,15 @@ public:
 };
 */
 
-class DecorationManager : public GenElementManager {
+class DecorationManager : public ObjDefManager {
 public:
-	static const char *ELEMENT_TITLE;
-	static const size_t ELEMENT_LIMIT = 0x10000;
-
 	DecorationManager(IGameDef *gamedef);
 	~DecorationManager() {}
+
+	const char *getObjectTitle() const
+	{
+		return "decoration";
+	}
 
 	Decoration *create(int type)
 	{

commit 64f51916a4ab646319a9557a7ef5093bc8f00d4f

Fix MSVC error caused by ed10005

src/mapgen.cpp
@@ -32,6 +32,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "voxelalgorithms.h"
 #include "profiler.h"
 #include "settings.h"
+#include "porting.h" // For str functions
 #include "treegen.h"
 #include "serialization.h"
 #include "util/serialize.h"