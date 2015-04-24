ab04b7fe941003cf33350d7def69e4c1cc751314
nerzhul <loic.blot@unix-experience.fr>
2015-04-24 15:33:47 +0200

    Remove unused variable Client::m_active_blocks

--- src/client.h
+++ src/client.h
@@ -573,7 +573,6 @@ private:
 	bool m_inventory_updated;
 	Inventory *m_inventory_from_server;
 	float m_inventory_from_server_age;
-	std::set<v3s16> m_active_blocks;
 	PacketCounter m_packetcounter;
 	bool m_show_highlighted;
 	// Block mesh animation parameters
