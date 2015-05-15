33a633a6317d272ffd06320c92416644a367b186
est31 <MTest31@outlook.com>
2015-05-15 05:02:19 +0200

    Fix libgmp for Android
    Android terminated immediately after start since commit
    181f7baa453c58d4070de7196fd74663110946a8
    because library wasn't loaded, and libminetest.so required
    libgmp.so.10 instead of libgmp.so.
    
    The second issue has been solved by the linker flags.

--- build/android/src/net/minetest/minetest/MtNativeActivity.java
+++ build/android/src/net/minetest/minetest/MtNativeActivity.java
@@ -86,6 +86,12 @@ public class MtNativeActivity extends NativeActivity {
 		System.loadLibrary("vorbis");
 		System.loadLibrary("ssl");
 		System.loadLibrary("crypto");
+		System.loadLibrary("gmp");
+
+		// We don't have to load libminetest.so ourselves,
+		// but if we do, we get nicer logcat errors when
+		// loading fails.
+		System.loadLibrary("minetest");
 	}
 	
 	private int m_MessagReturnCode;
