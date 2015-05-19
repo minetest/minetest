43702ec6ac0fae7c8ef04be82bcca72ba9bec017
kwolekr <kwolekr@minetest.net>
2015-05-19 02:38:48 -0400

    Fix build with libstdc++
    libstdc++ doesn't include <cstring> inside of <string>, unlike libc++.

--- src/objdef.cpp
+++ src/objdef.cpp
@@ -17,9 +17,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <strings.h>
 #include "objdef.h"
 #include "util/numeric.h"
 #include "debug.h"
+#include "porting.h"
 #include "log.h"
 #include "gamedef.h"
 
