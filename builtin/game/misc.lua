82dfff0e4e698a01e1fbdafbd7b5d83e702398ea
HybridDog <adresse_test@t-online.de>
2015-06-03 15:58:38 +0200

    minetest.after: abort if theres no active timer and only reduce the timer.time of all timers when its necessary to reduce lag caused by the after globalstep

--- builtin/game/misc.lua
+++ builtin/game/misc.lua
@@ -4,31 +4,64 @@
 -- Misc. API functions
 --
 
-core.timers_to_add = {}
-core.timers = {}
-core.register_globalstep(function(dtime)
-	for _, timer in ipairs(core.timers_to_add) do
-		table.insert(core.timers, timer)
-	end
-	core.timers_to_add = {}
-	local index = 1
-	while index <= #core.timers do
-		local timer = core.timers[index]
-		timer.time = timer.time - dtime
+local timers_to_add = {}
+local timers = {}
+local mintime
+local function update_timers(delay)
+	mintime = false
+	local sub = 0
+	for index = 1,#timers do
+		index = index - sub
+		local timer = timers[index]
+		timer.time = timer.time - delay
 		if timer.time <= 0 then
 			timer.func(unpack(timer.args or {}))
-			table.remove(core.timers,index)
+			table.remove(timers, index)
+			sub = sub + 1
+		elseif mintime
+		and mintime ~= true then
+			mintime = math.min(mintime, timer.time)
 		else
-			index = index + 1
+			mintime = timer.time
 		end
 	end
+end
+
+local delay = 0
+core.register_globalstep(function(dtime)
+	if not mintime then
+		return
+	end
+	if #timers_to_add ~= 0 then
+		update_timers(delay)
+		delay = 0
+		for _, timer in ipairs(timers_to_add) do
+			table.insert(timers, timer)
+			if mintime then
+				mintime = math.min(mintime, timer.time)
+			else
+				mintime = timer.time
+			end
+		end
+		timers_to_add = {}
+	end
+	delay = delay+dtime
+	if delay < mintime then
+		return
+	end
+	update_timers(delay)
+	delay = 0
 end)
 
 function core.after(time, func, ...)
 	assert(tonumber(time) and type(func) == "function",
 			"Invalid core.after invocation")
-	table.insert(core.timers_to_add, {time=time, func=func, args={...}})
+	mintime = true
+	table.insert(timers_to_add, {time=time, func=func, args={...}})
 end
+
+core.timers_to_add = timers_to_add
+core.timers = timers
 
 function core.check_player_privs(name, privs)
 	local player_privs = core.get_player_privs(name)
