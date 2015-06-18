10140f21386859ccc06601f6ce5efeb4df197933
figec <raptor.ext@gmail.com>
2015-06-18 21:34:17 +0300

    Fix wrap_rows at inner byte of char

--- src/util/string.h
+++ src/util/string.h
@@ -426,12 +426,19 @@ inline std::string wrap_rows(const std::string &from,
 {
 	std::string to;
 
+	bool need_to_wrap = false;
+
 	size_t character_idx = 0;
 	for (size_t i = 0; i < from.size(); i++) {
 		if (character_idx > 0 && character_idx % row_len == 0)
-			to += '\n';
-		if (!IS_UTF8_MULTB_INNER(from[i]))
+			need_to_wrap = true;
+		if (!IS_UTF8_MULTB_INNER(from[i])) {
+			if (need_to_wrap) {
+				to += '\n';
+				need_to_wrap = false;
+			}
 			character_idx++;
+		}
 		to += from[i];
 	}
 
