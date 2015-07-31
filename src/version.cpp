845d0501c14f35f377b84159995f175f9ec14ee8
est31 <MTest31@outlook.com>
2015-07-31 16:38:36 +0200

    Android: Add githash header to spare rebuilds after new commits
    Before, android_version.h got changed at every new commit. Now, we
    only change it with new minetest releases. Analogous to the cmake
    progress, we add an android_version_githash.h file that communicates
    the git hash file to C++ code.
    
    Also, unify VERS_MAJOR, VERS_MINOR and VERS_PATCH version variable
    calculation inside the whole makefile.

--- src/version.cpp
+++ src/version.cpp
@@ -22,6 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #if defined(__ANDROID__)
 	#include "android_version.h"
+	#include "android_version_githash.h"
 #elif defined(USE_CMAKE_CONFIG_H)
 	#include "cmake_config_githash.h"
 #endif
