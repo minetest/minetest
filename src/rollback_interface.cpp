0ba1cf82033a810dd2e6178e23794bb135ede46a
kwolekr <kwolekr@minetest.net>
2015-08-15 14:45:44 -0400

    Rollback: Fail on bad precondition instead of causing assertion error

--- src/rollback_interface.cpp
+++ src/rollback_interface.cpp
@@ -210,6 +210,7 @@ bool RollbackAction::applyRevert(Map *map, InventoryManager *imgr, IGameDef *gam
 					<< inventory_index << " too large in "
 					<< "inventory list \"" << inventory_list << "\" in "
 					<< inventory_location << std::endl;
+				return false;
 			}
 			// If item was added, take away item, otherwise add removed item
 			if (inventory_add) {
