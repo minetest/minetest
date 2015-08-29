9c44aace4fce5f4b496201214d3215899f1481c3
est31 <MTest31@outlook.com>
2015-08-30 01:17:03 +0200

    l_mainmenu.h: remove unused l_get_dirlist function
    The commit
    8f9af57314f71aae1cc77e13f9996e13015d776d "Add core.get_dir_list" by @ShadowNinja
    has removed the implementation of the l_get_dirlist function and all its usages
    from the l_mainmenu.cpp file, but hasn't removed it from the header file.
    
    The reason why this hasn't been detected earlier is that C++ has this interesting
    feature to still make it possible to create instances of classes whose never used
    private methods are declared but not defined.

--- src/script/lua_api/l_mainmenu.h
+++ src/script/lua_api/l_mainmenu.h
@@ -119,8 +119,6 @@ private:
 
 	static int l_get_texturepath_share(lua_State *L);
 
-	static int l_get_dirlist(lua_State *L);
-
 	static int l_create_dir(lua_State *L);
 
 	static int l_delete_dir(lua_State *L);
