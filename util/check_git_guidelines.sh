0149534adabe32873c4ef35d1a8743931af146e4
est31 <MTest31@outlook.com>
2015-09-03 09:45:15 +0200

    add Some further tests
    everything valid here :)

--- util/check_git_guidelines.sh
+++ util/check_git_guidelines.sh
@@ -2,23 +2,36 @@
 
 git diff master --check || exit 1
 
-https://git-scm.com/book/uz/v2/Customizing-Git-An-Example-Git-Enforced-Policy
-for commitid in `git rev-list master..HEAD`; do
-	commitmsg=`git cat-file commit $commitid | sed sed '1,/^$/d'`
+fail=false
+
+print_commit_err() {
+	echo "$1: commit message: $2"
+}
+
+git rev-list master..HEAD | while read -r commitid; do
+	commitmsg=`git cat-file commit $commitid | sed '1,/^$/d'`
 	linenum=0
-	for line in $commitmsg; do
+	printf '%s\n' "$commitmsg" | while read -r line; do
 		case "$linenum" in
-			0)	if [ "`echo \"$line\" | wc -L`" -gt "70" ]; then
-					echo "$commitid: commit message: First line is longer than 70 chars"; exit 1
+			0)	if [ "`printf \"$line\" | wc -L`" -gt "70" ]; then
+					print_commit_err "$commitid" "First line is longer than 70 chars"; fail=true
+				fi
+				if ! printf "$line" | cut -c 1 | grep -q '[[:upper:]]'; then
+					print_commit_err "$commitid" "First line doesn't start with capital letter"
+					fail=true
 				fi
 				;;
-			1)	if [ "`echo \"$line\" | wc -L`" -gt "0" ]; then
-					echo "$commitid: commit message: Second line is not blank"; exit 1
+			1)	if [ "`printf \"$line\" | wc -L`" -gt "0" ]; then
+					print_commit_err "$commitid" "Second line is not blank"; fail=true
 				fi
 				;;
 			*)
 		esac
-		let linenum=$linenum+1
+		linenum=$(($linenum+1))
 	done
-	$commitmsg
+	# echo "$commitid is fine"
 done
+
+if [ $fail ]; then
+	exit 1
+fi
