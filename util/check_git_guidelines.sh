31d02e04fbb76472f83fa1d7118ecd993ec74b22
est31 <MTest31@outlook.com>
2015-09-03 09:06:26 +0200

    Let travis also do basic commit format checks ggg ggggg

--- util/check_git_guidelines.sh
+++ util/check_git_guidelines.sh
@@ -3,5 +3,22 @@
 git diff master --check || exit 1
 
 https://git-scm.com/book/uz/v2/Customizing-Git-An-Example-Git-Enforced-Policy
-
-
+for commitid in `git rev-list master..HEAD`; do
+	commitmsg=`git cat-file commit $commitid | sed sed '1,/^$/d'`
+	linenum=0
+	for line in $commitmsg; do
+		case "$linenum" in
+			0)	if [ "`echo \"$line\" | wc -L`" -gt "70" ]; then
+					echo "$commitid: commit message: First line is longer than 70 chars"; exit 1
+				fi
+				;;
+			1)	if [ "`echo \"$line\" | wc -L`" -gt "0" ]; then
+					echo "$commitid: commit message: Second line is not blank"; exit 1
+				fi
+				;;
+			*)
+		esac
+		let linenum=$linenum+1
+	done
+	$commitmsg
+done
