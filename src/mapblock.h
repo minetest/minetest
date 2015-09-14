8e83ca4b624a9bcc49087c126993c23504d1a97a
est31 <MTest31@outlook.com>
2015-09-14 06:02:41 +0200

    Serialisation: documentation fixes, clarifying renames and whitespace fixes
    1. Do two renames:
    	* SER_FMT_CLIENT_VER_LOWEST -> SER_FMT_VER_LOWEST_WRITE
    	* SER_FMT_VER_LOWEST -> SER_FMT_VER_LOWEST_READ
    Now the two define values are consistently named with the _WRITE defines
    SER_FMT_VER_{HIGHEST,LOWEST}_WRITE, and to better point out what the two
    serialisation versions actually are for.
    
    2. wrap some lines in doc/worldformat.txt, and point out that the node
    timers are serialized at a later point, as this can cause confusion about
    what now happens (if one doesn't strictly read the if block's conditions).
    
    3. some whitespace fixes in NodeTimerList::serialize, and one new comment.

--- src/mapblock.h
+++ src/mapblock.h
@@ -504,7 +504,7 @@ public:
 
 	// These don't write or read version by itself
 	// Set disk to true for on-disk format, false for over-the-network format
-	// Precondition: version >= SER_FMT_CLIENT_VER_LOWEST
+	// Precondition: version >= SER_FMT_VER_LOWEST_WRITE
 	void serialize(std::ostream &os, u8 version, bool disk);
 	// If disk == true: In addition to doing other things, will add
 	// unknown blocks from id-name mapping to wndef
