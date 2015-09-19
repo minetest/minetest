efb0293ca04acedfaf0bf370503bf6f3985eda93
Rui <Rui914@users.noreply.github.com>
2015-09-19 18:55:59 +0900

    Tweak the code style of builtin/init.lua

--- builtin/init.lua
+++ builtin/init.lua
@@ -12,27 +12,26 @@ os.setlocale("C", "numeric")
 minetest = core
 
 -- Load other files
-local scriptdir = core.get_builtin_path()..DIR_DELIM
-local gamepath = scriptdir.."game"..DIR_DELIM
-local commonpath = scriptdir.."common"..DIR_DELIM
-local asyncpath = scriptdir.."async"..DIR_DELIM
+local scriptdir = core.get_builtin_path() .. DIR_DELIM
+local gamepath = scriptdir .."game" .. DIR_DELIM
+local commonpath = scriptdir .."common" .. DIR_DELIM
+local asyncpath = scriptdir .."async" .. DIR_DELIM
 
-dofile(commonpath.."strict.lua")
-dofile(commonpath.."serialize.lua")
-dofile(commonpath.."misc_helpers.lua")
+dofile(commonpath .. "strict.lua")
+dofile(commonpath .. "serialize.lua")
+dofile(commonpath .. "misc_helpers.lua")
 
 if INIT == "game" then
-	dofile(gamepath.."init.lua")
+	dofile(gamepath .. "init.lua")
 elseif INIT == "mainmenu" then
 	local mainmenuscript = core.setting_get("main_menu_script")
-	if mainmenuscript ~= nil and mainmenuscript ~= "" then
+	if mainmenuscript and mainmenuscript ~= "" then
 		dofile(mainmenuscript)
 	else
-		dofile(core.get_mainmenu_path()..DIR_DELIM.."init.lua")
+		dofile(core.get_mainmenu_path() .. DIR_DELIM .. "init.lua")
 	end
 elseif INIT == "async" then
-	dofile(asyncpath.."init.lua")
+	dofile(asyncpath .. "init.lua")
 else
 	error(("Unrecognized builtin initialization type %s!"):format(tostring(INIT)))
 end
-
