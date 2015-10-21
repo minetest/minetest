85cad0f30abebffa00135f9a2a3cb48f7b2d347e
paramat <mat.gregory@virginmedia.com>
2015-10-21 08:51:59 +0100

    Decoration API: Add flag for placement on liquid surface
    Add findLiquidSurface() function to mapgen.cpp
    Update lua_api.txt

--- src/mg_decoration.h
+++ src/mg_decoration.h
@@ -41,6 +41,7 @@ enum DecorationType {
 #define DECO_PLACE_CENTER_Z  0x04
 #define DECO_USE_NOISE       0x08
 #define DECO_FORCE_PLACEMENT 0x10
+#define DECO_LIQUID_SURFACE  0x20
 
 extern FlagDesc flagdesc_deco[];
 
