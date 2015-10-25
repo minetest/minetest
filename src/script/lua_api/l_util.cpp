3936a5e3f71d46e61f819b8f6c85dd47a440f21e
est31 <MTest31@outlook.com>
2015-10-26 00:12:39 +0100

    Correct comment in l_util.cpp
    Remove outdated loglevel list from a l_util.cpp comment,
    and rather point to the updated code.

--- src/script/lua_api/l_util.cpp
+++ src/script/lua_api/l_util.cpp
@@ -37,7 +37,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 // log([level,] text)
 // Writes a line to the logger.
 // The one-argument version logs to infostream.
-// The two-argument version accept a log level: error, action, info, or verbose.
+// The two-argument version accepts a log level.
+// Either the special case "deprecated" for deprecation notices, or any specified in
+// Logger::stringToLevel(name).
 int ModApiUtil::l_log(lua_State *L)
 {
 	NO_MAP_LOCK_REQUIRED;
