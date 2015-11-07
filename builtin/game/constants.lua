f9e394a4669e667fcef8992946458c769b4e2770
kwolekr <kwolekr@minetest.net>
2015-11-07 03:27:33 -0500

    Improve LuaVoxelManip documentation

--- builtin/game/constants.lua
+++ builtin/game/constants.lua
@@ -4,6 +4,11 @@
 -- Constants values for use with the Lua API
 --
 
+-- Built-in Content IDs (for use with VoxelManip API)
+core.CONTENT_UNKNOWN = 125
+core.CONTENT_AIR     = 126
+core.CONTENT_IGNORE  = 127
+
 -- Block emerge status constants (for use with core.emerge_area)
 core.EMERGE_CANCELLED   = 0
 core.EMERGE_ERRORED     = 1
