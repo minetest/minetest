ad469b60c1348e229eeb545060b9cfadffe92672
sfan5 <sfan5@live.de>
2015-12-23 23:51:06 +0100

    Update URLs for buildbot & travis
    The freehoster these files were hosted on is shutting down soon-ish, they're now hosted on my VPS.

--- util/travis/before_install.sh
+++ util/travis/before_install.sh
@@ -15,16 +15,16 @@ if [[ $PLATFORM == "Linux" ]]; then
 		libjpeg-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev \
 		libhiredis-dev libogg-dev libgmp-dev libvorbis-dev libopenal-dev gettext
 	# Linking to LevelDB is broken, use a custom build
-	wget http://sfan5.pf-control.de/libleveldb-1.18-ubuntu12.04.7z
+	wget http://minetest.kitsunemimi.pw/libleveldb-1.18-ubuntu12.04.7z
 	sudo 7z x -o/usr libleveldb-1.18-ubuntu12.04.7z
 elif [[ $PLATFORM == "Win32" ]]; then
-	wget http://sfan5.pf-control.de/mingw_w64_i686_ubuntu12.04_4.9.1.7z -O mingw.7z
+	wget http://minetest.kitsunemimi.pw/mingw_w64_i686_ubuntu12.04_4.9.1.7z -O mingw.7z
 	sed -e "s|%PREFIX%|i686-w64-mingw32|" \
 		-e "s|%ROOTPATH%|/usr/i686-w64-mingw32|" \
 		< util/travis/toolchain_mingw.cmake.in > util/buildbot/toolchain_mingw.cmake
 	sudo 7z x -y -o/usr mingw.7z
 elif [[ $PLATFORM == "Win64" ]]; then
-	wget http://sfan5.pf-control.de/mingw_w64_x86_64_ubuntu12.04_4.9.1.7z -O mingw.7z
+	wget http://minetest.kitsunemimi.pw/mingw_w64_x86_64_ubuntu12.04_4.9.1.7z -O mingw.7z
 	sed -e "s|%PREFIX%|x86_64-w64-mingw32|" \
 		-e "s|%ROOTPATH%|/usr/x86_64-w64-mingw32|" \
 		< util/travis/toolchain_mingw.cmake.in > util/buildbot/toolchain_mingw64.cmake
