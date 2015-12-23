affe2b0c28e207e451f22268545879dd89598226
Duane Robertson <duane@duanerobertson.com>
2015-12-23 08:47:06 -0600

    Correct license info in headers.

--- src/mapgen_valleys.h
+++ src/mapgen_valleys.h
@@ -21,10 +21,6 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3.0 of the License, or
-(at your option) any later version.
 */
 
 #ifndef MAPGEN_VALLEYS_HEADER
