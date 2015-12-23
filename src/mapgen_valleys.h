b897a5cfa8ec600c72389ed90150d4aa2273df4e
Duane Robertson <duane@duanerobertson.com>
2015-12-23 07:40:09 -0600

    Change flag numbering.

--- src/mapgen_valleys.h
+++ src/mapgen_valleys.h
@@ -33,8 +33,8 @@ the Free Software Foundation; either version 3.0 of the License, or
 #include "mapgen.h"
 
 /////////////////// Mapgen Valleys flags
-#define MG_VALLEYS_CLIFFS 0x02
-#define MG_VALLEYS_RUGGED 0x04
+#define MG_VALLEYS_CLIFFS 0x01
+#define MG_VALLEYS_RUGGED 0x02
 
 class BiomeManager;
 
