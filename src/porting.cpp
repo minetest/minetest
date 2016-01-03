fa6cf022395e4b602e63460412dd5810d58da982
gregorycu <gregory.currie@gmail.com>
2016-01-03 18:30:39 +1100

    Add MinSizeRel and RelWithDebInfo to MSVCBuildDir check

--- src/porting.cpp
+++ src/porting.cpp
@@ -162,6 +162,8 @@ bool detectMSVCBuildDir(const std::string &path)
 {
 	const char *ends[] = {
 		"bin\\Release",
+		"bin\\MinSizeRel",
+		"bin\\RelWithDebInfo",
 		"bin\\Debug",
 		"bin\\Build",
 		NULL
