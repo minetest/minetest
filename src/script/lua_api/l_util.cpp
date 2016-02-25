df62d96ba5a982448fac2d5b54dc07c6a3a43de5
Jeija <norrepli@gmail.com>
2016-02-25 09:47:28 +0100

    Fix minetest.request_insecure_environment() always returning nil
    Fixes #3765

--- src/script/lua_api/l_util.cpp
+++ src/script/lua_api/l_util.cpp
@@ -27,6 +27,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "cpp_api/s_security.h"
 #include "areastore.h"
 #include "porting.h"
+#include "debug.h"
 #include "log.h"
 #include "tool.h"
 #include "filesys.h"
@@ -372,8 +373,8 @@ int ModApiUtil::l_request_insecure_environment(lua_State *L)
 	if (lua_getstack(L, 2, &info)) {
 		return 0;
 	}
-	assert(lua_getstack(L, 1, &info));
-	assert(lua_getinfo(L, "S", &info));
+	FATAL_ERROR_IF(!lua_getstack(L, 1, &info), "lua_getstack() failed");
+	FATAL_ERROR_IF(!lua_getinfo(L, "S", &info), "lua_getinfo() failed");
 	// ...and that that item is the main file scope.
 	if (strcmp(info.what, "main") != 0) {
 		return 0;
