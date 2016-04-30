ae75073944f6cdea22d5d4cc40b81937afe4dfa6
Craig Robbins <kde.psych@gmail.com>
2016-04-30 12:29:52 +1000

    Fix prepreprocessor error in thread.h (related to C++11 threads)

--- src/threading/thread.h
+++ src/threading/thread.h
@@ -157,11 +157,11 @@ private:
 	Atomic<bool> m_running;
 	Mutex m_mutex;
 
-#if !USE_CPP11_THREADS
+#ifndef USE_CPP11_THREADS
 	threadhandle_t m_thread_handle;
-#if _WIN32
-	threadid_t m_thread_id;
-#endif
+#   if _WIN32
+        threadid_t m_thread_id;
+#   endif
 #endif
 
 	static ThreadStartFunc threadProc;
