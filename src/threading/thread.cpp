minetest/minetest

commit 46fd114e9a4e05b74576dce682e24357363298e7

Fix race on thread creation
This often broke the threading tests on OSX.

src/threading/thread.cpp
@@ -116,9 +116,7 @@ bool Thread::start()
 #if USE_CPP11_THREADS
 
 	try {
-		m_thread_obj    = new std::thread(threadProc, this);
-		m_thread_id     = m_thread_obj->get_id();
-		m_thread_handle = m_thread_obj->native_handle();
+		m_thread_obj = new std::thread(threadProc, this);
 	} catch (const std::system_error &e) {
 		return false;
 	}
@@ -134,8 +132,6 @@ bool Thread::start()
 	int status = pthread_create(&m_thread_handle, NULL, threadProc, this);
 	if (status)
 		return false;
-
-	m_thread_id = m_thread_handle;
 
 #endif
 
@@ -231,12 +227,6 @@ bool Thread::getReturnValue(void **ret)
 
 	*ret = m_retval;
 	return true;
-}
-
-
-bool Thread::isCurrentThread()
-{
-	return thr_is_current_thread(m_thread_id);
 }
 
 

src/threading/thread.h
@@ -90,12 +90,22 @@ public:
 	/*
 	 * Returns true if the calling thread is this Thread object.
 	 */
-	bool isCurrentThread();
+	bool isCurrentThread() { return thr_is_current_thread(getThreadId()); }
 
 	inline bool isRunning() { return m_running; }
 	inline bool stopRequested() { return m_request_stop; }
+
+#if USE_CPP11_THREADS
+	inline threadid_t getThreadId() { return m_thread_obj->get_id(); }
+	inline threadhandle_t getThreadHandle() { return m_thread_obj->native_handle(); }
+#else
+#  if USE_WIN_THREADS
 	inline threadid_t getThreadId() { return m_thread_id; }
+#  else
+	inline threadid_t getThreadId() { return m_thread_handle; }
+#  endif
 	inline threadhandle_t getThreadHandle() { return m_thread_handle; }
+#endif
 
 	/*
 	 * Gets the thread return value.
@@ -147,8 +157,12 @@ private:
 	Atomic<bool> m_running;
 	Mutex m_mutex;
 
-	threadid_t m_thread_id;
+#if !USE_CPP11_THREADS
 	threadhandle_t m_thread_handle;
+#if _WIN32
+	threadid_t m_thread_id;
+#endif
+#endif
 
 	static ThreadStartFunc threadProc;
 

commit a53357d2917d57966503348e7c030c024e89bb1b

Fix POSIX C++11 build
I broke this in 46fd114e9a4e05b74576dce682e24357363298e7.

src/threading/thread.cpp
@@ -205,9 +205,9 @@ bool Thread::kill()
 	// We need to pthread_kill instead on Android since NDKv5's pthread
 	// implementation is incomplete.
 # ifdef __ANDROID__
-	pthread_kill(m_thread_handle, SIGKILL);
+	pthread_kill(getThreadHandle(), SIGKILL);
 # else
-	pthread_cancel(m_thread_handle);
+	pthread_cancel(getThreadHandle());
 # endif
 	wait();
 #endif
@@ -361,7 +361,7 @@ bool Thread::bindToProcessor(unsigned int proc_number)
 
 #elif defined(_WIN32)
 
-	return SetThreadAffinityMask(m_thread_handle, 1 << proc_number);
+	return SetThreadAffinityMask(getThreadHandle(), 1 << proc_number);
 
 #elif __FreeBSD_version >= 702106 || defined(__linux) || defined(linux)
 
@@ -370,7 +370,7 @@ bool Thread::bindToProcessor(unsigned int proc_number)
 	CPU_ZERO(&cpuset);
 	CPU_SET(proc_number, &cpuset);
 
-	return pthread_setaffinity_np(m_thread_handle, sizeof(cpuset), &cpuset) == 0;
+	return pthread_setaffinity_np(getThreadHandle(), sizeof(cpuset), &cpuset) == 0;
 
 #elif defined(__sun) || defined(sun)
 
@@ -385,13 +385,13 @@ bool Thread::bindToProcessor(unsigned int proc_number)
 	pthread_spu_t answer;
 
 	return pthread_processor_bind_np(PTHREAD_BIND_ADVISORY_NP,
-			&answer, proc_number, m_thread_handle) == 0;
+			&answer, proc_number, getThreadHandle()) == 0;
 
 #elif defined(__APPLE__)
 
 	struct thread_affinity_policy tapol;
 
-	thread_port_t threadport = pthread_mach_thread_np(m_thread_handle);
+	thread_port_t threadport = pthread_mach_thread_np(getThreadHandle());
 	tapol.affinity_tag = proc_number + 1;
 	return thread_policy_set(threadport, THREAD_AFFINITY_POLICY,
 			(thread_policy_t)&tapol,
@@ -409,21 +409,21 @@ bool Thread::setPriority(int prio)
 {
 #if defined(_WIN32)
 
-	return SetThreadPriority(m_thread_handle, prio);
+	return SetThreadPriority(getThreadHandle(), prio);
 
 #else
 
 	struct sched_param sparam;
 	int policy;
 
-	if (pthread_getschedparam(m_thread_handle, &policy, &sparam) != 0)
+	if (pthread_getschedparam(getThreadHandle(), &policy, &sparam) != 0)
 		return false;
 
 	int min = sched_get_priority_min(policy);
 	int max = sched_get_priority_max(policy);
 
 	sparam.sched_priority = min + prio * (max - min) / THREAD_PRIORITY_HIGHEST;
-	return pthread_setschedparam(m_thread_handle, policy, &sparam) == 0;
+	return pthread_setschedparam(getThreadHandle(), policy, &sparam) == 0;
 
 #endif
 }