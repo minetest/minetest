e618604e7e5de4d240f3d581a2f92763fd3ed3e8
Eric R. Monson <eriixblaike+git@gmail.com>
2016-06-09 23:59:36 -0700

    Now fix building with LMDB

--- src/database_lmdb.cpp
+++ src/database_lmdb.cpp
@@ -17,6 +17,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include "config.h"
+
 #if USE_LMDB
 
 #include <vector>
