1dfd977ec43370da6931b11a8d0469792c8ebc36
Rogier-5 <Rogier-5@users.noreply.github.com>
2016-07-04 21:00:57 +0200

    Fix & make linux conditionals uniform (#4278)
    The source used a hodge-podge of different combinations of different
    macros to check for linux: 'linux', '__linux', '__linux__'.
    
    As '__linux__' is standard (Posix), and the others are not, the source
    now uniformly uses __linux__. If either linux or __linux are defined,
    it is made sure that __linux__ is defined as well.

--- src/guiChatConsole.cpp
+++ src/guiChatConsole.cpp
@@ -630,7 +630,7 @@ bool GUIChatConsole::OnEvent(const SEvent& event)
 		}
 		else if(event.KeyInput.Char != 0 && !event.KeyInput.Control)
 		{
-			#if (defined(linux) || defined(__linux))
+			#if (defined(__linux__))
 				wchar_t wc = L'_';
 				mbtowc( &wc, (char *) &event.KeyInput.Char, sizeof(event.KeyInput.Char) );
 				prompt.input(wc);
