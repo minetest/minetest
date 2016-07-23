c8b4bedaa89ac5246161b1bb9e3826dcce083fcf
Tim <t4im@users.noreply.github.com>
2016-07-23 13:29:53 +0200

    Builtin: Add core.MAP_BLOCKSIZE constant

--- builtin/game/constants.lua
+++ builtin/game/constants.lua
@@ -4,14 +4,19 @@
 -- Constants values for use with the Lua API
 --
 
+-- mapnode.h
 -- Built-in Content IDs (for use with VoxelManip API)
 core.CONTENT_UNKNOWN = 125
 core.CONTENT_AIR     = 126
 core.CONTENT_IGNORE  = 127
 
+-- emerge.h
 -- Block emerge status constants (for use with core.emerge_area)
 core.EMERGE_CANCELLED   = 0
 core.EMERGE_ERRORED     = 1
 core.EMERGE_FROM_MEMORY = 2
 core.EMERGE_FROM_DISK   = 3
 core.EMERGE_GENERATED   = 4
+
+-- constants.h
+core.MAP_BLOCKSIZE = 16
