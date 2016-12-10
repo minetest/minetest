6c2c75c417dcf3a2d12e2b0c34117dbbf600e67d
Rogier <rogier777@gmail.com>
2016-12-10 21:07:58 +0100

    Fix computation of viewing range (in blocks) sent to server
    Fixes #4878
    
    Also remove an artificial viewing range reduction that
    (presumably) was added to compensate for miscomputed
    viewing ranges, and that doesn't seem to be needed any
    more (thanks to lhofhansl).

--- src/network/networkprotocol.h
+++ src/network/networkprotocol.h
@@ -652,7 +652,7 @@ enum ToServerCommand
 		[2+12+12+4] s32 yaw*100
 		[2+12+12+4+4] u32 keyPressed
 		[2+12+12+4+4+1] u8 fov*80
-		[2+12+12+4+4+4+1] u8 wanted_range / MAP_BLOCKSIZE
+		[2+12+12+4+4+4+1] u8 ceil(wanted_range / MAP_BLOCKSIZE)
 	*/
 
 	TOSERVER_GOTBLOCKS = 0x24,
