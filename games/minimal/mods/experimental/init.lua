789692620880682c5bb1c35477bc660768478972
sfan5 <sfan5@live.de>
2017-01-14 18:48:14 +0100

    docs & example code

--- games/minimal/mods/experimental/init.lua
+++ games/minimal/mods/experimental/init.lua
@@ -523,6 +523,42 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_craftitem("experimental:tester_tool_2", {
+	description = "Tester Tool 2",
+	inventory_image = "experimental_tester_tool_1.png^[invert:g",
+    on_use = function(itemstack, user, pointed_thing)
+		local pos = minetest.get_pointed_thing_position(pointed_thing, true)
+		if pos == nil then return end
+		pos = vector.add(pos, {x=0, y=0.5, z=0})
+		local tex, anim
+		if math.random(0, 1) == 0 then
+			tex = "default_lava_source_animated.png"
+			anim = {type="sheet_2d", frames_w=3, frames_h=2, frame_length=0.5}
+		else
+			tex = "default_lava_flowing_animated.png"
+			anim = {type="vertical_frames", aspect_w=16, aspect_h=16, length=3.3}
+		end
+		minetest.add_particle({
+			pos = pos,
+		    velocity = {x=0, y=0, z=0},
+		    acceleration = {x=0, y=0.04, z=0},
+		    expirationtime = 6,
+		    collisiondetection = true,
+		    vertical = true,
+		    texture = tex,
+		    animation = anim,
+		    size = 4,
+        })
+	end,
+})
+
+minetest.register_craft({
+	output = 'experimental:tester_tool_2',
+	recipe = {
+		{'group:crumbly','group:crumbly'},
+	}
+})
+
 --[[minetest.register_on_joinplayer(function(player)
 	minetest.after(3, function()
 		player:set_inventory_formspec("size[8,7.5]"..
