6e813044dd1b236560c4fe004657de4f89f627a4
red-001 <red-001@outlook.ie>
2017-01-22 08:05:09 +0000

    Add client-sided chat commands (#5092)
    CSM: Add client-sided chat commands

---
+++ builtin/common/chatcommands.lua
@@ -0,0 +1,30 @@
+-- Minetest: builtin/common/chatcommands.lua
+
+core.registered_chatcommands = {}
+
+function core.register_chatcommand(cmd, def)
+	def = def or {}
+	def.params = def.params or ""
+	def.description = def.description or ""
+	def.privs = def.privs or {}
+	def.mod_origin = core.get_current_modname() or "??"
+	core.registered_chatcommands[cmd] = def
+end
+
+function core.unregister_chatcommand(name)
+	if core.registered_chatcommands[name] then
+		core.registered_chatcommands[name] = nil
+	else
+		core.log("warning", "Not unregistering chatcommand " ..name..
+			" because it doesn't exist.")
+	end
+end
+
+function core.override_chatcommand(name, redefinition)
+	local chatcommand = core.registered_chatcommands[name]
+	assert(chatcommand, "Attempt to override non-existent chatcommand "..name)
+	for k, v in pairs(redefinition) do
+		rawset(chatcommand, k, v)
+	end
+	core.registered_chatcommands[name] = chatcommand
+end
