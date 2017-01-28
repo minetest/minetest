ba66fce8339f818a638f97679bd29da064a8c1c6
Loic Blot <loic.blot@unix-experience.fr>
2017-01-28 18:31:23 +0100

    [CSM] storage + fixes

--- src/script/lua_api/l_client.h
+++ src/script/lua_api/l_client.h
@@ -23,8 +23,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "lua_api/l_base.h"
 
-class ModApiClient:
-	public ModApiBase
+class ModApiClient: public ModApiBase
 {
 private:
 	// get_current_modname()
