fe7bf42f2031488bd6f69d02ef238060ceb3ca34
Jacob Gustafson <neoarmor@yahoo.com>
2017-01-31 15:56:45 -0500

    fixed redundant language I introduced
    These lines in the pull request are not necessary, it is just changed for my repo which is seen by students.

--- util/minetestmapper.py
+++ util/minetestmapper.py
@@ -3,8 +3,8 @@
 
 # This program is free software. It comes without any warranty, to
 # the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the WTFPL
-# Public License, Version 2, as published by Sam Hocevar. See
+# and/or modify it under the terms of the WTFPL,
+# Version 2, as published by Sam Hocevar. See
 # COPYING for more details.
 
 # Made by Jogge, modified by celeron55
