9a6676807ed217ce38fbcc83ef8e8a949d6ebbc1
Auke Kok <sofar@foo-projects.org>
2017-02-08 13:46:25 -0800

    Change default nodetimer_interval to 0.2s.
    We want to reduce the chance that we get lots and lots of node
    timers all happening once a second, because we're better off doing
    small bits of work as they are available.
    
    Reducing this to 0.2 seconds will greatly reduce the total amount
    of nodetimers that elapse at the same instance, while not effecting
    total work load. This results in a far better chance of the server
    keeping up with work loads.

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -295,7 +295,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("dedicated_server_step", "0.1");
 	settings->setDefault("active_block_mgmt_interval", "2.0");
 	settings->setDefault("abm_interval", "1.0");
-	settings->setDefault("nodetimer_interval", "1.0");
+	settings->setDefault("nodetimer_interval", "0.2");
 	settings->setDefault("ignore_world_load_errors", "false");
 	settings->setDefault("remote_media", "");
 	settings->setDefault("debug_log_level", "action");
