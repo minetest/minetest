minetest/minetest

commit e2ad76f91087215309012300eb9f1ebdd7fa40ee

No longer auto-generate a 'guest####' player name when name is empty
You can't join most servers with a 'guest####' player name anyway so
it's only logical to remove them.

src/client/clientlauncher.cpp
@@ -403,11 +403,12 @@ bool ClientLauncher::launch_game(std::string &error_message,
 		return false;
 	}
 
-	if (menudata.name == "")
-		menudata.name = std::string("Guest") + itos(myrand_range(1000, 9999));
-	else
-		playername = menudata.name;
+	if (menudata.name == "" && !simple_singleplayer_mode) {
+		error_message = gettext("Please choose a name!");
+		return false;
+	}
 
+	playername = menudata.name;
 	password = menudata.password;
 
 	g_settings->set("name", playername);

commit 654cc50b7de06e31f7ee7341a1380f54116fa965

Fix a small regression caused by e2ad76f.

src/client/clientlauncher.cpp
@@ -411,8 +411,6 @@ bool ClientLauncher::launch_game(std::string &error_message,
 	playername = menudata.name;
 	password = menudata.password;
 
-	g_settings->set("name", playername);
-
 	current_playername = playername;
 	current_password   = password;
 	current_address    = address;
@@ -425,13 +423,16 @@ bool ClientLauncher::launch_game(std::string &error_message,
 		current_password = "";
 		current_address = "";
 		current_port = myrand_range(49152, 65535);
-	} else if (address != "") {
-		ServerListSpec server;
-		server["name"] = menudata.servername;
-		server["address"] = menudata.address;
-		server["port"] = menudata.port;
-		server["description"] = menudata.serverdescription;
-		ServerList::insert(server);
+	} else {
+		g_settings->set("name", playername);
+		if (address != "") {
+			ServerListSpec server;
+			server["name"] = menudata.servername;
+			server["address"] = menudata.address;
+			server["port"] = menudata.port;
+			server["description"] = menudata.serverdescription;
+			ServerList::insert(server);
+		}
 	}
 
 	infostream << "Selected world: " << worldspec.name