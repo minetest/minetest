6d02fc8dfd2c895ee57cd9da3d49437af72ae03b
MarkuBu <markus.burrer@gmail.com>
2017-02-24 14:44:01 +0100

    some fixes and doc

--- builtin/game/fine_pointed.lua
+++ builtin/game/fine_pointed.lua
@@ -1,10 +1,9 @@
-
-
+-- Returns the exact coordinate of a pointed surface
 
 local function fine_pointed(pos, camera_pos, pos_off, look_dir)
 	local offset, nc
 	local oc = {}
-	for c,v in pairs(pos_off) do
+	for c, v in pairs(pos_off) do
 		if v == 0 then
 			oc[#oc + 1] = c
 		else
@@ -18,7 +17,7 @@ local function fine_pointed(pos, camera_pos, pos_off, look_dir)
 	camera_pos.y = camera_pos.y + 1.625
 
 	local f = (pos[nc] + offset - camera_pos[nc]) / look_dir[nc]
-	for i = 1,#oc do
+	for i = 1, #oc do
 		fine_pos[oc[i]] = camera_pos[oc[i]] + look_dir[oc[i]] * f
 	end
 
@@ -26,11 +25,11 @@ local function fine_pointed(pos, camera_pos, pos_off, look_dir)
 	return fine_pos
 end
 
-function minetest.fine_pointed_position(player, pt)
+function minetest.fine_pointed_position(placer, pointed_thing)
 	return fine_pointed(
-		pt.under,
-		player:getpos(),
-		vector.multiply(vector.subtract(pt.above, pt.under), 0.5),
-		player:get_look_dir()
+		pointed_thing.under,
+		placer:get_pos(),
+		vector.multiply(vector.subtract(pointed_thing.above, pointed_thing.under), 0.5),
+		placer:get_look_dir()
 	)
 end
