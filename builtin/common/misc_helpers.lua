minetest/minetest

commit dd2f1d7551bfd70357ad4f3fb180704194272147

Add multiline support to colorize. (#5444)

builtin/common/misc_helpers.lua
@@ -667,7 +667,14 @@ else
 	end
 
 	function core.colorize(color, message)
-		return core.get_color_escape_sequence(color) .. message .. core.get_color_escape_sequence("#ffffff")
+		local lines = message:split("\n", true)
+		local color_code = core.get_color_escape_sequence(color)
+
+		for i, line in ipairs(lines) do
+			lines[i] = colour_code .. line
+		end
+
+		return table.concat(lines, "\n") .. core.get_color_escape_sequence("#ffffff")
 	end
 
 end

commit 1b299b40391b4ea0fe058b63aad14e3360e917d9

Fix a type bug in colorize function
This bug was introduced by dd2f1d7551bfd70357ad4f3fb180704194272147 and reported by @kilbith

builtin/common/misc_helpers.lua
@@ -667,7 +667,7 @@ else
 	end
 
 	function core.colorize(color, message)
-		local lines = message:split("\n", true)
+		local lines = tostring(message):split("\n", true)
 		local color_code = core.get_color_escape_sequence(color)
 
 		for i, line in ipairs(lines) do