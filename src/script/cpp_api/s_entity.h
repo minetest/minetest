f07b0167bcf82e3a95bf224ed745216c0f0ead23
Hybrid Dog <ovvv@web.de>
2017-04-02 14:08:48 +0200

    Move object on_step execution to lua

--- src/script/cpp_api/s_entity.h
+++ src/script/cpp_api/s_entity.h
@@ -37,7 +37,6 @@ public:
 	std::string luaentity_GetStaticdata(u16 id);
 	void luaentity_GetProperties(u16 id,
 			ObjectProperties *prop);
-	void luaentity_Step(u16 id, float dtime);
 	bool luaentity_Punch(u16 id,
 			ServerActiveObject *puncher, float time_from_last_punch,
 			const ToolCapabilities *toolcap, v3f dir, s16 damage);
