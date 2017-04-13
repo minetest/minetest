5f4b4b18eafbc2ed75f0a0571fc457c9391832b7
MarkuBu <markus.burrer@gmail.com>
2017-04-13 13:58:34 +0200

    Fix Lua crash if opening door with slab or stair #1700

--- builtin/common/misc_helpers.lua
+++ builtin/common/misc_helpers.lua
@@ -713,6 +713,11 @@ function core.pointed_thing_to_face_pos(placer, pointed_thing)
 			nc = c
 		end
 	end
+
+	if not oc["y"] then
+		oc[#oc + 1] = "y"
+	end
+
 	local fine_pos = {[nc] = node_pos[nc] + offset}
 	camera_pos.y = camera_pos.y + 1.625 + eye_offset_first.y / 10
 	local f = (node_pos[nc] + offset - camera_pos[nc]) / look_dir[nc]
@@ -722,4 +727,3 @@ function core.pointed_thing_to_face_pos(placer, pointed_thing)
 	end
 	return fine_pos
 end
-
