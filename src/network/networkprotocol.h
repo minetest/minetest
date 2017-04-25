c2c5c703c4bb3db9acca25325c890fb9dd8f78af
red-001 <red-001@outlook.ie>
2017-04-25 18:08:24 +0100

    Fix #5655
    Client: Don't send `TOSERVER_RECEIVED_MEDIA` since it's not used anymore
    Server: Handle `TOSERVER_RECEIVED_MEDIA` using `Server::handleCommand_Deprecated`

--- src/network/networkprotocol.h
+++ src/network/networkprotocol.h
@@ -106,7 +106,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 		add swap_node
 	PROTOCOL_VERSION 23:
 		Obsolete TOSERVER_RECEIVED_MEDIA
-		Add TOSERVER_CLIENT_READY
+		Server: Stop using TOSERVER_CLIENT_READY
 	PROTOCOL_VERSION 24:
 		ContentFeatures version 7
 		ContentFeatures: change number of special tiles to 6 (CF_SPECIAL_COUNT)
@@ -152,6 +152,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 			with pre-30 clients by sending sqrt(visual_scale)
 	PROTOCOL VERSION 31:
 		Add tile overlay
+		Stop sending TOSERVER_CLIENT_READY
 */
 
 #define LATEST_PROTOCOL_VERSION 31
