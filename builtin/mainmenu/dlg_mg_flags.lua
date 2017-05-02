38c7bad816f7a9e53c222f2a5be3dd12f2bc2645
Muhammad Rifqi Priyo Susanto <muhammadrifqipriyosusanto@gmail.com>
2017-05-02 11:17:01 +0700

    Only update minetest.conf when needed
    Only write setting to minetest.conf when it is different than default or the setting already there.
    Update flags lists.
    Change from "Close" to "Save" and "Cancel".
    Don't set for nil value by assuming uses default.
    Use tostring instead of dump.

--- builtin/mainmenu/dlg_mg_flags.lua
+++ builtin/mainmenu/dlg_mg_flags.lua
@@ -17,166 +17,197 @@
 
 local mg_first_time = true
 
--- Use default if not defined
-
--- Water level
-local water_level = 1
-
--- General attributes
-local flag_list_g = {
-	caves		= true,
-	dungeons	= true,
-	light		= true,
-	decorations	= true
+-- Default settings
+-- Note: Because we have two mapgens which uses same flag name: mgv5 and mgv7, we add prefix _n to caverns.
+local default = {
+	-- Water level
+	water_level = 1,
+	-- General attributes
+	flag_list_general = {
+		caves		= true,
+		dungeons	= true,
+		light		= true,
+		decorations	= true
+	},
+	-- v5 mapgen
+	flag_list_5 = {
+		_5caverns	= true
+	},
+	-- v6 mapgen
+	flag_list_6 = {
+		jungles		= true,
+		biomeblend	= true,
+		mudflow		= true,
+		snowbiomes	= true,
+		flat		= false,
+		trees		= true
+	},
+	-- v7 mapgen
+	flag_list_7 = {
+		mountains	= true,
+		ridges		= true,
+		floatlands	= false,
+		_7caverns	= true
+	},
+	-- flat mapgen
+	flag_list_flat = {
+		lakes		= false,
+		hills		= false
+	},
+	-- valleys mapgen
+	flag_list_valleys = {
+		altitude_chill	= true,
+		humid_rivers	= true
+	}
 }
 
--- v6 mapgen
-local flag_list_6 = {
-	biomeblend	= true,
-	mudflow		= true,
-	snowbiomes	= true,
-	jungles		= true,
-	flat		= false,
-	trees		= true
-}
-
--- v7 mapgen
-local flag_list_7 = {
-	mountains	= true,
-	ridges		= true
-}
-
--- flat mapgen
-local flag_list_f = {
-	lakes		= false,
-	hills		= false
-}
-
--- valleys mapgen
-local flag_list_v = {
-	altitude_chill	= true,
-	humid_rivers	= true
+-- This table has data from minetest.conf which then will be from player's input.
+local temporary = {
+	water_level = 1,
+	flag_list_general = {},
+	flag_list_5 = {},
+	flag_list_6 = {},
+	flag_list_7 = {},
+	flag_list_flat = {},
+	flag_list_valleys = {}
 }
 
 local function update_mg_flags()
-	local function mg_flags_helper(flag_list)
-		local first = true
-		local flag_str = ""
-		for flag, val in pairs(flag_list) do
-			flag_str = flag_str .. (first and "" or ", ") .. (val == true and "" or "no") .. flag
-			first = false
+	local function mg_flags_helper(setting_name, list_name)
+		-- Only write to minetest.conf when it has changed or already there.
+		local changed = false
+		if core.setting_get(setting_name) ~= nil then
+			changed = true
 		end
-		return flag_str
+		if changed == false then
+			for flag, val in pairs(default[list_name]) do
+				if temporary[list_name][flag] ~= val then
+					changed = true
+					break
+				end
+			end
+		end
+		if changed == true then
+			local flag_str = ""
+			local first = true
+			for flag, val in pairs(temporary[list_name]) do
+				local name = flag
+				if flag:sub(1, 1) == "_" then
+					name = flag:sub(3)  -- Remove the prefix.
+				end
+				flag_str = flag_str .. (first and "" or ",") .. (val == true and "" or "no") .. name
+				first = false
+			end
+			core.setting_set(setting_name, flag_str)
+		end
 	end
-	
-	core.setting_set("mg_flags", mg_flags_helper(flag_list_g))
-	core.setting_set("mgv6_spflags", mg_flags_helper(flag_list_6))
-	core.setting_set("mgv7_spflags", mg_flags_helper(flag_list_7))
-	core.setting_set("mgflat_spflags", mg_flags_helper(flag_list_f))
-	core.setting_set("mg_valleys_spflags", mg_flags_helper(flag_list_v))
-	core.setting_set("water_level", water_level)
-end
 
--- Load default settings if not exist
-if core.setting_get("water_level") == nil then
-	core.setting_set("water_level", 1)
-end
-if core.setting_get("mg_flags") == nil then
-	core.setting_set("mg_flags", "caves, dungeons, light, decorations")
-end
-if core.setting_get("mgv6_spflags") == nil then
-	core.setting_set("mgv6_spflags", "jungles, biomeblend, mudflow, snowbiomes, trees")
-end
-if core.setting_get("mgv7_spflags") == nil then
-	core.setting_set("mgv7_spflags", "mountains, ridges")
-end
-if core.setting_get("mgflat_spflags") == nil then
-	-- prevents error because of nil value
-	core.setting_set("mgflat_spflags", "")
-end
-if core.setting_get("mg_valleys_spflags") == nil then
-	core.setting_set("mg_valleys_spflags", "altitude_chill, humid_rivers")
+	mg_flags_helper("mg_flags", "flag_list_general")
+	mg_flags_helper("mgv5_spflags", "flag_list_5")
+	mg_flags_helper("mgv6_spflags", "flag_list_6")
+	mg_flags_helper("mgv7_spflags", "flag_list_7")
+	mg_flags_helper("mgflat_spflags", "flag_list_flat")
+	mg_flags_helper("mg_valleys_spflags", "flag_list_valleys")
+	if core.setting_get("water_level") ~= temporary.water_level then
+		core.setting_set("water_level", temporary.water_level)
+	end
 end
 
 local function load_mg_flags()
-	local function mg_flags_helper(flag_list,text)
-		local text = string.split(text, ", ")
+	local function mg_flags_helper(list_name, text)
+		temporary[list_name] = table.copy(default[list_name])
+		if text == nil then
+			return
+		end
+		local text = string.split(text, ",", false)
 		for i = 1, #text do
-			if text[i]:sub(1, 2) == "no" then
-				flag_list[text[i]:sub(3)] = false
+			-- Dealing with two mapgens which uses same flag name: mgv5 and mgv7.
+			local name = text[i]
+			if name:sub(1, 2) == "no" then
+				name = name:sub(3)
+				if name == "caverns" then
+					name = list_name:sub(10) .. name
+				end
+				temporary[list_name][name] = false
 			else
-				flag_list[text[i]] = true
+				if name == "caverns" then
+					name = list_name:sub(10) .. name
+				end
+				temporary[list_name][name] = true
 			end
 		end
 	end
-	mg_flags_helper(flag_list_g, core.setting_get("mg_flags"))
-	mg_flags_helper(flag_list_6, core.setting_get("mgv6_spflags"))
-	mg_flags_helper(flag_list_7, core.setting_get("mgv7_spflags"))
-	mg_flags_helper(flag_list_f, core.setting_get("mgflat_spflags"))
-	mg_flags_helper(flag_list_v, core.setting_get("mg_valleys_spflags"))
-	water_level = core.setting_get("water_level")
-	
-	update_mg_flags()
+
+	mg_flags_helper("flag_list_general", core.setting_get("mg_flags"))
+	mg_flags_helper("flag_list_5", core.setting_get("mgv5_spflags"))
+	mg_flags_helper("flag_list_6", core.setting_get("mgv6_spflags"))
+	mg_flags_helper("flag_list_7", core.setting_get("mgv7_spflags"))
+	mg_flags_helper("flag_list_flat", core.setting_get("mgflat_spflags"))
+	mg_flags_helper("flag_list_valleys", core.setting_get("mg_valleys_spflags"))
+	temporary.water_level = core.setting_get("water_level")
 end
 
 local function mg_flags_formspec(dialogdata)
-	-- Load minetest.conf for first time
+	-- Load minetest.conf for first time.
 	if mg_first_time == true then
 		load_mg_flags()
-		update_mg_flags()
 	end
 	mg_first_time = false
-	
 	local mgname = "[" .. core.setting_get("mg_name") .. "]"
 	local formspec =
-		"size[8,5,true]" ..
-		"button[5.5,4.6;2.5,0.5;mg_close;" .. fgettext("Close") .. "]" ..
-		
+		"size[8,6,true]" ..
+		"button[3.0,5.6;2.5,0.5;mg_save;" .. fgettext("Save") .. "]" ..
+		"button[5.5,5.6;2.5,0.5;mg_cancel;" .. fgettext("Cancel") .. "]" ..
 	-- Special mapgen-specific flags
 		"label[0,0;" .. minetest.formspec_escape(mgname) .. " " .. fgettext("Mapgen flags") .. "]"
 	if core.setting_get("mg_name") == "v5" then
 		formspec = formspec ..
-			"label[0,1.0;" .. fgettext("No flags in v5 mapgen") .. "]"
+			"checkbox[0.25,0.5;cb__5caverns;" .. fgettext("Caverns") .. ";" ..
+				tostring(temporary.flag_list_5["_5caverns"]) .. "]"
 	elseif core.setting_get("mg_name") == "v6" then
 		-- If snowbiomes enabled, tell player if jungle is auto-enabled, too.
-		if flag_list_6["snowbiomes"] == true then
-			flag_list_6["jungles"] = true  -- Do not make the player get confused
+		if temporary.flag_list_6["snowbiomes"] == true then
+			temporary.flag_list_6["jungles"] = true  -- Do not make the player get confused.
 			formspec = formspec ..
 			"label[0.7,2.2;" .. fgettext("(with Jungles)") .. "]"
 		else
 			formspec = formspec ..
 			"checkbox[0.25,2.0;cb_jungles;" .. fgettext("Jungles") .. ";" ..
-				dump(flag_list_6["jungles"]) .. "]"
+				tostring(temporary.flag_list_6["jungles"]) .. "]"
 		end
 		formspec = formspec ..
 			"checkbox[0.25,0.5;cb_biomeblend;" .. fgettext("Biome blend") .. ";" ..
-				dump(flag_list_6["biomeblend"]) .. "]" ..
+				tostring(temporary.flag_list_6["biomeblend"]) .. "]" ..
 			"checkbox[0.25,1.0;cb_mudflow;" .. fgettext("Mud flow") .. ";" ..
-				dump(flag_list_6["mudflow"]) .. "]" ..
+				tostring(temporary.flag_list_6["mudflow"]) .. "]" ..
 			"checkbox[0.25,1.5;cb_snowbiomes;" .. fgettext("Additional Biomes") .. ";" ..
-				dump(flag_list_6["snowbiomes"]) .. "]" ..
+				tostring(temporary.flag_list_6["snowbiomes"]) .. "]" ..
 			"checkbox[0.25,2.5;cb_flat;" .. fgettext("Flat world") .. ";" ..
-				dump(flag_list_6["flat"]) .. "]" ..
+				tostring(temporary.flag_list_6["flat"]) .. "]" ..
 			"checkbox[0.25,3.0;cb_trees;" .. fgettext("Trees") .. ";" ..
-				dump(flag_list_6["trees"]) .. "]"
+				tostring(temporary.flag_list_6["trees"]) .. "]"
 	elseif core.setting_get("mg_name") == "v7" then
 		formspec = formspec ..
 			"checkbox[0.25,0.5;cb_mountains;" .. fgettext("Mountains") .. ";" ..
-				dump(flag_list_7["mountains"]) .. "]" ..
+				tostring(temporary.flag_list_7["mountains"]) .. "]" ..
 			"checkbox[0.25,1.0;cb_ridges;" .. fgettext("Ridges") .. ";" ..
-				dump(flag_list_7["ridges"]) .. "]"
+				tostring(temporary.flag_list_7["ridges"]) .. "]" ..
+			"checkbox[0.25,1.5;cb_floatlands;" .. fgettext("Floatlands") .. ";" ..
+				tostring(temporary.flag_list_7["floatlands"]) .. "]" ..
+			"checkbox[0.25,2.0;cb__7caverns;" .. fgettext("Caverns") .. ";" ..
+				tostring(temporary.flag_list_7["_7caverns"]) .. "]"
 	elseif core.setting_get("mg_name") == "flat" then
 		formspec = formspec ..
 			"checkbox[0.25,0.5;cb_lakes;" .. fgettext("Lakes") .. ";" ..
-				dump(flag_list_f["lakes"]) .. "]" ..
+				tostring(temporary.flag_list_flat["lakes"]) .. "]" ..
 			"checkbox[0.25,1.0;cb_hills;" .. fgettext("Hills") .. ";" ..
-				dump(flag_list_f["hills"]) .. "]"
+				tostring(temporary.flag_list_flat["hills"]) .. "]"
 	elseif core.setting_get("mg_name") == "valleys" then
 		formspec = formspec ..
 			"checkbox[0.25,0.5;cb_altitude_chill;" .. fgettext("Height temperature") .. ";" ..
-				dump(flag_list_v["altitude_chill"]) .. "]" ..
+				tostring(temporary.flag_list_valleys["altitude_chill"]) .. "]" ..
 			"checkbox[0.25,1.0;cb_humid_rivers;" .. fgettext("Humid rivers") .. ";" ..
-				dump(flag_list_v["humid_rivers"]) .. "]"
+				tostring(temporary.flag_list_valleys["humid_rivers"]) .. "]"
 	elseif core.setting_get("mg_name") == "fractal" then
 		formspec = formspec ..
 			"label[0,1.0;" .. fgettext("No flags in fractal mapgen for now") .. "]"
@@ -184,60 +215,59 @@ local function mg_flags_formspec(dialogdata)
 		formspec = formspec ..
 			"label[0,1.0;" .. fgettext("No flags in singlenode mapgen") .. "]"
 	end
-	
 	-- General attributes a.k.a. generated structure
 	formspec = formspec ..
 		"label[4,0;" .. fgettext("Generated structure") .. "]" ..
 		"checkbox[4.25,0.5;cb_caves;" .. fgettext("Caves") .. ";" ..
-			dump(flag_list_g["caves"]) .. "]" ..
+			tostring(temporary.flag_list_general["caves"]) .. "]" ..
 		"checkbox[4.25,1.0;cb_dungeons;" .. fgettext("Dungeons") .. ";" ..
-			dump(flag_list_g["dungeons"]) .. "]" ..
+			tostring(temporary.flag_list_general["dungeons"]) .. "]" ..
 		"checkbox[4.25,1.5;cb_decorations;" .. fgettext("Decorations") .. ";" ..
-			dump(flag_list_g["decorations"]) .. "]"
-	
+			tostring(temporary.flag_list_general["decorations"]) .. "]"
 	-- Water level
 	formspec = formspec ..
-		"field[0.5,4.9;2.5,0.5;te_water_level;" .. fgettext("Water level") .. ";" .. water_level .. "] " ..
-		"button[2.75,4.6;2.75,0.5;mg_water_level;" .. fgettext("Set water level") .. "]"
-	
+		"field[0.5,4.9;5.0,0.5;te_water_level;" .. fgettext("Water level") .. ";" .. temporary.water_level .. "] " ..
+		"button[5.25,4.6;2.75,0.5;mg_water_level;" .. fgettext("Set water level") .. "]"
 	return formspec
 end
 
 local function mg_flags_buttonhandler(this, fields)
-	if fields["mg_close"] then
+	if fields["mg_save"] then
 		update_mg_flags()
 		this:delete()
 		return true
 	end
-	
-	local flag_lists_list = {flag_list_g, flag_list_6, flag_list_7, flag_list_f, flag_list_v}
-	for idx, flag_list in pairs(flag_lists_list) do
-		for flag, val in pairs(flag_list) do
+	if fields["mg_cancel"] then
+		load_mg_flags()
+		this:delete()
+		return true
+	end
+
+	local flag_lists_name = {
+		"flag_list_general", "flag_list_5", "flag_list_6",
+		"flag_list_7", "flag_list_flat", "flag_list_valleys"
+	}
+	for idx, flag_list in pairs(flag_lists_name) do
+		for flag, val in pairs(default[flag_list]) do
 			local cbflag = "cb_" .. flag
 			if fields[cbflag] then
-				flag_list[flag] = core.is_yes(fields[cbflag])
-				update_mg_flags()
+				temporary[flag_list][flag] = core.is_yes(fields[cbflag])
 				return true
 			end
 		end
 	end
-	
-	-- Player need to press enter on the textbox to run this
-	-- Or just click the button
+
+	-- Player need to press enter on the textbox to run this or just click the button.
 	if fields["te_water_level"] or fields["mg_water_level"] then
-		water_level = tonumber(fields["te_water_level"])
-		update_mg_flags()
+		temporary.water_level = tonumber(fields["te_water_level"])
 		return true
 	end
-	
 	return false
 end
 
 function create_mg_flags_dlg()
-	local retval = dialog_create("sp_mg_flags",
-					mg_flags_formspec,
-					mg_flags_buttonhandler,
-					nil)
-	
-	return retval
+	return dialog_create("sp_mg_flags",
+			mg_flags_formspec,
+			mg_flags_buttonhandler,
+			nil)
 end
