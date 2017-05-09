a32a06e088df34efb82e3ec8633d331e3b177796
Auke Kok <sofar+github@foo-projects.org>
2017-05-08 23:56:57 -0700

    Move Pilzadam to previous developers. (#5735)

--- builtin/mainmenu/tab_credits.lua
+++ builtin/mainmenu/tab_credits.lua
@@ -20,7 +20,6 @@
 local core_developers = {
 	"Perttu Ahola (celeron55) <celeron55@gmail.com>",
 	"Ryan Kwolek (kwolekr) <kwolekr@minetest.net>",
-	"PilzAdam <pilzadam@minetest.net>",
 	"sfan5 <sfan5@live.de>",
 	"kahrl <kahrl@gmx.net>",
 	"sapier",
@@ -54,6 +53,7 @@ local previous_core_developers = {
 	"Lisa Milne (darkrose) <lisa@ltmnet.com>",
 	"proller",
 	"Ilya Zhuravlev (xyz) <xyz@minetest.net>",
+	"PilzAdam <pilzadam@minetest.net>",
 }
 
 local previous_contributors = {
