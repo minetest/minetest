minetest/minetest

commit 43d1f375d18a2fbc547a9b4f23d1354d645856ca

Use a settings object for the main settings
This unifies the settings APIs.

This also unifies the sync and async registration APIs, since the async
registration API did not support adding non-functions to the API table.

builtin/mainmenu/tab_simple_main.lua
@@ -25,12 +25,12 @@ local function get_formspec(tabview, name, tabdata)
 	local retval =
 		"label[9.5,0;".. fgettext("Name / Password") .. "]" ..
 		"field[0.25,3.35;5.5,0.5;te_address;;" ..
-			core.formspec_escape(core.setting_get("address")) .."]" ..
+			core.formspec_escape(core.settings:get("address")) .."]" ..
 		"field[5.75,3.35;2.25,0.5;te_port;;" ..
-			core.formspec_escape(core.setting_get("remote_port")) .."]" ..
+			core.formspec_escape(core.settings:get("remote_port")) .."]" ..
 		"button[10,2.6;2,1.5;btn_mp_connect;".. fgettext("Connect") .. "]" ..
 		"field[9.8,1;2.6,0.5;te_name;;" ..
-			core.formspec_escape(core.setting_get("name")) .."]" ..
+			core.formspec_escape(core.settings:get("name")) .."]" ..
 		"pwdfield[9.8,2;2.6,0.5;te_pwd;]"
 
 
@@ -89,9 +89,9 @@ local function get_formspec(tabview, name, tabdata)
 	-- checkboxes
 	retval = retval ..
 		"checkbox[8.0,3.9;cb_creative;".. fgettext("Creative Mode") .. ";" ..
-			dump(core.setting_getbool("creative_mode")) .. "]"..
+			dump(core.settings:get_bool("creative_mode")) .. "]"..
 		"checkbox[8.0,4.4;cb_damage;".. fgettext("Enable Damage") .. ";" ..
-			dump(core.setting_getbool("enable_damage")) .. "]"
+			dump(core.settings:get_bool("enable_damage")) .. "]"
 	-- buttons
 	retval = retval ..
 		"button[0,3.7;8,1.5;btn_start_singleplayer;" .. fgettext("Start Singleplayer") .. "]" ..
@@ -128,8 +128,8 @@ local function main_button_handler(tabview, fields, name, tabdata)
 				end
 
 				if address and port then
-					core.setting_set("address", address)
-					core.setting_set("remote_port", port)
+					core.settings:set("address", address)
+					core.settings:set("remote_port", port)
 				end
 				tabdata.fav_selected = event.row
 			end
@@ -145,18 +145,18 @@ local function main_button_handler(tabview, fields, name, tabdata)
 		asyncOnlineFavourites()
 		tabdata.fav_selected = nil
 
-		core.setting_set("address", "")
-		core.setting_set("remote_port", "30000")
+		core.settings:set("address", "")
+		core.settings:set("remote_port", "30000")
 		return true
 	end
 
 	if fields.cb_creative then
-		core.setting_set("creative_mode", fields.cb_creative)
+		core.settings:set("creative_mode", fields.cb_creative)
 		return true
 	end
 
 	if fields.cb_damage then
-		core.setting_set("enable_damage", fields.cb_damage)
+		core.settings:set("enable_damage", fields.cb_damage)
 		return true
 	end
 
@@ -186,12 +186,8 @@ local function main_button_handler(tabview, fields, name, tabdata)
 
 		gamedata.selected_world = 0
 
-		core.setting_set("address", fields.te_address)
-		core.setting_set("remote_port", fields.te_port)
-
-		core.start()
-		return true
-	end
+		core.settings:set("address", fields.te_address)
+		core.settings:set("remote_port", fields.te_port)
 
 	if fields.btn_config_sp_world then
 		local configdialog = create_configure_world_dlg(1)

builtin/common/misc_helpers.lua
@@ -463,7 +463,7 @@ if INIT == "game" then
 
 	core.rotate_node = function(itemstack, placer, pointed_thing)
 		core.rotate_and_place(itemstack, placer, pointed_thing,
-				core.setting_getbool("creative_mode"),
+				core.settings:get_bool("creative_mode"),
 				{invert_wall = placer:get_player_control().sneak})
 		return itemstack
 	end
@@ -642,8 +642,8 @@ end
 
 local ESCAPE_CHAR = string.char(0x1b)
 
--- Client-sided mods don't have access to getbool
-if core.setting_getbool and core.setting_getbool("disable_escape_sequences") then
+-- Client-side mods don't have access to settings
+if core.settings and core.settings:get_bool("disable_escape_sequences") then
 
 	function core.get_color_escape_sequence(color)
 		return ""

builtin/fstk/tabview.lua
@@ -167,7 +167,7 @@ local function switch_to_tab(self, index)
 	self.current_tab = self.tablist[index].name
 
 	if (self.autosave_tab) then
-		core.setting_set(self.name .. "_LAST",self.current_tab)
+		core.settings:set(self.name .. "_LAST",self.current_tab)
 	end
 
 	-- call for tab to enter

builtin/game/auth.lua
@@ -106,7 +106,7 @@ core.builtin_auth_handler = {
 				end
 			end
 		-- For the admin, give everything
-		elseif name == core.setting_get("name") then
+		elseif name == core.settings:get("name") then
 			for priv, def in pairs(core.registered_privileges) do
 				privileges[priv] = true
 			end
@@ -125,7 +125,7 @@ core.builtin_auth_handler = {
 		core.log('info', "Built-in authentication handler adding player '"..name.."'")
 		core.auth_table[name] = {
 			password = password,
-			privileges = core.string_to_privs(core.setting_get("default_privs")),
+			privileges = core.string_to_privs(core.settings:get("default_privs")),
 			last_login = os.time(),
 		}
 		save_auth_file()
@@ -148,7 +148,7 @@ core.builtin_auth_handler = {
 		if not core.auth_table[name] then
 			core.builtin_auth_handler.create_auth(name,
 				core.get_password_hash(name,
-					core.setting_get("default_password")))
+					core.settings:get("default_password")))
 		end
 		core.auth_table[name].privileges = privileges
 		core.notify_authentication_modified(name)

builtin/game/chatcommands.lua
@@ -39,7 +39,7 @@ core.register_on_chat_message(function(name, message)
 	return true  -- Handled chat message
 end)
 
-if core.setting_getbool("profiler.load") then
+if core.settings:get_bool("profiler.load") then
 	-- Run after register_chatcommand and its register_on_chat_message
 	-- Before any chattcommands that should be profiled
 	profiler.init_chatcommand()
@@ -82,7 +82,7 @@ core.register_chatcommand("me", {
 core.register_chatcommand("admin", {
 	description = "Show the name of the server owner",
 	func = function(name)
-		local admin = minetest.setting_get("name")
+		local admin = minetest.settings:get("name")
 		if admin then
 			return true, "The administrator of this server is "..admin.."."
 		else
@@ -119,7 +119,7 @@ local function handle_grant_command(caller, grantname, grantprivstr)
 	local privs = core.get_player_privs(grantname)
 	local privs_unknown = ""
 	local basic_privs =
-		core.string_to_privs(core.setting_get("basic_privs") or "interact,shout")
+		core.string_to_privs(core.settings:get("basic_privs") or "interact,shout")
 	for priv, _ in pairs(grantprivs) do
 		if not basic_privs[priv] and not caller_privs.privs then
 			return false, "Your privileges are insufficient."
@@ -185,7 +185,7 @@ core.register_chatcommand("revoke", {
 		local revoke_privs = core.string_to_privs(revoke_priv_str)
 		local privs = core.get_player_privs(revoke_name)
 		local basic_privs =
-			core.string_to_privs(core.setting_get("basic_privs") or "interact,shout")
+			core.string_to_privs(core.settings:get("basic_privs") or "interact,shout")
 		for priv, _ in pairs(revoke_privs) do
 			if not basic_privs[priv] and
 					not core.check_player_privs(name, {privs=true}) then
@@ -419,20 +419,20 @@ core.register_chatcommand("set", {
 	func = function(name, param)
 		local arg, setname, setvalue = string.match(param, "(-[n]) ([^ ]+) (.+)")
 		if arg and arg == "-n" and setname and setvalue then
-			core.setting_set(setname, setvalue)
+			core.settings:set(setname, setvalue)
 			return true, setname .. " = " .. setvalue
 		end
 		local setname, setvalue = string.match(param, "([^ ]+) (.+)")
 		if setname and setvalue then
-			if not core.setting_get(setname) then
+			if not core.settings:get(setname) then
 				return false, "Failed. Use '/set -n <name> <value>' to create a new setting."
 			end
-			core.setting_set(setname, setvalue)
+			core.settings:set(setname, setvalue)
 			return true, setname .. " = " .. setvalue
 		end
 		local setname = string.match(param, "([^ ]+)")
 		if setname then
-			local setvalue = core.setting_get(setname)
+			local setvalue = core.settings:get(setname)
 			if not setvalue then
 				setvalue = "<not set>"
 			end
@@ -667,7 +667,7 @@ core.register_chatcommand("rollback_check", {
 			.. " seconds = 86400 = 24h, limit = 5",
 	privs = {rollback=true},
 	func = function(name, param)
-		if not core.setting_getbool("enable_rollback_recording") then
+		if not core.settings:get_bool("enable_rollback_recording") then
 			return false, "Rollback functions are disabled."
 		end
 		local range, seconds, limit =
@@ -718,7 +718,7 @@ core.register_chatcommand("rollback", {
 	description = "Revert actions of a player. Default for <seconds> is 60",
 	privs = {rollback=true},
 	func = function(name, param)
-		if not core.setting_getbool("enable_rollback_recording") then
+		if not core.settings:get_bool("enable_rollback_recording") then
 			return false, "Rollback functions are disabled."
 		end
 		local target_name, seconds = string.match(param, ":([^ ]+) *(%d*)")

builtin/game/deprecated.lua
@@ -49,3 +49,24 @@ setmetatable(core.env, {
 function core.rollback_get_last_node_actor(pos, range, seconds)
 	return core.rollback_get_node_actions(pos, range, seconds, 1)[1]
 end
+
+--
+-- core.setting_*
+--
+
+local settings = core.settings
+
+local function setting_proxy(name)
+	return function(...)
+		core.log("deprecated", "WARNING: minetest.setting_* "..
+			"functions are deprecated.  "..
+			"Use methods on the minetest.settings object.")
+		return settings[name](settings, ...)
+	end
+end
+
+core.setting_set = setting_proxy("set")
+core.setting_get = setting_proxy("get")
+core.setting_setbool = setting_proxy("set_bool")
+core.setting_getbool = setting_proxy("get_bool")
+core.setting_save = setting_proxy("write")

builtin/game/forceloading.lua
@@ -40,7 +40,7 @@ function core.forceload_block(pos, transient)
 	elseif other_table[hash] ~= nil then
 		relevant_table[hash] = 1
 	else
-		if total_forceloaded >= (tonumber(core.setting_get("max_forceloaded_blocks")) or 16) then
+		if total_forceloaded >= (tonumber(core.settings:get("max_forceloaded_blocks")) or 16) then
 			return false
 		end
 		total_forceloaded = total_forceloaded+1

builtin/game/init.lua
@@ -13,7 +13,7 @@ dofile(gamepath.."constants.lua")
 assert(loadfile(gamepath.."item.lua"))(builtin_shared)
 dofile(gamepath.."register.lua")
 
-if core.setting_getbool("profiler.load") then
+if core.settings:get_bool("profiler.load") then
 	profiler = dofile(scriptpath.."profiler"..DIR_DELIM.."init.lua")
 end
 

commit 34c52d0ad76f627c719adc8806ca71c4968b8b39

Android: Fix crash while loading main menu (#5736)
Broken by 43d1f37

builtin/mainmenu/tab_simple_main.lua
@@ -188,6 +188,10 @@ local function main_button_handler(tabview, fields, name, tabdata)
 
 		core.settings:set("address", fields.te_address)
 		core.settings:set("remote_port", fields.te_port)
+			
+ 		core.start()		
+ 		return true		
+ 	end
 
 	if fields.btn_config_sp_world then
 		local configdialog = create_configure_world_dlg(1)