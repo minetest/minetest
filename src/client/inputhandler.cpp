1aedd398174fe6ad589c5197a732fc3e6940c7d3
Loic Blot <loic.blot@unix-experience.fr>
2017-05-13 10:27:35 +0200

    Include typo fix

--- src/client/inputhandler.cpp
+++ src/client/inputhandler.cpp
@@ -18,7 +18,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include <util/numeric.h>
+#include "util/numeric.h"
 #include "inputhandler.h"
 #include "mainmenumanager.h"
 
