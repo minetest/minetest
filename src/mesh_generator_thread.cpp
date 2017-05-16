582ee15d8e861efb9cc843963a95c087dae91bcc
Loic Blot <loic.blot@unix-experience.fr>
2017-05-16 08:27:18 +0200

    Fix a memleak pointed by @Zeno- in MeshUpdateQueue
    This leak was introduced when added cache to MeshUpdateQueue and only occurs when leaving world.

--- src/mesh_generator_thread.cpp
+++ src/mesh_generator_thread.cpp
@@ -83,6 +83,11 @@ MeshUpdateQueue::~MeshUpdateQueue()
 {
 	MutexAutoLock lock(m_mutex);
 
+	for (std::map<v3s16, CachedMapBlockData *>::iterator i = m_cache.begin();
+			i != m_cache.end(); ++i) {
+		delete i->second;
+	}
+
 	for (std::vector<QueuedMeshUpdate*>::iterator i = m_queue.begin();
 			i != m_queue.end(); ++i) {
 		QueuedMeshUpdate *q = *i;
