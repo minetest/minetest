minetest/minetest

commit a4a377ecad6f1732cc54e7ac329fdef9949f9bf7

Fix textarea bug: clicking right of the end of a line caused the cursor to be in one of the next lines (#3474)

src/intlGUIEditBox.cpp
@@ -1120,6 +1120,8 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 
 	if (x < CurrentTextRect.UpperLeftCorner.X)
 		x = CurrentTextRect.UpperLeftCorner.X;
+	else if (x > CurrentTextRect.LowerRightCorner.X)
+		x = CurrentTextRect.LowerRightCorner.X;
 
 	s32 idx = font->getCharacterFromPos(Text.c_str(), x - CurrentTextRect.UpperLeftCorner.X);
 
@@ -1127,7 +1129,7 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 	if (idx != -1)
 		return idx + startPos;
 
-	// click was off the right edge of the line, go to end.
+	// click was off the right edge of the last line, go to end.
 	return txtLine->size() + startPos;
 }
 

commit 1469424075affce7c27bb44e5a5cbd51485e44b2

Fix input regression introduced by a4a377ecad6f1732cc54e7ac329fdef9949f9bf7
Fix #5776

src/intlGUIEditBox.cpp
@@ -1120,8 +1120,8 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 
 	if (x < CurrentTextRect.UpperLeftCorner.X)
 		x = CurrentTextRect.UpperLeftCorner.X;
-	else if (x > CurrentTextRect.LowerRightCorner.X)
-		x = CurrentTextRect.LowerRightCorner.X;
+	else if (x > CurrentTextRect.LowerRightCorner.X + 1)
+		x = CurrentTextRect.LowerRightCorner.X + 1;
 
 	s32 idx = font->getCharacterFromPos(Text.c_str(), x - CurrentTextRect.UpperLeftCorner.X);
 