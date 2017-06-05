668de0e963016ae992aa5f92e29fcfad1e75b541
Loic Blot <loic.blot@unix-experience.fr>
2017-06-05 13:47:45 +0200

    Implement win32 & win64 builds in Gitlab CI + pkg improvements
    * windows buildbot: split build & deploy steps
    * move deb artifacts to root folder to have a nicer artifact
    * add windows build + packaging on xenial

--- util/buildbot/buildwin64.sh
+++ util/buildbot/buildwin64.sh
@@ -150,6 +150,10 @@ cmake .. \
 	-DLEVELDB_LIBRARY=$libdir/leveldb/lib/libleveldb.dll.a \
 	-DLEVELDB_DLL=$libdir/leveldb/bin/libleveldb.dll
 
-make package -j2
+make -j2
+
+if [ "x$NO_PACKAGE" = "x" ]; then
+	make package
+fi
 
 # EOF
