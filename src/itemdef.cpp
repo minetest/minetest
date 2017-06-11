672dee733956a5c796a5750a3e747f4884ee3877
Rui <rui.minetest@gmail.com>
2017-06-12 08:08:16 +0900

    Fix no sound bug

--- src/itemdef.cpp
+++ src/itemdef.cpp
@@ -124,8 +124,8 @@ void ItemDefinition::reset()
 
 void ItemDefinition::serialize(std::ostream &os, u16 protocol_version) const
 {
-
-	writeU8(os, 3); // version (proto > 20)
+	u8 version = (protocol_version >= 34) ? 4 : 3;
+	writeU8(os, version);
 	writeU8(os, type);
 	os << serializeString(name);
 	os << serializeString(description);
@@ -156,8 +156,11 @@ void ItemDefinition::serialize(std::ostream &os, u16 protocol_version) const
 	writeF1000(os, sound_place_failed.gain);
 	os << serializeString(palette_image);
 	writeU32(os, color.color);
-	writeF1000(os, sound_place.pitch);
-	writeF1000(os, sound_place_failed.pitch);
+
+	if (version >= 4) {
+		writeF1000(os, sound_place.pitch);
+		writeF1000(os, sound_place_failed.pitch);
+	}
 }
 
 void ItemDefinition::deSerialize(std::istream &is)
@@ -167,7 +170,7 @@ void ItemDefinition::deSerialize(std::istream &is)
 
 	// Deserialize
 	int version = readU8(is);
-	if(version < 1 || version > 3)
+	if (version < 1 || version > 4)
 		throw SerializationError("unsupported ItemDefinition version");
 	type = (enum ItemType)readU8(is);
 	name = deSerializeString(is);
@@ -216,8 +219,11 @@ void ItemDefinition::deSerialize(std::istream &is)
 		sound_place_failed.gain = readF1000(is);
 		palette_image = deSerializeString(is);
 		color.set(readU32(is));
-		sound_place.pitch = readF1000(is);
-		sound_place_failed.pitch = readF1000(is);
+
+		if (version >= 4) {
+			sound_place.pitch = readF1000(is);
+			sound_place_failed.pitch = readF1000(is);
+		}
 	} catch(SerializationError &e) {};
 }
 
