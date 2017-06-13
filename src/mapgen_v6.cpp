47f652d715c96611df15646a4734421724b8f78a
paramat <paramat@users.noreply.github.com>
2017-06-13 01:02:52 +0100

    Mgv6 mudflow: Remove stacked decorations half-buried in placed mud
    Improve 'moveMud()' code and use index arguments instead of pointers.

--- src/mapgen_v6.cpp
+++ src/mapgen_v6.cpp
@@ -884,12 +884,11 @@ void MapgenV6::flowMud(s16 &mudflow_minpos, s16 &mudflow_maxpos)
 					} while (ndef->get(*n2).walkable == false);
 					// Loop one up so that we're in air
 					vm->m_area.add_y(em, i2, 1);
-					n2 = &vm->m_data[i2];
 
-					// Move mud to new place. Outside mapchunk
-					// remove any decorations above removed mud.
+					// Move mud to new place. Outside mapchunk remove
+					// any decorations above removed or placed mud.
 					if (!dropped_to_unknown)
-						moveMud(n, n2, n3, p2d, i3, em);
+						moveMud(i, i2, i3, p2d, em);
 
 					// Done
 					break;
@@ -901,28 +900,36 @@ void MapgenV6::flowMud(s16 &mudflow_minpos, s16 &mudflow_maxpos)
 }
 
 
-void MapgenV6::moveMud(MapNode *n, MapNode *n2, MapNode *n3,
-	v2s16 p2d, u32 i3, v3s16 em)
+void MapgenV6::moveMud(u32 remove_index, u32 place_index,
+	u32 above_remove_index, v2s16 pos, v3s16 em)
 {
-	// Move mud to new place
-	*n2 = *n;
+	MapNode n_air(CONTENT_AIR);
+	// Copy mud from old place to new place
+	vm->m_data[place_index] = vm->m_data[remove_index];
 	// Set old place to be air
-	*n = MapNode(CONTENT_AIR);
-	// Upper (n3) is not walkable or is NULL. If it is
-	// not NULL and not air it is a decoration that
-	// needs removing to avoid unsupported decorations.
-	// This only happens outside the mapchunk.
-	if ((p2d.X > node_max.X || p2d.X < node_min.X ||
-			p2d.Y > node_max.Z || p2d.Y < node_min.Z) &&
-			n3 && n3->getContent() != CONTENT_AIR) {
-		*n3 = MapNode(CONTENT_AIR);
-		// Check above for a stacked decoration and remove nodes
-		u32 i4 = i3;
-		vm->m_area.add_y(em, i4, 1);
-		while (vm->m_area.contains(i4) &&
-				vm->m_data[i4].getContent() != CONTENT_AIR) {
-			vm->m_data[i4] = MapNode(CONTENT_AIR);
-			vm->m_area.add_y(em, i4, 1);
+	vm->m_data[remove_index] = n_air;
+	// Outside the mapchunk decorations may need to be removed if above removed
+	// mud or if half-buried in placed mud. Placed mud is to the side of pos so
+	// use 'pos.X >= node_max.X' etc.
+	if (pos.X >= node_max.X || pos.X <= node_min.X ||
+			pos.Y >= node_max.Z || pos.Y <= node_min.Z) {
+		// 'above remove' node is above removed mud. If it is not air and not
+		// water it is a decoration that needs removing. Also search upwards
+		// for a possible stacked decoration.
+		while (vm->m_area.contains(above_remove_index) &&
+				vm->m_data[above_remove_index].getContent() != CONTENT_AIR &&
+				vm->m_data[above_remove_index].getContent() != c_water_source) {
+			vm->m_data[above_remove_index] = n_air;
+			vm->m_area.add_y(em, above_remove_index, 1);
+		}
+		// Mud placed may have half-buried a tall decoration, search above and
+		// remove.
+		vm->m_area.add_y(em, place_index, 1);
+		while (vm->m_area.contains(place_index) &&
+				vm->m_data[place_index].getContent() != CONTENT_AIR &&
+				vm->m_data[place_index].getContent() != c_water_source) {
+			vm->m_data[place_index] = n_air;
+			vm->m_area.add_y(em, place_index, 1);
 		}
 	}
 }
