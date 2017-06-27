e3b380dc67c75ba586900b7c172e1484c7da1827
Jesse McDonald <nybble41@gmail.com>
2017-06-27 05:34:11 -0500

    Fix for empty key/value when reading item string with wear but no metadata (#6058)

--- src/itemstackmetadata.cpp
+++ src/itemstackmetadata.cpp
@@ -28,16 +28,18 @@ void ItemStackMetadata::deSerialize(std::istream &is)
 
 	m_stringvars.clear();
 
-	if (!in.empty() && in[0] == DESERIALIZE_START) {
-		Strfnd fnd(in);
-		fnd.to(1);
-		while (!fnd.at_end()) {
-			std::string name = fnd.next(DESERIALIZE_KV_DELIM_STR);
-			std::string var  = fnd.next(DESERIALIZE_PAIR_DELIM_STR);
-			m_stringvars[name] = var;
+	if (!in.empty()) {
+		if (in[0] == DESERIALIZE_START) {
+			Strfnd fnd(in);
+			fnd.to(1);
+			while (!fnd.at_end()) {
+				std::string name = fnd.next(DESERIALIZE_KV_DELIM_STR);
+				std::string var  = fnd.next(DESERIALIZE_PAIR_DELIM_STR);
+				m_stringvars[name] = var;
+			}
+		} else {
+			// BACKWARDS COMPATIBILITY
+			m_stringvars[""] = in;
 		}
-	} else {
-		// BACKWARDS COMPATIBILITY
-		m_stringvars[""] = in;
 	}
 }
