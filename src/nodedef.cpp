a5755e4cee081699bdb4b25faf8840eafb3fc179
Vitaliy <silverunicorn2011@yandex.ru>
2017-06-27 20:50:31 +0300

    Make GCC happy

--- src/nodedef.cpp
+++ src/nodedef.cpp
@@ -813,11 +813,12 @@ void ContentFeatures::updateTextures(ITextureSource *tsrc, IShaderSource *shdsrc
 				tdef[j].backface_culling, overlay_material);
 	}
 
-	if (drawtype == NDT_PLANTLIKE_ROOTED)
+	if (drawtype == NDT_PLANTLIKE_ROOTED) {
 		if (waving == 1)
 			material_type = TILE_MATERIAL_WAVING_PLANTS;
 		else if (waving == 2)
 			material_type = TILE_MATERIAL_WAVING_LEAVES;
+	}
 
 	// Special tiles (fill in f->special_tiles[])
 	for (u16 j = 0; j < CF_SPECIAL_COUNT; j++) {
