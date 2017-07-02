824db6febece35b45b7602e3a7bddb9c8fc409ac
rubenwardy <rw@rubenwardy.com>
2017-07-02 19:10:21 +0100

    Use JSON serialization to store ItemStack Metadata

--- src/itemstackmetadata.cpp
+++ src/itemstackmetadata.cpp
@@ -1,13 +1,9 @@
 #include "itemstackmetadata.h"
 #include "util/serialize.h"
 #include "util/strfnd.h"
+#include <json/json.h>
 
-#define DESERIALIZE_START '\x01'
-#define DESERIALIZE_KV_DELIM '\x02'
-#define DESERIALIZE_PAIR_DELIM '\x03'
-#define DESERIALIZE_START_STR "\x01"
-#define DESERIALIZE_KV_DELIM_STR "\x02"
-#define DESERIALIZE_PAIR_DELIM_STR "\x03"
+#define SERIALIZATION_VERSION_IDENTIFIER '\x02'
 
 #define TOOLCAP_KEY "tool_capabilities"
 
@@ -27,15 +23,21 @@ bool ItemStackMetadata::setString(const std::string &name, const std::string &va
 
 void ItemStackMetadata::serialize(std::ostream &os) const
 {
-	std::ostringstream os2;
-	os2 << DESERIALIZE_START;
+	Json::Value root(Json::objectValue);
 	for (const auto &stringvar : m_stringvars) {
 		if (!stringvar.first.empty() || !stringvar.second.empty())
-			os2 << stringvar.first << DESERIALIZE_KV_DELIM
-				<< stringvar.second << DESERIALIZE_PAIR_DELIM;
+			root[stringvar.first] = stringvar.second;
 	}
-	os << serializeJsonStringIfNeeded(os2.str());
+	os << serializeJsonStringIfNeeded((char)SERIALIZATION_VERSION_IDENTIFIER +
+			Json::FastWriter().write(root));
 }
+
+#define DESERIALIZE_START '\x01'
+#define DESERIALIZE_KV_DELIM '\x02'
+#define DESERIALIZE_PAIR_DELIM '\x03'
+#define DESERIALIZE_START_STR "\x01"
+#define DESERIALIZE_KV_DELIM_STR "\x02"
+#define DESERIALIZE_PAIR_DELIM_STR "\x03"
 
 void ItemStackMetadata::deSerialize(std::istream &is)
 {
@@ -43,19 +45,36 @@ void ItemStackMetadata::deSerialize(std::istream &is)
 
 	m_stringvars.clear();
 
-	if (!in.empty()) {
-		if (in[0] == DESERIALIZE_START) {
-			Strfnd fnd(in);
-			fnd.to(1);
-			while (!fnd.at_end()) {
-				std::string name = fnd.next(DESERIALIZE_KV_DELIM_STR);
-				std::string var  = fnd.next(DESERIALIZE_PAIR_DELIM_STR);
-				m_stringvars[name] = var;
+	if (in.empty())
+		return;
+
+	if (in[0] == SERIALIZATION_VERSION_IDENTIFIER) {
+		in.erase(0, 1);
+
+		Json::Reader reader;
+		Json::Value attr_root;
+		if (reader.parse(in, attr_root) && attr_root.type() == Json::objectValue) {
+			const Json::Value::Members attr_list = attr_root.getMemberNames();
+			for (Json::Value::Members::const_iterator it = attr_list.begin();
+					it != attr_list.end(); ++it) {
+				m_stringvars[*it] = attr_root[*it].asString();
 			}
 		} else {
-			// BACKWARDS COMPATIBILITY
-			m_stringvars[""] = in;
+			errorstream << "ItemStackMetadata::deSerialize():"
+				<< " expected JSON data, but failed to parse. Ignoring." << std::endl;
 		}
+	} else if (in[0] == DESERIALIZE_START) {
+		// BACKWARDS COMPATIBILITY
+		Strfnd fnd(in);
+		fnd.to(1);
+		while (!fnd.at_end()) {
+			std::string name = fnd.next(DESERIALIZE_KV_DELIM_STR);
+			std::string var  = fnd.next(DESERIALIZE_PAIR_DELIM_STR);
+			m_stringvars[name] = var;
+		}
+	} else {
+		// BACKWARDS COMPATIBILITY
+		m_stringvars[""] = in;
 	}
 	updateToolCapabilities();
 }
--- src/unittest/test_inventory.cpp
+++ src/unittest/test_inventory.cpp
@@ -23,6 +23,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "gamedef.h"
 #include "inventory.h"
+#include "itemstackmetadata.h"
 
 class TestInventory : public TestBase {
 public:
@@ -32,6 +33,7 @@ public:
 	void runTests(IGameDef *gamedef);
 
 	void testSerializeDeserialize(IItemDefManager *idef);
+	void testDeSerializeItemStackMetadata();
 
 	static const char *serialized_inventory;
 	static const char *serialized_inventory_2;
@@ -42,6 +44,7 @@ static TestInventory g_test_instance;
 void TestInventory::runTests(IGameDef *gamedef)
 {
 	TEST(testSerializeDeserialize, gamedef->getItemDefManager());
+	TEST(testDeSerializeItemStackMetadata);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -141,3 +144,74 @@ const char *TestInventory::serialized_inventory_2 =
 	"Empty\n"
 	"EndInventoryList\n"
 	"EndInventory\n";
+
+#include "util/serialize.h"
+
+#define DESERIALIZE_START '\x01'
+#define DESERIALIZE_KV_DELIM '\x02'
+#define DESERIALIZE_PAIR_DELIM '\x03'
+#define DESERIALIZE_START_STR "\x01"
+#define DESERIALIZE_KV_DELIM_STR "\x02"
+#define DESERIALIZE_PAIR_DELIM_STR "\x03"
+#define SERIALIZATION_VERSION_IDENTIFIER '\x02'
+
+void TestInventory::testDeSerializeItemStackMetadata() {
+	{
+		std::istringstream is(serializeJsonString("some meta\nasdkmskdmsds\nasasasas"));
+
+		ItemStackMetadata metadata;
+		metadata.deSerialize(is);
+
+		UASSERT(metadata.size() == 1);
+		UASSERT(metadata.getString("") == "some meta\nasdkmskdmsds\nasasasas");
+	}
+
+	{
+		std::ostringstream is;
+		is << DESERIALIZE_START
+		   << "foo"
+		   << DESERIALIZE_KV_DELIM
+		   << "bar"
+		   << DESERIALIZE_PAIR_DELIM
+		   << "description"
+		   << DESERIALIZE_KV_DELIM
+		   << "A foo bar thing\nbleh"
+		   << DESERIALIZE_PAIR_DELIM;
+
+		std::istringstream is2(serializeJsonString(is.str()));
+
+		ItemStackMetadata metadata;
+		metadata.deSerialize(is2);
+
+		UASSERT(metadata.size() == 2);
+		UASSERT(metadata.getString("foo") == "bar");
+		UASSERT(metadata.getString("description") == "A foo bar thing\nbleh");
+	}
+
+	{
+		std::ostringstream is;
+		is << SERIALIZATION_VERSION_IDENTIFIER
+		   << "{ \"foo\": \"bar\", \"description\": \"this is a JSON string!\\nHow exciting!\"}";
+
+		std::istringstream is2(serializeJsonString(is.str()));
+
+		ItemStackMetadata metadata;
+		metadata.deSerialize(is2);
+
+		UASSERT(metadata.size() == 2);
+		UASSERT(metadata.getString("foo") == "bar");
+		UASSERT(metadata.getString("description") == "this is a JSON string!\nHow exciting!");
+	}
+
+	{
+		const std::string test = "{ \"foo\": \"bar\", \"description\": \"this is a JSON string!\\nHow exciting!\"}";
+
+		std::istringstream is2(serializeJsonString(test));
+
+		ItemStackMetadata metadata;
+		metadata.deSerialize(is2);
+
+		UASSERT(metadata.size() == 1);
+		UASSERT(metadata.getString("") == test);
+	}
+}
