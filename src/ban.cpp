64c7a689ad8a596f6b72afe200d9ced20cdd9630
Loic Blot <loic.blot@unix-experience.fr>
2017-08-15 09:39:58 +0200

    bab.cpp: code modernization
    * Use for range based loops
    * Simplify some tests
    * Code style fixes

--- src/ban.cpp
+++ src/ban.cpp
@@ -48,14 +48,12 @@ void BanManager::load()
 	MutexAutoLock lock(m_mutex);
 	infostream<<"BanManager: loading from "<<m_banfilepath<<std::endl;
 	std::ifstream is(m_banfilepath.c_str(), std::ios::binary);
-	if(is.good() == false)
-	{
+	if (!is.good()) {
 		infostream<<"BanManager: failed loading from "<<m_banfilepath<<std::endl;
 		throw SerializationError("BanManager::load(): Couldn't open file");
 	}
 
-	while(!is.eof() && is.good())
-	{
+	while (!is.eof() && is.good()) {
 		std::string line;
 		std::getline(is, line, '\n');
 		Strfnd f(line);
@@ -74,8 +72,8 @@ void BanManager::save()
 	infostream << "BanManager: saving to " << m_banfilepath << std::endl;
 	std::ostringstream ss(std::ios_base::binary);
 
-	for (StringMap::iterator it = m_ips.begin(); it != m_ips.end(); ++it)
-		ss << it->first << "|" << it->second << "\n";
+	for (const auto &ip : m_ips)
+		ss << ip.first << "|" << ip.second << "\n";
 
 	if (!fs::safeWriteToFile(m_banfilepath, ss.str())) {
 		infostream << "BanManager: failed saving to " << m_banfilepath << std::endl;
@@ -94,11 +92,11 @@ bool BanManager::isIpBanned(const std::string &ip)
 std::string BanManager::getBanDescription(const std::string &ip_or_name)
 {
 	MutexAutoLock lock(m_mutex);
-	std::string s = "";
-	for (StringMap::iterator it = m_ips.begin(); it != m_ips.end(); ++it) {
-		if (it->first  == ip_or_name || it->second == ip_or_name
-				|| ip_or_name == "") {
-			s += it->first + "|" + it->second + ", ";
+	std::string s;
+	for (const auto &ip : m_ips) {
+		if (ip.first  == ip_or_name || ip.second == ip_or_name
+				|| ip_or_name.empty()) {
+			s += ip.first + "|" + ip.second + ", ";
 		}
 	}
 	s = s.substr(0, s.size() - 2);
