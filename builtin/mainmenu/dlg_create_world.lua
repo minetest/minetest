80f1cec1d0f7667ae2e6ef260b4790327d8100f2
Stefan Vukanović <lisacvukhome@gmail.com>
2017-08-17 19:35:09 +0200

    Revert "Rename a variable"
    This reverts commit b6598d33574a4b349bb368d5208d34c12d1261eb.

--- builtin/mainmenu/dlg_create_world.lua
+++ builtin/mainmenu/dlg_create_world.lua
@@ -87,8 +87,8 @@ local function create_world_buttonhandler(this, fields)
 
 		if gameindex ~= nil then
 			if worldname == "" then
-				local time = os.date("!%Y-%m-%dT%TZ")
-				local random_world_name = "World" .. time
+				local random_number = os.date("!%Y-%m-%dT%TZ")
+				local random_world_name = "World" .. random_number
 				worldname = random_world_name
 			end
 			local message = nil
