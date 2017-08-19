557bbc670451f82c384032383df80a1a92ffe724
rubenwardy <rubenwardy@gmail.com>
2017-08-19 19:43:02 +0100

    Fix empty legacy meta being persisted

--- src/itemstackmetadata.cpp
+++ src/itemstackmetadata.cpp
@@ -14,8 +14,9 @@ void ItemStackMetadata::serialize(std::ostream &os) const
 	std::ostringstream os2;
 	os2 << DESERIALIZE_START;
 	for (const auto &stringvar : m_stringvars) {
-		os2 << stringvar.first << DESERIALIZE_KV_DELIM
-		    << stringvar.second << DESERIALIZE_PAIR_DELIM;
+		if (!stringvar.first.empty() || !stringvar.second.empty())
+			os2 << stringvar.first << DESERIALIZE_KV_DELIM
+				<< stringvar.second << DESERIALIZE_PAIR_DELIM;
 	}
 	os << serializeJsonStringIfNeeded(os2.str());
 }
