01c319d7784c3db614a53745e5af62c43cf45c90
Dániel Juhász <juhdanad@gmail.com>
2017-08-24 06:31:33 +0000

    Fix Android node selection distance (#6187)

--- src/game.cpp
+++ src/game.cpp
@@ -3544,6 +3544,9 @@ void Game::processPlayerInteraction(f32 dtime, bool show_hud, bool show_debug)
 
 	if ((g_settings->getBool("touchtarget")) && (g_touchscreengui)) {
 		shootline = g_touchscreengui->getShootline();
+		// Scale shootline to the acual distance the player can reach
+		shootline.end = shootline.start
+			+ shootline.getVector().normalize() * BS * d;
 		shootline.start += intToFloat(camera_offset, BS);
 		shootline.end += intToFloat(camera_offset, BS);
 	}
