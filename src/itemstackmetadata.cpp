74812f296da546c561f7bc3dd51ce6235b66b360
SmallJoker <SmallJoker@users.noreply.github.com>
2017-09-03 21:24:37 +0200

    empty

--- src/itemstackmetadata.cpp
+++ src/itemstackmetadata.cpp
@@ -14,7 +14,7 @@ void ItemStackMetadata::serialize(std::ostream &os) const
 	std::ostringstream os2;
 	os2 << DESERIALIZE_START;
 	for (const auto &stringvar : m_stringvars) {
-		if (stringvar.first != "" || stringvar.second != "")
+		if (!stringvar.firs.empty() || !stringvar.second.empty())
 			os2 << stringvar.first << DESERIALIZE_KV_DELIM
 				<< stringvar.second << DESERIALIZE_PAIR_DELIM;
 	}
