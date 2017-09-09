a0827b272e0bdd3371275ced4d8c0a5f4458cae2
Ekdohibs <nathanael.courant@laposte.net>
2017-09-09 17:26:11 +0200

    Cleanup & document better conversions between block position and integers.

--- src/database.cpp
+++ src/database.cpp
@@ -21,11 +21,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "irrlichttypes.h"
 
 
-/****************
- * Black magic! *
- ****************
- * The position hashing is very messed up.
- * It's a lot more complicated than it looks.
+/* The position hashing is done by the
+ * straightforward function getBlockAsInteger.
+ *
+ * To invert it, we use unsigned_to_signed to get
+ * X, Y, Z in that order, since knowing the low
+ * 12 bits of the hash uniquely determines X, and
+ * the we can iterate to get Y then Z.
  */
 
 static inline s16 unsigned_to_signed(u16 i, u16 max_positive)
@@ -36,17 +38,6 @@ static inline s16 unsigned_to_signed(u16 i, u16 max_positive)
 
 	return i - (max_positive * 2);
 }
-
-
-// Modulo of a negative number does not work consistently in C
-static inline s64 pythonmodulo(s64 i, s16 mod)
-{
-	if (i >= 0) {
-		return i % mod;
-	}
-	return mod - ((-i) % mod);
-}
-
 
 s64 MapDatabase::getBlockAsInteger(const v3s16 &pos)
 {
@@ -59,11 +50,11 @@ s64 MapDatabase::getBlockAsInteger(const v3s16 &pos)
 v3s16 MapDatabase::getIntegerAsBlock(s64 i)
 {
 	v3s16 pos;
-	pos.X = unsigned_to_signed(pythonmodulo(i, 4096), 2048);
+	pos.X = unsigned_to_signed(i & 0xfff, 2048);
 	i = (i - pos.X) / 4096;
-	pos.Y = unsigned_to_signed(pythonmodulo(i, 4096), 2048);
+	pos.Y = unsigned_to_signed(i & 0xfff, 2048);
 	i = (i - pos.Y) / 4096;
-	pos.Z = unsigned_to_signed(pythonmodulo(i, 4096), 2048);
+	pos.Z = unsigned_to_signed(i & 0xfff, 2048);
 	return pos;
 }
 
