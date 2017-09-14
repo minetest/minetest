73a4f7fef6fb9191a2ea4c68eaf318635984e72f
DTA7 <dta7e@t-online.de>
2017-09-14 20:51:51 +0200

    Small cleanup
    Add space before { and change += to = for setting empty string.

--- src/guiFormSpecMenu.cpp
+++ src/guiFormSpecMenu.cpp
@@ -2390,10 +2390,10 @@ void GUIFormSpecMenu::drawList(const ListDrawSpec &s, int phase,
 				else
 					tooltip_text = utf8_to_wide(desc);
 
-				if (!item.name.empty()){
+				if (!item.name.empty()) {
 					//Append itemstring if enabled in settings
 					if (tooltip_text.empty())
-						tooltip_text += utf8_to_wide(item.name);
+						tooltip_text = utf8_to_wide(item.name);
 					if (m_tooltip_show_itemstring){
 						tooltip_text += utf8_to_wide(" [" + item.name + "]");
 					}
