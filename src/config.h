50b2185ced9dc64e054ce743bbc22d15ecdf6192
SmallJoker <SmallJoker@users.noreply.github.com>
2017-09-26 20:30:42 +0200

    Update version correctly again (#6462)
    * Update version correctly again
    Partial revert of PR 6331
    
    * It doesn't update automatically

--- src/config.h
+++ src/config.h
@@ -5,6 +5,10 @@
 
 #pragma once
 
+#define STRINGIFY(x) #x
+#define STR(x) STRINGIFY(x)
+
+
 #if defined USE_CMAKE_CONFIG_H
 	#include "cmake_config.h"
 #elif defined (__ANDROID__) || defined (ANDROID)
@@ -24,3 +28,13 @@
 		#define BUILD_TYPE "Debug"
 	#endif
 #endif
+
+#define BUILD_INFO \
+	"BUILD_TYPE=" BUILD_TYPE "\n"          \
+	"RUN_IN_PLACE=" STR(RUN_IN_PLACE) "\n" \
+	"USE_GETTEXT=" STR(USE_GETTEXT) "\n"   \
+	"USE_SOUND=" STR(USE_SOUND) "\n"       \
+	"USE_CURL=" STR(USE_CURL) "\n"         \
+	"USE_FREETYPE=" STR(USE_FREETYPE) "\n" \
+	"USE_LUAJIT=" STR(USE_LUAJIT) "\n"     \
+	"STATIC_SHAREDIR=" STR(STATIC_SHAREDIR);
