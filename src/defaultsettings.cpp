3c93a56e16f03893f97b7d28b0a8a0e302cb55ca
Rob Blanckaert <basicer@basicer.com>
2017-10-07 09:11:48 -0400

    Add setting for near plane distance. (#6395)
    * Allow setting the near plane
    
    * - Add near_plane limit of 0.5 to prevent x-ray.
    - Add more details to near_plane setting.

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -149,6 +149,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("fps_max", "60");
 	settings->setDefault("pause_fps_max", "20");
 	settings->setDefault("viewing_range", "100");
+	settings->setDefault("near_plane", "0.1");
 	settings->setDefault("screen_w", "1024");
 	settings->setDefault("screen_h", "600");
 	settings->setDefault("autosave_screensize", "true");
