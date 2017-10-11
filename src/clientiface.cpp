c582e577b9773ad42e8e2d799530a996628f5a0d
Lars Hofhansl <larsh@apache.org>
2017-10-11 15:52:32 -0700

    Fix the speed retrieval cone

--- src/clientiface.cpp
+++ src/clientiface.cpp
@@ -264,12 +264,13 @@ void RemoteClient::GetNextBlocks (
 				Don't generate or send if not in sight
 				FIXME This only works if the client uses a small enough
 				FOV setting. The default of 72 degrees is fine.
+				Also retrieve a smaller view cone in the direction of the player's movement.
+				(0.15 is about 6 degrees)
 			*/
-
 			f32 dist;
 			if (!(isBlockInSight(p, camera_pos, camera_dir, camera_fov, d_blocks_in_sight, &dist) ||
 				(playerspeed.getLength() > 1.0f * BS &&
-				isBlockInSight(p, camera_pos, playerspeeddir, camera_fov / 10.0f, d_blocks_in_sight)))) {
+				isBlockInSight(p, camera_pos, playerspeeddir, 0.15f, d_blocks_in_sight)))) {
 				continue;
 			}
 
