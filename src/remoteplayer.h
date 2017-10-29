416c4535c7e302191903c2a5c174de4f41a62e1c
rubenwardy <rw@rubenwardy.com>
2017-10-29 18:31:50 +0000

    Fix day_night_ratio_do_override not being initialised server-side
    Causes get_day_night_ratio() to return unpredictable results.

--- src/remoteplayer.h
+++ src/remoteplayer.h
@@ -156,7 +156,7 @@ private:
 	float m_chat_message_allowance;
 	u16 m_message_rate_overhead;
 
-	bool m_day_night_ratio_do_override;
+	bool m_day_night_ratio_do_override = false;
 	float m_day_night_ratio;
 	std::string hud_hotbar_image;
 	std::string hud_hotbar_selected_image;
