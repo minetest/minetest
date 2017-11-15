26e933bc5e50364682370980a117fa66343b34c1
stujones11 <stujones111@gmail.com>
2017-11-15 19:52:19 +0000

    Android: Update build system for ndk-r15x

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -1,9 +1,9 @@
 # NDK_TOOLCHAIN_VERSION := clang3.8
 
-APP_PLATFORM := android-9
+APP_PLATFORM := android-14
 APP_MODULES := minetest
-APP_STL := gnustl_static
+APP_STL := c++_static
 
-APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -fexceptions -std=c++11
 APP_GNUSTL_FORCE_CPP_FEATURES := rtti
 
