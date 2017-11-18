587e5fff9fe812f30686c42b42ee99afea3d708c
ezhh <owlecho@live.com>
2017-11-18 13:41:34 +0000

    Adjust Del. Favorite button

--- builtin/mainmenu/tab_online.lua
+++ builtin/mainmenu/tab_online.lua
@@ -53,11 +53,11 @@ local function get_formspec(tabview, name, tabdata)
 		"box[7.73,2.25;4.25,2.6;#999999]"..
 
 		-- Connect
-		"button[10,4.9;2.18,1;btn_mp_connect;" .. fgettext("Connect") .. "]"
+		"button[9.88,4.9;2.3,1;btn_mp_connect;" .. fgettext("Connect") .. "]"
 
 	if tabdata.fav_selected and fav_selected then
 		if gamedata.fav then
-			retval = retval .. "button[7.75,5.15;2.3,0.5;btn_delete_favorite;" ..
+			retval = retval .. "button[7.73,4.9;2.3,1;btn_delete_favorite;" ..
 				fgettext("Del. Favorite") .. "]"
 		end
 		if fav_selected.description then
