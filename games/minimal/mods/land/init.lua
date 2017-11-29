d8f6af67ceeb1907d6a21fb7fec91b923991a528
number Zero <silverunicorn2011@yandex.ru>
2017-11-30 00:15:57 +0300

    Truly minimal =)

---
+++ games/minimal/mods/land/init.lua
@@ -0,0 +1,64 @@
+local PATH = minetest.get_modpath("land")
+-- dofile(PATH.."/mapgen.lua")
+
+minetest.register_node("land:stone", {
+	description = "Stone",
+	tiles = {{name="stone.png", align_style="world"}},
+	groups = {cracky=3},
+})
+
+minetest.register_node("land:dirt", {
+	description = "Dirt",
+	tiles = {{name="dirt.png", align_style="world"}},
+	groups = {crumbly=3, soil=1},
+})
+
+minetest.register_node("land:grass", {
+	description = "Dirt",
+	tiles = {
+		{name="grass.png", align_style="world"},
+		{name="dirt.png", align_style="world"},
+	},
+	overlay_tiles = {
+		nil,
+		nil,
+		{name="grass_overlay.png", align_style="world"}
+	},
+	groups = {crumbly=3, soil=1},
+})
+
+minetest.register_alias("mapgen_stone", "land:stone")
+minetest.register_alias("mapgen_dirt", "land:dirt")
+minetest.register_alias("mapgen_dirt_with_grass", "land:grass")
+-- minetest.register_alias("mapgen_sand", "default:sand")
+-- minetest.register_alias("mapgen_water_source", "default:water_source")
+-- minetest.register_alias("mapgen_river_water_source", "default:river_water_source")
+-- minetest.register_alias("mapgen_lava_source", "default:lava_source")
+-- minetest.register_alias("mapgen_gravel", "default:gravel")
+
+minetest.clear_registered_biomes()
+minetest.clear_registered_decorations()
+
+minetest.register_biome({
+        name = "land:grassland",
+        node_top = "land:grass",
+        depth_top = 1,
+        node_filler = "land:dirt",
+        depth_filler = 2,
+        y_min = 5,
+        y_max = 31000,
+        heat_point = 50,
+        humidity_point = 50,
+})
+
+minetest.register_biome({
+        name = "land:ocean",
+        node_top = "land:dirt",
+        depth_top = 1,
+        node_filler = "land:dirt",
+        depth_filler = 2,
+        y_min = -31000,
+        y_max = 4,
+        heat_point = 50,
+        humidity_point = 50,
+})
---
+++ games/minimal/mods/player/init.lua
@@ -0,0 +1,22 @@
+minetest.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	minetest.after(2.0, minetest.chat_send_player, name, "This is the [minimal] \"Minimal Development Test\" game. Use [minetest_game] for the real thing.")
+end)
+
+-- The hand
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "hand.png",
+	wield_scale = {x=1, y=1, z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level = 0,
+		groupcaps = {
+			fleshy = {times={[2]=2.00, [3]=1.00}, uses=0, maxlevel=1},
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0, maxlevel=3},
+		},
+		damage_groups = {fleshy=1},
+	}
+})
