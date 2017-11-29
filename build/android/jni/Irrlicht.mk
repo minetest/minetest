21c1b016d6006326708c1ca3ad018070b50bc960
stujones11 <stujones111@gmail.com>
2017-11-29 19:50:11 +0000

    Updates for ndk-r16

--- build/android/jni/Irrlicht.mk
+++ build/android/jni/Irrlicht.mk
@@ -1,11 +1,10 @@
-# NDK_TOOLCHAIN_VERSION := clang3.8
-
 APP_PLATFORM := android-14
 APP_ABI := armeabi-v7a
 APP_STL := gnustl_static
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_DEPRECATED_HEADERS := true
-APP_MODULES := minetest
+APP_MODULES := Irrlicht
 
+APP_CLAFGS += -mfloat-abi=softfp -mfpu=vfpv3 -O3
 APP_CPPFLAGS += -fexceptions
-APP_GNUSTL_FORCE_CPP_FEATURES := rtti
+
