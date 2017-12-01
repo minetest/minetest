5deffa0819ba9069695d006d619aead7ddc83156
number Zero <silverunicorn2011@yandex.ru>
2017-12-01 21:27:07 +0300

    Add gravel&sand

--- games/minimal/mods/land/init.lua
+++ games/minimal/mods/land/init.lua
@@ -13,6 +13,18 @@ minetest.register_node("land:dirt", {
 	groups = {crumbly=3, soil=1},
 })
 
+minetest.register_node("land:gravel", {
+	description = "Gravel",
+	tiles = {{name="gravel.png", align_style="world"}},
+	groups = {falling_node=1, crumbly=2},
+})
+
+minetest.register_node("land:sand", {
+	description = "Sand",
+	tiles = {{name="sand.png", align_style="world"}},
+	groups = {falling_node=1, crumbly=3},
+})
+
 minetest.register_node("land:grass", {
 	description = "Dirt with grass",
 	use_texture_alpha = true,
@@ -31,11 +43,11 @@ minetest.register_node("land:grass", {
 minetest.register_alias("mapgen_stone", "land:stone")
 minetest.register_alias("mapgen_dirt", "land:dirt")
 minetest.register_alias("mapgen_dirt_with_grass", "land:grass")
--- minetest.register_alias("mapgen_sand", "default:sand")
+minetest.register_alias("mapgen_sand", "land:sand")
 -- minetest.register_alias("mapgen_water_source", "default:water_source")
 -- minetest.register_alias("mapgen_river_water_source", "default:river_water_source")
 -- minetest.register_alias("mapgen_lava_source", "default:lava_source")
--- minetest.register_alias("mapgen_gravel", "default:gravel")
+minetest.register_alias("mapgen_gravel", "land:gravel")
 
 minetest.clear_registered_biomes()
 minetest.clear_registered_decorations()
@@ -54,9 +66,9 @@ minetest.register_biome({
 
 minetest.register_biome({
         name = "land:ocean",
-        node_top = "land:dirt",
+        node_top = "land:sand",
         depth_top = 1,
-        node_filler = "land:dirt",
+        node_filler = "land:gravel",
         depth_filler = 2,
         y_min = -31000,
         y_max = 4,
