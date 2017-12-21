142fa54a8d70c3bd14cba1e49f79e7b3769931b2
number Zero <silverunicorn2011@yandex.ru>
2017-12-21 19:09:37 +0300

    Add IrrLicht-specific smart pointer

---
+++ src/irr_ptr.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <type_traits>
+#include "irrlichttypes.h"
+#include "IReferenceCounted.h"
+
+enum class Grab
+{
+	already_owned = 0,
+	do_grab = 1,
+};
+
+template <class ReferenceCounted, class = typename std::enable_if <
+	std::is_base_of <IReferenceCounted, ReferenceCounted>::value>::type>
+class irr_ptr
+{
+	ReferenceCounted *value = nullptr;
+public:
+	irr_ptr() = default;
+
+	irr_ptr(std::nullptr_t) noexcept
+	{
+	}
+
+	template <typename B, class = typename std::enable_if <
+		std::is_convertible <B *, ReferenceCounted *>::value>::type>
+	irr_ptr(const irr_ptr<B> &b) noexcept
+	{
+		reset(b.value);
+	}
+
+	template <typename B, class = typename std::enable_if <
+		std::is_convertible <B *, ReferenceCounted *>::value>::type>
+	irr_ptr(irr_ptr<B> &&b) noexcept : value(b.value)
+	{
+		b.value = nullptr;
+	}
+
+	irr_ptr(ReferenceCounted *object, Grab grab) noexcept
+	{
+		reset(object, grab);
+	}
+
+	~irr_ptr()
+	{
+		reset();
+	}
+
+	template <typename B, class = typename std::enable_if <
+		std::is_convertible <B *, ReferenceCounted *>::value>::type>
+	irr_ptr &operator= (const irr_ptr<B> &b) noexcept
+	{
+		reset(b.value);
+		return *this;
+	}
+
+	template <typename B, class = typename std::enable_if <
+		std::is_convertible <B *, ReferenceCounted *>::value>::type>
+	irr_ptr &operator= (irr_ptr<B> &&b) noexcept
+	{
+		reset();
+		value = b.value;
+		b.value = nullptr;
+		return *this;
+	}
+
+	ReferenceCounted &operator*() const noexcept { return *value; }
+	ReferenceCounted *operator->() const noexcept { return value; }
+	explicit operator ReferenceCounted*() const noexcept { return value; }
+	explicit operator bool() const noexcept { return !!value; }
+
+	ReferenceCounted *get() const noexcept
+	{
+		return value;
+	}
+
+	void reset() noexcept
+	{
+		if (value)
+			value->drop();
+		value = nullptr;
+	}
+
+	void reset(ReferenceCounted *object, Grab grab) noexcept
+	{
+		reset();
+		value = object;
+		if (value && grab == Grab::do_grab)
+			value->grab();
+	}
+};
