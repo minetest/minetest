a39ae56f209837cec032807c208321cd872328cb
number Zero <silverunicorn2011@yandex.ru>
2017-12-21 20:55:49 +0300

    Improve extrusion

--- src/meshgen/extrusion.cpp
+++ src/meshgen/extrusion.cpp
@@ -6,6 +6,22 @@
 
 static std::map<video::ITexture *, ExtrudedMesh> cache;
 
+static void setupMaterial(video::SMaterial &m, video::ITexture *texture)
+{
+	m.MaterialType = video::EMT_TRANSPARENT_ALPHA_CHANNEL_REF;
+	m.setFlag(video::EMF_LIGHTING, false);
+	m.setFlag(video::EMF_BILINEAR_FILTER, false);
+	m.setFlag(video::EMF_TRILINEAR_FILTER, false);
+	m.setFlag(video::EMF_BACK_FACE_CULLING, true);
+#if (IRRLICHT_VERSION_MAJOR >= 1 && IRRLICHT_VERSION_MINOR >= 8) || IRRLICHT_VERSION_MAJOR >= 2
+	m.setFlag(video::EMF_USE_MIP_MAPS, false);
+#endif
+	auto &l = m.TextureLayer[0];
+	l.Texture = texture;
+	l.TextureWrapU = video::ETC_CLAMP_TO_EDGE;
+	l.TextureWrapV = video::ETC_CLAMP_TO_EDGE;
+}
+
 irr_ptr<scene::IMesh> createExtrusionMesh(video::ITexture *texture,
 		video::ITexture *overlay_texture)
 {
@@ -17,18 +33,16 @@ irr_ptr<scene::IMesh> createExtrusionMesh(video::ITexture *texture,
 	}
 	irr_ptr<scene::SMesh> mesh(new scene::SMesh(), Grab::already_owned);
 	irr_ptr<scene::SMeshBuffer> buf(new scene::SMeshBuffer(), Grab::already_owned);
-	buf->getMaterial().MaterialType = video::EMT_TRANSPARENT_ALPHA_CHANNEL_REF;
-	buf->getMaterial().setTexture(0, texture);
 	buf->append(extruded.vertices.data(), extruded.vertices.size(),
 			extruded.indices.data(), extruded.indices.size());
+	setupMaterial(buf->getMaterial(), texture);
 	mesh->addMeshBuffer(buf.get());
 	if (!overlay_texture)
 		return mesh;
 	buf.reset(new scene::SMeshBuffer(), Grab::already_owned);
-	buf->getMaterial().MaterialType = video::EMT_TRANSPARENT_ALPHA_CHANNEL_REF;
-	buf->getMaterial().setTexture(0, overlay_texture);
 	buf->append(extruded.overlay_vertices.data(), extruded.overlay_vertices.size(),
 			extruded.overlay_indices.data(), extruded.overlay_indices.size());
+	setupMaterial(buf->getMaterial(), overlay_texture);
 	mesh->addMeshBuffer(buf.get());
 	return mesh;
 }
