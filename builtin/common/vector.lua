21440ccfe5b099eaed0e87f7d68b07e7b12b7742
rubenwardy <rw@rubenwardy.com>
2018-01-05 18:24:38 +0000

    vector -> Vector, more tests

--- builtin/common/vector.lua
+++ builtin/common/vector.lua
@@ -1,92 +1,93 @@
 
-vector = {}
-vector.__index = vector
+Vector = {}
+Vector.__index = Vector
+vector = Vector
 
--- Create a new vector
+-- Create a new Vector
 --
--- vector.new(pos)
--- vector.new(x, y, z)
--- vector:new(pos)
--- vector:new(x, y, z)
-function vector:new(a, b, c)
+-- Vector.new(pos)
+-- Vector.new(x, y, z)
+-- Vector:new(pos)
+-- Vector:new(x, y, z)
+function Vector:new(a, b, c)
 	local obj = { x = 0, y = 0, z = 0 }
-	if self == vector then -- called using : or vector.new(vector, ...)
+	if self == Vector then -- called using : or Vector.new(Vector, ...)
 		if type(a) == "table" then
-			assert(a.x and a.y and a.z, "Invalid vector passed to vector:new()")
+			assert(a.x and a.y and a.z, "Invalid Vector passed to Vector:new()")
 			obj = { x = a.x, y = a.y, z = a.z }
 		elseif a then
-			assert(b and c, "Invalid arguments passed to vector:new()")
+			assert(b and c, "Invalid arguments passed to Vector:new()")
 			obj = { x = a, y = b, z = c }
 		end
-	elseif type(self) == "table" then -- vector.new(vec)
-		assert(self.x and self.y and self.z, "Invalid vector passed to vector.new()")
-		assert(a == nil and b == nil and c == nil, "Unexpected extra params passed to vector.new()")
+	elseif type(self) == "table" then -- Vector.new(vec)
+		assert(self.x and self.y and self.z, "Invalid Vector passed to Vector.new()")
+		assert(a == nil and b == nil and c == nil, "Unexpected extra params passed to Vector.new()")
 		obj = { x = self.x, y = self.y, z = self.z }
 	elseif self then
-		assert(a and b, "Invalid arguments for vector.new()")
-		assert(c == nil, "Unexpected extra params passed to vector.new()")
+		assert(a and b, "Invalid arguments for Vector.new()")
+		assert(c == nil, "Unexpected extra params passed to Vector.new()")
 		obj = { x = self, y = a, z = b }
 	end
 
-	setmetatable(obj, vector)
+	setmetatable(obj, Vector)
 	return obj
 end
 
-function vector:equals(other)
+function Vector:equals(other)
 	return self.x == other.x and
 	       self.y == other.y and
 	       self.z == other.z
 end
 
-function vector:length()
+function Vector:length()
 	return math.hypot(self.x, math.hypot(self.y, self.z))
 end
 
-function vector:normalize()
-	local len = vector.length(self)
+function Vector:normalize()
+	local len = Vector.length(self)
 	if len == 0 then
-		return vector:new()
+		return Vector:new()
 	else
-		return vector.divide(self, len)
+		return Vector.divide(self, len)
 	end
 end
 
-function vector:floor()
-	return vector:new(math.floor(self.x), math.floor(self.y), math.floor(self.z))
+function Vector:floor()
+	return Vector:new(math.floor(self.x), math.floor(self.y), math.floor(self.z))
 end
 
-function vector:round()
-	return vector:new(math.floor(self.x + 0.5), math.floor(self.y + 0.5), math.floor(self.z + 0.5))
+function Vector:round()
+	return Vector:new(math.floor(self.x + 0.5), math.floor(self.y + 0.5), math.floor(self.z + 0.5))
 end
 
-function vector:apply(func)
-	return vector:new(func(self.x), func(self.y), func(self.z))
+function Vector:apply(func)
+	return Vector:new(func(self.x), func(self.y), func(self.z))
 end
 
-function vector:distance(other)
+function Vector:distance(other)
 	local x = self.x - other.x
 	local y = self.y - other.y
 	local z = self.z - other.z
 	return math.hypot(x, math.hypot(y, z))
 end
 
-function vector:direction(other)
-	return vector.normalize({
+function Vector:direction(other)
+	return Vector.normalize({
 		x = self.x - other.x,
 		y = self.y - other.y,
 		z = self.z - other.z
 	})
 end
 
-function vector:add(other)
+function Vector:add(other)
 	if type(other) == "table" then
-		return vector:new({
+		return Vector:new({
 			x = self.x + other.x,
 			y = self.y + other.y,
 			z = self.z + other.z
 		})
 	else
-		return vector:new({
+		return Vector:new({
 			x = self.x + other,
 			y = self.y + other,
 			z = self.z + other
@@ -94,15 +95,15 @@ function vector:add(other)
 	end
 end
 
-function vector:subtract(other)
+function Vector:subtract(other)
 	if type(other) == "table" then
-		return vector:new({
+		return Vector:new({
 			x = self.x - other.x,
 			y = self.y - other.y,
 			z = self.z - other.z
 		})
 	else
-		return vector:new({
+		return Vector:new({
 			x = self.x - other,
 			y = self.y - other,
 			z = self.z - other
@@ -110,15 +111,15 @@ function vector:subtract(other)
 	end
 end
 
-function vector:multiply(other)
+function Vector:multiply(other)
 	if type(other) == "table" then
-		return vector:new({
+		return Vector:new({
 			x = self.x * other.x,
 			y = self.y * other.y,
 			z = self.z * other.z
 		})
 	else
-		return vector:new({
+		return Vector:new({
 			x = self.x * other,
 			y = self.y * other,
 			z = self.z * other
@@ -126,15 +127,15 @@ function vector:multiply(other)
 	end
 end
 
-function vector:divide(other)
+function Vector:divide(other)
 	if type(other) == "table" then
-		return vector:new({
+		return Vector:new({
 			x = self.x / other.x,
 			y = self.y / other.y,
 			z = self.z / other.z
 		})
 	else
-		return vector:new({
+		return Vector:new({
 			x = self.x / other,
 			y = self.y / other,
 			z = self.z / other
@@ -142,9 +143,9 @@ function vector:divide(other)
 	end
 end
 
-function vector.sort(a, b)
-	return vector:new({x = math.min(a.x, b.x), y = math.min(a.y, b.y), z = math.min(a.z, b.z)}),
-		vector:new({x = math.max(a.x, b.x), y = math.max(a.y, b.y), z = math.max(a.z, b.z)})
+function Vector.sort(a, b)
+	return Vector:new({x = math.min(a.x, b.x), y = math.min(a.y, b.y), z = math.min(a.z, b.z)}),
+		Vector:new({x = math.max(a.x, b.x), y = math.max(a.y, b.y), z = math.max(a.z, b.z)})
 end
 
 
@@ -152,10 +153,10 @@ end
 -- tests
 
 local function assertE(a, b, msg)
-	assert(vector.equals(a, b), msg)
+	assert(Vector.equals(a, b), msg)
 end
 local function assertNE(a, b, msg)
-	assert(not vector.equals(a, b), msg)
+	assert(not Vector.equals(a, b), msg)
 end
 
 assertE({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
@@ -164,11 +165,17 @@ local a = { x = 2, y = 4, z = -10 }
 assertE(a, a)
 assertNE({x = -1, y = 0, z = 1}, a)
 
-assertE(vector:new(), { x = 0, y = 0, z = 0 })
-assertE(vector:new(1, 2, 3), { x = 1, y = 2, z = 3 })
-assertE(vector:new({ x = 1, y = 2, z = 3}), { x = 1, y = 2, z = 3 })
-assert(vector:new().add)
-assertE(vector.new(), { x = 0, y = 0, z = 0 })
-assertE(vector.new(1, 2, 3), { x = 1, y = 2, z = 3 })
-assertE(vector.new({ x = 1, y = 2, z = 3}), { x = 1, y = 2, z = 3 })
-assertE(vector:new(vector:new()), { x = 0, y = 0, z = 0 })
+assertE(Vector:new(), { x = 0, y = 0, z = 0 })
+assertE(Vector:new(1, 2, 3), { x = 1, y = 2, z = 3 })
+assertE(Vector:new({ x = 1, y = 2, z = 3}), { x = 1, y = 2, z = 3 })
+assert(Vector:new().add)
+assertE(Vector.new(), { x = 0, y = 0, z = 0 })
+assertE(Vector.new(1, 2, 3), { x = 1, y = 2, z = 3 })
+assertE(Vector.new({ x = 1, y = 2, z = 3}), { x = 1, y = 2, z = 3 })
+assertE(Vector:new(Vector:new()), { x = 0, y = 0, z = 0 })
+
+local vec = Vector:new(1, 2, 3)
+assertE(vec, { x = 1, y = 2, z = 3 })
+assertE(vec:add({ x = 1, y = 2, z = 3 }), { x = 2, y = 4, z = 6 })
+assertE(vec, { x = 1, y = 2, z = 3 })
+
