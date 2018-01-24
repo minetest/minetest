2a26374a34bcc2c65e4ec2ad9fd2707d2d2f20cb
ThomasMonroe314 <ThomasMonroe@protonmail.com>
2018-01-24 14:01:54 -0600

    Change to image button

--- builtin/mainmenu/tab_online.lua
+++ builtin/mainmenu/tab_online.lua
@@ -33,9 +33,10 @@ local function get_formspec(tabview, name, tabdata)
 
 	local retval =
 		-- Search
-		"field[0.15,0.075;5.05,1;te_search;;"..core.formspec_escape(tabdata.search_for).."]"..
-		"button[4.845,-0.25;1.5,1;btn_mp_search;" .. fgettext("Search") .. "]" ..
-		"button[6.3,-0.25;1.5,1;btn_mp_refresh;" .. fgettext("Refresh") .. "]" ..
+		"field[0.15,0.075;5.91,1;te_search;;"..core.formspec_escape(tabdata.search_for).."]"..
+		"button[5.62,-0.25;1.5,1;btn_mp_search;" .. fgettext("Search") .. "]" ..
+		"image_button[6.97,-.165;.83,.83;" .. core.formspec_escape(defaulttexturedir .. "refresh.png") 
+			.. ";btn_mp_refresh;".. fgettext("") .."]" ..
 
 		-- Address / Port
 		"label[7.75,-0.25;" .. fgettext("Address / Port") .. "]" ..
