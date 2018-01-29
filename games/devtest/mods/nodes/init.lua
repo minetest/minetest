69b10a8c5a06fcf08243e699725e907be384ea71
number Zero <silverunicorn2011@yandex.ru>
2018-01-29 18:34:24 +0300

    Cleanup

--- games/devtest/mods/nodes/init.lua
+++ games/devtest/mods/nodes/init.lua
@@ -1,15 +1,25 @@
+local cube_tiles = {
+	{name="white_transparent.png^[colorize:#FF0000^arrow.png"},
+	{name="white_transparent.png^[colorize:#FFFF00^arrow.png"},
+	{name="white_transparent.png^[colorize:#00FF00^arrow.png"},
+	{name="white_transparent.png^[colorize:#00FFFF^arrow.png"},
+	{name="white_transparent.png^[colorize:#0000FF^arrow.png"},
+	{name="white_transparent.png^[colorize:#FF00FF^arrow.png"},
+}
+
+minetest.register_node("nodes:solid", {
+	description = "Solid",
+	drawtype = "normal",
+	paramtype2 = "facedir",
+	tiles = cube_tiles,
+	groups = {cracky=3},
+})
+
 minetest.register_node("nodes:glasslike", {
 	description = "Glass",
 	drawtype = "glasslike",
 	paramtype2 = "facedir",
-	tiles = {
-		{name="white_transparent.png^[colorize:#FF0000^arrow.png"},
-		{name="white_transparent.png^[colorize:#FFFF00^arrow.png"},
-		{name="white_transparent.png^[colorize:#00FF00^arrow.png"},
-		{name="white_transparent.png^[colorize:#00FFFF^arrow.png"},
-		{name="white_transparent.png^[colorize:#0000FF^arrow.png"},
-		{name="white_transparent.png^[colorize:#FF00FF^arrow.png"},
-	},
+	tiles = cube_tiles,
 	groups = {cracky=3},
 })
 
@@ -17,13 +27,20 @@ minetest.register_node("nodes:glasslike_framed", {
 	description = "Framed Glass",
 	drawtype = "glasslike_framed",
 	paramtype2 = "facedir",
-	tiles = {
-		{name="white_transparent.png^[colorize:#FF0000^arrow.png"},
-		{name="white_transparent.png^[colorize:#FFFF00^arrow.png"},
-		{name="white_transparent.png^[colorize:#00FF00^arrow.png"},
-		{name="white_transparent.png^[colorize:#00FFFF^arrow.png"},
-		{name="white_transparent.png^[colorize:#0000FF^arrow.png"},
-		{name="white_transparent.png^[colorize:#FF00FF^arrow.png"},
-	},
+	tiles = cube_tiles,
 	groups = {cracky=3},
 })
+
+--[[
+- `allfaces`
+- `allfaces_optional`
+- `torchlike`
+- `signlike`
+- `plantlike`
+- `firelike`
+- `fencelike`
+- `raillike`
+- `nodebox` -- See below
+- `mesh` -- Use models for nodes, see below
+- `plantlike_rooted` -- See below
+]]
