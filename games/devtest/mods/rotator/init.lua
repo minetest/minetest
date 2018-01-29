f4fef3b823aa853059b5d96159e50a8af7b994c6
number Zero <silverunicorn2011@yandex.ru>
2018-01-29 18:31:43 +0300

    Add node rotator

---
+++ games/devtest/mods/rotator/init.lua
@@ -0,0 +1,34 @@
+local function rotate(itemstack, user, pointed_thing, dir)
+	if pointed_thing.type ~= "node" then return end
+	local pos = pointed_thing.under
+	local node = minetest.get_node(pos)
+	local ndef = minetest.registered_nodes[node.name]
+	if not ndef then return end
+	if ndef.paramtype2 ~= "facedir" and ndef.paramtype2 ~= "colorfacedir" then return end
+
+	local facedir = node.param2 % 32
+	local data = node.param2 - facedir
+	local rotation = facedir % 4
+	local direction = (facedir - rotation) / 4
+	if dir then
+		direction = (direction + 1) % 6
+	else
+		rotation = (rotation + 1) % 4
+	end
+	facedir = 4 * direction + rotation
+	node.param2 = data + facedir
+	minetest.swap_node(pos, node)
+end
+
+minetest.register_craftitem("rotator:rotator", {
+	description = "Node rotator",
+	inventory_image = "rotator.png",
+	on_use = function(itemstack, user, pointed_thing)
+		rotate(itemstack, user, pointed_thing, false)
+		return itemstack
+	end,
+	on_place = function(itemstack, user, pointed_thing)
+		rotate(itemstack, user, pointed_thing, true)
+		return itemstack
+	end,
+})
