6912efffa3bf439454c1829dcd3c9baaca6eae15
number Zero <silverunicorn2011@yandex.ru>
2018-02-15 23:58:36 +0300

    Document it.

--- src/client/meshgen/extruder.h
+++ src/client/meshgen/extruder.h
@@ -34,9 +34,14 @@ struct ExtrudedMesh
 	explicit operator bool() const noexcept { return !vertices.empty(); }
 };
 
-/*
- * Handles conversion of textures to extruded meshes.
- * Each instance may only be used once.
+/** Handles conversion of textures to extruded meshes.
+ * @note Each instance may only be used once.
+ *
+ * Usage:
+ *
+ *     Extruder extruder(texture);
+ *     extruder.extrude();
+ *     extruded = extruder.takeMesh();
  */
 class Extruder
 {
@@ -48,19 +53,43 @@ class Extruder
 		Right,
 	};
 
+	/** Minimal pixel alpha for it to be considered opaque */
 	static constexpr int threshold = 128;
+
+	/** Extruded mesh thickness */
 	static constexpr float thickness = 0.1f;
+
 	video::ITexture *texture;
 	const video::SColor *data;
 	u32 w, h;
 	float dw, dh;
 	ExtrudedMesh mesh;
+
+	/** Flags controlling face creation.
+	 *
+	 * Array is indexed by #FaceDir. Vector is indexed by row (for horizontal faces)
+	 * or column (for vertical ones) number, starting from the upper-left corner.
+	 * Each entry is a *boolean* flag.
+	 * @note @c char is used as @c vector<bool> is optimized for space and not for speed.
+	 */
 	std::array<std::vector<char>, 4> faces;
 
 	video::SColor pixel(u32 i, u32 j);
 	bool isOpaque(u32 i, u32 j);
+
+	/** Marks the @p k -th face facing @p dir for creation.
+	 * May be called many times for the same face.
+	 */
 	void createEdgeFace(u32 k, FaceDir dir);
+
+	/** Actually creates a "long" face starting at pixel @p i, @p j, facing @p dir
+	 * @p i should be zero for horizontal faces; analogously for @p j.
+	 */
 	void addLongFace(u32 i, u32 j, FaceDir dir);
+
+	/** Creates the front (0) or back (1) quad.
+	 * @note These are the only faces generated for the overlay too.
+	 */
 	void addLargeFace(int id);
 
 public:
@@ -70,6 +99,29 @@ public:
 	~Extruder();
 	Extruder &operator=(const Extruder &) = delete;
 	Extruder &operator=(Extruder &&) = delete;
+
+	/** Does the actual work.
+	 *
+	 * @note This function treats each pixel as either transparent or opaque,
+	 * according to the #threshold.
+	 *
+	 * Algorithm:
+	 *
+	 * 1. Scan the texture top-to-bottom. For each row, scan it, left-to-right.
+	 * If there is a transparent-to-opaque transition, mark the corresponding
+	 * face (left-facing) for creation. If there is an opaque-to-transparent
+	 * transition, mark the right-facing one for creation.
+	 *
+	 * 2. The same for vertical faces (left-to-right, then top-to-bottom).
+	 *
+	 * 3. Create front and back faces using #addLargeFace.
+	 *
+	 * 4. Now actually create all marked faces using #addLongFace.
+	 */
 	void extrude();
+
+	/** Returns the generated mesh.
+	 * @note After calling this, the Extruder is no more usable.
+	 */
 	ExtrudedMesh takeMesh() noexcept;
 };
