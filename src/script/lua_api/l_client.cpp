7d2d6480181a0d0b62edfcb8f216e792474cba97
nOOb3167 <nOOb3167@gmail.com>
2018-03-05 01:06:30 +0100

    sound offset propagate offsets from Lua APIs downwards to ISoundManager

--- src/script/lua_api/l_client.cpp
+++ src/script/lua_api/l_client.cpp
@@ -242,13 +242,16 @@ int ModApiClient::l_sound_play(lua_State *L)
 			v3f pos = read_v3f(L, -1) * BS;
 			lua_pop(L, 1);
 			handle = sound->playSoundAt(
-					spec.name, looped, gain * spec.gain, pos, pitch);
+					spec.name, looped, gain * spec.gain, pos, pitch,
+					spec.offset_start, spec.offset_end);
 			lua_pushinteger(L, handle);
 			return 1;
 		}
 	}
 
-	handle = sound->playSound(spec.name, looped, gain * spec.gain, 0.0f, pitch);
+	handle = sound->playSound(
+			spec.name, looped, gain * spec.gain, 0.0f, pitch,
+			spec.offset_start, spec.offset_end);
 	lua_pushinteger(L, handle);
 
 	return 1;
