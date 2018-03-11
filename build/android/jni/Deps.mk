69247ca2233d1b2e77485b8d32aca41c7e525e74
Loic Blot <loic.blot@unix-experience.fr>
2018-03-11 16:52:53 +0100

    Android: update tools (gradle, NDK) & prepare to switch to clang

--- build/android/jni/Deps.mk
+++ build/android/jni/Deps.mk
@@ -1,7 +1,7 @@
 APP_PLATFORM := ${APP_PLATFORM}
 APP_ABI := ${TARGET_ABI}
-APP_STL := gnustl_static
-NDK_TOOLCHAIN_VERSION := 4.9
+APP_STL := c++_shared
+NDK_TOOLCHAIN_VERSION := ${COMPILER_VERSION}
 APP_DEPRECATED_HEADERS := true
 
 APP_CLAFGS += -mfloat-abi=softfp -mfpu=vfpv3 -O3
