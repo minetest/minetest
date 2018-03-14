ddac259304cde5c063308bf098f0fb889efbc147
Loic Blot <loic.blot@unix-experience.fr>
2018-03-14 21:56:14 +0100

    Windows compat

--- src/unittest/test_servermodmanager.cpp
+++ src/unittest/test_servermodmanager.cpp
@@ -46,7 +46,11 @@ static TestServerModManager g_test_instance;
 
 void TestServerModManager::runTests(IGameDef *gamedef)
 {
+#if defined(_MSC_VER)
+	_putenv(("MINETEST_SUBGAME_PATH=" + TEST_SUBGAME_PATH).c_str());
+#else
 	setenv("MINETEST_SUBGAME_PATH", TEST_SUBGAME_PATH, 1);
+#endif
 
 	TEST(testCreation);
 	TEST(testIsConsistent);
