51ef2ebeebb36afbb0ace55618575cdbb72ef18d
paramat <paramat@users.noreply.github.com>
2018-03-17 22:05:21 +0000

    Minetest ASCII art: Move from actionstream to rawstream

--- src/server.cpp
+++ src/server.cpp
@@ -366,7 +366,7 @@ void Server::start()
 	m_thread->start();
 
 	// ASCII art for the win!
-	actionstream
+	rawstream
 		<< "        .__               __                   __   " << std::endl
 		<< "  _____ |__| ____   _____/  |_  ____   _______/  |_ " << std::endl
 		<< " /     \\|  |/    \\_/ __ \\   __\\/ __ \\ /  ___/\\   __\\" << std::endl
