305d4020adbfee06a5e487ce19289b3cebe77707
rubenwardy <rw@rubenwardy.com>
2018-03-25 18:50:20 +0100

    Remove test_config

--- src/test_config.h
+++
@@ -1,6 +0,0 @@
-// Filled in by the build system
-
-#pragma once
-
-#define TEST_WORLDDIR "/home/ruben/dev/minetest/src/unittest/test_world"
-#define TEST_SUBGAME_PATH "/home/ruben/dev/minetest/src/unittest/../../games/minimal"
