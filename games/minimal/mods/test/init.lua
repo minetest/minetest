db91f909bfae38a9d698c24549dcb3699bb0c575
rubenwardy <rw@rubenwardy.com>
2018-03-27 15:10:32 +0100

    Add tests to minimal

--- games/minimal/mods/test/init.lua
+++ games/minimal/mods/test/init.lua
@@ -9,3 +9,32 @@ assert(pseudo:next() == 22290)
 assert(pseudo:next() == 13854)
 
 
+--
+-- HP Change Reasons
+--
+local expect = nil
+minetest.register_on_joinplayer(function(player)
+	expect = { type = "set_hp", from = "mod" }
+	player:set_hp(3)
+	assert(expect == nil)
+
+	expect = { a = 234, type = "set_hp", from = "mod" }
+	player:set_hp(10, { a= 234 })
+	assert(expect == nil)
+
+	expect = { df = 3458973454, type = "fall", from = "mod" }
+	player:set_hp(10, { type = "fall", df = 3458973454 })
+	assert(expect == nil)
+end)
+
+minetest.register_on_player_hpchange(function(player, hp, reason)
+	for key, value in pairs(reason) do
+		assert(expect[key] == value)
+	end
+
+	for key, value in pairs(expect) do
+		assert(reason[key] == value)
+	end
+
+	expect = nil
+end)
