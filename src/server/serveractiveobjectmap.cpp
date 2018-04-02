76402d0ed92e3f01e5d5ed9666f642832f224f99
number Zero <silverunicorn2011@yandex.ru>
2018-04-03 01:47:05 +0300

    Fix dates =)

--- src/server/serveractiveobjectmap.cpp
+++ src/server/serveractiveobjectmap.cpp
@@ -1,6 +1,6 @@
 /*
 Minetest
-Copyright (C) 2010-2017 numZero, Lobachevsky Vitaly <numzer0@yandex.com>
+Copyright (C) 2018 numZero, Lobachevsky Vitaly <numzer0@yandex.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
