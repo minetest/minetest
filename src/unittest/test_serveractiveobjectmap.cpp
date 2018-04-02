90e5140879d3927d778d4decb610b6d28b6a10e4
rubenwardy <rubenwardy@gmail.com>
2018-04-02 15:46:16 +0100

    Add more tests

--- src/unittest/test_serveractiveobjectmap.cpp
+++ src/unittest/test_serveractiveobjectmap.cpp
@@ -20,6 +20,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "test.h"
 
 #include "server/serveractiveobjectmap.h"
+#include "content_sao.h"
 
 class TestServerActiveObjectMap : public TestBase
 {
@@ -55,12 +56,38 @@ void TestServerActiveObjectMap::runTests(IGameDef *gamedef)
 
 void TestServerActiveObjectMap::testAddObject()
 {
-	// @TODO
+	ServerActiveObjectMap saom;
+	UASSERT(saom.getObjects().empty());
+
+	LuaEntitySAO ob1(nullptr, v3f(0,0,0), "", "");
+	saom.addObject(&ob1);
+
+	UASSERT(saom.getObjects().size() == 1);
+	bool found = false;
+	for (const auto &pair : saom.getObjects()) {
+		UASSERT(pair.second.object == &ob1);
+		found = true;
+	}
+	UASSERT(found);
 }
 
 void TestServerActiveObjectMap::testRemoveObject()
 {
-	// @TODO
+	ServerActiveObjectMap saom;
+	UASSERT(saom.getObjects().empty());
+
+	LuaEntitySAO ob1(nullptr, v3f(0,0,0), "", "");
+	saom.addObject(&ob1);
+
+	UASSERT(saom.getObjects().size() == 1);
+	bool found = false;
+	for (const auto &pair : saom.getObjects()) {
+		UASSERT(pair.second.object == &ob1);
+		found = true;
+	}
+	UASSERT(found);
+
+	saom.removeObject(&ob1);
 }
 
 void TestServerActiveObjectMap::testUpdateObject()
@@ -76,7 +103,9 @@ void TestServerActiveObjectMap::testGetObject()
 void TestServerActiveObjectMap::testIsFreeID()
 {
 	ServerActiveObjectMap saom;
-	saom.isFreeId(0);
+	UASSERT(!saom.isFreeId(0));
+	UASSERT(saom.isFreeId(1));
+	UASSERT(saom.isFreeId(2));
 }
 
 void TestServerActiveObjectMap::testGetFreeID()
@@ -89,7 +118,30 @@ void TestServerActiveObjectMap::testGetFreeID()
 
 void TestServerActiveObjectMap::testGetObjectsInsideRadius()
 {
-	// @TODO
+	ServerActiveObjectMap saom;
+
+	auto results = saom.getObjectsInsideRadius(v3f(0,0,0), 5);
+	UASSERT(results.empty());
+
+	LuaEntitySAO ob1(nullptr, v3f(0,0,0), "", "");
+	saom.addObject(&ob1);
+	UASSERT(results.size() == 1);
+
+	LuaEntitySAO ob2(nullptr, v3f(-1,-1,-1), "", "");
+	saom.addObject(&ob2);
+	UASSERT(results.size() == 2);
+
+	LuaEntitySAO ob3(nullptr, v3f(4.9,0,0), "", "");
+	saom.addObject(&ob3);
+	UASSERT(results.size() == 3);
+
+	LuaEntitySAO ob4(nullptr, v3f(5,0,0), "", "");
+	saom.addObject(&ob4);
+	UASSERT(results.size() == 3);
+
+	LuaEntitySAO ob5(nullptr, v3f(3,3,3), "", "");
+	saom.addObject(&ob5);
+	UASSERT(results.size() == 3);
 }
 
 void TestServerActiveObjectMap::testGetObjectsTouchingBox()
