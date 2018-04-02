88883a57812107f0e746bb4d8b9cbef68a826193
Loic Blot <loic.blot@unix-experience.fr>
2018-04-02 20:20:33 +0200

    Another fix

--- src/mapgen/dungeongen.cpp
+++ src/mapgen/dungeongen.cpp
@@ -19,6 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #include "dungeongen.h"
+#include <cmath>
 #include "mapgen.h"
 #include "voxel.h"
 #include "noise.h"
