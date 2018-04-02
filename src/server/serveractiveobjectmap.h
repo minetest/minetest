5c9d1cbff83d136f2d3c10c0612dda727d5f20b8
number Zero <silverunicorn2011@yandex.ru>
2018-04-03 01:26:00 +0300

    Document the public API of the SAOM

--- src/server/serveractiveobjectmap.h
+++ src/server/serveractiveobjectmap.h
@@ -35,20 +35,92 @@ struct ServerActiveObjectMap
 		bool has_box;
 	};
 
+	/*!
+	 * Adds object to the map. It must have valid ID.
+	 *
+	 * If an object with the same ID already exists in the map,
+	 * std::logic_error is thrown.
+	 */
 	void addObject(ServerActiveObject *object);
+
+	/*!
+	 * Removes object from the map. The pointer must be valid.
+	 * See `removeObject(u16)` for details.
+	 */
 	void removeObject(ServerActiveObject *object);
+
+	/*!
+	 * Removes object from the map.
+	 *
+	 * If the object is not found, the call is ignored.
+	 * The function never throws, unless the underlying container throws.
+	 */
 	ServerActiveObject *removeObject(u16 id);
+
+	/*!
+	 * Updates object metadata stored in the map.
+	 * See `updateObject(u16)` for details.
+	 */
 	void updateObject(ServerActiveObject *object);
+
+	/*!
+	 * Updates object metadata stored in the map.
+	 *
+	 * The metadata includes (approximate) absolute collision box and
+	 * its existence (`physical` property for Lua entities).
+	 * This function must be called after each change of these properties,
+	 * including each object movement.
+	 */
 	void updateObject(u16 id);
+
+	/*!
+	 * Returns the object with given ID, if any.
+	 * Returns NULL otherwise.
+	 */
 	ServerActiveObject *getObject(u16 id) const;
 
+	/*!
+	 * Checks if the given ID is free and valid (i.e. non-zero).
+	 */
 	bool isFreeId(u16 id);
+
+	/*!
+	 * Returns a free ID, if any. Returns 0 in the case of failure.
+	 *
+	 * @note This function doesn't reserve the ID; it remains free until
+	 * an object with that ID is added.
+	 * @note This function tries to reclaim freed IDs as late as possible.
+	 * However, there is no guarantee.
+	 */
 	u16 getFreeId();
+
+	/*!
+	 * Returns a list of objects whose base position is at distance less
+	 * than @p radius from @p pos.
+	 *
+	 * @note Due to inexact nature of floating-point computations, it is
+	 * undefined whether an object lying exactly at the boundary is included
+	 * in the list or not.
+	 */
 	std::vector<u16> getObjectsInsideRadius(v3f pos, float radius);
+
+	/*!
+	 * Returns a list of objects whose collision box intersects with @p box
+	 *
+	 * @note Due to inexact nature of floating-point computations, it is
+	 * undefined whether an object lying exactly at the boundary is included
+	 * in the list or not.
+	 */
 	std::vector<u16> getObjectsTouchingBox(const aabb3f &box);
 
+	/*!
+	 * Returns count of objects in the map.
+	 */
 	std::size_t size() const { return objects.size(); }
 
+	/*!
+	 * Returns reference to the underlying conatiner.
+	 */
 	const std::unordered_map<u16, Wrapper> &getObjects() const { return objects; }
 
 private:
