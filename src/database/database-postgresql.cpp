4e1e4b6715e821941909bd1124a1d81ca9691271
rubenwardy <rw@rubenwardy.com>
2018-04-05 11:06:26 +0100

    Set unmodified after saving

--- src/database/database-postgresql.cpp
+++ src/database/database-postgresql.cpp
@@ -527,6 +527,7 @@ void PlayerDatabasePostgreSQL::savePlayer(RemotePlayer *player)
 		};
 		execPrepared("save_player_metadata", 3, meta_values);
 	}
+    sao->getMeta().setModified(false);
 	endSave();
 }
 
