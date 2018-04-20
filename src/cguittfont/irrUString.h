10300de78fc67b5e68f980501302a3c2b1336652
mazocomp <mazocomp@mazocomp.pc>
2018-04-20 03:10:03 +0300

    ...

--- src/cguittfont/irrUString.h
+++ src/cguittfont/irrUString.h
@@ -31,7 +31,7 @@
 #ifndef __IRR_USTRING_H_INCLUDED__
 #define __IRR_USTRING_H_INCLUDED__
 
-#if (__cplusplus > 199711L) || (_MSC_VER >= 1600) || defined(__GXX_EXPERIMENTAL_CXX0X__)
+#if (__cplusplus > 199711L) || (defined(_MSC_VER) && _MSC_VER >= 1600) || defined(__GXX_EXPERIMENTAL_CXX0X__)
 #	define USTRING_CPP0X
 #	if defined(__GXX_EXPERIMENTAL_CXX0X__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5)))
 #		define USTRING_CPP0X_NEWLITERALS
