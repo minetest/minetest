0268c9d7c9925daeeb2ac847aca090de635ba43c
mazocomp <33579456+mazocomp@users.noreply.github.com>
2018-04-21 14:07:25 +0300

    "static constexpr v3s16 light_dirs[8]" fails to compile, sync it with master! (#7261)

--- src/content_mapblock.cpp
+++ src/content_mapblock.cpp
@@ -43,7 +43,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 // Corresponding offsets are listed in g_27dirs
 #define FRAMED_NEIGHBOR_COUNT 18
 
-static constexpr v3s16 light_dirs[8] = {
+static const v3s16 light_dirs[8] = {
 	v3s16(-1, -1, -1),
 	v3s16(-1, -1,  1),
 	v3s16(-1,  1, -1),
