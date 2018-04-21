minetest/minetest

commit 87ad4d8e7f25210cd28d9f2b372aa00aa3dab929

Add online content repository
Replaces mods and texture pack tabs with a single content tab

build/android/jni/Android.mk
@@ -130,6 +130,9 @@ LOCAL_SRC_FILES := \
 		jni/src/content_mapnode.cpp               \
 		jni/src/content_nodemeta.cpp              \
 		jni/src/content_sao.cpp                   \
+		jni/src/content/contentdb.cpp             \
+		jni/src/content/mods.cpp                  \
+		jni/src/content/subgames.cpp              \
 		jni/src/convert_json.cpp                  \
 		jni/src/craftdef.cpp                      \
 		jni/src/database/database-dummy.cpp       \
@@ -198,7 +201,6 @@ LOCAL_SRC_FILES := \
 		jni/src/mapgen/mg_ore.cpp                 \
 		jni/src/mapgen/mg_schematic.cpp           \
 		jni/src/minimap.cpp                       \
-		jni/src/mods.cpp                          \
 		jni/src/modchannels.cpp                   \
 		jni/src/nameidmapping.cpp                 \
 		jni/src/nodedef.cpp                       \
@@ -229,7 +231,6 @@ LOCAL_SRC_FILES := \
 		jni/src/shader.cpp                        \
 		jni/src/sky.cpp                           \
 		jni/src/staticobject.cpp                  \
-		jni/src/subgame.cpp                       \
 		jni/src/tileanimation.cpp                 \
 		jni/src/translation.cpp                   \
 		jni/src/tool.cpp                          \

builtin/common/filterlist.lua
@@ -47,17 +47,17 @@ function filterlist.create(raw_fct,compare_fct,uid_match_fct,filter_fct,fetch_pa
 
 	assert((raw_fct ~= nil) and (type(raw_fct) == "function"))
 	assert((compare_fct ~= nil) and (type(compare_fct) == "function"))
-	
+
 	local self = {}
-	
+
 	self.m_raw_list_fct  = raw_fct
 	self.m_compare_fct   = compare_fct
 	self.m_filter_fct    = filter_fct
 	self.m_uid_match_fct = uid_match_fct
-	
+
 	self.m_filtercriteria = nil
 	self.m_fetch_param = fetch_param
-	
+
 	self.m_sortmode = "none"
 	self.m_sort_list = {}
 
@@ -79,7 +79,7 @@ function filterlist.create(raw_fct,compare_fct,uid_match_fct,filter_fct,fetch_pa
 	self.refresh            = filterlist.refresh
 
 	filterlist.process(self)
-	
+
 	return self
 end
 
@@ -128,49 +128,49 @@ function filterlist.get_raw_element(self,idx)
 	if type(idx) ~= "number" then
 		idx = tonumber(idx)
 	end
-	
+
 	if idx ~= nil and idx > 0 and idx <= #self.m_raw_list then
 		return self.m_raw_list[idx]
 	end
-	
+
 	return nil
 end
 
 --------------------------------------------------------------------------------
 function filterlist.get_raw_index(self,listindex)
 	assert(self.m_processed_list ~= nil)
-	
+
 	if listindex ~= nil and listindex > 0 and
 		listindex <= #self.m_processed_list then
 		local entry = self.m_processed_list[listindex]
-		
+
 		for i,v in ipairs(self.m_raw_list) do
-		
+
 			if self.m_compare_fct(v,entry) then
 				return i
 			end
 		end
 	end
-	
+
 	return 0
 end
 
 --------------------------------------------------------------------------------
 function filterlist.get_current_index(self,listindex)
 	assert(self.m_processed_list ~= nil)
-	
+
 	if listindex ~= nil and listindex > 0 and
 		listindex <= #self.m_raw_list then
 		local entry = self.m_raw_list[listindex]
-		
+
 		for i,v in ipairs(self.m_processed_list) do
-		
+
 			if self.m_compare_fct(v,entry) then
 				return i
 			end
 		end
 	end
-	
+
 	return 0
 end
 
@@ -183,23 +183,23 @@ function filterlist.process(self)
 		self.m_processed_list = self.m_raw_list
 		return
 	end
-	
+
 	self.m_processed_list = {}
-	
+
 	for k,v in pairs(self.m_raw_list) do
 		if self.m_filtercriteria == nil or
 			self.m_filter_fct(v,self.m_filtercriteria) then
 			self.m_processed_list[#self.m_processed_list + 1] = v
 		end
 	end
-	
+
 	if self.m_sortmode == "none" then
 		return
 	end
-	
+
 	if self.m_sort_list[self.m_sortmode] ~= nil and
 		type(self.m_sort_list[self.m_sortmode]) == "function" then
-		
+
 		self.m_sort_list[self.m_sortmode](self)
 	end
 end
@@ -209,7 +209,7 @@ function filterlist.size(self)
 	if self.m_processed_list == nil then
 		return 0
 	end
-	
+
 	return #self.m_processed_list
 end
 
@@ -233,8 +233,8 @@ function filterlist.raw_index_by_uid(self, uid)
 			elementidx = i
 		end
 	end
-	
-	
+
+
 	-- If there are more elements than one with same name uid can't decide which
 	-- one is meant. self shouldn't be possible but just for sure.
 	if elementcount > 1 then
@@ -254,11 +254,11 @@ function compare_worlds(world1,world2)
 	if world1.path ~= world2.path then
 		return false
 	end
-	
+
 	if world1.name ~= world2.name then
 		return false
 	end
-	
+
 	if world1.gameid ~= world2.gameid then
 		return false
 	end
@@ -288,11 +288,11 @@ function sort_mod_list(self)
 
 	table.sort(self.m_processed_list, function(a, b)
 		-- Show game mods at bottom
-		if a.typ ~= b.typ then
-			if b.typ == "game" then	
-				return a.typ ~= "game_mod"
+		if a.type ~= b.type or a.loc ~= b.loc then
+			if b.type == "game" then
+				return a.loc ~= "game"
 			end
-			return b.typ == "game_mod"
+			return b.loc == "game"
 		end
 		-- If in same or no modpack, sort by name
 		if a.modpack == b.modpack then
@@ -308,7 +308,7 @@ function sort_mod_list(self)
 			elseif b.name == a.modpack then
 				return false
 			end
-			
+
 			local name_a = a.modpack or a.name
 			local name_b = b.modpack or b.name
 			if name_a:lower() == name_b:lower() then

builtin/common/misc_helpers.lua
@@ -551,6 +551,16 @@ function table.copy(t, seen)
 	end
 	return n
 end
+
+
+function table.insert_all(t, other)
+	for i=1, #other do
+		t[#t + 1] = other[i]
+	end
+	return t
+end
+
+
 --------------------------------------------------------------------------------
 -- mainmenu only functions
 --------------------------------------------------------------------------------

builtin/mainmenu/common.lua
@@ -41,7 +41,7 @@ local function render_client_count(n)
 end
 
 local function configure_selected_world_params(idx)
-	local worldconfig = modmgr.get_worldconfig(menudata.worldlist:get_list()[idx].path)
+	local worldconfig = pkgmgr.get_worldconfig(menudata.worldlist:get_list()[idx].path)
 	if worldconfig.creative_mode then
 		core.settings:set("creative_mode", worldconfig.creative_mode)
 	end

builtin/mainmenu/dlg_config_world.lua
@@ -35,7 +35,7 @@ local function get_formspec(data)
 		mod = {name=""}
 	end
 
-	local hard_deps, soft_deps = modmgr.get_dependencies(mod.path)
+	local hard_deps, soft_deps = pkgmgr.get_dependencies(mod.path)
 
 	retval = retval ..
 		"label[0,0.7;" .. fgettext("Mod:") .. "]" ..
@@ -88,7 +88,7 @@ local function get_formspec(data)
 	retval = retval ..
 		"tablecolumns[color;tree;text]" ..
 		"table[5.5,0.75;5.75,6;world_config_modlist;"
-	retval = retval .. modmgr.render_modlist(data.list)
+	retval = retval .. pkgmgr.render_packagelist(data.list)
 	retval = retval .. ";" .. data.selected_mod .."]"
 
 	return retval
@@ -237,7 +237,7 @@ function create_configure_world_dlg(worldidx)
 	dlg.data.worldspec = core.get_worlds()[worldidx]
 	if dlg.data.worldspec == nil then dlg:delete() return nil end
 
-	dlg.data.worldconfig = modmgr.get_worldconfig(dlg.data.worldspec.path)
+	dlg.data.worldconfig = pkgmgr.get_worldconfig(dlg.data.worldspec.path)
 
 	if dlg.data.worldconfig == nil or dlg.data.worldconfig.id == nil or
 			dlg.data.worldconfig.id == "" then
@@ -247,8 +247,8 @@ function create_configure_world_dlg(worldidx)
 	end
 
 	dlg.data.list = filterlist.create(
-			modmgr.preparemodlist, --refresh
-			modmgr.comparemod, --compare
+			pkgmgr.preparemodlist, --refresh
+			pkgmgr.comparemod, --compare
 			function(element,uid) --uid match
 					if element.name == uid then
 						return true

builtin/mainmenu/dlg_contentstore.lua
@@ -1,0 +1,451 @@
+--Minetest
+--Copyright (C) 2018 rubenwardy
+--
+--This program is free software; you can redistribute it and/or modify
+--it under the terms of the GNU Lesser General Public License as published by
+--the Free Software Foundation; either version 2.1 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Lesser General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public License along
+--with this program; if not, write to the Free Software Foundation, Inc.,
+--51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+local function download_package(param)
+	if core.download_file(param.package.url, param.filename) then
+		return {
+			package = param.package,
+			filename = param.filename,
+			successful = true,
+		}
+	else
+		core.log("error", "downloading " .. dump(param.package.url) .. " failed")
+		return {
+			package = param.package,
+			successful = false,
+		}
+	end
+end
+
+local function start_install(calling_dialog, package)
+	local params = {
+		package = package,
+		filename = os.tempfolder() .. "_MODNAME_" .. package.name .. ".zip",
+	}
+
+	local function callback(result)
+		if result.successful then
+			local path, msg = pkgmgr.install(result.package.type, result.filename, result.package.name)
+			if not path then
+				gamedata.errormessage = msg
+			else
+				local conf_path
+				local name_is_title = false
+				if result.package.type == "mod" then
+					conf_path = path .. DIR_DELIM .. "mod.conf"
+				elseif result.package.type == "game" then
+					conf_path = path .. DIR_DELIM .. "game.conf"
+					name_is_title = true
+				elseif result.package.type == "txp" then
+					conf_path = path .. DIR_DELIM .. "texture_pack.conf"
+				end
+
+				if conf_path then
+					local conf = Settings(conf_path)
+					local function set_def(key, value)
+						if conf:get(key) == nil then
+							conf:set(key, value)
+						end
+					end
+					if name_is_title then
+						set_def("name",    result.package.title)
+					else
+						set_def("title",   result.package.title)
+						set_def("name",    result.package.name)
+					end
+					set_def("description", result.package.short_description)
+					set_def("author",      result.package.author)
+					conf:write()
+				end
+			end
+			os.remove(result.filename)
+		else
+			gamedata.errormessage = fgettext("Failed to download $1", package.name)
+		end
+
+		if gamedata.errormessage == nil then
+			core.button_handler({btn_hidden_close_download=result})
+		else
+			core.button_handler({btn_hidden_close_download={successful=false}})
+		end
+	end
+
+	if not core.handle_async(download_package, params, callback) then
+		minetest.log("error", "ERROR: async event failed")
+		gamedata.errormessage = fgettext("Failed to download $1", package.name)
+	end
+
+	local new_dlg = dialog_create("store_downloading",
+		function(data)
+			return "size[7,2]label[0.25,0.75;" ..
+				fgettext("Downloading and installing $1, please wait...", data.title) .. "]"
+		end,
+		function(this,fields)
+			if fields["btn_hidden_close_download"] ~= nil then
+				this:delete()
+				return true
+			end
+
+			return false
+		end,
+		nil)
+
+	new_dlg:set_parent(calling_dialog)
+	new_dlg.data.title = package.title
+	calling_dialog:hide()
+	new_dlg:show()
+end
+
+
+local package_dialog = {}
+
+function package_dialog.get_formspec()
+	local package = package_dialog.package
+
+	local formspec = {
+		"size[8,4;true]",
+		"label[2.5,0.2;", core.formspec_escape(package.title), "]",
+		"label[0,1;", core.formspec_escape(package.short_description), "]",
+		"button[0,0;2,1;back;", fgettext("Back"), "]",
+		"button[6,0;2,1;install;", fgettext("Install"), "]",
+	}
+
+	-- TODO: screenshots
+
+	return table.concat(formspec, "")
+end
+
+function package_dialog.handle_submit(this, fields, tabname, tabdata)
+	if fields.back then
+		this:delete()
+		return true
+	end
+
+	if fields.install then
+		start_install(package_dialog.package)
+		return true
+	end
+
+	return false
+end
+
+function package_dialog.create(package)
+	package_dialog.package = package
+	return dialog_create("package_view",
+		package_dialog.get_formspec,
+		package_dialog.handle_submit,
+		nil)
+end
+
+
+
+
+local store = {}
+local search_string = ""
+local cur_page = 1
+local num_per_page = 5
+local filter_type = 1
+local filter_types_titles = {
+	fgettext("All packages"),
+	fgettext("Games"),
+	fgettext("Mods"),
+	fgettext("Texture packs"),
+}
+
+local filter_types_type = {
+	nil,
+	"game",
+	"mod",
+	"txp",
+}
+
+function store.load()
+	store.packages_full = core.get_package_list()
+	store.packages = store.packages_full
+	store.loaded = true
+end
+
+function store.update_paths()
+	local mod_hash = {}
+	pkgmgr.refresh_globals()
+	for _, mod in pairs(pkgmgr.global_mods:get_list()) do
+		mod_hash[mod.name] = mod
+	end
+
+	local game_hash = {}
+	pkgmgr.update_gamelist()
+	for _, game in pairs(pkgmgr.games) do
+		game_hash[game.id] = game
+	end
+
+	local txp_hash = {}
+	for _, txp in pairs(pkgmgr.get_texture_packs()) do
+		txp_hash[txp.name] = txp
+	end
+
+	for _, package in pairs(store.packages_full) do
+		local content
+		if package.type == "mod" then
+			content = mod_hash[package.name]
+		elseif package.type == "game" then
+			content = game_hash[package.name]
+		elseif package.type == "txp" then
+			content = txp_hash[package.name]
+		end
+
+		if content and content.author == package.author then
+			package.path = content.path
+		else
+			package.path = nil
+		end
+	end
+end
+
+function store.filter_packages(query)
+	if query == "" and filter_type == 1 then
+		store.packages = store.packages_full
+		return
+	end
+
+	local keywords = {}
+	for word in query:lower():gmatch("%S+") do
+		table.insert(keywords, word)
+	end
+
+	local function matches_keywords(package, keywords)
+		for k = 1, #keywords do
+			local keyword = keywords[k]
+
+			if string.find(package.name:lower(), keyword, 1, true) or
+					string.find(package.title:lower(), keyword, 1, true) or
+					string.find(package.author:lower(), keyword, 1, true) or
+					string.find(package.short_description:lower(), keyword, 1, true) then
+				return true
+			end
+		end
+
+		return false
+	end
+
+	store.packages = {}
+	for _, package in pairs(store.packages_full) do
+		if (query == "" or matches_keywords(package, keywords)) and
+				(filter_type == 1 or package.type == filter_types_type[filter_type]) then
+			store.packages[#store.packages + 1] = package
+		end
+	end
+
+end
+
+function store.get_formspec()
+	assert(store.loaded)
+
+	store.update_paths()
+
+	local pages = math.ceil(#store.packages / num_per_page)
+	if cur_page > pages then
+		cur_page = 1
+	end
+
+	local formspec = {
+		"size[12,6.5;true]",
+		"field[0.3,0.1;10.2,1;search_string;;", core.formspec_escape(search_string), "]",
+		"field_close_on_enter[search_string;false]",
+		"button[10.2,-0.2;2,1;search;", fgettext("Search"), "]",
+		"dropdown[0,1;2.4;type;",
+		table.concat(filter_types_titles, ","),
+		";",
+		filter_type,
+		"]",
+		-- "textlist[0,1;2.4,5.6;a;",
+		-- table.concat(taglist, ","),
+		-- "]"
+	}
+
+	local start_idx = (cur_page - 1) * num_per_page + 1
+	for i=start_idx, math.min(#store.packages, start_idx+num_per_page-1) do
+		local package = store.packages[i]
+		formspec[#formspec + 1] = "container[3,"
+		formspec[#formspec + 1] = i - start_idx + 1
+		formspec[#formspec + 1] = "]"
+
+		-- image
+		formspec[#formspec + 1] = "image[-0.4,0;1.5,1;"
+		formspec[#formspec + 1] = defaulttexturedir
+		formspec[#formspec + 1] = "no_screenshot.png"
+		formspec[#formspec + 1] = "]"
+
+		-- title
+		formspec[#formspec + 1] = "label[1,0;"
+		formspec[#formspec + 1] = core.formspec_escape(package.title ..
+				" by " .. package.author)
+		formspec[#formspec + 1] = "]"
+
+		-- description
+		local short = package.short_description
+		if #short > 60 then
+			short = short:sub(1, 59) .. "…"
+		end
+		formspec[#formspec + 1] = "label[1,0.3;"
+		formspec[#formspec + 1] = core.formspec_escape(short)
+		formspec[#formspec + 1] = "]"
+
+		-- buttons
+		if package.path then
+			formspec[#formspec + 1] = "button[6,0;1.5,1;uninstall_"
+			formspec[#formspec + 1] = tostring(i)
+			formspec[#formspec + 1] = ";"
+			formspec[#formspec + 1] = fgettext("Uninstall")
+			formspec[#formspec + 1] = "]"
+		else
+			formspec[#formspec + 1] = "button[6,0;1.5,1;install_"
+			formspec[#formspec + 1] = tostring(i)
+			formspec[#formspec + 1] = ";"
+			formspec[#formspec + 1] = fgettext("Install")
+			formspec[#formspec + 1] = "]"
+		end
+		formspec[#formspec + 1] = "button[7.5,0;1.5,1;view_"
+		formspec[#formspec + 1] = tostring(i)
+		formspec[#formspec + 1] = ";"
+		formspec[#formspec + 1] = fgettext("View")
+		formspec[#formspec + 1] = "]"
+
+		formspec[#formspec + 1] = "container_end[]"
+	end
+
+	formspec[#formspec + 1] = "container[0,"
+	formspec[#formspec + 1] = num_per_page + 1
+	formspec[#formspec + 1] = "]"
+	formspec[#formspec + 1] = "button[2.6,0;3,1;back;"
+	formspec[#formspec + 1] = fgettext("Back to Main Menu")
+	formspec[#formspec + 1] = "]"
+	formspec[#formspec + 1] = "button[7,0;1,1;pstart;<<]"
+	formspec[#formspec + 1] = "button[8,0;1,1;pback;<]"
+	formspec[#formspec + 1] = "label[9.2,0.2;"
+	formspec[#formspec + 1] = tonumber(cur_page)
+	formspec[#formspec + 1] = " / "
+	formspec[#formspec + 1] = tonumber(pages)
+	formspec[#formspec + 1] = "]"
+	formspec[#formspec + 1] = "button[10,0;1,1;pnext;>]"
+	formspec[#formspec + 1] = "button[11,0;1,1;pend;>>]"
+	formspec[#formspec + 1] = "container_end[]"
+
+	formspec[#formspec + 1] = "]"
+	return table.concat(formspec, "")
+end
+
+function store.handle_submit(this, fields, tabname, tabdata)
+	if fields.search or fields.key_enter_field == "search_string" then
+		search_string = fields.search_string:trim()
+		cur_page = 1
+		store.filter_packages(search_string)
+		core.update_formspec(store.get_formspec())
+		return true
+	end
+
+	if fields.back then
+		this:delete()
+		return true
+	end
+
+	if fields.pstart then
+		cur_page = 1
+		core.update_formspec(store.get_formspec())
+		return true
+	end
+
+	if fields.pend then
+		cur_page = math.ceil(#store.packages / num_per_page)
+		core.update_formspec(store.get_formspec())
+		return true
+	end
+
+	if fields.pnext then
+		cur_page = cur_page + 1
+		local pages = math.ceil(#store.packages / num_per_page)
+		if cur_page > pages then
+			cur_page = 1
+		end
+		core.update_formspec(store.get_formspec())
+		return true
+	end
+
+	if fields.pback then
+		if cur_page == 1 then
+			local pages = math.ceil(#store.packages / num_per_page)
+			cur_page = pages
+		else
+			cur_page = cur_page - 1
+		end
+		core.update_formspec(store.get_formspec())
+		return true
+	end
+
+	if fields.type then
+		local new_type = table.indexof(filter_types_titles, fields.type)
+		if new_type ~= filter_type then
+			filter_type = new_type
+			store.filter_packages(search_string)
+			return true
+		end
+	end
+
+	local start_idx = (cur_page - 1) * num_per_page + 1
+	assert(start_idx ~= nil)
+	for i=start_idx, math.min(#store.packages, start_idx+num_per_page-1) do
+		local package = store.packages[i]
+		assert(package)
+
+		if fields["install_" .. i] then
+			start_install(this, package)
+			return true
+		end
+
+		if fields["uninstall_" .. i] then
+			local dlg_delmod = create_delete_content_dlg(package)
+			dlg_delmod:set_parent(this)
+			this:hide()
+			dlg_delmod:show()
+			return true
+		end
+
+		if fields["view_" .. i] then
+			local dlg = package_dialog.create(package)
+			dlg:set_parent(this)
+			this:hide()
+			dlg:show()
+			return true
+		end
+	end
+
+	return false
+end
+
+function create_store_dlg(type)
+	if not store.loaded then
+		store.load()
+	end
+
+	search_string = ""
+	cur_page = 1
+	store.filter_packages(search_string)
+
+	return dialog_create("store",
+			store.get_formspec,
+			store.handle_submit,
+			nil)
+end

builtin/mainmenu/dlg_create_world.lua
@@ -26,7 +26,7 @@ local function create_world_formspec(dialogdata)
 
 	local game, gameidx = nil , 0
 	if gameid ~= nil then
-		game, gameidx = gamemgr.find_by_gameid(gameid)
+		game, gameidx = pkgmgr.find_by_gameid(gameid)
 		
 		if gameidx == nil then
 			gameidx = 0
@@ -77,17 +77,17 @@ local function create_world_formspec(dialogdata)
 		"dropdown[4.2,2;6.3;dd_mapgen;" .. mglist .. ";" .. selindex .. "]" ..
 
 		"label[2,3;" .. fgettext("Game") .. "]"..
-		"textlist[4.2,3;5.8,2.3;games;" .. gamemgr.gamelist() ..
+		"textlist[4.2,3;5.8,2.3;games;" .. pkgmgr.gamelist() ..
 		";" .. gameidx .. ";true]" ..
 
 		"button[3.25,6;2.5,0.5;world_create_confirm;" .. fgettext("Create") .. "]" ..
 		"button[5.75,6;2.5,0.5;world_create_cancel;" .. fgettext("Cancel") .. "]"
 		
-	if #gamemgr.games == 0 then
+	if #pkgmgr.games == 0 then
 		retval = retval .. "box[2,4;8,1;#ff8800]label[2.25,4;" ..
 				fgettext("You have no games installed.") .. "]label[2.25,4.4;" ..
 				fgettext("Download one from minetest.net") .. "]"
-	elseif #gamemgr.games == 1 and gamemgr.games[1].id == "minimal" then
+	elseif #pkgmgr.games == 1 and pkgmgr.games[1].id == "minimal" then
 		retval = retval .. "box[1.75,4;8.7,1;#ff8800]label[2,4;" ..
 				fgettext("Warning: The minimal development test is meant for developers.") .. "]label[2,4.4;" ..
 				fgettext("Download a game, such as minetest_game, from minetest.net") .. "]"
@@ -125,10 +125,10 @@ local function create_world_buttonhandler(this, fields)
 			if message ~= nil then
 				gamedata.errormessage = message
 			else
-				core.settings:set("menu_last_game",gamemgr.games[gameindex].id)
+				core.settings:set("menu_last_game",pkgmgr.games[gameindex].id)
 				if this.data.update_worldlist_filter then
-					menudata.worldlist:set_filtercriteria(gamemgr.games[gameindex].id)
-					mm_texture.update("singleplayer", gamemgr.games[gameindex].id)
+					menudata.worldlist:set_filtercriteria(pkgmgr.games[gameindex].id)
+					mm_texture.update("singleplayer", pkgmgr.games[gameindex].id)
 				end
 				menudata.worldlist:refresh()
 				core.settings:set("mainmenu_last_selected_world",
@@ -145,7 +145,7 @@ local function create_world_buttonhandler(this, fields)
 
 	if fields["games"] then
 		local gameindex = core.get_textlist_index("games")
-		core.settings:set("menu_last_game", gamemgr.games[gameindex].id)
+		core.settings:set("menu_last_game", pkgmgr.games[gameindex].id)
 		return true
 	end
 

builtin/mainmenu/dlg_delete_content.lua
@@ -17,39 +17,38 @@
 
 --------------------------------------------------------------------------------
 
-local function delete_mod_formspec(dialogdata)
-	
-	dialogdata.mod = modmgr.global_mods:get_list()[dialogdata.selected]
-
+local function delete_content_formspec(dialogdata)
 	local retval =
 		"size[11.5,4.5,true]" ..
 		"label[2,2;" ..
-		fgettext("Are you sure you want to delete \"$1\"?", dialogdata.mod.name) .. "]"..
-		"button[3.25,3.5;2.5,0.5;dlg_delete_mod_confirm;" .. fgettext("Delete") .. "]" ..
-		"button[5.75,3.5;2.5,0.5;dlg_delete_mod_cancel;" .. fgettext("Cancel") .. "]"
-	
+		fgettext("Are you sure you want to delete \"$1\"?", dialogdata.content.name) .. "]"..
+		"button[3.25,3.5;2.5,0.5;dlg_delete_content_confirm;" .. fgettext("Delete") .. "]" ..
+		"button[5.75,3.5;2.5,0.5;dlg_delete_content_cancel;" .. fgettext("Cancel") .. "]"
+
 	return retval
 end
 
 --------------------------------------------------------------------------------
-local function delete_mod_buttonhandler(this, fields)
-	if fields["dlg_delete_mod_confirm"] ~= nil then
+local function delete_content_buttonhandler(this, fields)
+	if fields["dlg_delete_content_confirm"] ~= nil then
 
-		if this.data.mod.path ~= nil and
-			this.data.mod.path ~= "" and
-			this.data.mod.path ~= core.get_modpath() then
-			if not core.delete_dir(this.data.mod.path) then
-				gamedata.errormessage = fgettext("Modmgr: failed to delete \"$1\"", this.data.mod.path)
+		if this.data.content.path ~= nil and
+				this.data.content.path ~= "" and
+				this.data.content.path ~= core.get_modpath() and
+				this.data.content.path ~= core.get_gamepath() and
+				this.data.content.path ~= core.get_texturepath() then
+			if not core.delete_dir(this.data.content.path) then
+				gamedata.errormessage = fgettext("pkgmgr: failed to delete \"$1\"", this.data.content.path)
 			end
-			modmgr.refresh_globals()
+			pkgmgr.refresh_globals()
 		else
-			gamedata.errormessage = fgettext("Modmgr: invalid modpath \"$1\"", this.data.mod.path)
+			gamedata.errormessage = fgettext("pkgmgr: invalid path \"$1\"", this.data.content.path)
 		end
 		this:delete()
 		return true
 	end
-	
-	if fields["dlg_delete_mod_cancel"] then
+
+	if fields["dlg_delete_content_cancel"] then
 		this:delete()
 		return true
 	end
@@ -58,12 +57,13 @@ local function delete_mod_buttonhandler(this, fields)
 end
 
 --------------------------------------------------------------------------------
-function create_delete_mod_dlg(selected_index)
+function create_delete_content_dlg(content)
+	assert(content.name)
 
-	local retval = dialog_create("dlg_delete_mod",
-					delete_mod_formspec,
-					delete_mod_buttonhandler,
+	local retval = dialog_create("dlg_delete_content",
+					delete_content_formspec,
+					delete_content_buttonhandler,
 					nil)
-	retval.data.selected = selected_index
+	retval.data.content = content
 	return retval
 end

commit 63ca604b64a7c60f1c2764e1467902ceb9e04394

Fix Android build, broken since 87ad4d8

build/android/jni/Android.mk
@@ -130,8 +130,9 @@ LOCAL_SRC_FILES := \
 		jni/src/content_mapnode.cpp               \
 		jni/src/content_nodemeta.cpp              \
 		jni/src/content_sao.cpp                   \
-		jni/src/content/contentdb.cpp             \
+		jni/src/content/content.cpp               \
 		jni/src/content/mods.cpp                  \
+		jni/src/content/packages.cpp              \
 		jni/src/content/subgames.cpp              \
 		jni/src/convert_json.cpp                  \
 		jni/src/craftdef.cpp                      \