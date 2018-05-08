50c33d9638c31e70e6111687b19d58ee990e744b
rubenwardy <rw@rubenwardy.com>
2018-05-08 19:06:55 +0100

    Fix luajit include not being found

--- cmake/Modules/FindLuaJIT.cmake
+++ cmake/Modules/FindLuaJIT.cmake
@@ -9,7 +9,7 @@
 FIND_PATH(LUA_INCLUDE_DIR luajit.h
 	HINTS
 	$ENV{LUA_DIR}
-	PATH_SUFFIXES include/luajit-2.0 include/luajit-5_1-2.0 include
+	PATH_SUFFIXES include/luajit-2.1 include/luajit-2.0 include/luajit-5_1-2.1 include/luajit-5_1-2.0 include
 	PATHS
 	~/Library/Frameworks
 	/Library/Frameworks
