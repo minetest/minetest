80f246ff5386435d9f7045a16c93af6ed1190d08
Vincent Glize <vincent.glize@live.fr>
2018-05-10 14:43:27 +0200

    Check text buffer size

--- src/gui/guiConfirmRegistration.cpp
+++ src/gui/guiConfirmRegistration.cpp
@@ -98,8 +98,25 @@ void GUIConfirmRegistration::regenerateGui(v2u32 screensize)
 		char info_text_buf[1024];
 
 #ifdef _MSC_VER
-		sprintf(info_text_buf, info_text_template.c_str(), address.c_str(),
-				m_playername.c_str());
+		int sizeBuffer = sizeof(info_text_buf) -
+				  (info_text_template.length() - 8 + address.length() +
+						  m_playername.length());
+
+		if (sizeBuffer >= 0) {
+			sprintf(info_text_buf, info_text_template.c_str(), address.c_str(), 
+					m_playername.c_str());
+		} else {
+			warningstream << "the info_text_buf buffer is too small" << std::endl;
+			int new_size_text_template =
+					(int)info_text_template.size() + (sizeBuffer - 4);
+			if (new_size_text_template > 0) {
+				std::string info_text_template_trunq = info_text_template.substr(0,
+								new_size_text_template);
+				info_text_template_trunq.append("...");
+				sprintf(info_text_buf, info_text_template_trunq.c_str(),
+						address.c_str(), m_playername.c_str());
+			}
+		}
 #else
 		snprintf(info_text_buf, sizeof(info_text_buf), info_text_template.c_str(),
 				address.c_str(), m_playername.c_str());
