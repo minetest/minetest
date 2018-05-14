0cecc1d1a91e7e3b3b5dd8708b7b416d4b907425
lhofhansl <lhofhansl@yahoo.com>
2018-05-14 03:03:39 -0700

    Make sure color returns to normal after a damage flash (#7332)

--- src/content_cao.h
+++ src/content_cao.h
@@ -198,8 +198,8 @@ public:
 	void updateTexturePos();
 
 	// std::string copy is mandatory as mod can be a class member and there is a swap
-	// on those class members
-	void updateTextures(const std::string &mod);
+	// on those class members... do NOT pass by reference
+	void updateTextures(std::string mod);
 
 	void updateAnimation();
 
