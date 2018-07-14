5cdc5806c5856b04197fa03dc023ae3313eb0187
Lars Hofhansl <larsh@apache.org>
2018-07-14 02:41:48 -0700

    More code style changes

--- src/network/connection.cpp
+++ src/network/connection.cpp
@@ -663,45 +663,35 @@ void Channel::UpdateTimers(float dtime)
 
 		if (packets_successful > 0) {
 			successful_to_lost_ratio = packet_loss/packets_successful;
-		}
-		else if (packet_loss > 0)
-		{
+		} else if (packet_loss > 0) {
 			window_size = std::max(
 					(window_size - 10),
 					MIN_RELIABLE_WINDOW_SIZE);
 			done = true;
 		}
 
-		if (!done)
-		{
+		if (!done) {
 			if ((successful_to_lost_ratio < 0.01f) &&
-				(window_size < MAX_RELIABLE_WINDOW_SIZE))
-			{
+				(window_size < MAX_RELIABLE_WINDOW_SIZE)) {
 				/* don't even think about increasing if we didn't even
 				 * use major parts of our window */
 				if (reasonable_amount_of_data_transmitted)
 					window_size = std::min(
 							(window_size + 100),
 							MAX_RELIABLE_WINDOW_SIZE);
-			}
-			else if ((successful_to_lost_ratio < 0.05f) &&
-					(window_size < MAX_RELIABLE_WINDOW_SIZE))
-			{
+			} else if ((successful_to_lost_ratio < 0.05f) &&
+					(window_size < MAX_RELIABLE_WINDOW_SIZE)) {
 				/* don't even think about increasing if we didn't even
 				 * use major parts of our window */
 				if (reasonable_amount_of_data_transmitted)
 					window_size = std::min(
 							(window_size + 50),
 							MAX_RELIABLE_WINDOW_SIZE);
-			}
-			else if (successful_to_lost_ratio > 0.15f)
-			{
+			} else if (successful_to_lost_ratio > 0.15f) {
 				window_size = std::max(
 						(window_size - 100),
 						MIN_RELIABLE_WINDOW_SIZE);
-			}
-			else if (successful_to_lost_ratio > 0.1f)
-			{
+			} else if (successful_to_lost_ratio > 0.1f) {
 				window_size = std::max(
 						(window_size - 50),
 						MIN_RELIABLE_WINDOW_SIZE);
@@ -901,10 +891,8 @@ void Peer::Drop()
 UDPPeer::UDPPeer(u16 a_id, Address a_address, Connection* connection) :
 	Peer(a_address,a_id,connection)
 {
-	for(unsigned int i=0; i< CHANNEL_COUNT; i++)
-	{
-		channels[i].setWindowSize(g_settings->getU16("max_packets_per_iteration"));
-	}
+	for (Channel &channel : channels)
+		channel.setWindowSize(g_settings->getU16("max_packets_per_iteration"));
 }
 
 bool UDPPeer::getAddress(MTProtocols type,Address& toset)
