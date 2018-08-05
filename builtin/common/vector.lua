0f4d5c6fb922a39c1841f13fdbd816b99ee4da0c
rubenwardy <rw@rubenwardy.com>
2018-08-05 16:27:00 +0100

    Add tests to vector.lua

--- builtin/common/vector.lua
+++ builtin/common/vector.lua
@@ -122,3 +122,36 @@ function vector.sort(a, b)
 	return {x = math.min(a.x, b.x), y = math.min(a.y, b.y), z = math.min(a.z, b.z)},
 		{x = math.max(a.x, b.x), y = math.max(a.y, b.y), z = math.max(a.z, b.z)}
 end
+
+if describe then
+	describe("vector", function()
+		it("new", function()
+			assert.same({ x = 0, y = 0, z = 0 }, vector.new())
+			assert.same({ x = 1, y = 2, z = 3 }, vector.new(1, 2, 3))
+			assert.same({ x = 3, y = 2, z = 1 }, vector.new({ x = 3, y = 2, z = 1 }))
+
+			local v = vector.new({ x = 3, y = 2, z = 1 })
+			assert.same(v, vector.new(v))
+		end)
+
+		it("equal", function()
+			local function assertE(a, b)
+				assert.is_true(vector.equals(a, b))
+			end
+			local function assertNE(a, b)
+				assert.is_false(vector.equals(a, b))
+			end
+
+
+			assertE({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
+			assertE({x = -1, y = 0, z = 1}, {x = -1, y = 0, z = 1})
+			local a = { x = 2, y = 4, z = -10 }
+			assertE(a, a)
+			assertNE({x = -1, y = 0, z = 1}, a)
+		end)
+
+		it("add", function()
+			assert.same({ x = 2, y = 4, z = 6 }, vector.add(vector.new(1, 2, 3), { x = 1, y = 2, z = 3 }))
+		end)
+	end)
+end
