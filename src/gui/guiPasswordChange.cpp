5f8e79f215627864279ff4ab215251930487da0e
stujones11 <stujones111@gmail.com>
2018-08-07 20:58:26 +0100

    Remove un-needed header inclusion

--- src/gui/guiPasswordChange.cpp
+++ src/gui/guiPasswordChange.cpp
@@ -23,7 +23,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <IGUIButton.h>
 #include <IGUIStaticText.h>
 #include <IGUIFont.h>
-#include "keycode.h"
+
 #include "porting.h"
 #include "gettext.h"
 
