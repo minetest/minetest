b5617dff1f25f8c8e2b622a0ee4c1868024a413e
ClobberXD <ClobberXD@gmail.com>
2018-08-27 12:38:38 +0530

    Remove duplication of code block

--- builtin/common/misc_helpers.lua
+++ builtin/common/misc_helpers.lua
@@ -495,14 +495,6 @@ function core.string_to_pos(value)
 		p.z = tonumber(p.z)
 		return p
 	end
-	local p = {}
-	p.x, p.y, p.z = string.match(value, "^%( *([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+) *%)$")
-	if p.x and p.y and p.z then
-		p.x = tonumber(p.x)
-		p.y = tonumber(p.y)
-		p.z = tonumber(p.z)
-		return p
-	end
 	return nil
 end
 
