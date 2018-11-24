172fb960212b672f1e2542f8bd5dd251ba27ad01
paramat <paramat@users.noreply.github.com>
2018-11-24 04:36:49 +0000

    Move CSM death formspec code to a dedicated file

---
+++ builtin/client/death_formspec.lua
@@ -0,0 +1,16 @@
+-- CSM death formspec. Only used when clientside modding is enabled, otherwise
+-- handled by the engine.
+
+core.register_on_death(function()
+	core.display_chat_message("You died.")
+	local formspec = "size[11,5.5]bgcolor[#320000b4;true]" ..
+		"label[4.85,1.35;" .. fgettext("You died") ..
+		"]button_exit[4,3;3,0.5;btn_respawn;".. fgettext("Respawn") .."]"
+	core.show_formspec("bultin:death", formspec)
+end)
+
+core.register_on_formspec_input(function(formname, fields)
+	if formname == "bultin:death" then
+		core.send_respawn()
+	end
+end)
