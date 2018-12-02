f70f7875e2a2e49f80470832ae97e73f171d6460
Maksim <MoNTE48@mail.ua>
2018-12-03 00:39:35 +0100

    Update Android java code (#7820)
    Targets SDK 26 as required by the playstore.
    Fixes screen auto-rotation closing game.
    Hides on-screen navigation bar if present.
    
    Update gradlew.
    Fix display aspect on 18+/:9 displays (like a Samsung Galaxy S9).
    Remove small app icons, not required.
    Fix xml in unpacking activity.
    Support Android permission: On Android 6.0+ you need to manually give write
    permission (as required by google).
    Background during unpacking (just a demo for now).
    Material Design: no more Android 2 interface.
    Immersive mode (Android 4.4+ - hide NavBar for fullscreen mode).

--- build/android/src/main/java/net.minetest.minetest/MinetestTextEntry.java
+++ build/android/src/main/java/net.minetest.minetest/MinetestTextEntry.java
@@ -6,63 +6,59 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
 
 public class MinetestTextEntry extends Activity {
+	private final int MultiLineTextInput = 1;
+	private final int SingleLineTextInput = 2;
+	private final int SingleLinePasswordInput = 3;
 	public AlertDialog mTextInputDialog;
 	public EditText mTextInputWidget;
-	
-	private final int MultiLineTextInput              = 1;
-	private final int SingleLineTextInput             = 2;
-	private final int SingleLinePasswordInput         = 3;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		Bundle b = getIntent().getExtras();
 		String acceptButton = b.getString("EnterButton");
-		String hint         = b.getString("hint");
-		String current      = b.getString("current");
-		int    editType     = b.getInt("editType");
-		
+		String hint = b.getString("hint");
+		String current = b.getString("current");
+		int editType = b.getInt("editType");
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		mTextInputWidget = new EditText(this);
 		mTextInputWidget.setHint(hint);
 		mTextInputWidget.setText(current);
 		mTextInputWidget.setMinWidth(300);
 		if (editType == SingleLinePasswordInput) {
-			mTextInputWidget.setInputType(InputType.TYPE_CLASS_TEXT | 
+			mTextInputWidget.setInputType(InputType.TYPE_CLASS_TEXT |
 					InputType.TYPE_TEXT_VARIATION_PASSWORD);
-		}
-		else {
+		} else {
 			mTextInputWidget.setInputType(InputType.TYPE_CLASS_TEXT);
 		}
-		
-		
+
 		builder.setView(mTextInputWidget);
-		
+
 		if (editType == MultiLineTextInput) {
 			builder.setPositiveButton(acceptButton, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int whichButton) 
-				{ pushResult(mTextInputWidget.getText().toString()); }
-				});
+				public void onClick(DialogInterface dialog, int whichButton) {
+					pushResult(mTextInputWidget.getText().toString());
+				}
+			});
 		}
-		
+
 		builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
 			public void onCancel(DialogInterface dialog) {
 				cancelDialog();
 			}
 		});
-		
+
 		mTextInputWidget.setOnKeyListener(new OnKeyListener() {
 			@Override
 			public boolean onKey(View view, int KeyCode, KeyEvent event) {
-				if ( KeyCode == KeyEvent.KEYCODE_ENTER){
+				if (KeyCode == KeyEvent.KEYCODE_ENTER) {
 
 					pushResult(mTextInputWidget.getText().toString());
 					return true;
@@ -70,19 +66,19 @@ public class MinetestTextEntry extends Activity {
 				return false;
 			}
 		});
-		
+
 		mTextInputDialog = builder.create();
 		mTextInputDialog.show();
 	}
-	
+
 	public void pushResult(String text) {
 		Intent resultData = new Intent();
 		resultData.putExtra("text", text);
-		setResult(Activity.RESULT_OK,resultData);
+		setResult(Activity.RESULT_OK, resultData);
 		mTextInputDialog.dismiss();
 		finish();
 	}
-	
+
 	public void cancelDialog() {
 		setResult(Activity.RESULT_CANCELED);
 		mTextInputDialog.dismiss();
