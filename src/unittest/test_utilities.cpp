30f3653a076c9fcaad479b09cae70d729302d9f6
Loic Blot <loic.blot@unix-experience.fr>
2018-12-04 19:27:55 +0100

    indent fix here

--- src/unittest/test_utilities.cpp
+++ src/unittest/test_utilities.cpp
@@ -171,8 +171,8 @@ void TestUtilities::testWrapDegrees_0_360_v3f()
 void TestUtilities::testLowercase()
 {
 	UASSERT(lowercase("Foo bAR") == "foo bar");
-    UASSERT(lowercase("eeeeeeaaaaaaaaaaaààààà") == "eeeeeeaaaaaaaaaaaààààà");
-    UASSERT(lowercase("MINETEST-powa") == "minetest-powa");
+	UASSERT(lowercase("eeeeeeaaaaaaaaaaaààààà") == "eeeeeeaaaaaaaaaaaààààà");
+	UASSERT(lowercase("MINETEST-powa") == "minetest-powa");
 }
 
 
