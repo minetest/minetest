fadd48b5e4cdac1b80d0e00db76de3746c46dd61
Wuzzy <Wuzzy2@mail.ru>
2019-01-06 19:50:06 +0100

    Fix some typos in user-facing strings

--- builtin/mainmenu/pkgmgr.lua
+++ builtin/mainmenu/pkgmgr.lua
@@ -557,7 +557,7 @@ function pkgmgr.preparemodlist(data)
 		retval[#retval + 1] = {
 			type = "game",
 			is_game_content = true,
-			name = fgettext(gamespec.name .. " mods"),
+			name = fgettext("$1 mods", gamespec.name),
 			path = gamespec.path
 		}
 	end
