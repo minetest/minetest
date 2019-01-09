0acdf936830f0bee6bff553201febedecb12a482
Loïc Blot <loic.blot@unix-experience.fr>
2019-01-09 14:39:43 +0100

    Android build fixes
    This fixes #8079

--- src/client/tile.cpp
+++ src/client/tile.cpp
@@ -19,6 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "tile.h"
 
+#include <cstring>
 #include <algorithm>
 #include <ICameraSceneNode.h>
 #include "util/string.h"
