8d3630435446f72e97e9049d0a414e6367770cf1
rubenwardy <rw@rubenwardy.com>
2019-02-03 13:12:09 +0000

    s

--- src/network/address.cpp
+++ src/network/address.cpp
@@ -278,6 +278,7 @@ bool Address::isLocalhost() const {
 				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
 
 		return memcmp(m_address.ipv6.sin6_addr.s6_addr, localhost_bytes, 16) == 0;
-	} else
+	} else {
 		return m_address.ipv4.sin_addr.s_addr == 0x0100007F;
+	}
 }
