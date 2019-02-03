68c4c3865b9b9fb033ce81d1ad2a61762b195448
rubenwardy <rw@rubenwardy.com>
2019-02-03 01:08:03 +0000

    S

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -300,7 +300,11 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("font_size", font_size_str);
 	settings->setDefault("mono_font_size", font_size_str);
 	settings->setDefault("contentdb_url", "https://content.minetest.net");
-	settings->setDefault("hide_content_ratings", "mature");
+#ifdef __ANDROID__
+	settings->setDefault("hide_content_ratings", "android_default");
+#else
+	settings->setDefault("hide_content_ratings", "desktop_default");
+#endif
 	settings->setDefault("show_nonfree_packages", "false");
 
 
