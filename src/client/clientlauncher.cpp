c7706070e9c43dc9e1f5af8c5bb09661cb6eff58
Aspen <cardboardtube19@gmail.com>
2019-02-03 18:12:39 -0800

    Fix cloud color in loading screen and main menu

--- src/client/clientlauncher.cpp
+++ src/client/clientlauncher.cpp
@@ -169,7 +169,7 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 	if (!g_menuclouds)
 		g_menuclouds = new Clouds(g_menucloudsmgr, -1, rand());
 	g_menuclouds->setHeight(100.0f);
-	g_menuclouds->update(v3f(0, 0, 0), video::SColor(255, 200, 200, 255));
+	g_menuclouds->update(v3f(0, 0, 0), video::SColor(255, 240, 240, 255));
 	scene::ICameraSceneNode* camera;
 	camera = g_menucloudsmgr->addCameraSceneNode(NULL, v3f(0, 0, 0), v3f(0, 60, 100));
 	camera->setFarValue(10000);
