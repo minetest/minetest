705b454026d9c49e62bf94857f7b83d19d3ef81c
Wuzzy <Wuzzy2@mail.ru>
2019-02-09 19:35:13 +0100

    Remove fgettext("") in builtin
    This call is useless and wrong, the empty string has a special meaning in Gettext.

--- builtin/mainmenu/tab_online.lua
+++ builtin/mainmenu/tab_online.lua
@@ -36,7 +36,7 @@ local function get_formspec(tabview, name, tabdata)
 		"field[0.15,0.075;5.91,1;te_search;;" .. core.formspec_escape(tabdata.search_for) .. "]" ..
 		"button[5.62,-0.25;1.5,1;btn_mp_search;" .. fgettext("Search") .. "]" ..
 		"image_button[6.97,-.165;.83,.83;" .. core.formspec_escape(defaulttexturedir .. "refresh.png")
-			.. ";btn_mp_refresh;" .. fgettext("") .. "]" ..
+			.. ";btn_mp_refresh;]" ..
 
 		-- Address / Port
 		"label[7.75,-0.25;" .. fgettext("Address / Port") .. "]" ..
