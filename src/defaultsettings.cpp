1c87d57e1d144262419fd534e3dcd391e2e5bcbd
Paramat <paramat@users.noreply.github.com>
2019-03-05 22:58:38 +0000

    Change 'num_emerge_threads' default to 1 (#8303)

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -374,7 +374,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("emergequeue_limit_total", "512");
 	settings->setDefault("emergequeue_limit_diskonly", "64");
 	settings->setDefault("emergequeue_limit_generate", "64");
-	settings->setDefault("num_emerge_threads", "0");
+	settings->setDefault("num_emerge_threads", "1");
 	settings->setDefault("secure.enable_security", "true");
 	settings->setDefault("secure.trusted_mods", "");
 	settings->setDefault("secure.http_mods", "");
