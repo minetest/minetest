557afdc42979916c54d9e1240845dcce3726721e
sfan5 <sfan5@live.de>
2019-03-07 08:17:01 +0100

    Update a few dependency versions for buildbot (#8319)

--- util/buildbot/buildwin64.sh
+++ util/buildbot/buildwin64.sh
@@ -16,12 +16,12 @@ toolchain_file=$dir/toolchain_mingw64.cmake
 irrlicht_version=1.8.4
 ogg_version=1.3.2
 vorbis_version=1.3.5
-curl_version=7.54.0
+curl_version=7.64.0
 gettext_version=0.19.8.1
-freetype_version=2.8
-sqlite3_version=3.19.2
+freetype_version=2.9.1
+sqlite3_version=3.27.2
 luajit_version=2.1.0-beta3
-leveldb_version=1.19
+leveldb_version=1.20
 zlib_version=1.2.11
 
 mkdir -p $packagedir
