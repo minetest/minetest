1354e925d4639ad59d91aefee5408b7dea7ceb28
SmallJoker <mk939@ymail.com>
2019-03-16 15:26:03 +0100

    Trigger for 'all' as well

--- builtin/common/information_formspecs.lua
+++ builtin/common/information_formspecs.lua
@@ -97,7 +97,7 @@ help_command.func = function(name, param)
 			build_privs_formspec(name))
 		return
 	end
-	if param == "" then
+	if param == "" or param == "all" then
 		core.show_formspec(name, "__builtin:help_cmds",
 			build_chatcommands_formspec(name))
 		return 
