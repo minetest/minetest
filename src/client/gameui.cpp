36db3e37ac4c16e911d32935060f2e9fca4baa22
Thomas--S <Thomas--S@users.noreply.github.com>
2019-04-27 15:41:48 +0530

    Restore whitespace to separate yaw and cardinal direction
    Co-Authored-By: ClobberXD <ClobberXD@gmail.com>

--- src/client/gameui.cpp
+++ src/client/gameui.cpp
@@ -126,7 +126,7 @@ void GameUI::update(const RunStats &stats, Client *client, MapDrawControl *draw_
 			<< "pos: (" << (player_position.X / BS)
 			<< ", " << (player_position.Y / BS)
 			<< ", " << (player_position.Z / BS)
-			<< ") | yaw: " << (wrapDegrees_0_360(cam.camera_yaw)) << "°"
+			<< ") | yaw: " << (wrapDegrees_0_360(cam.camera_yaw)) << "° "
 			<< yawToDirectionString(cam.camera_yaw)
 			<< " | pitch: " << (-wrapDegrees_180(cam.camera_pitch)) << "°"
 			<< " | seed: " << ((u64)client->getMapSeed());
