777c75e9a63ec84c5adbaa7590dd91529d6f6bdb
rubenwardy <rw@rubenwardy.com>
2019-06-20 23:02:14 +0100

    Add formspec test framework

---
+++ util/fstest/init.lua
@@ -0,0 +1,37 @@
+core.set_clouds(false)
+
+local tests = {}
+
+local function load_tests(path)
+	local list = core.get_dir_list(path, false)
+	for _, filename in ipairs(list) do
+		if filename:sub(#filename - 3, #filename) == ".txt" then
+			local name = filename:sub(1, #filename - 4)
+
+			local f = io.open(path .. DIR_DELIM .. filename)
+			local formspec = f:read("*all")
+			f:close()
+
+			tests[#tests + 1] = {
+				name = name,
+				formspec = formspec
+			}
+		end
+	end
+end
+
+load_tests("util/fstest/tests")
+
+local co = coroutine.create(function()
+	for i=1, #tests do
+		core.update_formspec(tests[i].formspec)
+		coroutine.yield()
+		core.take_screenshot("util/fstest/tests/" .. tests[i].name .. ".out.png")
+	end
+	core.close()
+end)
+
+
+function core.on_step()
+	coroutine.resume(co)
+end
---
+++ util/fstest/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp util/fstest/test.conf /tmp/fstest.conf
+./bin/minetest --config /tmp/fstest.conf
