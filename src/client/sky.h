9999561ff0ef14fbaf80ffa43d65ff1295829d92
Methacrylon <mathieu.vander.p@gmail.com>
2019-07-16 16:05:10 +0200

    Need to include <array> to pass the build with Xcode on UNIX

--- src/client/sky.h
+++ src/client/sky.h
@@ -18,6 +18,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #include <ISceneNode.h>
+#include <array>
 #include "camera.h"
 #include "irrlichttypes_extrabloated.h"
 
