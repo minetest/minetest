f4e182c5bb175d5beb95e94e05b3c889b4dbdccb
TheTermos <55103816+TheTermos@users.noreply.github.com>
2020-02-29 14:46:15 +0100

    tidy

--- src/client/localplayer.h
+++ src/client/localplayer.h
@@ -141,7 +141,7 @@ public:
 
 	void setCollisionbox(const aabb3f &box) { m_collisionbox = box; }
 
-	const aabb3f& getCollisionbox() const { return m_collisionbox; } //TheTermos
+	const aabb3f& getCollisionbox() const { return m_collisionbox; }
 
 	float getZoomFOV() const { return m_zoom_fov; }
 	void setZoomFOV(float zoom_fov) { m_zoom_fov = zoom_fov; }
