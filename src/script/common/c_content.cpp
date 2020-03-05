30f1295deecce7a3a59a3d2014afc10fdca842da
Lars Mueller <appgurulars@gmx.de>
2020-03-05 20:02:21 +0100

    Rename to precision in API

--- src/script/common/c_content.cpp
+++ src/script/common/c_content.cpp
@@ -1852,7 +1852,7 @@ void read_hud_element(lua_State *L, HudElement *elem)
 	elem->text    = getstringfield_default(L, 2, "text", "");
 	elem->number  = getintfield_default(L, 2, "number", 0);
 	if (elem->type == HUD_ELEM_WAYPOINT)
-		elem->item = getintfield_default(L, 2, "item", -1) + 1;
+		elem->item = getintfield_default(L, 2, "precision", -1) + 1;
 	else
 		elem->item = getintfield_default(L, 2, "item", 0);
 	elem->dir     = getintfield_default(L, 2, "direction", 0);
