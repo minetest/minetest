50870f071855a249b9bd5900c2ee345e287e9641
v-rob <31123645+v-rob@users.noreply.github.com>
2020-03-04 17:06:46 -0800

    Change commas to semicolons

--- games/minimal/mods/test/formspec.lua
+++ games/minimal/mods/test/formspec.lua
@@ -17,7 +17,7 @@ local clip_fs = [[
 	scrollbar[0,9;3,0.8;horizontal;x9;3]
 	tablecolumns[text;text]
 	table[0,10;3,1;x10;one,two,three,four;1]
-	animated_image[0,11;3,1;test_animation.png,4,100]
+	animated_image[0,11;3,1;test_animation.png;4;100]
 ]]
 
 
@@ -172,19 +172,19 @@ local pages = {
 		[[
 			formspec_version[3]
 			size[12,12]
-			animated_image[0.5,0.5;1,1;;test_animation.png,4,100]
-			animated_image[0.5,1.75;1,1;;test_animation.jpg,4,100]
-			animated_image[1.75,0.5;1,1;;test_animation.png,100,100]
-			animated_image[3,0.5;1,1;ani_image;test_animation.png,4,1000]
+			animated_image[0.5,0.5;1,1;;test_animation.png;4;100]
+			animated_image[0.5,1.75;1,1;;test_animation.jpg;4;100]
+			animated_image[1.75,0.5;1,1;;test_animation.png;100;100]
+			animated_image[3,0.5;1,1;ani_image;test_animation.png;4;1000]
 			button[4.25,0.5;1,1;;Current
 Number]
-			animated_image[3,1.75;1,1;;test_animation.png,4,1000;2]
+			animated_image[3,1.75;1,1;;test_animation.png;4;1000;2]
 			button[4.25,1.75;1,1;;Current
 Number]
-			animated_image[3,3;1,1;;test_animation.png,4,0]
-			animated_image[3,4.25;1,1;;test_animation.png,4,0;3]
-			animated_image[5.5,0.5;5,2;;test_animation.png,4,100]
-			animated_image[5.5,2.75;5,2;;test_animation.jpg,4,100]
+			animated_image[3,3;1,1;;test_animation.png;4;0]
+			animated_image[3,4.25;1,1;;test_animation.png;4;0;3]
+			animated_image[5.5,0.5;5,2;;test_animation.png;4;100]
+			animated_image[5.5,2.75;5,2;;test_animation.jpg;4;100]
 		]]
 }
 
--- src/gui/guiFormSpecMenu.cpp
+++ src/gui/guiFormSpecMenu.cpp
@@ -784,7 +784,7 @@ void GUIFormSpecMenu::parseAnimatedImage(parserData *data, const std::string &el
 {
 	std::vector<std::string> parts = split(element, ';');
 
-	if (parts.size() != 4 && parts.size() != 5 &&
+	if (parts.size() != 6 && parts.size() != 7 &&
 		!(parts.size() > 4 && m_formspec_version > FORMSPEC_API_VERSION))
 	{
 		warningstream << "Invalid animated_image element(" << parts.size()
@@ -795,24 +795,9 @@ void GUIFormSpecMenu::parseAnimatedImage(parserData *data, const std::string &el
 	std::vector<std::string> v_pos  = split(parts[0], ',');
 	std::vector<std::string> v_geom = split(parts[1], ',');
 	std::string name = parts[2];
-
-	std::string texture_string = unescape_string(parts[3]);
-	std::vector<std::string> texture = split(texture_string, ',');
-
-	std::string texture_name = texture[0];
-	s32 frame_count = 1;
-	s32 frame_duration = 0;
-
-	if (texture.size() == 3 ||
-		(texture.size() > 3 && m_formspec_version > FORMSPEC_API_VERSION))
-	{
-		frame_count = stoi(texture[1]);
-		frame_duration = stoi(texture[2]);
-	} else {
-		warningstream << "Invalid animated_image texture format \""
-			<< texture_string << "\". Expected format: \"texture_name,frame_count,"
-			"frame_duration\"." << std::endl;
-	}
+	std::string texture_name = unescape_string(parts[3]);
+	s32 frame_count = stoi(parts[4]);
+	s32 frame_duration = stoi(parts[5]);
 
 	MY_CHECKPOS("animated_image", 0);
 	MY_CHECKGEOM("animated_image", 1);
@@ -846,8 +831,8 @@ void GUIFormSpecMenu::parseAnimatedImage(parserData *data, const std::string &el
 	GUIAnimatedImage *e = new GUIAnimatedImage(Environment, this, spec.fid,
 		rect, texture_name, frame_count, frame_duration, m_tsrc);
 
-	if (parts.size() == 5)
-		e->setFrameIndex(stoi(parts[4]) - 1);
+	if (parts.size() == 7)
+		e->setFrameIndex(stoi(parts[6]) - 1);
 
 	auto style = getStyleForElement("animated_image", spec.fname, "image");
 	e->setNotClipped(style.getBool(StyleSpec::NOCLIP, false));
