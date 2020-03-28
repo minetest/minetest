minetest/minetest

commit f3032a637c53f3c98fbbed1d3b691898aabe1687

Fix incorrect light updates for wielditem and item visuals (#9540)
An alternative to #9537

src/client/content_cao.cpp
@@ -826,7 +826,11 @@ void GenericCAO::setNodeLight(u8 light)
 {
 	video::SColor color(255, light, light, light);
 
-	if (m_enable_shaders) {
+	if (m_prop.visual == "wielditem" || m_prop.visual == "item") {
+		// Since these types of visuals are using their own shader
+		// they should be handled separately
+		m_wield_meshnode->setColor(color);
+	} else if (m_enable_shaders) {
 		scene::ISceneNode *node = getSceneNode();
 
 		if (node == nullptr)
@@ -850,8 +854,6 @@ void GenericCAO::setNodeLight(u8 light)
 			setMeshColor(m_meshnode->getMesh(), color);
 		} else if (m_animated_meshnode) {
 			setAnimatedMeshColor(m_animated_meshnode, color);
-		} else if (m_wield_meshnode) {
-			m_wield_meshnode->setColor(color);
 		} else if (m_spritenode) {
 			m_spritenode->setColor(color);
 		}

commit 04961491c197b4d1aecc1e40c6cb5e738860b31c

Fix crash on wielditem/item entities
bug was introduced in f3032a637c53f3c98fbbed1d3b691898aabe1687

src/client/content_cao.cpp
@@ -829,7 +829,8 @@ void GenericCAO::setNodeLight(u8 light)
 	if (m_prop.visual == "wielditem" || m_prop.visual == "item") {
 		// Since these types of visuals are using their own shader
 		// they should be handled separately
-		m_wield_meshnode->setColor(color);
+		if (m_wield_meshnode)
+			m_wield_meshnode->setColor(color);
 	} else if (m_enable_shaders) {
 		scene::ISceneNode *node = getSceneNode();
 