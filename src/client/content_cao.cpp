4ddd627e8e0a22896670777bae09979f272ed02c
theviper121 <theviper121@gmail.com>
2020-03-31 23:04:37 -0500

    Fix UpdateBonePosition() breaking animations

--- src/client/content_cao.cpp
+++ src/client/content_cao.cpp
@@ -1360,6 +1360,7 @@ void GenericCAO::updateBonePosition()
 		return;
 
 	m_animated_meshnode->setJointMode(irr::scene::EJUOR_CONTROL); // To write positions to the mesh on render
+	m_animated_meshnode->animateJoints(); //apply animation from mesh before updating bones
 	for(std::unordered_map<std::string, core::vector2d<v3f>>::const_iterator
 			ii = m_bone_position.begin(); ii != m_bone_position.end(); ++ii) {
 		std::string bone_name = (*ii).first;
