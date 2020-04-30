422a09939d45dbde629058014ad635ff3b356d9f
rubenwardy <rw@rubenwardy.com>
2020-04-30 20:13:34 +0100

    Fix build issues

--- build/android/app/src/main/java/net/minetest/minetest/GameActivity.java
+++ build/android/app/src/main/java/net/minetest/minetest/GameActivity.java
@@ -22,6 +22,7 @@ package net.minetest.minetest;
 
 import android.app.NativeActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
