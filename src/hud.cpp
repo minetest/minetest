a584af34eb0bb1dd4bcb1682ab9ba9bf01bf21dd
SmallJoker <SmallJoker@users.noreply.github.com>
2020-05-10 21:23:00 +0200

    aLiGN THis

--- src/hud.cpp
+++ src/hud.cpp
@@ -46,7 +46,7 @@ const struct EnumString es_HudElementStat[] =
 	{HUD_STAT_WORLD_POS, "world_pos"},
 	{HUD_STAT_SIZE,    "size"},
 	{HUD_STAT_Z_INDEX, "z_index"},
-	{HUD_STAT_TEXT2,  "text2"},
+	{HUD_STAT_TEXT2,   "text2"},
 	{0, NULL},
 };
 
