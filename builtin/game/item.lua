3514768655d9b53ce5de9308171ca87a94ebeaac
numzero <numzer0@yandex.ru>
2020-05-18 00:16:37 +0300

    Add support into builtin

--- builtin/game/item.lua
+++ builtin/game/item.lua
@@ -157,7 +157,7 @@ end
 
 function core.is_colored_paramtype(ptype)
 	return (ptype == "color") or (ptype == "colorfacedir") or
-		(ptype == "colorwallmounted")
+		(ptype == "colorwallmounted") or (ptype == "colordegrotate")
 end
 
 function core.strip_param2_color(param2, paramtype2)
@@ -168,6 +168,8 @@ function core.strip_param2_color(param2, paramtype2)
 		param2 = math.floor(param2 / 32) * 32
 	elseif paramtype2 == "colorwallmounted" then
 		param2 = math.floor(param2 / 8) * 8
+	elseif paramtype2 == "colordegrotate" then
+		param2 = math.floor(param2 / 32) * 32
 	end
 	-- paramtype2 == "color" requires no modification.
 	return param2
@@ -344,6 +346,8 @@ function core.item_place_node(itemstack, placer, pointed_thing, param2,
 		elseif def.paramtype2 == "colorwallmounted" then
 			color_divisor = 8
 		elseif def.paramtype2 == "colorfacedir" then
+			color_divisor = 32
+		elseif def.paramtype2 == "colordegrotate" then
 			color_divisor = 32
 		end
 		if color_divisor then
