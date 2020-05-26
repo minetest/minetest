42acc33b126bea4f5aaee03ffe9641084a552e27
rubenwardy <rw@rubenwardy.com>
2020-05-26 21:31:01 +0100

    Add tests

---
+++ games/devtest/mods/unittests/itemdescription.lua
@@ -0,0 +1,41 @@
+minetest.register_tool("unittests:colorful_pick", {
+	description = minetest.colorize("#ff0", "Colorful Pickaxe") .. "\nThe best pick.",
+	inventory_image = "basetools_mesepick.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=3,
+		groupcaps={
+			cracky={times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=20, maxlevel=3},
+			crumbly={times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=20, maxlevel=3},
+			snappy={times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=20, maxlevel=3}
+		},
+		damage_groups = {fleshy=4},
+	},
+})
+
+minetest.register_chatcommand("item_description", {
+	param = "",
+	description = "Show the short and full description of the wielded item.",
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		local item = player:get_wielded_item()
+		return true, string.format("short_description: %s\ndescription: %s",
+				item:get_short_description(), item:get_description())
+	end
+})
+
+function unittests.test_short_desc()
+	local stack = ItemStack("unittests:colorful_pick")
+	print(stack:get_short_description())
+	assert(stack:get_short_description() == "Colorful Pickaxe")
+	assert(stack:get_description() ~= stack:get_short_description())
+	assert(stack:get_description() == minetest.registered_items["unittests:colorful_pick"].description)
+
+	local new_msg = minetest.colorize("red", "Hello") .. " World"
+	stack:get_meta():set_string("description", new_msg)
+	assert(stack:get_short_description() == "Hello World")
+	assert(stack:get_description() ~= stack:get_short_description())
+	assert(stack:get_description() == new_msg)
+
+	return true
+end
