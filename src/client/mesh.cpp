765665fb24f7d8c419a05fdb86b76b8d252b4893
Danila Shutov <dcbrwn2@gmail.com>
2020-06-07 02:30:39 +0300

    Simplify condition of normal validation

--- src/client/mesh.cpp
+++ src/client/mesh.cpp
@@ -341,7 +341,7 @@ bool checkMeshNormals(scene::IMesh *mesh)
 		// hurting the performance and covering only really weird broken models.
 		f32 length = buffer->getNormal(0).getLength();
 
-		if (length == 0 || isinf(length) || isnan(length))
+		if (length == 0 || !isfinite(length))
 			return false;
 	}
 
