64380cf92e8d0000f75ec372e3e1d09bdccd12e7
sfan5 <sfan5@live.de>
2020-06-10 10:58:13 +0200

    Fix build error on Ubuntu 16.04

--- src/client/content_mapblock.cpp
+++ src/client/content_mapblock.cpp
@@ -17,6 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <cmath>
 #include "content_mapblock.h"
 #include "util/numeric.h"
 #include "util/directiontables.h"
