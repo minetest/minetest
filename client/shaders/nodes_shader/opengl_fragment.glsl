98ce66cd19d481ef10b603cb68ad4f65f5a425be
mntmn <lukas@mntmn.com>
2020-06-14 16:50:49 +0200

    shaders: fix transparency on GC7000L where mesa doesn't implement GL_ALPHA_TEST
    - add USE_DISCARD for discard, toggled by GC7000 renderer
    - check gl_renderer only on __unix__

--- client/shaders/nodes_shader/opengl_fragment.glsl
+++ client/shaders/nodes_shader/opengl_fragment.glsl
@@ -190,6 +190,14 @@ void main(void)
 #endif
 	vec4 base = texture2D(baseTexture, uv).rgba;
 
+#ifdef USE_DISCARD
+	// If alpha is zero, we can just discard the pixel. This fixes transparency
+	// on GPUs like GC7000L, where GL_ALPHA_TEST is not implemented in mesa.
+	if (base.a == 0.0) {
+		discard;
+	}
+#endif
+
 #ifdef ENABLE_BUMPMAPPING
 	if (use_normalmap) {
 		vec3 L = normalize(lightVec);
