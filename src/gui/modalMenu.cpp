f346731614a474125691edcb17495d7a4eecb798
Marco <4279489-marco_a@users.noreply.gitlab.com>
2020-07-01 21:39:47 +0200

    Suggested fixes

--- src/gui/modalMenu.cpp
+++ src/gui/modalMenu.cpp
@@ -145,9 +145,8 @@ bool GUIModalMenu::DoubleClickDetection(const SEvent &event)
 	} else if (event.MouseInput.Event == EMIE_LMOUSE_LEFT_UP) {
 		u64 delta = porting::getDeltaMs(
 			m_doubleclickdetect[0].time, porting::getTimeMs());
-		if (delta > 400) {
+		if (delta > 400)
 			return false;
-		}
 
 		double squaredistance = m_doubleclickdetect[0].pos.
 			getDistanceFromSQ(m_doubleclickdetect[1].pos);
@@ -158,7 +157,6 @@ bool GUIModalMenu::DoubleClickDetection(const SEvent &event)
 
 		SEvent translated = SEvent();
 		// translate doubleclick to escape
-		memset(&translated, 0, sizeof(SEvent));
 		translated.EventType            = EET_KEY_INPUT_EVENT;
 		translated.KeyInput.Key         = KEY_ESCAPE;
 		translated.KeyInput.Control     = false;
