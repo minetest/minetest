70df3d54f37c280f7afe60f6e964b8406577f39f
sfan5 <sfan5@live.de>
2020-07-09 22:01:46 +0200

    Quick workaround for crashing destructor on Mingw32
    closes #10137

--- src/script/common/c_converter.cpp
+++ src/script/common/c_converter.cpp
@@ -457,9 +457,17 @@ size_t read_stringlist(lua_State *L, int index, std::vector<std::string> *result
 	Table field getters
 */
 
+#if defined(__MINGW32__) && !defined(__MINGW64__)
+/* MinGW 32-bit somehow crashes in the std::set destructor when this
+ * variable is thread-local, so just don't do that. */
+static std::set<u64> warned_msgs;
+#endif
+
 bool check_field_or_nil(lua_State *L, int index, int type, const char *fieldname)
 {
-	static thread_local std::set<u64> warned_msgs;
+#if !defined(__MINGW32__) || defined(__MINGW64__)
+	thread_local std::set<u64> warned_msgs;
+#endif
 
 	int t = lua_type(L, index);
 	if (t == LUA_TNIL)
