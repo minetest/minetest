6701fba35dbd7fce0d7c189b99b236e558eff341
Lejo1 <Lejo_1@web.de>
2020-07-23 11:02:22 +0200

    Unify

--- src/script/lua_api/l_http.cpp
+++ src/script/lua_api/l_http.cpp
@@ -49,21 +49,6 @@ void ModApiHttp::read_http_fetch_request(lua_State *L, HTTPFetchRequest &req)
 	req.multipart = getboolfield_default(L, 1, "multipart", false);
 	req.timeout = getintfield_default(L, 1, "timeout", 3) * 1000;
 
-	// post_data: if table, post form data, otherwise raw data DEPRECATED
-	lua_getfield(L, 1, "post_data");
-	if (lua_istable(L, 2)) {
-		req.method = HTTP_POST;
-		lua_pushnil(L);
-		while (lua_next(L, 2) != 0) {
-			req.fields[readParam<std::string>(L, -2)] = readParam<std::string>(L, -1);
-			lua_pop(L, 1);
-		}
-	} else if (lua_isstring(L, 2)) {
-		req.method = HTTP_POST;
-		req.raw_data = readParam<std::string>(L, 2);
-	}
-	lua_pop(L, 1);
-
 	lua_getfield(L, 1, "method");
 	if (lua_isstring(L, -1)) {
 		std::string mth = getstringfield_default(L, 1, "method", "");
@@ -78,7 +63,16 @@ void ModApiHttp::read_http_fetch_request(lua_State *L, HTTPFetchRequest &req)
 	}
 	lua_pop(L, 1);
 
-	lua_getfield(L, 1, "data");
+	// post_data: if table, post form data, otherwise raw data DEPRECATED use data and method instead
+	lua_getfield(L, 1, "post_data");
+	if (lua_isnil(L, 2)) {
+		lua_pop(L, 1);
+		lua_getfield(L, 1, "data");
+	}
+	else {
+		req.method = HTTP_POST;
+	}
+
 	if (lua_istable(L, 2)) {
 		lua_pushnil(L);
 		while (lua_next(L, 2) != 0) {
@@ -88,6 +82,7 @@ void ModApiHttp::read_http_fetch_request(lua_State *L, HTTPFetchRequest &req)
 	} else if (lua_isstring(L, 2)) {
 		req.raw_data = readParam<std::string>(L, 2);
 	}
+
 	lua_pop(L, 1);
 
 	lua_getfield(L, 1, "extra_headers");
