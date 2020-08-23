40b2e991379cf8ea6e1100024a353ab3e0d7725b
hecktest <>
2020-08-23 20:27:48 +0200

    Erase remaining traces of normalmap generation

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -253,9 +253,6 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("tone_mapping", "false");
 	settings->setDefault("enable_bumpmapping", "false");
 	settings->setDefault("enable_parallax_occlusion", "false");
-	settings->setDefault("generate_normalmaps", "false");
-	settings->setDefault("normalmaps_strength", "0.6");
-	settings->setDefault("normalmaps_smooth", "1");
 	settings->setDefault("parallax_occlusion_mode", "1");
 	settings->setDefault("parallax_occlusion_iterations", "4");
 	settings->setDefault("parallax_occlusion_scale", "0.08");
