287f3fb2e3317bb6bd6d8608f1aa051c601aeca4
EvidenceB <49488517+EvidenceBKidscode@users.noreply.github.com>
2020-08-23 22:50:14 +0200

    Avoid drawing clipped out formspec elements (#10095)

--- src/gui/guiFormSpecMenu.cpp
+++ src/gui/guiFormSpecMenu.cpp
@@ -3483,10 +3483,14 @@ void GUIFormSpecMenu::drawMenu()
 		e->setVisible(true);
 
 	/*
-		Call base class
-		(This is where all the drawing happens.)
+		This is where all the drawing happens.
 	*/
-	gui::IGUIElement::draw();
+	core::list<IGUIElement*>::Iterator it = Children.begin();
+	for (; it != Children.end(); ++it)
+		if ((*it)->isNotClipped() ||
+				AbsoluteClippingRect.isRectCollided(
+						(*it)->getAbsolutePosition()))
+			(*it)->draw();
 
 	for (gui::IGUIElement *e : m_clickthrough_elements)
 		e->setVisible(false);
