minetest/minetest

commit eca6ee911a99f863abf79ab956d4c757e9e4bae3

Fix mapgen settings in minetest.conf being ignored (#9737)
broken since e8a8185d24897ccf964327017effae81aa1c9d40

src/map_settings_manager.cpp
@@ -32,7 +32,6 @@ MapSettingsManager::MapSettingsManager(Settings *user_settings,
 	m_user_settings(user_settings)
 {
 	assert(m_user_settings != NULL);
-	Mapgen::setDefaultSettings(m_map_settings);
 }
 
 
@@ -179,6 +178,16 @@ MapgenParams *MapSettingsManager::makeMapgenParams()
 		return nullptr;
 
 	params->mgtype = mgtype;
+
+	// Load the mapgen param defaults
+	/* FIXME: Why is it done like this? MapgenParams should just
+	 * set the defaults in its constructor instead. */
+	{
+		Settings default_settings;
+		Mapgen::setDefaultSettings(&default_settings);
+		params->MapgenParams::readParams(&default_settings);
+		params->readParams(&default_settings);
+	}
 
 	// Load the rest of the mapgen params from our active settings
 	params->MapgenParams::readParams(m_user_settings);

commit 14d521f7c2ad000e4541b14c61d9998f14f97b6c

Mapgen: Fix on-by-default flags broken since eca6ee9

src/map_settings_manager.cpp
@@ -32,6 +32,10 @@ MapSettingsManager::MapSettingsManager(Settings *user_settings,
 	m_user_settings(user_settings)
 {
 	assert(m_user_settings != NULL);
+
+	Mapgen::setDefaultSettings(m_map_settings);
+	// This inherits the combined defaults provided by loadGameConfAndInitWorld.
+	m_map_settings->overrideDefaults(user_settings);
 }
 
 
@@ -179,20 +183,8 @@ MapgenParams *MapSettingsManager::makeMapgenParams()
 
 	params->mgtype = mgtype;
 
-	// Load the mapgen param defaults
-	/* FIXME: Why is it done like this? MapgenParams should just
-	 * set the defaults in its constructor instead. */
-	{
-		Settings default_settings;
-		Mapgen::setDefaultSettings(&default_settings);
-		params->MapgenParams::readParams(&default_settings);
-		params->readParams(&default_settings);
-	}
-
 	// Load the rest of the mapgen params from our active settings
-	params->MapgenParams::readParams(m_user_settings);
 	params->MapgenParams::readParams(m_map_settings);
-	params->readParams(m_user_settings);
 	params->readParams(m_map_settings);
 
 	// Hold onto our params