06830241b748ee941dd3e29ff3f1550044e1c242
Lars Mueller <appgurulars@gmx.de>
2020-08-28 23:08:21 +0200

    Fix

--- src/network/serverpackethandler.cpp
+++ src/network/serverpackethandler.cpp
@@ -629,21 +629,19 @@ void Server::handleCommand_InventoryAction(NetworkPacket* pkt)
 		if (ma->from_inv != ma->to_inv)
 			m_inventory_mgr->setInventoryModified(ma->to_inv);
 
-		bool from_inv_is_current_player = false;
+		bool from_inv_is_current_player = ma->from_inv.type != InventoryLocation::CURRENT_PLAYER;
 		if (ma->from_inv.type == InventoryLocation::PLAYER) {
 			if (ma->from_inv.name != player->getName())
 				return;
 			from_inv_is_current_player = true;
-		} else if (ma->from_inv.type != InventoryLocation::CURRENT_PLAYER)
-			from_inv_is_current_player = true;
+		}
 		
-		bool to_inv_is_current_player = false;
+		bool to_inv_is_current_player = ma->to_inv.type != InventoryLocation::CURRENT_PLAYER;
 		if (ma->to_inv.type == InventoryLocation::PLAYER) {
 			if (ma->to_inv.name != player->getName())
 				return;
 			to_inv_is_current_player = true;
-		} else if (ma->to_inv.type != InventoryLocation::CURRENT_PLAYER)
-			to_inv_is_current_player = true;
+		}
 
 		InventoryLocation *remote = from_inv_is_current_player ?
 			&ma->to_inv : &ma->from_inv;
