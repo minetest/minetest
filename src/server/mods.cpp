316375772d19da971b6ceb34aa74a5b798ae719f
Lars Mueller <appgurulars@gmx.de>
2020-09-06 12:57:08 +0200

    Make clang-format happy

--- src/server/mods.cpp
+++ src/server/mods.cpp
@@ -104,6 +104,6 @@ void ServerModManager::getModsMediaPaths(std::vector<std::string> &paths) const
 		fs::GetRecursiveDirs(paths, spec.path + DIR_DELIM + "media");
 		fs::GetRecursiveDirs(paths, spec.path + DIR_DELIM + "models");
 		fs::GetRecursiveDirs(paths, spec.path + DIR_DELIM + "locale");
-    fs::GetRecursiveDirs(paths, spec.path + DIR_DELIM + "shaders");
+		fs::GetRecursiveDirs(paths, spec.path + DIR_DELIM + "shaders");
 	}
 }
