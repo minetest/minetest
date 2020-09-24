163366019d4572b71b45fd8c97da821016d30e06
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-24 11:01:01 +0200

    don't allocate a tty for "docker run" commands

--- util/integration-test.sh
+++ util/integration-test.sh
@@ -3,7 +3,7 @@
 CFG=/tmp/minetest.conf
 MINETEST_VOLUME=minetest
 
-docker run --rm -it -v ${MINETEST_VOLUME}:/minetest alpine /bin/sh -c "chown -R 30000.30000 /minetest"
+docker run --rm -i -v ${MINETEST_VOLUME}:/minetest alpine /bin/sh -c "chown -R 30000.30000 /minetest"
 
 # settings
 cat <<EOF > ${CFG}
@@ -17,4 +17,4 @@ docker run --rm -i \
 	-v ${MINETEST_VOLUME}:/var/lib/minetest/.minetest \
 	minetest
 
-docker run --rm -it -v ${MINETEST_VOLUME}:/minetest alpine /bin/sh -c "test -f /minetest/worlds/world/test_passed" && exit 0 || exit 1
+docker run --rm -i -v ${MINETEST_VOLUME}:/minetest alpine /bin/sh -c "test -f /minetest/worlds/world/test_passed" && exit 0 || exit 1
