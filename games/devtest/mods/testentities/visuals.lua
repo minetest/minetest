8c357f151fa704d1d853fb5e0b76522fd3fe2353
rubenwardy <rw@rubenwardy.com>
2020-10-13 17:01:50 +0100

    Vary nametag background based on text color, add test entity to devtest

--- games/devtest/mods/testentities/visuals.lua
+++ games/devtest/mods/testentities/visuals.lua
@@ -82,3 +82,33 @@ minetest.register_entity("testentities:yawsprite", {
 		self.object:set_sprite({x=0, y=0}, 1, 0, true)
 	end,
 })
+
+minetest.register_entity("testentities:nametag", {
+	initial_properties = {
+		visual = "sprite",
+		textures = { "testentities_sprite.png" },
+	},
+
+	on_activate = function(self, staticdata)
+		if staticdata ~= "" then
+			print(staticdata)
+			self.color = minetest.deserialize(staticdata).color
+		else
+			self.color = {
+				r = math.random(0, 255),
+				g = math.random(0, 255),
+				b = math.random(0, 255),
+			}
+		end
+
+		assert(self.color)
+		self.object:set_properties({
+			nametag = tostring(math.random(1000, 10000)),
+			nametag_color = self.color,
+		})
+	end,
+
+	get_staticdata = function(self)
+		return minetest.serialize({ color = self.color })
+	end,
+})
