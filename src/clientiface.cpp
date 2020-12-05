5abe9ee8fba6d77122e016dbb61c7317e49a75b1
hecktest <>
2020-12-05 06:44:12 +0100

    Log the packets damn it

--- src/clientiface.cpp
+++ src/clientiface.cpp
@@ -700,7 +700,7 @@ u8 ClientInterface::patchLegacyChannel(u8 channel, u16 proto_version)
 	if (proto_version < 40) {
 		channel = legacyChannelMap.at(channel);
 		assert(channel < LEGACY_CHANNEL_COUNT);
-		verbosestream << "Patching legacy channel to " << channel << "\n";
+		verbosestream << "Patching legacy channel to " << ((int)channel) << "\n";
 	}
 	return channel;
 }
@@ -713,7 +713,7 @@ void ClientInterface::send(session_t peer_id, u8 channelnum,
 	if (rcl) {
 		channelnum = patchLegacyChannel(channelnum, rcl->net_proto_version);
 	}
-	verbosestream << "Sending out packet on channel " << channelnum << "\n";
+	verbosestream << "Sending out packet on channel " << ((int)channelnum) << "\n";
 	m_con->Send(peer_id, channelnum, pkt, reliable);
 }
 
@@ -725,8 +725,10 @@ void ClientInterface::sendToAll(NetworkPacket *pkt)
 
 		if (client->net_proto_version != 0) {
 			u8 channel = clientCommandFactoryTable[pkt->getCommand()].channel;
+			channel = patchLegacyChannel(channel, client->net_proto_version);
+			verbosestream << "Broadcasting packet on channel " << ((int)channel) << "\n";
 			m_con->Send(client->peer_id,
-					patchLegacyChannel(channel, client->net_proto_version), pkt,
+					channel, pkt,
 					clientCommandFactoryTable[pkt->getCommand()].reliable);
 		}
 	}
