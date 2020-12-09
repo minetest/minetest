961ecff8f8e380184aa283ddd47d47f3680ce8ee
hecktest <>
2020-12-09 10:16:55 +0100

    Fix missing header

--- src/util/numeric.h
+++ src/util/numeric.h
@@ -20,6 +20,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #pragma once
 
 #include "basic_macros.h"
+#include "constants.h"
 #include "irrlichttypes.h"
 #include "irr_v2d.h"
 #include "irr_v3d.h"
