045f9a6c81b4df8277133febddef2ed8affedb4c
hecktest <>
2020-12-09 09:40:02 +0100

    break one long line

--- src/client/clientmap.cpp
+++ src/client/clientmap.cpp
@@ -229,7 +229,8 @@ void ClientMap::updateDrawList()
 			v3s16 block_position = block_coord * MAP_BLOCKSIZE + MAP_BLOCKSIZE / 2;
 
 			// First, perform a simple distance check.
-			if (!m_control.range_all && block_position.getDistanceFrom(cam_pos_nodes) > range + UNLOAD_PADDING)
+			if (!m_control.range_all &&
+					block_position.getDistanceFrom(cam_pos_nodes) > range + UNLOAD_PADDING)
 				continue; // Out of range, skip.
 
 			// This block is in range. Reset usage timer.
