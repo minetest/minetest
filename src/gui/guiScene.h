d0a38f694d483fbd9c0554c8d7175a94097fd67e
Thomas--S <Thomas--S@users.noreply.github.com>
2020-12-15 19:06:36 +0100

    Formspec: Allow to specify frame loop for model[] (#10679)
    Add the ability to specify an animation frame loop range for the model[] formspec element.

--- src/gui/guiScene.h
+++ src/gui/guiScene.h
@@ -36,6 +36,7 @@ public:
 	scene::IAnimatedMeshSceneNode *setMesh(scene::IAnimatedMesh *mesh = nullptr);
 	void setTexture(u32 idx, video::ITexture *texture);
 	void setBackgroundColor(const video::SColor &color) noexcept { m_bgcolor = color; };
+	void setFrameLoop(s32 begin, s32 end);
 	void enableMouseControl(bool enable) noexcept { m_mouse_ctrl = enable; };
 	void setRotation(v2f rot) noexcept { m_custom_rot = rot; };
 	void enableContinuousRotation(bool enable) noexcept { m_inf_rot = enable; };
