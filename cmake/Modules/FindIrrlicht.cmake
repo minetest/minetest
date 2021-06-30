f4b9e1ee0c4b8bead49026dd0312afe29870c85b
Josiah <josiah_vanderzee@mediacombb.net>
2021-06-30 07:46:50 -0500

    invoke FindIrrlicht module to find headers
    The build will now use the FindIrrlicht module to locate installed Irrlicht headers, if IrrlichtMt is not installed. The previous behavior was to allow the include path to be specified on the command line only. Specifying them on the command line will still function as expected.

--- cmake/Modules/FindIrrlicht.cmake
+++ cmake/Modules/FindIrrlicht.cmake
@@ -3,31 +3,25 @@ mark_as_advanced(IRRLICHT_DLL)
 
 # Find include directory and libraries
 
-# find our fork first, then upstream (TODO: remove this?)
-foreach(libname IN ITEMS IrrlichtMt Irrlicht)
-	string(TOLOWER "${libname}" libname2)
+set(libname Irrlicht)
+string(TOLOWER "${libname}" libname2)
 
-	find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h
-		DOC "Path to the directory with IrrlichtMt includes"
-		PATHS
-		/usr/local/include/${libname2}
-		/usr/include/${libname2}
-		/system/develop/headers/${libname2} #Haiku
-		PATH_SUFFIXES "include/${libname2}"
-	)
+find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h
+	DOC "Path to the directory with IrrlichtMt includes"
+	PATHS
+	/usr/local/include/${libname2}
+	/usr/include/${libname2}
+	/system/develop/headers/${libname2} #Haiku
+	PATH_SUFFIXES "include/${libname2}"
+)
 
-	find_library(IRRLICHT_LIBRARY NAMES lib${libname} ${libname}
-		DOC "Path to the IrrlichtMt library file"
-		PATHS
-		/usr/local/lib
-		/usr/lib
-		/system/develop/lib # Haiku
-	)
-
-	if(IRRLICHT_INCLUDE_DIR OR IRRLICHT_LIBRARY)
-		break()
-	endif()
-endforeach()
+find_library(IRRLICHT_LIBRARY NAMES lib${libname} ${libname}
+	DOC "Path to the IrrlichtMt library file"
+	PATHS
+	/usr/local/lib
+	/usr/lib
+	/system/develop/lib # Haiku
+)
 
 # Handholding for users
 if(IRRLICHT_INCLUDE_DIR AND (NOT IS_DIRECTORY "${IRRLICHT_INCLUDE_DIR}" OR
