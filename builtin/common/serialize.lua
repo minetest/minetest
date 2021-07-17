5092dbfd4fe6428bbd030aa24068f514e2510206
Lars Mueller <appgurulars@gmx.de>
2021-07-17 20:10:47 +0200

    Code quality

--- builtin/common/serialize.lua
+++ builtin/common/serialize.lua
@@ -17,7 +17,9 @@ local function count_objects(value)
 	end
 	local function count_values(val)
 		local type_ = type(val)
-		if type_ == "boolean" or type_ == "number" then return end
+		if type_ == "boolean" or type_ == "number" then
+            return
+        end
 		local count = counts[val]
 		counts[val] = (count or 0) + 1
 		if not count and type_ == "table" then
@@ -32,6 +34,7 @@ local function count_objects(value)
 end
 
 -- Build a table with the succeeding character from A-Z
+-- Used to increment references quickly
 local succ = {}
 for char = ("A"):byte(), ("Z"):byte() - 1 do
 	succ[string.char(char)] = string.char(char + 1)
@@ -41,28 +44,45 @@ local function quote(string)
 	return string_format("%q", string)
 end
 
+local function dump_func(func)
+    return string_format("loadstring(%q)", string_dump(func))
+end
+
+-- Serializes Lua nil, booleans, numbers, strings, tables and even functions
+-- Tables are referenced by reference, strings are referenced by value. Supports circular tables.
 local function serialize(value, write)
+    -- References are stored as tables of characters A-Z, which are digits of a base 26 number
 	local reference = {"A"}
 	local function increment_reference(place)
 		if not reference[place] then
 			reference[place] = "B"
 		elseif reference[place] == "Z" then
+            -- Carriage
 			reference[place] = "A"
 			return increment_reference(place + 1)
 		else
 			reference[place] = succ[reference[place]]
 		end
 	end
+    -- [object] = reference string
 	local references = {}
+    -- Circular tables that must be filled using `table[key] = value` statements
 	local to_fill = {}
 	for object, count in pairs(count_objects(value)) do
 		local type_ = type(object)
+        -- Object must appear more than once. If it is a string, the reference has to be shorter than the string.
 		if count >= 2 and (type_ ~= "string" or #reference + 2 < #object) then
 			local ref = table_concat(reference)
 			write(ref)
-			write"="
-			write(type_ == "table" and "{}" or quote(object))
-			write";"
+			write("=")
+            if type_ == "table" then
+                write("{}")
+            elseif type_ == "function" then
+                write(dump_func(object))
+            elseif type_ == "string" then
+                write(quote(object))
+            end
+			write(";")
 			references[object] = ref
 			if type_ == "table" then
 				to_fill[object] = ref
@@ -70,79 +90,86 @@ local function serialize(value, write)
 			increment_reference(1)
 		end
 	end
-	local function is_short_key(key)
+	local function use_short_key(key)
 		return not references[key] and type(key) == "string" and string_match(key, "^[%a_][%a%d_]*$")
 	end
 	local function dump(value)
 		-- Primitive types
 		if value == nil then
-			return write"nil"
+			return write("nil")
 		end
 		if value == true then
-			return write"true"
+			return write("true")
 		end
 		if value == false then
-			return write"false"
+			return write("false")
 		end
 		local type_ = type(value)
 		if type_ == "number" then
 			return write(string_format("%.17g", value))
 		end
-		if type_ == "function" then
-			return write(string_format("loadstring(%q)", string_dump(value)))
-		end
-		-- Reference types: table and string
+		-- Reference types: table, function and string
 		local ref = references[value]
 		if ref then
 			return write(ref)
-		elseif type_ == "string" then
+		end
+        if type_ == "string" then
 			return write(quote(value))
-		elseif type_ == "table" then
+		end
+        if type_ == "function" then
+			return write(dump_func(value))
+		end
+        if type_ == "table" then
 			local first = true
-			write"{"
+			write("{")
+            -- First write list keys
 			local len = #value
 			for i = 1, len do
-				if not first then write";" end
+				if not first then write(";") end
 				dump(value[i])
 				first = false
 			end
+            -- Now write map keys ([key] = value)
 			for k, v in next, value do
+                -- Check whether this is a non-list key (hash key)
 				if type(k) ~= "number" or k % 1 ~= 0 or k < 1 or k > len then
-					if not first then write";" end
-					if is_short_key(k) then
+					if not first then
+                        write(";")
+                    end
+					if use_short_key(k) then
 						write(k)
 					else
-						write"["
+						write("[")
 						dump(k)
-						write"]"
+						write("]")
 					end
-					write"="
+					write("=")
 					dump(v)
 					first = false
 				end
 			end
-			write"}"
-		else
-			error("unsupported type: " .. type_)
-		end
+			write("}")
+            return
+        end
+		error("unsupported type: " .. type_)
 	end
 	for table, ref in pairs(to_fill) do
 		for k, v in pairs(table) do
 			write(ref)
-			if is_short_key(k) then
-				write"."
+			if use_short_key(k) then
+				write(".")
 				write(k)
 			else
-				write"["
+				write("[")
 				dump(k)
-				write"]"
+				write("]")
 			end
-			write"="
+			write("=")
 			dump(v)
-			write";"
+			write(";")
 		end
 	end
-	write"return "
+	write("return ")
 	dump(value)
 end
 
