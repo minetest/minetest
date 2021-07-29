1e2b6388818fec0d4cdc52f796850bfb7ec3a22e
hecks <42101236+hecktest@users.noreply.github.com>
2021-07-29 22:36:25 +0200

    Remove unsupported formats from the media enumerator

--- src/server.cpp
+++ src/server.cpp
@@ -2438,10 +2438,9 @@ bool Server::addMediaFile(const std::string &filename,
 	}
 	// If name is not in a supported format, ignore it
 	const char *supported_ext[] = {
-		".png", ".jpg", ".bmp", ".tga",
-		".pcx", ".ppm", ".psd", ".wal", ".rgb",
+		".png", ".jpg", ".bmp",
 		".ogg",
-		".x", ".b3d", ".md2", ".obj",
+		".x", ".b3d", ".obj",
 		// Custom translation file format
 		".tr",
 		NULL
