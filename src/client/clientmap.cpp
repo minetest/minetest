4b6d05735be23326982420818f9bdfa6da893652
x2048 <codeforsmile@gmail.com>
2021-10-15 21:00:19 +0200

    Fix typo

--- src/client/clientmap.cpp
+++ src/client/clientmap.cpp
@@ -477,7 +477,7 @@ void ClientMap::renderMap(video::IVideoDriver* driver, s32 pass)
 			buf = descriptor.m_buffer;
 		}
 
-		Check and abort if the machine is swapping a lot
+		// Check and abort if the machine is swapping a lot
 		if (draw.getTimerTime() > 2000) {
 			infostream << "ClientMap::renderMap(): Rendering took >2s, " <<
 					"returning." << std::endl;
