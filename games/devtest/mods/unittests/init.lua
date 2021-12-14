eab4c79df3cd32dfe451982f138b39f6d96e2f45
sfan5 <sfan5@live.de>
2021-12-14 18:28:27 +0100

    async shit works but is awful

--- games/devtest/mods/unittests/init.lua
+++ games/devtest/mods/unittests/init.lua
@@ -3,10 +3,15 @@ unittests = {}
 unittests.list = {}
 
 -- name: Name of the test
--- func: function(player, pos), should error on failure
+-- func:
+--   for sync: function(player, pos), should error on failure
+--   for async: function(callback, player, pos)
+--     MUST call callback() or callback("error msg") in case of error once test is finished
+--     this means you cannot use assert() in the test implementation
 -- opts: {
 --   player = false, -- Does test require a player?
 --   map = false, -- Does test require map access?
+--   async = false, -- Does the test run asynchronously? (read notes above!)
 -- }
 function unittests.register(name, func, opts)
 	local def = table.copy(opts or {})
@@ -19,34 +24,51 @@ function unittests.on_finished(all_passed)
 	-- free to override
 end
 
-function unittests.run_one(idx, counters, player, pos)
+function unittests.run_one(idx, counters, out_callback, player, pos)
 	local def = unittests.list[idx]
 	if not def.player then
 		player = nil
 	elseif player == nil then
+		core.after(0, out_callback) -- fuck
 		return false
 	end
 	if not def.map then
 		pos = nil
 	elseif pos == nil then
+		core.after(0, out_callback) -- fuck
 		return false
 	end
 
 	local tbegin = core.get_us_time()
-	local status, err = pcall(def.func, player, pos)
-	local tend = core.get_us_time()
+	local function done(status, err)
+		local tend = core.get_us_time()
+		local ms_taken = (tend - tbegin) / 1000
 
-	local ms_taken = (tend - tbegin) / 1000
-	if not status then
-		core.log("error", err)
+		if not status then
+			core.log("error", err)
+		end
+		print(string.format("[%s] %s - %dms",
+			status and "PASS" or "FAIL", def.name, ms_taken))
+		counters.time = counters.time + ms_taken
+		counters.total = counters.total + 1
+		if status then
+			counters.passed = counters.passed + 1
+		end
 	end
-	print(string.format("[%s] %s - %dms",
-		status and "PASS" or "FAIL", def.name, ms_taken))
-	counters.time = counters.time + ms_taken
-	counters.total = counters.total + 1
-	if status then
-		counters.passed = counters.passed + 1
+
+	if def.async then
+		core.log("info", "[unittest] running " .. def.name .. " (async)")
+		def.func(function(err)
+			done(err == nil, err)
+			core.after(0, out_callback) -- fuck
+		end, player, pos)
+	else
+		core.log("info", "[unittest] running " .. def.name)
+		local status, err = pcall(def.func, player, pos)
+		done(status, err)
+		core.after(0, out_callback) -- fuck
 	end
+	
 	return true
 end
 
@@ -72,30 +94,56 @@ local function wait_for_map(player, callback)
 end
 
 function unittests.run_all()
+	-- I tried to make this code as good as it can be but async is still a mess in Lua
+	local stage = 1
+	local idx = 1
+	local player, pos
 	local counters = { time = 0, total = 0, passed = 0 }
-	-- Run standalone tests first
-	for idx, def in ipairs(unittests.list) do
-		def.done = unittests.run_one(idx, counters, nil, nil)
-	end
 
-	-- Wait for a player to join, run tests that require a player
-	wait_for_player(function(player)
-		for idx, def in ipairs(unittests.list) do
-			if not def.done then
-				def.done = unittests.run_one(idx, counters, player, nil)
+	local function next()
+		if stage == 1 then -- Stage 1: standalone tests
+			if idx > #unittests.list then
+				stage = 2
+				idx = 1
+				-- Wait for a player to join
+				return wait_for_player(function(player_)
+					player = player_
+					next()
+				end)
 			end
-		end
-
-		-- Wait for the world to generate/load, run tests that require map access
-		wait_for_map(player, function()
-			local pos = vector.round(player:get_pos())
-			for idx, def in ipairs(unittests.list) do
-				if not def.done then
-					def.done = unittests.run_one(idx, counters, player, pos)
-				end
+			local def = unittests.list[idx]
+			def.done = unittests.run_one(idx, counters, next, nil, nil)
+			idx = idx + 1
+		elseif stage == 2 then -- Stage 2: tests that require a player
+			if idx > #unittests.list then
+				stage = 3
+				idx = 1
+				-- Wait for world to generate/load
+				return wait_for_map(player, function()
+					pos = vector.round(player:get_pos())
+					next()
+				end)
 			end
-
-			-- Print stats
+			local def = unittests.list[idx]
+			if def.done then
+				idx = idx + 1
+				return next()
+			end
+			def.done = unittests.run_one(idx, counters, next, player, nil)
+			idx = idx + 1
+		elseif stage == 3 then -- Stage 3: tests that require map access
+			if idx > #unittests.list then
+				stage = 4
+				return next()
+			end
+			local def = unittests.list[idx]
+			if def.done then
+				idx = idx + 1
+				return next()
+			end
+			def.done = unittests.run_one(idx, counters, next, player, pos)
+			idx = idx + 1
+		elseif stage == 4 then -- Stage 4: print stats and exit, phew
 			assert(#unittests.list == counters.total)
 			print(string.rep("+", 80))
 			print(string.format("Unit Test Results: %s",
@@ -105,8 +153,10 @@ function unittests.run_all()
 			print(string.format("    Testing took %dms total.", counters.time))
 			print(string.rep("+", 80))
 			unittests.on_finished(counters.total == counters.passed)
-		end)
-	end)
+		end
+	end
+
+	next()
 end
 
 --------------
@@ -122,4 +172,3 @@ dofile(modpath .. "/itemdescription.lua")
 if core.settings:get_bool("devtest_unittests_autostart", false) then
 	core.after(0, unittests.run_all)
 end
-
