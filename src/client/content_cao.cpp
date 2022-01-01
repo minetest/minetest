da55fe80d9f60262f2dfd0840ee13fa259b7b024
Dmitry Kostenko <codeforsmile@gmail.com>
2022-01-01 01:17:36 +0100

    Recalculate normals before adding mesh to the scene.
    Fixes calulation of normals for models where the first animation frame is not the same as static position.

--- src/client/content_cao.cpp
+++ src/client/content_cao.cpp
@@ -764,10 +764,9 @@ void GenericCAO::addToScene(ITextureSource *tsrc, scene::ISceneManager *smgr)
 		grabMatrixNode();
 		scene::IAnimatedMesh *mesh = m_client->getMesh(m_prop.mesh, true);
 		if (mesh) {
-			m_animated_meshnode = m_smgr->addAnimatedMeshSceneNode(mesh, m_matrixnode);
-			m_animated_meshnode->grab();
-			mesh->drop(); // The scene node took hold of it
-
+			// It is important to recalculate normals before adding the mesh to the scene,
+			// because the latter starts animation process and changes vertex positions,
+			// which are treated as a source of truth when recalculating normals
 			if (!checkMeshNormals(mesh)) {
 				infostream << "GenericCAO: recalculating normals for mesh "
 					<< m_prop.mesh << std::endl;
@@ -775,6 +774,9 @@ void GenericCAO::addToScene(ITextureSource *tsrc, scene::ISceneManager *smgr)
 						recalculateNormals(mesh, true, false);
 			}
 
+			m_animated_meshnode = m_smgr->addAnimatedMeshSceneNode(mesh, m_matrixnode);
+			m_animated_meshnode->grab();
+			mesh->drop(); // The scene node took hold of it
 			m_animated_meshnode->animateJoints(); // Needed for some animations
 			m_animated_meshnode->setScale(m_prop.visual_size);
 
