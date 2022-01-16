e10ab382466d8e039eb45ab33bcb4627b25e843b
Dmitry Kostenko <codeforsmile@gmail.com>
2022-01-16 02:52:42 +0100

    Test brightness and tint

--- games/devtest/mods/experimental/lighting.lua
+++ games/devtest/mods/experimental/lighting.lua
@@ -1,7 +1,13 @@
 core.register_chatcommand("set_lighting", {
-    params = "",
-    description = "Set lighting parameters.",
+    params = "<brightness> [color]",
+    description = "Set lighting parameters. Brightness is a number from 0 to 1, color is an #RRGGBB color",
     func = function(player_name, param)
-        minetest.get_player_by_name(player_name):set_lighting({})
+        local brightness, color_tint = string.match(param, "([^ ]+)(.*)$");
+        if not brightness then
+            return false, "Invalid input, see /help set_lighting"
+        end
+        color_tint = string.gsub(color_tint, " ", "")
+        if color_tint == "" then color_tint = nil end
+        minetest.get_player_by_name(player_name):set_lighting({brightness=tonumber(brightness), color_tint = color_tint})
     end
 })
