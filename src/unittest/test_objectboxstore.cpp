26ecd2dedbbf680d70263c016514f2183062482c
JosiahWI <josiah_vanderzee@mediacombb.net>
2022-01-17 17:19:48 -0600

    add name to copyright notices

--- src/unittest/test_objectboxstore.cpp
+++ src/unittest/test_objectboxstore.cpp
@@ -1,6 +1,6 @@
 /*
 Minetest
-Copyright (C) 2015 est31, <MTest31@outlook.com>
+Copyright (C) 2022 JosiahWI, <MTest31@outlook.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
--- src/util/spatial_tools.cpp
+++ src/util/spatial_tools.cpp
@@ -1,6 +1,6 @@
 /*
 Minetest
-Copyright (C) 2022 est31 <mtest31@outlook.com>
+Copyright (C) 2022 JosiahWI <mtest31@outlook.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
