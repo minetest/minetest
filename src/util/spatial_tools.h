e459c5e81297679cab683975c5a7a889c8725387
JosiahWI <josiah_vanderzee@mediacombb.net>
2022-01-18 16:05:46 -0600

    use correct include headers for irrlicht types

--- src/util/spatial_tools.h
+++ src/util/spatial_tools.h
@@ -19,7 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #pragma once
 
-#include "irr_aabb3d.h"
+#include "irrlichttypes.h"
 #include "util/areastore.h"
 
 #include <spatialindex/SpatialIndex.h>
