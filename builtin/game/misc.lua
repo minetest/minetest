e11fd787e349b3aba10f9c189e23e7cfdc119113
AFCMS <afcm.contact@gmail.com>
2022-03-30 19:55:34 +0200

    expose `minetest.setting_get_pos` to CSM API

--- builtin/game/misc.lua
+++ builtin/game/misc.lua
@@ -121,6 +121,44 @@ function core.get_player_radius_area(player_name, radius)
 end
 
 
+function core.hash_node_position(pos)
+	return (pos.z + 32768) * 65536 * 65536
+		 + (pos.y + 32768) * 65536
+		 +  pos.x + 32768
+end
+
+
+function core.get_position_from_hash(hash)
+	local x = (hash % 65536) - 32768
+	hash  = math.floor(hash / 65536)
+	local y = (hash % 65536) - 32768
+	hash  = math.floor(hash / 65536)
+	local z = (hash % 65536) - 32768
+	return vector.new(x, y, z)
+end
+
+
+function core.get_item_group(name, group)
+	if not core.registered_items[name] or not
+			core.registered_items[name].groups[group] then
+		return 0
+	end
+	return core.registered_items[name].groups[group]
+end
+
+
+function core.get_node_group(name, group)
+	core.log("deprecated", "Deprecated usage of get_node_group, use get_item_group instead")
+	return core.get_item_group(name, group)
+end
+
+
+-- See l_env.cpp for the other functions
+function core.get_artificial_light(param1)
+	return math.floor(param1 / 16)
+end
+
+
 -- To be overriden by protection mods
 
 function core.is_protected(pos, name)
