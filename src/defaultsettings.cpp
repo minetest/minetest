8f1b8ad1308a10465a39dc0e7696b0ec3fa7e4da
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-05-04 15:56:04 -0500

    Remove comment
    Co-authored-by: rubenwardy <rw@rubenwardy.com>

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -65,7 +65,6 @@ void set_default_settings()
 	settings->setDefault("max_out_chat_queue_size", "20");
 	settings->setDefault("pause_on_lost_focus", "false");
 	settings->setDefault("enable_register_confirmation", "true");
-	// settings->setDefault("clickable_chat_weblinks", "true"); // Moved below
 	settings->setDefault("chat_weblink_color", "#8888FF");
 
 	// Keymap
