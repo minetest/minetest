943239d19bc71f69a83f4324c9cf790177ad3b9f
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-05-04 15:51:48 -0500

    Update defaultsettings.cpp

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -65,7 +65,7 @@ void set_default_settings()
 	settings->setDefault("max_out_chat_queue_size", "20");
 	settings->setDefault("pause_on_lost_focus", "false");
 	settings->setDefault("enable_register_confirmation", "true");
-	settings->setDefault("clickable_chat_weblinks", "true");
+	// settings->setDefault("clickable_chat_weblinks", "true"); // Moved below
 	settings->setDefault("chat_weblink_color", "#8888FF");
 
 	// Keymap
@@ -465,6 +465,9 @@ void set_default_settings()
 	settings->setDefault("touchscreen_threshold","20");
 	settings->setDefault("fixed_virtual_joystick", "false");
 	settings->setDefault("virtual_joystick_triggers_aux1", "false");
+	settings->setDefault("clickable_chat_weblinks", "false");
+#else
+	settings->setDefault("clickable_chat_weblinks", "true");
 #endif
 	// Altered settings for Android
 #ifdef __ANDROID__
