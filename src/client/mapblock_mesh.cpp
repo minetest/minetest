16f1a4622284142caea92fd35df619da32ad3850
paradust7 <102263465+paradust7@users.noreply.github.com>
2022-05-06 09:29:15 -0700

    Update src/client/mapblock_mesh.cpp
    Co-authored-by: sfan5 <sfan5@live.de>

--- src/client/mapblock_mesh.cpp
+++ src/client/mapblock_mesh.cpp
@@ -1167,6 +1167,7 @@ void PartialMeshBuffer::beforeDraw() const
 }
 
 void PartialMeshBuffer::afterDraw() const {
+	// Take the data back
 	m_vertex_indexes = std::move(m_buffer->Indices.steal());
 }
 
