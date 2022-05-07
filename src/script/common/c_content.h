a214087b9d394ca98ecd8beb665d0d771499a10c
rubenwardy <rw@rubenwardy.com>
2022-05-07 16:45:17 +0100

    Add check_mod_configuration to main menu

--- src/script/common/c_content.h
+++ src/script/common/c_content.h
@@ -42,6 +42,7 @@ extern "C" {
 // We do a explicit path include because by default c_content.h include src/client/hud.h
 // prior to the src/hud.h, which is not good on server only build
 #include "../../hud.h"
+#include "content/mods.h"
 
 namespace Json { class Value; }
 
@@ -204,3 +205,5 @@ void push_hud_element          (lua_State *L, HudElement *elem);
 bool read_hud_change           (lua_State *L, HudElementStat &stat, HudElement *elem, void **value);
 
 void push_collision_move_result(lua_State *L, const collisionMoveResult &res);
+
+void push_mod_spec(lua_State *L, const ModSpec &spec, bool include_unsatisfied);
