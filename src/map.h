eaf3f125ef14b7554402fffdc959ad52be1aff7a
Richard Try <richardtrybox@protonmail.com>
2022-05-11 22:47:08 +0300

    unordered_map instead of map for MapSectors

--- src/map.h
+++ src/map.h
@@ -266,7 +266,7 @@ protected:
 
 	std::set<MapEventReceiver*> m_event_receivers;
 
-	std::map<v2s16, MapSector*> m_sectors;
+	std::unordered_map<v2s16, MapSector*> m_sectors;
 
 	// Be sure to set this to NULL when the cached sector is deleted
 	MapSector *m_sector_cache = nullptr;
