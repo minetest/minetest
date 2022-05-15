e47ce539cd1108e0afb80573cbf111fa212615a9
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-05-15 22:25:24 +0200

    Raise default windowed resolution to 1280x720 (720p)

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -174,8 +174,8 @@ void set_default_settings()
 #if ENABLE_GLES
 	settings->setDefault("near_plane", "0.1");
 #endif
-	settings->setDefault("screen_w", "1024");
-	settings->setDefault("screen_h", "600");
+	settings->setDefault("screen_w", "1280");
+	settings->setDefault("screen_h", "720");
 	settings->setDefault("autosave_screensize", "true");
 	settings->setDefault("fullscreen", "false");
 	settings->setDefault("vsync", "false");
