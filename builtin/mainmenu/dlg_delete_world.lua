d6754bd9f8990da93268c78501e0d48d88905a70
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-05-15 22:24:14 +0200

    Rework confirmation dialogs

--- builtin/mainmenu/dlg_delete_world.lua
+++ builtin/mainmenu/dlg_delete_world.lua
@@ -17,14 +17,10 @@
 
 
 local function delete_world_formspec(dialogdata)
-	local retval =
-		"size[10,2.5,true]" ..
-		"label[0.5,0.5;" ..
-		fgettext("Delete World \"$1\"?", dialogdata.delete_name) .. "]" ..
-		"style[world_delete_confirm;bgcolor=red]" ..
-		"button[0.5,1.5;2.5,0.5;world_delete_confirm;" .. fgettext("Delete") .. "]" ..
-		"button[7.0,1.5;2.5,0.5;world_delete_cancel;" .. fgettext("Cancel") .. "]"
-	return retval
+	return confirmation_formspec(
+		fgettext("Delete World \"$1\"?", dialogdata.delete_name),
+		'world_delete_confirm', fgettext("Delete"),
+		'world_delete_cancel', fgettext("Cancel"))
 end
 
 local function delete_world_buttonhandler(this, fields)
