bc59fcf5c5cc44ea18e93f64aca9c20be71c1b07
sfan5 <sfan5@live.de>
2022-05-22 00:11:22 +0200

    Bump IrrlichtMt version in CI

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -13,7 +13,7 @@ install_linux_deps() {
 		shift
 		pkgs+=(libirrlicht-dev)
 	else
-		wget "https://github.com/minetest/irrlicht/releases/download/1.9.0mt5/ubuntu-bionic.tar.gz"
+		wget "https://github.com/minetest/irrlicht/releases/download/1.9.0mt6/ubuntu-bionic.tar.gz"
 		sudo tar -xaf ubuntu-bionic.tar.gz -C /usr/local
 	fi
 
