16a30556dfa8e27c82d026bd63467f82d2e37a1c
sfan5 <sfan5@live.de>
2022-05-22 00:52:11 +0200

    Formally drop support for building with upstream Irrlicht
    It stopped working with (at least) the last commit.

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -9,9 +9,6 @@ install_linux_deps() {
 
 	if [[ "$1" == "--no-irr" ]]; then
 		shift
-	elif [[ "$1" == "--old-irr" ]]; then
-		shift
-		pkgs+=(libirrlicht-dev)
 	else
 		wget "https://github.com/minetest/irrlicht/releases/download/1.9.0mt6/ubuntu-bionic.tar.gz"
 		sudo tar -xaf ubuntu-bionic.tar.gz -C /usr/local
