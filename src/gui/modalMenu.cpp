a5170c3cd2e03befafb5591b799f2395ec32dcb7
sfan5 <sfan5@live.de>
2022-05-25 18:27:34 +0200

    Improve a translation string
    fixes #11442

--- src/gui/modalMenu.cpp
+++ src/gui/modalMenu.cpp
@@ -252,13 +252,11 @@ bool GUIModalMenu::preprocessEvent(const SEvent &event)
 				return retval;
 
 			m_jni_field_name = field_name;
-			/*~ Imperative, as in "Enter/type in text".
-			Don't forget the space. */
-			std::string message = gettext("Enter ");
 			std::string label = wide_to_utf8(getLabelByID(hovered->getID()));
 			if (label.empty())
 				label = "text";
-			message += strgettext(label) + ":";
+			/*~ Imperative, as in "Type in text" */
+			std::string message = fmtgettext("Enter %s:");
 
 			// single line text input
 			int type = 2;
