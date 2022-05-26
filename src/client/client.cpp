minetest/minetest

commit 2f32044273d107e82fb1c35d4a0f616fa480cdf0

Don't ignore server disconnects in client code
If the server stops talking to us without saying bye we
should actually end the in-game session with an error message.

src/client/client.cpp
@@ -786,16 +786,18 @@ void Client::peerAdded(con::Peer *peer)
 	infostream << "Client::peerAdded(): peer->id="
 			<< peer->id << std::endl;
 }
+
 void Client::deletingPeer(con::Peer *peer, bool timeout)
 {
 	infostream << "Client::deletingPeer(): "
 			"Server Peer is getting deleted "
 			<< "(timeout=" << timeout << ")" << std::endl;
 
-	if (timeout) {
-		m_access_denied = true;
+	m_access_denied = true;
+	if (timeout)
 		m_access_denied_reason = gettext("Connection timed out.");
-	}
+	else
+		m_access_denied_reason = gettext("Connection aborted (protocol error?).");
 }
 
 /*

commit 440e57b1f8af7886fdd6abe328b06dd217e5a353

Make sure real disconnect reason isn't overwritten
bug introduced in 2f32044273d107e82fb1c35d4a0f616fa480cdf0

src/client/client.cpp
@@ -796,7 +796,7 @@ void Client::deletingPeer(con::Peer *peer, bool timeout)
 	m_access_denied = true;
 	if (timeout)
 		m_access_denied_reason = gettext("Connection timed out.");
-	else
+	else if (m_access_denied_reason.empty())
 		m_access_denied_reason = gettext("Connection aborted (protocol error?).");
 }
 