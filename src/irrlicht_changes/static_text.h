622d857bed52404c7d54bcf7ff056d0c0250f2be
Nathanaël Courant <nathanael.courant@inria.fr>
2022-06-16 21:20:12 +0200

    Update my name

--- src/irrlicht_changes/static_text.h
+++ src/irrlicht_changes/static_text.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2002-2012 Nikolaus Gebhardt
-// Copyright (C) 2016 Nathanaël Courant
+// Copyright (C) 2016 Nathanaëlle Courant
 //   Modified this class to work with EnrichedStrings too
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in irrlicht.h
