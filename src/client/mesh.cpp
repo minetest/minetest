4db7d40ec2426f996968e3317046b5c67d0ae0e1
Dmitry Kostenko <codeforsmile@gmail.com>
2022-06-19 20:47:06 +0200

    Remove workaround for normals not matching winding order

--- src/client/mesh.cpp
+++ src/client/mesh.cpp
@@ -346,19 +346,6 @@ bool checkMeshNormals(scene::IMesh *mesh)
 
 		if (!std::isfinite(length) || length < 1e-10f)
 			return false;
-
-		const u16 count = MYMIN(MAX_FACES_TO_CHECK * 3, buffer->getIndexCount() - 3);
-		for (u16 i = 0; i < count; i += 3) {
-
-			core::plane3df plane(buffer->getPosition(buffer->getIndices()[i]),
-					buffer->getPosition(buffer->getIndices()[i+1]),
-					buffer->getPosition(buffer->getIndices()[i+2]));
-
-			for (u16 j = 0; j < 3; j++)
-				if (plane.Normal.dotProduct(buffer->getNormal(buffer->getIndices()[i+j])) <= 0)
-					return false;
-		}
-
 	}
 
 	return true;
