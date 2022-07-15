43c40338fbc237c55cc66881a3628a0671af1e24
Dmitry Kostenko <codeforsmile@gmail.com>
2022-07-15 03:56:32 +0200

    Move tonemapping code to post-processing shader

--- client/shaders/nodes_shader/opengl_fragment.glsl
+++ client/shaders/nodes_shader/opengl_fragment.glsl
@@ -362,40 +362,6 @@ float getShadow(sampler2D shadowsampler, vec2 smTexCoord, float realDistance)
 #endif
 #endif
 
-#if ENABLE_TONE_MAPPING
-
-/* Hable's UC2 Tone mapping parameters
-	A = 0.22;
-	B = 0.30;
-	C = 0.10;
-	D = 0.20;
-	E = 0.01;
-	F = 0.30;
-	W = 11.2;
-	equation used:  ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F
-*/
-
-vec3 uncharted2Tonemap(vec3 x)
-{
-	return ((x * (0.22 * x + 0.03) + 0.002) / (x * (0.22 * x + 0.3) + 0.06)) - 0.03333;
-}
-
-vec4 applyToneMapping(vec4 color)
-{
-	color = vec4(pow(color.rgb, vec3(2.2)), color.a);
-	const float gamma = 1.6;
-	const float exposureBias = 5.5;
-	color.rgb = uncharted2Tonemap(exposureBias * color.rgb);
-	// Precalculated white_scale from
-	//vec3 whiteScale = 1.0 / uncharted2Tonemap(vec3(W));
-	vec3 whiteScale = vec3(1.036015346);
-	color.rgb *= whiteScale;
-	return vec4(pow(color.rgb, vec3(1.0 / gamma)), color.a);
-}
-#endif
-
-
-
 void main(void)
 {
 	vec3 color;
@@ -471,10 +437,6 @@ void main(void)
 						col.rgb * (1.0 - shadow_int * (1.0 - shadow_color)) +  // filtered texture color
 						dayLight * shadow_color * shadow_int);                 // reflected filtered sunlight/moonlight
 	}
-#endif
-
-#if ENABLE_TONE_MAPPING
-	col = applyToneMapping(col);
 #endif
 
 	// Due to a bug in some (older ?) graphics stacks (possibly in the glsl compiler ?),
