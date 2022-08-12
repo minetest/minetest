8a3d53271797221745e819f4a499277bad841fa7
Lars Mueller <appgurulars@gmx.de>
2022-08-12 13:37:32 +0200

    Make automatic rotate work serverside for attached entities

--- src/server/luaentity_sao.cpp
+++ src/server/luaentity_sao.cpp
@@ -180,11 +180,6 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 			m_velocity += dtime * m_acceleration;
 		}
 
-		if (fabs(m_prop.automatic_rotate) > 0.001f) {
-			m_rotation_add_yaw = modulo360f(m_rotation_add_yaw + dtime * core::RADTODEG *
-					m_prop.automatic_rotate);
-		}
-
 		if (m_prop.automatic_face_movement_dir &&
 				(fabs(m_velocity.Z) > 0.001 || fabs(m_velocity.X) > 0.001)) {
 			float target_yaw = atan2(m_velocity.Z, m_velocity.X) * 180 / M_PI
@@ -201,6 +196,11 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 				m_rotation.Y = target_yaw;
 			}
 		}
+	}
+
+	if (fabs(m_prop.automatic_rotate) > 0.001f) {
+		m_rotation_add_yaw = modulo360f(m_rotation_add_yaw + dtime * core::RADTODEG *
+				m_prop.automatic_rotate);
 	}
 
 	if(m_registered) {
