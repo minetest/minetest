54471fe5c658cd09a7cbee736b4f2d9ec919a79b
x2048 <codeforsmile@gmail.com>
2022-08-24 21:17:10 +0200

    Calibrate distance to wield tool to avoid clipping by environment

--- src/client/camera.cpp
+++ src/client/camera.cpp
@@ -631,7 +631,7 @@ void Camera::drawWieldedTool(irr::core::matrix4* translation)
 	scene::ICameraSceneNode* cam = m_wieldmgr->getActiveCamera();
 	cam->setAspectRatio(m_cameranode->getAspectRatio());
 	cam->setFOV(72.0*M_PI/180.0);
-	cam->setNearValue(10);
+	cam->setNearValue(40);
 	cam->setFarValue(1000);
 	if (translation != NULL)
 	{
