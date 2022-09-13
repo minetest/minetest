c3a51899270eb199018c0576b5143e322ec872db
Oliver Simmons <oliversimmo@gmail.com>
2022-09-13 03:21:38 +0100

    Gah, forgot to got add the h file. Luckily this'll get squashed away.

--- src/server.h
+++ src/server.h
@@ -254,8 +254,8 @@ public:
 
 	void deleteParticleSpawner(const std::string &playername, u32 id);
 
-	bool dynamicAddMedia(std::string filename, std::string filepath, u32 token,
-		const std::string &to_player, bool ephemeral, std::string *filedata);
+	bool dynamicAddMedia(const std::string &filename, std::string filepath, u32 token,
+		const std::string &to_player, bool ephemeral, std::string *filedata = nullptr);
 
 	ServerInventoryManager *getInventoryMgr() const { return m_inventory_mgr.get(); }
 	void sendDetachedInventory(Inventory *inventory, const std::string &name, session_t peer_id);
