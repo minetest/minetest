3db604ad3276185974c040252cc1a728b57b5826
Desour <vorunbekannt75@web.de>
2022-09-26 00:48:00 +0200

    Devtest: Add callback test item

--- games/devtest/mods/testitems/init.lua
+++ games/devtest/mods/testitems/init.lua
@@ -53,3 +53,112 @@ minetest.register_craftitem("testitems:overlay_global", {
 })
 
 
+--
+-- Item callbacks
+--
+
+minetest.register_craftitem("testitems:callback_1", {
+	description = S("Callback test item 1"),
+	inventory_image = "testitems_callback_1.png",
+	wield_image = "testitems_callback_1.png",
+
+	on_secondary_use = function(itemstack, user, pointed_thing)
+		minetest.chat_send_all("[testitems:callback_1 on_secondary_use]")
+		local ctrl = user and user:get_player_control() or {}
+		if ctrl.sneak then
+			itemstack = ItemStack(itemstack)
+			itemstack:set_name("testitems:callback_2")
+			return itemstack
+		end
+	end,
+
+	on_drop = function(itemstack, dropper, pos)
+		minetest.chat_send_all("[testitems:callback_1 on_drop]")
+		local ctrl = dropper and dropper:get_player_control() or {}
+		if ctrl.sneak then
+			itemstack = ItemStack(itemstack)
+			itemstack:set_name("testitems:callback_2")
+		end
+		return minetest.item_drop(itemstack, dropper, pos)
+	end,
+
+	on_pickup = function(itemstack, picker, obj, ...)
+		minetest.chat_send_all("[testitems:callback_1 on_pickup]")
+		assert(obj:get_luaentity().name == "__builtin:item")
+		local ctrl = picker and picker:get_player_control() or {}
+		if ctrl.left then
+			minetest.chat_send_all(dump({...}))
+		end
+		if ctrl.up then
+			return false
+		elseif ctrl.sneak then
+			itemstack = ItemStack(itemstack)
+			itemstack:set_name("testitems:callback_2")
+			return itemstack
+		else
+			return true
+		end
+	end,
+
+	on_use = function(itemstack, user, pointed_thing)
+		minetest.chat_send_all("[testitems:callback_1 on_use]")
+		local ctrl = user and user:get_player_control() or {}
+		if ctrl.sneak then
+			itemstack = ItemStack(itemstack)
+			itemstack:set_name("testitems:callback_2")
+			return itemstack
+		end
+	end,
+
+	after_use = function(itemstack, user, node, digparams) -- never called
+		minetest.chat_send_all("[testitems:callback_1 after_use]")
+		local ctrl = user and user:get_player_control() or {}
+		if ctrl.up then
+			itemstack = ItemStack(itemstack)
+			itemstack:set_name("testitems:callback_2")
+			return itemstack
+		end
+	end,
+})
+
+minetest.register_craftitem("testitems:callback_2", {
+	description = S("Callback test item 2"),
+	inventory_image = "testitems_callback_2.png",
+	wield_image = "testitems_callback_2.png",
+
+	on_use = function(itemstack, user, pointed_thing)
+		minetest.chat_send_all("[testitems:callback_2 on_use]")
+		itemstack = ItemStack(itemstack)
+		itemstack:set_name("testitems:callback_1")
+		return itemstack
+	end,
+})
+
+minetest.register_on_item_pickup(function(itemstack, picker, obj, time_from_last_punch,  ...)
+	local item_name = itemstack:get_name()
+	if item_name ~= "testitems:callback_1" and item_name ~= "testitems:callback_2" then
+		return
+	end
+	minetest.chat_send_all("["..item_name.." register_on_item_pickup (1)]")
+end)
+
+minetest.register_on_item_pickup(function(itemstack, picker, obj, time_from_last_punch,  ...)
+	local item_name = itemstack:get_name()
+	if item_name ~= "testitems:callback_1" and item_name ~= "testitems:callback_2" then
+		return
+	end
+	minetest.chat_send_all("["..item_name.." register_on_item_pickup (2)]")
+	assert(obj:get_luaentity().name == "__builtin:item")
+	local ctrl = picker and picker:get_player_control() or {}
+	if ctrl.down then
+		return true
+	end
+end)
+
+minetest.register_on_item_pickup(function(itemstack, picker, obj, time_from_last_punch,  ...)
+	local item_name = itemstack:get_name()
+	if item_name ~= "testitems:callback_1" and item_name ~= "testitems:callback_2" then
+		return
+	end
+	minetest.chat_send_all("["..item_name.." register_on_item_pickup (3)]")
+end)
