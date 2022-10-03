9364ff9b15fa4694d48f43c738977e7d193a03b3
Wuzzy <Wuzzy@disroot.org>
2022-10-03 12:41:03 +0200

    DevTest: Change texture for node-aligned node

--- games/devtest/mods/tiled/init.lua
+++ games/devtest/mods/tiled/init.lua
@@ -11,16 +11,15 @@ minetest.register_node("tiled:tiled", {
 minetest.register_node("tiled:tiled_n", {
         description = "Tiled Node (node-aligned)",
         tiles = {{
-                name = "tiled_tiled.png",
+                name = "tiled_tiled_node.png",
                 align_style = "node",
-                scale = 8,
         }},
         groups = {cracky=3},
 })
 
-stairs.register_stair_and_slab("tiled_n", "tiled:tiled",
+stairs.register_stair_and_slab("tiled_n", "tiled:tiled_n",
 		{cracky=3},
-		{{name="tiled_tiled.png", align_style="node", scale=8}},
+		{{name="tiled_tiled_node.png", align_style="node"}},
 		"Tiled Stair (node-aligned)",
 		"Tiled Slab (node-aligned)")
 
