3a7fffc5875add144ebc4d04b8d23741afb8357d
Wuzzy <Wuzzy@disroot.org>
2022-10-08 17:46:03 +0200

    DevTest: Move more logging to log mod

--- games/devtest/mods/experimental/init.lua
+++ games/devtest/mods/experimental/init.lua
@@ -13,11 +13,4 @@ function experimental.print_to_everything(msg)
 	minetest.chat_send_all(msg)
 end
 
-minetest.log("info", "[experimental] modname="..dump(minetest.get_current_modname()))
-minetest.log("info", "[experimental] modpath="..dump(minetest.get_modpath("experimental")))
-minetest.log("info", "[experimental] worldpath="..dump(minetest.get_worldpath()))
 
-
-minetest.register_on_mods_loaded(function()
-	minetest.log("action", "[experimental] on_mods_loaded()")
-end)
