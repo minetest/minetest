cdc41a0ce4955a6493661e7ef6510b4d5456bae5
Wuzzy <Wuzzy@disroot.org>
2022-10-08 19:49:28 +0200

    DevTest: Move detached inv tests to chest mod

--- games/devtest/mods/experimental/init.lua
+++ games/devtest/mods/experimental/init.lua
@@ -2,14 +2,5 @@
 -- Experimental things
 --
 
-experimental = {}
-
-dofile(minetest.get_modpath("experimental").."/detached.lua")
 dofile(minetest.get_modpath("experimental").."/commands.lua")
-
-function experimental.print_to_everything(msg)
-	minetest.log("action", msg)
-	minetest.chat_send_all(msg)
-end
-
 
