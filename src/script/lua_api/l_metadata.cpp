cb7f853b26d2089b27b9d742c37a2264d42de6a8
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2022-10-22 16:59:13 -0400

    Don't worry about overflow

--- src/script/lua_api/l_metadata.cpp
+++ src/script/lua_api/l_metadata.cpp
@@ -213,12 +213,11 @@ int MetaDataRef::l_get_keys(lua_State *L)
 	std::vector<std::string> keys_;
 	const std::vector<std::string> &keys = meta->getKeys(&keys_);
 
-	lua_Number i = 0;
-	lua_createtable(L, (int)MYMIN(keys.size(), INT_MAX), 0);
+	int i = 0;
+	lua_createtable(L, keys.size(), 0);
 	for (const std::string &key : keys) {
-		lua_pushnumber(L, ++i);
 		lua_pushlstring(L, key.c_str(), key.size());
-		lua_rawset(L, -3);
+		lua_rawseti(L, -2, ++i);
 	}
 	return 1;
 }
