0c1445a4b35c2a91bb4e8f4795c4a002f3d209ba
HybridDog <ovvv@web.de>
2023-01-03 15:15:05 +0100

    CI: Decrease compilation times with Ccache
    I've excluded the docker build since it would probably require a modified Dockerfile.

--- util/ci/build_prometheus_cpp.sh
+++ util/ci/build_prometheus_cpp.sh
@@ -1,10 +1,15 @@
 #! /bin/bash -eu
 
+# Use ccache if it is available
+extra_args=()
+command -v ccache >/dev/null && extra_args+=(-DCMAKE_{C,CXX}_COMPILER_LAUNCHER=ccache)
+
 cd /tmp
 git clone --recursive https://github.com/jupp0r/prometheus-cpp
 mkdir prometheus-cpp/build
 cd prometheus-cpp/build
 cmake .. \
+	"${extra_args[@]}" \
 	-DCMAKE_INSTALL_PREFIX=/usr/local \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DENABLE_TESTING=0
