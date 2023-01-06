5bfba386509bf6b671280ecb0c6593f43c2de570
x2048 <codeforsmile@gmail.com>
2023-01-06 21:41:55 +0100

    Change default culler to bfs + documentation

--- src/client/clientmap.cpp
+++ src/client/clientmap.cpp
@@ -103,7 +103,7 @@ ClientMap::ClientMap(
 	m_cache_bilinear_filter   = g_settings->getBool("bilinear_filter");
 	m_cache_anistropic_filter = g_settings->getBool("anisotropic_filter");
 	m_cache_transparency_sorting_distance = g_settings->getU16("transparency_sorting_distance");
-	m_new_occlusion_culler = g_settings->get("occlusion_culler") == "new";
+	m_new_occlusion_culler = g_settings->get("occlusion_culler") == "bfs";
 	g_settings->registerChangedCallback("occlusion_culler", on_settings_changed, this);
 	m_enable_raytraced_culling = g_settings->getBool("enable_raytraced_culling");
 	g_settings->registerChangedCallback("enable_raytraced_culling", on_settings_changed, this);
@@ -112,7 +112,7 @@ ClientMap::ClientMap(
 void ClientMap::onSettingChanged(const std::string &name)
 {
 	if (name == "occlusion_culler")
-		m_new_occlusion_culler = g_settings->get("occlusion_culler") == "new";
+		m_new_occlusion_culler = g_settings->get("occlusion_culler") == "bfs";
 	if (name == "enable_raytraced_culling")
 		m_enable_raytraced_culling = g_settings->getBool("enable_raytraced_culling");
 }
