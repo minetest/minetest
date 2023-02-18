6643c1638f4e664083d2ffbe688fa970df868105
numzero <numzer0@yandex.ru>
2023-02-18 23:44:53 +0300

    fixup! Make MinGW builds use bundled Irrlicht

--- util/buildbot/buildwin64.sh
+++ util/buildbot/buildwin64.sh
@@ -93,8 +93,8 @@ download "http://minetest.kitsunemimi.pw/luajit-$luajit_version-win64.zip"
 download "http://minetest.kitsunemimi.pw/libleveldb-$leveldb_version-win64.zip" leveldb-$leveldb_version.zip
 download "http://minetest.kitsunemimi.pw/openal-soft-$openal_version-win64.zip"
 # Irrlicht dependencies
-download "http://minetest.kitsunemimi.pw/libjpeg-$libjpeg_version-win32.zip"
-download "http://minetest.kitsunemimi.pw/libpng-$libpng_version-win32.zip"
+download "http://minetest.kitsunemimi.pw/libjpeg-$libjpeg_version-win64.zip"
+download "http://minetest.kitsunemimi.pw/libpng-$libpng_version-win64.zip"
 
 # Set source dir, downloading Minetest as needed
 if [ -n "$EXISTING_MINETEST_DIR" ]; then
