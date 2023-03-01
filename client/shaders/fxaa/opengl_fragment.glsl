ce4fdf4cd282dfe06f6f2be8b28bab046be937a1
x2048 <codeforsmile@gmail.com>
2023-03-01 22:10:22 +0100

    Fix a typo causing incorrect antialiasing

--- client/shaders/fxaa/opengl_fragment.glsl
+++ client/shaders/fxaa/opengl_fragment.glsl
@@ -79,19 +79,19 @@ vec4 fxaa(sampler2D tex, vec2 fragCoord, vec2 inverseVP,
     float lumaM  = dot(rgbM,  luma);
     float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
     float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
-    
+
     mediump vec2 dir;
     dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
     dir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));
-    
+
     float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) *
                           (0.25 * FXAA_REDUCE_MUL), FXAA_REDUCE_MIN);
-    
+
     float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
     dir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX),
               max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX),
               dir * rcpDirMin)) * inverseVP;
-    
+
     vec3 rgbA = 0.5 * (
         texture2D(tex, fragCoord + dir * (1.0 / 3.0 - 0.5)).xyz +
         texture2D(tex, fragCoord + dir * (2.0 / 3.0 - 0.5)).xyz);
@@ -110,7 +110,7 @@ vec4 fxaa(sampler2D tex, vec2 fragCoord, vec2 inverseVP,
 void main(void)
 {
 	vec2 uv = varTexCoord.st;
-	
+
 	gl_FragColor = fxaa(rendered, uv, texelSize0, 
-		sampleNW, sampleNE, sampleSE, sampleSW, uv);
+		sampleNW, sampleNE, sampleSW, sampleSE, uv);
 }
