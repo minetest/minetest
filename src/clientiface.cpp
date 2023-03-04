b2624b905bef6c8974abb932812ed1d1a29dd5cb
Lars <larsh@apache.org>
2023-03-03 18:17:33 -0800

    Restart map-send cycle if visible blocks are soon to be unloaded.

--- src/clientiface.cpp
+++ src/clientiface.cpp
@@ -101,6 +101,17 @@ void RemoteClient::GetNextBlocks (
 	m_nothing_to_send_pause_timer -= dtime;
 	m_map_send_completion_timer += dtime;
 
+	if (m_map_send_completion_timer > g_settings->getFloat("server_unload_unused_data_timeout") * 0.8f) {
+		warningstream << "Server: Player " << m_name << ", RemoteClient " << peer_id
+				<< ": full map send is taking too long "
+				<< m_map_send_completion_timer
+				<< "s, restarting to avoid visible blocks being unloaded. \n"
+				<< "Consider increasing `server_unload_unused_data_timeout`"
+				<< std::endl;
+		m_map_send_completion_timer = 0.0f;
+		m_nearest_unsent_d = 0;
+	}
+
 	if (m_nothing_to_send_pause_timer >= 0)
 		return;
 
@@ -259,16 +270,6 @@ void RemoteClient::GetNextBlocks (
 			if (d <= BLOCK_SEND_DISABLE_LIMITS_MAX_D)
 				max_simul_dynamic = m_max_simul_sends;
 
-			// Don't select too many blocks for sending
-			if (num_blocks_selected >= max_simul_dynamic) {
-				//queue_is_full = true;
-				goto queue_full_break;
-			}
-
-			// Don't send blocks that are currently being transferred
-			if (m_blocks_sending.find(p) != m_blocks_sending.end())
-				continue;
-
 			/*
 				Do not go over max mapgen limit
 			*/
@@ -303,6 +304,16 @@ void RemoteClient::GetNextBlocks (
 				// First: Reset usage timer, this block will be of use in the future.
 				block->resetUsageTimer();
 			}
+
+			// Don't select too many blocks for sending
+			if (num_blocks_selected >= max_simul_dynamic) {
+				//queue_is_full = true;
+				goto queue_full_break;
+			}
+
+			// Don't send blocks that are currently being transferred
+			if (m_blocks_sending.find(p) != m_blocks_sending.end())
+				continue;
 
 			/*
 				Don't send already sent blocks
