2d529104f25554aa33e165a3aa29711c562b3881
Stvk imension <76146430+oong819@users.noreply.github.com>
2023-03-20 12:11:25 +0700

    Update GameActivity.java

--- android/app/src/main/java/net/minetest/minetest/GameActivity.java
+++ android/app/src/main/java/net/minetest/minetest/GameActivity.java
@@ -64,7 +64,6 @@ public class GameActivity extends NativeActivity {
 	}
 
 	private void makeFullScreen() {
-		if (Build.VERSION.SDK_INT >= 19)
 			this.getWindow().getDecorView().setSystemUiVisibility(
 					View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
 					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
