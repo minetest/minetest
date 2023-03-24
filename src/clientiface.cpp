c6b498113cde72c3ad80b6788f31f816ecedfa64
lhofhansl <larsh@apache.org>
2023-03-24 15:08:09 -0700

    Better info message (sfan)
    Co-authored-by: sfan5 <sfan5@live.de>

--- src/clientiface.cpp
+++ src/clientiface.cpp
@@ -102,10 +102,10 @@ void RemoteClient::GetNextBlocks (
 	m_map_send_completion_timer += dtime;
 
 	if (m_map_send_completion_timer > g_settings->getFloat("server_unload_unused_data_timeout") * 0.8f) {
-		infostream << "Server: Player " << m_name << ", RemoteClient " << peer_id
-				<< ": full map send is taking too long "
+		infostream << "Server: Player " << m_name << ", peer_id=" << peer_id
+				<< ": full map send is taking too long ("
 				<< m_map_send_completion_timer
-				<< "s, restarting to avoid visible blocks being unloaded."
+				<< "s), restarting to avoid visible blocks being unloaded."
 				<< std::endl;
 		m_map_send_completion_timer = 0.0f;
 		m_nearest_unsent_d = 0;
