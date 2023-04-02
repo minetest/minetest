1c1f1b161512fea8a04fade4db274fa11659bf68
SmallJoker <mk939@ymail.com>
2023-04-02 20:55:32 +0200

    Revert "Enable connected glass by default (#13242)"
    This reverts commit a93f3542d92e87c3d728f067358e6f42b31cfe96 for
    the upcoming 5.7.0 release.
    
    See https://github.com/minetest/minetest/issues/8290 for a detailed discussion.

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -196,7 +196,7 @@ void set_default_settings()
 	settings->setDefault("vsync", "false");
 	settings->setDefault("fov", "72");
 	settings->setDefault("leaves_style", "fancy");
-	settings->setDefault("connected_glass", "true");
+	settings->setDefault("connected_glass", "false");
 	settings->setDefault("smooth_lighting", "true");
 	settings->setDefault("performance_tradeoffs", "false");
 	settings->setDefault("lighting_alpha", "0.0");
