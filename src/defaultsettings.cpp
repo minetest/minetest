252b2c713aae0010387e98f318aa32246cb1beb0
rubenwardy <rw@rubenwardy.com>
2023-04-14 15:39:39 +0100

    Update settingtypes
    * Move settings to correct sections
    * Improve titles and descriptions
    * Make settingtypes.txt default and defaultsettings.cpp match

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -173,7 +173,7 @@ void set_default_settings()
 	settings->setDefault("show_debug", "true");
 #endif
 	settings->setDefault("fsaa", "0");
-	settings->setDefault("undersampling", "0");
+	settings->setDefault("undersampling", "1");
 	settings->setDefault("world_aligned_mode", "enable");
 	settings->setDefault("autoscale_mode", "disable");
 	settings->setDefault("enable_fog", "true");
@@ -300,7 +300,7 @@ void set_default_settings()
 	settings->setDefault("continuous_forward", "false");
 	settings->setDefault("enable_joysticks", "false");
 	settings->setDefault("joystick_id", "0");
-	settings->setDefault("joystick_type", "");
+	settings->setDefault("joystick_type", "auto");
 	settings->setDefault("repeat_joystick_button_time", "0.17");
 	settings->setDefault("joystick_frustum_sensitivity", "170");
 	settings->setDefault("joystick_deadzone", "2048");
