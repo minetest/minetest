3bc657f19d99e81e4d64d9cb76797e8cc8ac1bdd
sfan5 <sfan5@live.de>
2023-05-24 20:14:09 +0200

    the thing 2

--- src/config.h
+++ src/config.h
@@ -1,27 +1,7 @@
-/*
-	If CMake is used, includes the cmake-generated cmake_config.h.
-	Otherwise use default values
-*/
-
 #pragma once
-
-#define STRINGIFY(x) #x
-#define STR(x) STRINGIFY(x)
-
 
 #if defined USE_CMAKE_CONFIG_H
 	#include "cmake_config.h"
 #else
-	#if defined (__ANDROID__)
-		#define PROJECT_NAME "minetest"
-		#define PROJECT_NAME_C "Minetest"
-		#define STATIC_SHAREDIR ""
-		#define ENABLE_UPDATE_CHECKER 0
-		#define VERSION_STRING STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_PATCH) STR(VERSION_EXTRA)
-	#endif
-	#ifdef NDEBUG
-		#define BUILD_TYPE "Release"
-	#else
-		#define BUILD_TYPE "Debug"
-	#endif
+	#warning Missing configuration
 #endif
