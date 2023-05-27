973c4dee9a992a33687bd19104c99ea26e4c156a
Desour <ds.desour@proton.me>
2023-05-27 07:37:05 +0200

    Remove a misleading MutexAutoLock in l_to_table
    The temporary is immediately destructed, so the mutex isn't locked
    after the line.
    Removed the lock, because the Settings member-functions used by
    push_settings_table lock the mutex and are thread-safe, but would
    cause a dead-lock.

--- src/script/lua_api/l_settings.cpp
+++ src/script/lua_api/l_settings.cpp
@@ -332,7 +332,6 @@ int LuaSettings::l_to_table(lua_State* L)
 	NO_MAP_LOCK_REQUIRED;
 	LuaSettings* o = checkObject<LuaSettings>(L, 1);
 
-	MutexAutoLock(o->m_settings->m_mutex);
 	push_settings_table(L, o->m_settings);
 	return 1;
 }
