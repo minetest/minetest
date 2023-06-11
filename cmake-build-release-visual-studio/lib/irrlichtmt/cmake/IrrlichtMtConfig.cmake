260d55140830a5e531aaf8cdd712686d90fbd51b
Ashwin Naren <arihant2math@gmail.com>
2023-06-10 21:27:46 -0700

    merge conflict fix

---
+++ cmake-build-release-visual-studio/lib/irrlichtmt/cmake/IrrlichtMtConfig.cmake
@@ -0,0 +1,12 @@
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was Config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+####################################################################################
+
+if(NOT TARGET IrrlichtMt::IrrlichtMt)
+	include("${CMAKE_CURRENT_LIST_DIR}/IrrlichtMtTargets.cmake")
+endif()
