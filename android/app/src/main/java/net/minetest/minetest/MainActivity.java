39edcabec8a5b3b2172a510fb4abf2487e5a536f
Stvk imension <76146430+oong819@users.noreply.github.com>
2023-06-13 13:47:12 +0700

    Remove permission request

--- android/app/src/main/java/net/minetest/minetest/MainActivity.java
+++ android/app/src/main/java/net/minetest/minetest/MainActivity.java
@@ -1,5 +1,42 @@
 /*
 Minetest
+Copyright (C) 2014-2020 
+import android.os.Environment;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static net.minetest.minetest.UnzipService.*;
+
+public class MainActivity extends AppCompatActivity {
+	private final static int versionCode = BuildConfig.VERSION_CODE;
+	private final static int PERMISSIONS = 1;
+	private static final String[] REQUIRED_SDK_PERMISSIONS =
+			new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+	private static final String SETTINGS = "MinetestSettings";
+	private static final String TAG_VERSION_CODE = "versionCode";
+
+	private ProgressBar mProgressBar;
+	private TextView mTextView;
+	private SharedPreferences sharedPreferences;
+
+	private final BroadcastReceiver myReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			int progress = 0;
+			@/*
+Minetest
 Copyright (C) 2014-2020 MoNTE48, Maksim Gamarnik <MoNTE48@mail.ua>
 Copyright (C) 2014-2020 ubulem,  Bektur Mambetov <berkut87@gmail.com>
 
@@ -18,7 +55,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-package net.minetest.minetest;
+package net.minetest.minetest;//MainActivity.java
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
@@ -49,9 +86,6 @@ import static net.minetest.minetest.UnzipService.*;
 
 public class MainActivity extends AppCompatActivity {
 	private final static int versionCode = BuildConfig.VERSION_CODE;
-	private final static int PERMISSIONS = 1;
-	private static final String[] REQUIRED_SDK_PERMISSIONS =
-			new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 	private static final String SETTINGS = "MinetestSettings";
 	private static final String TAG_VERSION_CODE = "versionCode";
 
@@ -101,56 +135,7 @@ public class MainActivity extends AppCompatActivity {
 		mTextView = findViewById(R.id.textView);
 		sharedPreferences = getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-				Build.VERSION.SDK_INT < Build.VERSION_CODES.R)
-			checkPermission();
-		else
-			checkAppVersion();
 	}
-
-	private void checkPermission() {
-		final List<String> missingPermissions = new ArrayList<>();
-		for (final String permission : REQUIRED_SDK_PERMISSIONS) {
-			final int result = ContextCompat.checkSelfPermission(this, permission);
-			if (result != PackageManager.PERMISSION_GRANTED)
-				missingPermissions.add(permission);
-		}
-		if (!missingPermissions.isEmpty()) {
-			final String[] permissions = missingPermissions
-					.toArray(new String[0]);
-			ActivityCompat.requestPermissions(this, permissions, PERMISSIONS);
-		} else {
-			final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
-			Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
-			onRequestPermissionsResult(PERMISSIONS, REQUIRED_SDK_PERMISSIONS, grantResults);
-		}
-	}
-
-	@Override
-	public void onRequestPermissionsResult(
-		int requestCode,
-		@NonNull String[] permissions,
-		@NonNull int[] grantResults
-	) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-		if (requestCode == PERMISSIONS) {
-			for (int grantResult : grantResults) {
-				if (grantResult != PackageManager.PERMISSION_GRANTED) {
-					Toast.makeText(this, R.string.not_granted, Toast.LENGTH_LONG).show();
-					finish();
-					return;
-				}
-			}
-			checkAppVersion();
-		}
-	}
-
-	private void checkAppVersion() {
-		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-			Toast.makeText(this, R.string.no_external_storage, Toast.LENGTH_LONG).show();
-			finish();
-			return;
-		}
 
 		if (UnzipService.getIsRunning()) {
 			mProgressBar.setVisibility(View.VISIBLE);
