197b0a10a053c4c684e6e65de2ca45c700143cbc
Stvk imension <76146430+oong819@users.noreply.github.com>
2023-06-13 21:47:54 +0700

    Remove comment (Episode 1)

--- android/app/src/main/java/net/minetest/minetest/MainActivity.java
+++ android/app/src/main/java/net/minetest/minetest/MainActivity.java
@@ -18,7 +18,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-package net.minetest.minetest;//MainActivity.java
+package net.minetest.minetest;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
