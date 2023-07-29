3a9221963c9eb179b2cd8b9e646b253334bdd4fc
Gregor Parzefall <gregor.parzefall@posteo.de>
2023-07-29 19:45:25 +0200

    Disable mipmapping again by default 😢

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -250,7 +250,7 @@ void set_default_settings()
 	// Effects
 	settings->setDefault("directional_colored_fog", "true");
 	settings->setDefault("inventory_items_animations", "false");
-	settings->setDefault("mip_map", "true");
+	settings->setDefault("mip_map", "false");
 	settings->setDefault("bilinear_filter", "false");
 	settings->setDefault("trilinear_filter", "false");
 	settings->setDefault("anisotropic_filter", "false");
