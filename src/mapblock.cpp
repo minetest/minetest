f5b35a074f1372fbfd9ef32721920235d616d5af
sfan5 <sfan5@live.de>
2023-12-12 15:43:39 +0100

    Get rid of parent pointer in MapBlock

--- src/mapblock.cpp
+++ src/mapblock.cpp
@@ -66,8 +66,7 @@ static const char *modified_reason_strings[] = {
 	MapBlock
 */
 
-MapBlock::MapBlock(Map *parent, v3s16 pos, IGameDef *gamedef):
-		m_parent(parent),
+MapBlock::MapBlock(v3s16 pos, IGameDef *gamedef):
 		m_pos(pos),
 		m_pos_relative(pos * MAP_BLOCKSIZE),
 		data(new MapNode[nodecount]),
@@ -144,25 +143,6 @@ void MapBlock::step(float dtime, const std::function<bool(v3s16, MapNode, f32)> 
 				setNodeTimer(NodeTimer(elapsed_timer.timeout, 0, elapsed_timer.position));
 		}
 	}
-}
-
-bool MapBlock::isValidPositionParent(v3s16 p)
-{
-	if (isValidPosition(p)) {
-		return true;
-	}
-
-	return m_parent->isValidPosition(getPosRelative() + p);
-}
-
-MapNode MapBlock::getNodeParent(v3s16 p, bool *is_valid_position)
-{
-	if (!isValidPosition(p))
-		return m_parent->getNode(getPosRelative() + p, is_valid_position);
-
-	if (is_valid_position)
-		*is_valid_position = true;
-	return data[p.Z * zstride + p.Y * ystride + p.X];
 }
 
 std::string MapBlock::getModifiedReasonString()
