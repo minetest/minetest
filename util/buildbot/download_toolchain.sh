5ab31445e916e07d582c179bcb8aa03235601bc4
sfan5 <sfan5@live.de>
2023-12-13 12:54:44 +0100

    MinGW toolchain refresh

--- util/buildbot/download_toolchain.sh
+++ util/buildbot/download_toolchain.sh
@@ -6,8 +6,11 @@ if [[ -z "$1" || -z "$2" ]]; then
 	exit 1
 fi
 
-ver=11.2.0
-os=ubuntu20.04
+# our current toolchain:
+# binutils 2.41 + GCC 13.2.0 + Mingw-w64 11.0.1 with UCRT enabled and winpthreads support
+# built from source on Ubuntu 22.04, so should work on any similarily up-to-date distro
+ver=13.2.0
+os=ubuntu22.04
 name="mingw-w64-${1}_${ver}_${os}.tar.xz"
 wget "http://minetest.kitsunemimi.pw/$name" -O "$name"
 sha256sum -w -c <(grep -F "$name" "$topdir/sha256sums.txt")
