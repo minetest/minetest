81b2ea8ea78e48a6571fa899aedb925b794dd923
SFENCE <sfence.software@gmail.com>
2023-12-14 16:44:34 +0100

    Add covered and sunken mesh node.

--- games/devtest/mods/testnodes/sunken_covered.lua
+++ games/devtest/mods/testnodes/sunken_covered.lua
@@ -47,13 +47,35 @@ minetest.register_node("testnodes:sunken_nodebox", {
 	inner_node = "testnodes:nodebox_fixed",
 })
 
+minetest.register_node("testnodes:sunken_mesh", {
+	description = "Sunken Mesh Test Node in Liquid Source Range 5",
+	drawtype = "sunken",
+	tiles = {"testnodes_liquidsource_r5.png"},
+	special_tiles = {
+		{name = "testnodes_liquidsource_r5.png", backface_culling = false},
+		{name = "testnodes_liquidsource_r5.png", backface_culling = true},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	liquidtype = "source",
+	--liquid_alternative_flowing = "testnodes:rliquid_flowing_5",
+	--liquid_alternative_source = "testnodes:rliquid_5",
+	liquid_alternative_flowing = "testnodes:sunken_nodebox",
+	liquid_alternative_source = "testnodes:sunken_nodebox",
+	liquid_range = 0,
+
+	inner_node = "testnodes:mesh",
+})
+
 
 -- Covered node example.
 -- An simple example nodebox with one centered box
 minetest.register_node("testnodes:covered_torchlike_node", {
 	description = S("Covered Tochlike Test Node").."\n"..
 		S("Torchlike node inside"),
-	--tiles = {"testnodes_nodebox.png"},
 	tiles = { "testnodes_glasslike.png" },
 	drawtype = "covered",
 	paramtype = "light",
@@ -66,12 +88,23 @@ minetest.register_node("testnodes:covered_torchlike_node", {
 minetest.register_node("testnodes:covered_nodebox_node", {
 	description = S("Covered Fixed Nodebox Test Node").."\n"..
 		S("Torchlike node inside"),
-	--tiles = {"testnodes_nodebox.png"},
 	tiles = { "testnodes_glasslike.png" },
 	drawtype = "covered",
 	paramtype = "light",
 
 	inner_node = "testnodes:nodebox_fixed",
+
+	groups = {dig_immediate=3},
+})
+
+minetest.register_node("testnodes:covered_mesh_node", {
+	description = S("Covered Fixed Mesh Test Node").."\n"..
+		S("Torchlike node inside"),
+	tiles = { "testnodes_nodebox.png" },
+	drawtype = "covered",
+	paramtype = "light",
+
+	inner_node = "testnodes:mesh",
 
 	groups = {dig_immediate=3},
 })
