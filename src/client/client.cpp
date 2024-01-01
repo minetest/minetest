1d7443e8d237b9e96066d16ef4f539edfd4c6b44
FoxLoveFire <ivanskato672@gmail.com>
2024-01-01 15:41:12 +0000

    Revert "Save chat message in debug.txt"
    This reverts commit e71a849c786e2fba1af70cecfc7b032f6edf5a8c.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1679,10 +1679,6 @@ bool Client::getChatMessage(std::wstring &res)
 	m_chat_queue.pop();
 
 	res = L"";
-	//Save messages in debug.txt
-	std::wstring msg;
-	msg = L"Client: CHAT:"+chatMessage->message;
-	actionstream<<wide_to_utf8(msg)<<std::endl;
 
 	switch (chatMessage->type) {
 		case CHATMESSAGE_TYPE_RAW:
