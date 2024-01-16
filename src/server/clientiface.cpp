42d46087876a10556bf41fde2fcdc2ac1d3ad3b1
Lars <larsh@apache.org>
2024-01-15 17:59:37 -0800

    Bring back block_send_optimize_distance

--- src/server/clientiface.cpp
+++ src/server/clientiface.cpp
@@ -70,6 +70,7 @@ RemoteClient::RemoteClient() :
 	m_min_time_from_building(
 		g_settings->getFloat("full_block_send_enable_min_time_from_building")),
 	m_max_send_distance(g_settings->getS16("max_block_send_distance")),
+	m_block_optimize_distance(g_settings->getS16("block_send_optimize_distance")),
 	m_block_cull_optimize_distance(g_settings->getS16("block_cull_optimize_distance")),
 	m_max_gen_distance(g_settings->getS16("max_block_generate_distance")),
 	m_occ_cull(g_settings->getBool("server_side_occlusion_culling"))
@@ -235,6 +236,8 @@ void RemoteClient::GetNextBlocks (
 
 	const s16 full_d_max = std::min(adjustDist(m_max_send_distance, prop_zoom_fov),
 		wanted_range);
+	const s16 d_opt = std::min(adjustDist(m_block_optimize_distance, prop_zoom_fov),
+		wanted_range);
 	const s16 d_cull_opt = std::min(adjustDist(m_block_cull_optimize_distance, prop_zoom_fov),
 		wanted_range);
 	// f32 to prevent overflow, it is also what isBlockInSight(...) expects
@@ -353,10 +356,9 @@ void RemoteClient::GetNextBlocks (
 				/*
 					Don't send if it consists of air only.
 				*/
-				if (block->isAir())
-					continue;
+				if (d >= d_opt && block->isAir())
+						continue;
 			}
-
 			/*
 				Check occlusion cache first.
 			 */
