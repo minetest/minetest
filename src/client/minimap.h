e269a7963e27092e3f12b0529e8feb5ee78fe79c
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2024-02-15 20:51:43 +0100

    swap

--- src/client/minimap.h
+++ src/client/minimap.h
@@ -78,8 +78,8 @@ struct MinimapData {
 	video::IImage *minimap_mask_round = nullptr;
 	video::IImage *minimap_mask_square = nullptr;
 	video::ITexture *texture = nullptr;
-	bool textures_initialised = false; // True if the following textures are not nullptrs.
 	video::ITexture *heightmap_texture = nullptr;
+	bool textures_initialised = false; // True if the following textures are not nullptrs.
 	video::ITexture *minimap_overlay_round = nullptr;
 	video::ITexture *minimap_overlay_square = nullptr;
 	video::ITexture *player_marker = nullptr;
