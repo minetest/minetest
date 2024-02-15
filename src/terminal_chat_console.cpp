222d7d8206ce1edf58ca94e2407e67b758851401
Gary Mindlin Miguel <garymm@astera.org>
2024-02-15 02:59:51 +0000

    include C++ header instead of C
    The C-style include causes compilation failures in some case I care
    about. It's also deprecated in C++.

--- src/terminal_chat_console.cpp
+++ src/terminal_chat_console.cpp
@@ -17,7 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include <inttypes.h>
+#include <cinttypes>
 #include "config.h"
 #if USE_CURSES
 #include "version.h"
