2f2971cae8b876003b97b06928e8e441cb7da667
sfan5 <sfan5@live.de>
2024-02-16 11:56:01 +0100

    sqlite3

--- src/database/database-sqlite3.h
+++ src/database/database-sqlite3.h
@@ -64,10 +64,28 @@ protected:
 		sqlite3_vrfy(sqlite3_bind_double(s, iCol, val));
 	}
 
-	inline std::string_view sqlite_to_string(sqlite3_stmt *s, int iCol)
+	// Note that the return value is only valid until the statement is stepped or reset.
+	inline std::string_view sqlite_to_string_view(sqlite3_stmt *s, int iCol)
 	{
 		const char* text = reinterpret_cast<const char*>(sqlite3_column_text(s, iCol));
 		return text ? std::string_view(text) : std::string_view();
+	}
+
+	// Avoid using this in favor of `sqlite_to_string_view`.
+	inline std::string sqlite_to_string(sqlite3_stmt *s, int iCol)
+	{
+		return std::string(sqlite_to_string_view(s, iCol));
+	}
+
+	// Converts a BLOB-type column into a string_view (null byte safe).
+	// Note that the return value is only valid until the statement is stepped or reset.
+	inline std::string_view sqlite_to_blob(sqlite3_stmt *s, int iCol)
+	{
+		const char *data = reinterpret_cast<const char*>(sqlite3_column_blob(s, iCol));
+		if (!data)
+			return std::string_view();
+		size_t len = sqlite3_column_bytes(s, iCol);
+		return std::string_view(data, len);
 	}
 
 	inline s32 sqlite_to_int(sqlite3_stmt *s, int iCol)
@@ -102,13 +120,16 @@ protected:
 	}
 
 	// Query verifiers helpers
-	inline void sqlite3_vrfy(int s, const std::string &m = "", int r = SQLITE_OK) const
+	inline void sqlite3_vrfy(int s, std::string_view m = "", int r = SQLITE_OK) const
 	{
-		if (s != r)
-			throw DatabaseException(m + ": " + sqlite3_errmsg(m_database));
+		if (s != r) {
+			std::string msg(m);
+			msg.append(": ").append(sqlite3_errmsg(m_database));
+			throw DatabaseException(msg);
+		}
 	}
 
-	inline void sqlite3_vrfy(const int s, const int r, const std::string &m = "") const
+	inline void sqlite3_vrfy(const int s, const int r, std::string_view m = "") const
 	{
 		sqlite3_vrfy(s, m, r);
 	}
