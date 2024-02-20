4537337693177962bf59db149209fbf2ad6b2aa4
sfan5 <sfan5@live.de>
2024-02-20 10:38:29 +0100

    Factor shadow constants out of MainShaderConstantSetter

--- src/client/shadows/dynamicshadowsrender.h
+++ src/client/shadows/dynamicshadowsrender.h
@@ -27,6 +27,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 class ShadowDepthShaderCB;
 class shadowScreenQuad;
 class shadowScreenQuadCB;
+class IWritableShaderSource;
 
 enum E_SHADOW_MODE : u8
 {
@@ -54,8 +55,12 @@ public:
 	static const int TEXTURE_LAYER_SHADOW = 3;
 
 	ShadowRenderer(IrrlichtDevice *device, Client *client);
-
 	~ShadowRenderer();
+
+	// Call before generating any shaders
+	// This is required because this class is initialized much later than all
+	// the shaders are dealt with.
+	static void preInit(IWritableShaderSource *shsrc);
 
 	void initialize();
 
