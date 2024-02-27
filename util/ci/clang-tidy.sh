6731ea48097eb87bc4737e9a973321dcbbb36beb
Gary Miguel <garymm@garymm.org>
2024-02-27 21:44:07 +0000

    GitHub: remove old GCC and clang jobs
    We're using newer C++ features not supported by them.
    
    Also enable gettext on all builds.
    Not sure why but without it we get ncurses errors.

--- util/ci/clang-tidy.sh
+++ util/ci/clang-tidy.sh
@@ -4,7 +4,6 @@ cmake -B build -DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 	-DRUN_IN_PLACE=TRUE \
 	-G Ninja \
-	-DENABLE_GETTEXT=FALSE \
 	-DBUILD_SERVER=TRUE
 cmake --build build --target GenerateVersion
 cmake --build build --target src/network/proto/remoteclient.capnp.h
