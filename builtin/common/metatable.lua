3c0f43bf582e6670bda4e989ce3915bff8674c23
y5nw <37980625+y5nw@users.noreply.github.com>
2024-03-05 00:23:52 +0100

    fix regression

--- builtin/common/metatable.lua
+++ builtin/common/metatable.lua
@@ -3,7 +3,8 @@ local known_metatables = {}
 function core.register_async_metatable(name, mt)
 	assert(type(name) == "string", ("attempt to use %s value as metatable name"):format(type(name)))
 	assert(type(mt) == "table", ("attempt to register a %s value as metatable"):format(type(mt)))
-	assert(known_metatables[name] == nil, ("attempt to override metatable %s"):format(name))
+	assert(known_metatables[name] == nil or known_metatables[name] == mt,
+			("attempt to override metatable %s"):format(name))
 	known_metatables[name] = mt
 	known_metatables[mt] = name
 end
