1405bef17de21c85d51f753d8da9647918096f66
Lars Mueller <appgurulars@gmx.de>
2024-05-06 21:20:30 +0200

    Deletion

--- src/unittest/test_k_d_tree.cpp
+++ src/unittest/test_k_d_tree.cpp
@@ -1,21 +1,5 @@
-/*
-Minetest
-Copyright (C) 2024 sfan5
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+// Copyright (C) 2024 Lars Müller
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include "noise.h"
 #include "test.h"
@@ -32,8 +16,8 @@ public:
 
 	void runTests(IGameDef *gamedef);
 
-	// TODO void cube();
-	void randomInserts();
+	// TODO basic small cube test
+	void randomOps();
 };
 
 template<uint8_t Dim, typename Component, typename Id>
@@ -44,9 +28,11 @@ public:
 		entries.push_back(Entry{p, id});
 	}
 	void remove(Id id) {
-		entries.erase(std::find_if(entries.begin(), entries.end(), [&](const auto &e) {
+		const auto it = std::find_if(entries.begin(), entries.end(), [&](const auto &e) {
 			return e.id == id;
-		}));
+		});
+		assert(it != entries.end());
+		entries.erase(it);
 	}
 	template<typename F>
 	void rangeQuery(const Point &min, const Point &max, const F &cb) {
@@ -71,10 +57,10 @@ static TestKdTree g_test_instance;
 void TestKdTree::runTests(IGameDef *gamedef)
 {
 	rawstream << "-------- k-d-tree" << std::endl;
-	TEST(randomInserts);
+	TEST(randomOps);
 }
 
-void TestKdTree::randomInserts() {
+void TestKdTree::randomOps() {
 	PseudoRandom pr(814538);
 
 	ObjectVector<3, f32, u16> objvec;
@@ -87,21 +73,32 @@ void TestKdTree::randomInserts() {
 		kds.insert(point, id);
 	}
 
-	for (int i = 0; i < 1000; ++i) {
-		std::array<f32, 3> min, max;
-		for (uint8_t d = 0; d < 3; ++d) {
-			min[d] = pr.range(-1500, 1500);
-			max[d] = min[d] + pr.range(1, 2500);
+	const auto testRandomQueries = [&]() {
+		for (int i = 0; i < 1000; ++i) {
+			std::array<f32, 3> min, max;
+			for (uint8_t d = 0; d < 3; ++d) {
+				min[d] = pr.range(-1500, 1500);
+				max[d] = min[d] + pr.range(1, 2500);
+			}
+			std::unordered_set<u16> expected_ids;
+			objvec.rangeQuery(min, max, [&](auto _, u16 id) {
+				UASSERT(expected_ids.count(id) == 0);
+				expected_ids.insert(id);
+			});
+			kds.rangeQuery(min, max, [&](auto point, u16 id) {
+				UASSERT(expected_ids.count(id) == 1);
+				expected_ids.erase(id);
+			});
+			UASSERT(expected_ids.empty());
 		}
-		std::unordered_set<u16> expected_ids;
-		objvec.rangeQuery(min, max, [&](auto _, u16 id) {
-			UASSERT(expected_ids.count(id) == 0);
-			expected_ids.insert(id);
-		});
-		kds.rangeQuery(min, max, [&](auto point, u16 id) {
-			UASSERT(expected_ids.count(id) == 1);
-			expected_ids.erase(id);
-		});
-		UASSERT(expected_ids.empty());
+	};
+
+	testRandomQueries();
+
+	for (u16 id = 1; id < 800; ++id) {
+		objvec.remove(id);
+		kds.remove(id);
 	}
+
+	testRandomQueries();
 }
