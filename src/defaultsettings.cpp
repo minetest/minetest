78d1c3ea0b2dba5d980a1721d2b3153d7c2336a2
Xeno333 <Xeno333@mailfence.com>
2024-05-20 14:31:46 -0500

    Replacing translucent_water with translucent_liquids

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -282,7 +282,7 @@ void set_default_settings()
 	settings->setDefault("enable_3d_clouds", "true");
 	settings->setDefault("cloud_radius", "12");
 	settings->setDefault("menu_clouds", "true");
-	settings->setDefault("opaque_water", "false");
+	settings->setDefault("translucent_liquids", "true");
 	settings->setDefault("console_height", "0.6");
 	settings->setDefault("console_color", "(0,0,0)");
 	settings->setDefault("console_alpha", "200");
