a9eb92cb2aebfa575fe877a42844f668d69a2333
Xeno333 <149852758+Xeno333@users.noreply.github.com>
2024-05-21 09:25:10 -0500

    .hpp to .h

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "filesys.h"
 #include "version.h"
 #include "defaultsettings.h"
-#include "migratesettings.hpp"
+#include "migratesettings.h"
 #include "gettext.h"
 #include "log.h"
 #include "util/quicktune.h"
