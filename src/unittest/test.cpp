2aeee7cc32899d92237f0285ec5868086179da98
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2024-05-23 11:50:42 -0500

    Fix building tests and benchmarks together
    Fixes #14692.
    
    The cause of the failure was defining CATCH_CONFIG_RUNNER in two places.
    This fixes the issue by defining it only in unittests if both tests and
    benchmarks are to be built, or in the correct executable of only one of
    them is to be built.

--- src/unittest/test.cpp
+++ src/unittest/test.cpp
@@ -17,6 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+// This must be set in only one place
 #define CATCH_CONFIG_RUNNER
 // we want to have catch write to rawstream (stderr) instead of stdout
 #define CATCH_CONFIG_NOSTDOUT
