fb4dbc8cd1548943845ec5f8e309af1b0dfbb8b7
Gregor Parzefall <gregor.parzefall@posteo.de>
2024-05-30 12:03:04 +0200

    Mainmenu: Unify gamedata.errormessage and messagebox formspecs

--- builtin/fstk/ui.lua
+++ builtin/fstk/ui.lua
@@ -51,6 +51,18 @@ function ui.find_by_name(name)
 end
 
 --------------------------------------------------------------------------------
+function ui.get_message_formspec(title, message, btn_id)
+	return table.concat({
+		"size[14,8]",
+		"real_coordinates[true]",
+		"set_focus[", btn_id, ";true]",
+		"box[0.5,1.2;13,5;#000]",
+		("textarea[0.5,1.2;13,5;;%s;%s]"):format(title, message),
+		"button[5,6.6;4,1;", btn_id, ";" .. fgettext("OK") .. "]",
+	})
+end
+
+--------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 -- Internal functions not to be called from user
 --------------------------------------------------------------------------------
@@ -84,15 +96,7 @@ function ui.update()
 		else
 			error_title = fgettext("An error occurred:")
 		end
-		formspec = {
-			"size[14,8]",
-			"real_coordinates[true]",
-			"set_focus[btn_error_confirm;true]",
-			"box[0.5,1.2;13,5;#000]",
-			("textarea[0.5,1.2;13,5;;%s;%s]"):format(
-				error_title, error_message),
-			"button[5,6.6;4,1;btn_error_confirm;" .. fgettext("OK") .. "]"
-		}
+		formspec = {ui.get_message_formspec(error_title, error_message, "btn_error_confirm")}
 		ui.overridden = true
 	else
 		local active_toplevel_ui_elements = 0
