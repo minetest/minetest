6385fa80ddeb5a53c971e346a9c92a3d8ed865b4
Lars Mueller <appgurulars@gmx.de>
2024-06-12 20:43:38 +0200

    Remove redundant model

--- games/devtest/mods/gltf/init.lua
+++ games/devtest/mods/gltf/init.lua
@@ -18,5 +18,30 @@ do
 	register_entity("blender_cube_scaled", cube_textures)
 	register_entity("blender_cube_matrix_transform", cube_textures)
 end
+
 register_entity("snow_man", {"gltf_snow_man.png"})
 register_entity("spider", {"gltf_spider.png"})
+
+minetest.register_entity("gltf:spider_animated", {
+	initial_properties = {
+		visual = "mesh",
+		mesh = "spider_animated.gltf",
+		textures = {"gltf_spider.png"},
+	},
+	on_activate = function(self)
+		self.object:set_animation({x = 0, y = 140}, 1)
+	end
+})
+
+minetest.register_entity("gltf:simple_skin", {
+	initial_properties = {
+		visual = "mesh",
+		visual_size = vector.new(5, 5, 5),
+		mesh = "gltf_simple_skin.gltf",
+		textures = {},
+		backface_culling = false
+	},
+	on_activate = function(self)
+		self.object:set_animation({x = 0, y = 5.5}, 1)
+	end
+})
