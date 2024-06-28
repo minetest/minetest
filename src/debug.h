e0cdc715aa28c46938f836a70324734ec6b3766d
DustyBagel <150477174+DustyBagel@users.noreply.github.com>
2024-06-28 15:37:11 -0500

    Fix accidental change to debug.h

--- src/debug.h
+++ src/debug.h
@@ -78,7 +78,7 @@ void debug_set_exception_handler();
 	These should be put into every thread
 */
 
-#if CATCH_UNHANDLED_EXCEPTIONS == 0
+#if CATCH_UNHANDLED_EXCEPTIONS == 1
 	#define BEGIN_DEBUG_EXCEPTION_HANDLER try {
 	#define END_DEBUG_EXCEPTION_HANDLER                        \
 		} catch (std::exception &e) {                          \
