6228f7edb38193330bb8c277c7961c0b4a064c91
1F616EMO <root@1f616emo.xyz>
2024-07-07 06:57:42 +0800

    __builtin_unreachable for clang

--- irr/src/os.h
+++ irr/src/os.h
@@ -10,7 +10,7 @@
 #include "ILogger.h"
 #include "ITimer.h"
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) || defined(__clang__)
 #define UNREACHABLE __builtin_unreachable();
 #elif defined(_MSC_VER)
 #define UNREACHABLE __assume(false);
