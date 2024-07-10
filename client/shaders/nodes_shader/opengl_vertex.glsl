d1fef497cf01f4d6c7f3d639f2b5ff2242d85699
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2024-07-10 06:53:21 -0500

    Fix opengl_vertex.glsl

--- client/shaders/nodes_shader/opengl_vertex.glsl
+++ client/shaders/nodes_shader/opengl_vertex.glsl
@@ -258,25 +258,6 @@ void main(void)
 	if (varTexCoord.y < 0.05) {
 		pos.x += disp_x;
 		pos.z += disp_z;vec3 a = floor(p);
-vec3 d = p - a;
-d = d * d * (3.0 - 2.0 * d);
-
-vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-vec4 k1 = perm(b.xyxy);
-vec4 k2 = perm(k1.xyxy + b.zzww);
-
-vec4 c = k2 + a.zzzz;
-vec4 k3 = perm(c);
-vec4 k4 = perm(c + 1.0);
-
-vec4 o1 = fract(k3 * (1.0 / 41.0));
-vec4 o2 = fract(k4 * (1.0 / 41.0));
-
-vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
-
-float noiseValue = o4.y * d.y + o4.x * (1.0 - d.y);
-return noiseValue * 0.5;
 	}
 #endif
 	worldPosition = (mWorld * pos).xyz;
