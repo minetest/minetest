75558f34a1dbcb1e14904b351f5acab508c4f4b0
1F616EMO <root@1f616emo.xyz>
2024-07-10 18:58:09 +0800

    Not sending clients_list if set to anonymize

--- src/server/serverlist.cpp
+++ src/server/serverlist.cpp
@@ -65,12 +65,10 @@ void sendAnnounce(AnnounceAction action,
 		server["game_time"]    = game_time;
 		server["clients"]      = (int) clients_names.size();
 		server["clients_max"]  = g_settings->getU16("max_users");
-		server["clients_list"] = Json::Value(Json::arrayValue);
-		for (std::size_t i = 0; i < clients_names.size(); i++) {
-			if (g_settings->getBool("server_announce_anonymize_names")) {
-				server["clients_list"].append("anon" + std::to_string(i));
-			} else {
-				server["clients_list"].append(clients_names[i]);
+		if (g_settings->getBool("server_announce_anonymize_names")) {
+			server["clients_list"] = Json::Value(Json::arrayValue);
+			for (const std::string &clients_name : clients_names) {
+				server["clients_list"].append(clients_name);
 			}
 		}
 		if (!gameid.empty())
