9dabef0f729b02557e39689cd62ef9414affce4b
y5nw <37980625+y5nw@users.noreply.github.com>
2024-07-27 02:59:14 +0200

    fixup midding header in keycode resolution

--- src/client/inputhandler.h
+++ src/client/inputhandler.h
@@ -21,6 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "irrlichttypes_extrabloated.h"
 #include "joystick_controller.h"
+#include <array>
 #include <functional>
 #include <list>
 #include <unordered_set>
