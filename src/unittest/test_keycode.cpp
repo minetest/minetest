7d42b5d888ee1237e20450917c334ba2e24ebd19
y5nw <37980625+y5nw@users.noreply.github.com>
2024-08-12 01:24:51 +0200

    partial

--- src/unittest/test_keycode.cpp
+++ src/unittest/test_keycode.cpp
@@ -31,35 +31,40 @@ void TestKeycode::runTests(IGameDef *gamedef)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define UASSERTEQ_STR(one, two) UASSERT(strcmp(one, two) == 0)
+#define UASSERTEQ_STR(one, two) UASSERT(one == two)
+#define UASSERT_HAS_NAME(k) UASSERT(!k.name().empty())
 
 void TestKeycode::testCreateFromString()
 {
 	KeyPress k;
 
+	k = KeyPress("");
+	UASSERTEQ_STR(k.sym(), "");
+	UASSERTEQ_STR(k.name(), "");
+
 	// Character key, from char
 	k = KeyPress("R");
 	UASSERTEQ_STR(k.sym(), "KEY_KEY_R");
-	UASSERTCMP(int, >, strlen(k.name()), 0); // should have human description
+	UASSERT_HAS_NAME(k);
 
 	// Character key, from identifier
 	k = KeyPress("KEY_KEY_B");
 	UASSERTEQ_STR(k.sym(), "KEY_KEY_B");
-	UASSERTCMP(int, >, strlen(k.name()), 0);
+	UASSERT_HAS_NAME(k);
 
 	// Non-Character key, from identifier
 	k = KeyPress("KEY_UP");
 	UASSERTEQ_STR(k.sym(), "KEY_UP");
-	UASSERTCMP(int, >, strlen(k.name()), 0);
+	UASSERT_HAS_NAME(k);
 
 	k = KeyPress("KEY_F6");
 	UASSERTEQ_STR(k.sym(), "KEY_F6");
-	UASSERTCMP(int, >, strlen(k.name()), 0);
+	UASSERT_HAS_NAME(k);
 
 	// Irrlicht-unknown key, from char
 	k = KeyPress("/");
 	UASSERTEQ_STR(k.sym(), "/");
-	UASSERTCMP(int, >, strlen(k.name()), 0);
+	UASSERT_HAS_NAME(k);
 }
 
 void TestKeycode::testCreateFromSKeyInput()
@@ -68,47 +73,46 @@ void TestKeycode::testCreateFromSKeyInput()
 	irr::SEvent::SKeyInput in;
 
 	// Character key
-	in.Key = irr::KEY_KEY_3;
-	in.Char = L'3';
+	in.Scancode = KeyCode(irr::KEY_KEY_3, L'3');
 	k = KeyPress(in);
 	UASSERTEQ_STR(k.sym(), "KEY_KEY_3");
+	UASSERT_HAS_NAME(k);
 
 	// Non-Character key
-	in.Key = irr::KEY_RSHIFT;
-	in.Char = L'\0';
+	in.Scancode = KeyCode(irr::KEY_RSHIFT, L'\0');
 	k = KeyPress(in);
 	UASSERTEQ_STR(k.sym(), "KEY_RSHIFT");
+	UASSERT_HAS_NAME(k);
 
 	// Irrlicht-unknown key
-	in.Key = irr::KEY_KEY_CODES_COUNT;
-	in.Char = L'?';
+	in.Scancode = KeyCode(KEY_KEY_CODES_COUNT, L'?');
 	k = KeyPress(in);
 	UASSERTEQ_STR(k.sym(), "?");
-
-	// prefer_character mode
-	in.Key = irr::KEY_COMMA;
-	in.Char = L'G';
-	k = KeyPress(in, true);
-	UASSERTEQ_STR(k.sym(), "KEY_KEY_G");
+	UASSERT_HAS_NAME(k);
 }
 
 void TestKeycode::testCompare()
 {
+	// "Empty" key
+	UASSERT(KeyPress() == KeyPress(""));
+
 	// Basic comparison
 	UASSERT(KeyPress("5") == KeyPress("KEY_KEY_5"));
 	UASSERT(!(KeyPress("5") == KeyPress("KEY_NUMPAD5")));
 
 	// Matching char suffices
 	// note: This is a real-world example, Irrlicht maps XK_equal to irr::KEY_PLUS on Linux
+	// TODO: Is this still relevant for SDL?
 	irr::SEvent::SKeyInput in;
+	/*
 	in.Key = irr::KEY_PLUS;
 	in.Char = L'=';
 	UASSERT(KeyPress("=") == KeyPress(in));
+	*/
 
 	// Matching keycode suffices
 	irr::SEvent::SKeyInput in2;
-	in.Key = in2.Key = irr::KEY_OEM_CLEAR;
-	in.Char = L'\0';
-	in2.Char = L';';
+	in.Scancode = KeyCode(irr::KEY_OEM_CLEAR, L'\0');
+	in2.Scancode = KeyCode(irr::KEY_OEM_CLEAR, L';');
 	UASSERT(KeyPress(in) == KeyPress(in2));
 }
