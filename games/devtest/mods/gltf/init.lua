85dfa506437918c68841d3f0da7d50476359645e
Lars Mueller <appgurulars@gmx.de>
2024-08-26 15:17:57 +0200

    Disable backface culling for some test models

--- games/devtest/mods/gltf/init.lua
+++ games/devtest/mods/gltf/init.lua
@@ -1,17 +1,18 @@
-local function register_entity(name, textures)
+local function register_entity(name, textures, backface_culling)
 	minetest.register_entity("gltf:" .. name, {
 		initial_properties = {
 			visual = "mesh",
 			mesh = "gltf_" .. name .. ".gltf",
 			textures = textures,
+			backface_culling = backface_culling,
 		},
 	})
 end
 
 -- These do not have texture coordinates; they simple render as black surfaces.
-register_entity("minimal_triangle", {})
-register_entity("triangle_with_vertex_stride", {})
-register_entity("triangle_without_indices", {})
+register_entity("minimal_triangle", {}, false)
+register_entity("triangle_with_vertex_stride", {}, false)
+register_entity("triangle_without_indices", {}, false)
 do
 	local cube_textures = {"no_texture.png"} -- TODO provide proper textures
 	register_entity("blender_cube", cube_textures)
@@ -21,11 +22,13 @@ end
 register_entity("snow_man", {"gltf_snow_man.png"})
 register_entity("spider", {"gltf_spider.png"})
 -- Note: Model has an animation, but we can use it as a static test nevertheless
-register_entity("frog", {"gltf_frog.png"})
+-- The claws rendering incorrectly from one side is expected behavior:
+-- They use an unsupported double-sided material.
+register_entity("frog", {"gltf_frog.png"}, false)
 
 minetest.register_node("gltf:frog", {
 	description = "glTF frog, but it's a node",
-	tiles = {"gltf_frog.png"},
+	tiles = {{name = "gltf_frog.png", backface_culling = false}},
 	drawtype = "mesh",
 	mesh = "gltf_frog.gltf",
 })
