6c121e10ee7c2c27027bf8ef46d8851c9f5ad251
y5nw <37980625+y5nw@users.noreply.github.com>
2024-08-31 22:48:46 +0200

    const std::string_view& -> std::string_view

--- src/client/keycode.h
+++ src/client/keycode.h
@@ -16,7 +16,7 @@ class KeyPress
 public:
 	KeyPress() {};
 
-	KeyPress(const std::string_view &name);
+	KeyPress(std::string_view name);
 
 	KeyPress(const irr::SEvent::SKeyInput &in) :
 		scancode(in.SystemKeyCode) {};
@@ -46,7 +46,7 @@ public:
 	static const KeyPress &getSpecialKey(const std::string &name);
 
 private:
-	bool loadFromScancode(const std::string_view &name);
+	bool loadFromScancode(std::string_view name);
 
 	inline std::string formatScancode() const
 	{
