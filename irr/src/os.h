1e00363f5a61373284c0f7e24c1b9139dde686a1
sfan5 <sfan5@live.de>
2024-09-12 17:18:13 +0200

    Add some debug helpers around this area

--- irr/src/os.h
+++ irr/src/os.h
@@ -52,7 +52,6 @@ public:
 	// prints out a string to the console out stdout or debug log or whatever
 	static void print(const c8 *message, ELOG_LEVEL ll = ELL_INFORMATION);
 	static void log(const c8 *message, ELOG_LEVEL ll = ELL_INFORMATION);
-	static void log(const wchar_t *message, ELOG_LEVEL ll = ELL_INFORMATION);
 
 	// The string ": " is added between message and hint
 	static void log(const c8 *message, const c8 *hint, ELOG_LEVEL ll = ELL_INFORMATION);
