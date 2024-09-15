ee881a465d78abd4aaebaf72d2bbb4bcb8ad8ef5
y5nw <37980625+y5nw@users.noreply.github.com>
2024-09-15 16:43:50 +0200

    move wrong testfiles elsewhere

--- src/unittest/test_translations.cpp
+++ src/unittest/test_translations.cpp
@@ -31,7 +31,7 @@ static std::string read_translation_file(const std::string &filename)
 {
 		auto gamespec = findSubgame("devtest");
 		REQUIRE(gamespec.isValid());
-		auto path = gamespec.gamemods_path + (DIR_DELIM "testtranslations" DIR_DELIM "locale" DIR_DELIM) + filename;
+		auto path = gamespec.gamemods_path + (DIR_DELIM "testtranslations" DIR_DELIM "test_locale" DIR_DELIM) + filename;
 		std::string content;
 		REQUIRE(fs::ReadFile(path, content));
 		return content;
