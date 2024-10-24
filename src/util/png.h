c571525b0d2316d04a2166dff71f5a73b98d8b04
SFENCE <sfence.software@gmail.com>
2024-10-24 10:29:32 +0200

    Replace licensing text in headers (LGPL 2.1).

--- src/util/png.h
+++ src/util/png.h
@@ -1,21 +1,6 @@
-/*
-Minetest
-Copyright (C) 2021 hecks
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+// Luanti
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2021 hecks
 
 #pragma once
 
