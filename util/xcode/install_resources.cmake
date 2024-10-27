5d10033ccabe7216783bca4cbb618598d42926e0
SFENCE <sfence.software@gmail.com>
2024-10-27 15:58:22 +0100

    Fix resources install in Xcode.

--- util/xcode/install_resources.cmake
+++ util/xcode/install_resources.cmake
@@ -38,9 +38,9 @@ execute_process(
 )
 execute_process(
 	COMMAND ${CMAKE_COMMAND} -E make_directory
-	"${RESOURCES_DIR}/minetest"
+	"${RESOURCES_DIR}/$ENV{PRODUCT_NAME}"
 )
-set(RESOURCE_MINETEST_FILES
+set(RESOURCE_LUANTI_FILES
 	"$ENV{SOURCE_ROOT}/README.md"
 	"$ENV{SOURCE_ROOT}/doc/client_lua_api.md"
 	"$ENV{SOURCE_ROOT}/doc/lua_api.md"
@@ -49,11 +49,11 @@ set(RESOURCE_MINETEST_FILES
 	"$ENV{SOURCE_ROOT}/doc/texture_packs.md"
 	"$ENV{SOURCE_ROOT}/doc/world_format.md"
 )
-foreach (file ${RESOURCE_MINETEST_FILES})
+foreach (file ${RESOURCE_LUANTI_FILES})
 	execute_process(
 		COMMAND ${CMAKE_COMMAND} -E copy
 		"${file}"
-		"${RESOURCES_DIR}/minetest/"
+		"${RESOURCES_DIR}/$ENV{PRODUCT_NAME}/"
 	)
 endforeach()
 execute_process(
