6c76fcb6600c51233cacaedbb76653c3cd8a1939
grorp <grorp@posteo.de>
2024-10-31 11:31:22 +0100

    Irrlicht 2D rendering: Avoid artifacts caused by bilinear downscaling + repeat texture wrapping

--- irr/src/CNullDriver.cpp
+++ irr/src/CNullDriver.cpp
@@ -108,13 +108,14 @@ CNullDriver::CNullDriver(io::IFileSystem *io, const core::dimension2d<u32> &scre
 	InitMaterial2D.ZBuffer = video::ECFN_DISABLED;
 	InitMaterial2D.UseMipMaps = false;
 	InitMaterial2D.forEachTexture([](auto &tex) {
-		// Using ETMINF_LINEAR_MIPMAP_NEAREST (bilinear) for 2D graphics looks
-		// much better and doesn't have any downsides (e.g. regarding pixel art).
+		// Using bilinear for downscaling for 2D graphics looks much better
+		// and doesn't blur pixel art (unlike bilinear for upscaling).
 		tex.MinFilter = video::ETMINF_LINEAR_MIPMAP_NEAREST;
 		tex.MagFilter = video::ETMAGF_NEAREST;
-		tex.TextureWrapU = video::ETC_REPEAT;
-		tex.TextureWrapV = video::ETC_REPEAT;
-		tex.TextureWrapW = video::ETC_REPEAT;
+		// Clamp to avoid artifacts caused by bilinear + repeat.
+		tex.TextureWrapU = video::ETC_CLAMP_TO_EDGE;
+		tex.TextureWrapV = video::ETC_CLAMP_TO_EDGE;
+		tex.TextureWrapW = video::ETC_CLAMP_TO_EDGE;
 	});
 	OverrideMaterial2D = InitMaterial2D;
 }
