78c2327529ea23e1219fbf8588b2dc1c56466de3
sfan5 <sfan5@live.de>
2024-11-11 21:57:48 +0100

    forgot this

--- util/test_multiplayer.sh
+++ util/test_multiplayer.sh
@@ -29,7 +29,7 @@ mkdir -p "$worldpath/worldmods"
 
 printf '%s\n' >"$testspath/client1.conf" \
 	video_driver=null name=client1 viewing_range=10 \
-	enable_{sound,minimap,shaders}=false
+	enable_{sound,minimap,post_processing}=false
 
 printf '%s\n' >"$testspath/server.conf" \
 	max_block_send_distance=1 active_block_range=1 \
