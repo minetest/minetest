92072a25678ae7001912643219cfbca54bb4384c
Desour <ds.desour@proton.me>
2024-11-28 16:16:44 +0100

    Use newer evp api for sha256, because sha256.c overrides SHA256

--- src/util/hashing.cpp
+++ src/util/hashing.cpp
@@ -4,9 +4,11 @@
 
 #include "hashing.h"
 
+#include "debug.h"
 #include "config.h"
 #if USE_OPENSSL
 #include <openssl/sha.h>
+#include <openssl/evp.h>
 #else
 #include "util/sha1.h"
 #include "my_sha256.h"
@@ -32,11 +34,16 @@ std::string sha1(std::string_view data)
 
 std::string sha256(std::string_view data)
 {
-	// same api in openssl and my_sha256.h
 	std::string digest(SHA256_DIGEST_SIZE, '\000');
 	auto src = reinterpret_cast<const uint8_t *>(data.data());
 	auto dst = reinterpret_cast<uint8_t *>(digest.data());
+#if USE_OPENSSL
+	// can't call SHA256(), because it's defined by our sha256.c fallback
+	auto success = EVP_Digest(src, data.size(), dst, nullptr, EVP_sha256(), nullptr) == 1;
+	FATAL_ERROR_IF(!success, "sha256 failed");
+#else
 	SHA256(src, data.size(), dst);
+#endif
 	return digest;
 }
 
