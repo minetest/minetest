6d5c7488e8f9244752d2768618c6520b282f7008
sfan5 <sfan5@live.de>
2024-12-11 18:10:32 +0100

    caching poc

--- src/client/clientmap.cpp
+++ src/client/clientmap.cpp
@@ -19,6 +19,7 @@
 #include "util/tracy_wrapper.h"
 #include "client/renderingengine.h"
 #include "util/quicktune.h"
+#include "util/hex.h"
 
 #include <queue>
 
@@ -789,13 +790,13 @@ void MeshBufListMaps::addFromBlock(v3s16 block_pos, MapBlockMesh *block_mesh,
  * @param src buffer list
  * @param dst draw order
  * @param get_world_pos returns correct translation for a buffer
- * @param buffer_trash output container for temporary mesh buffers
+ * @param dynamic_buffers
  * @return number of drawcalls that were saved due to merging
  */
-template <typename F, typename C>
+template <typename F>
 static u32 transformBuffersToDrawOrder(
 	const MeshBufListMaps::MeshBufList &src, DrawDescriptorList &draw_order,
-		F get_world_pos, C &buffer_trash)
+		F get_world_pos, CachedMeshBuffers &dynamic_buffers)
 {
 	u32 IDEAL_MIN_VERTICES = 200;
 	QUICKTUNE_AUTONAME(QVT_INT, IDEAL_MIN_VERTICES, 0, 2000);
@@ -833,6 +834,7 @@ static u32 transformBuffersToDrawOrder(
 		}
 	};
 
+	std::vector<std::pair<v3f, scene::IMeshBuffer*>> to_merge;
 	// iterate in reverse to get draw closest blocks first
 	for (auto it = src.rbegin(); it != src.rend(); ++it) {
 		v3f translate = get_world_pos(it->first);
@@ -840,28 +842,55 @@ static u32 transformBuffersToDrawOrder(
 		if (can_merge < 2 || buf->getVertexCount() >= IDEAL_MIN_VERTICES) {
 			draw_order.emplace_back(translate, buf);
 			continue;
+		} else {
+			to_merge.emplace_back(translate, buf);
 		}
-
-		bool new_buffer = false;
-		if (!tmp)
-			new_buffer = true;
-		else if (tmp->getVertexCount() + buf->getVertexCount() > U16_MAX)
-			new_buffer = true;
-		if (new_buffer) {
-			finish_buf();
-			tmp = new scene::SMeshBuffer();
-			buffer_trash.push_back(tmp);
-			assert(tmp->getPrimitiveType() == buf->getPrimitiveType());
-			tmp->Material = buf->getMaterial();
-			// preallocate
-			tmp->Vertices->Data.reserve(total_vtx);
-			tmp->Indices->Data.reserve(total_idx);
-		}
-
-		appendToMeshBuffer(tmp, buf, translate);
-		merged_count++;
 	}
-	finish_buf();
+
+	std::string kkk;
+	std::sort(to_merge.begin(), to_merge.end(), [] (const auto &l, const auto &r) {
+		return l.second < r.second;
+	});
+	for (auto &it : to_merge) {
+		// big assumptions here
+		kkk.append(reinterpret_cast<const char*>(&it.second), sizeof(it.second));
+	}	
+
+	auto it2 = dynamic_buffers.find(kkk);
+	if (it2 != dynamic_buffers.end()) {
+		g_profiler->avg("transformBuffersToDrawOrder: cache hit rate", 1);
+		for (auto *buf : it2->second.buf)
+			draw_order.emplace_back(v3f(0), buf);
+		it2->second.age = 0;
+	} else if (!kkk.empty()) {
+		g_profiler->avg("transformBuffersToDrawOrder: cache hit rate", 0);
+		auto &put_buffers = dynamic_buffers[kkk];
+		//rawstream << "new " << hex_encode(kkk) << std::endl;
+		for (auto &it : to_merge) {
+			v3f translate = it.first;
+			auto *buf = it.second;
+
+			bool new_buffer = false;
+			if (!tmp)
+				new_buffer = true;
+			else if (tmp->getVertexCount() + buf->getVertexCount() > U16_MAX)
+				new_buffer = true;
+			if (new_buffer) {
+				finish_buf();
+				tmp = new scene::SMeshBuffer();
+				put_buffers.buf.push_back(tmp);
+				assert(tmp->getPrimitiveType() == buf->getPrimitiveType());
+				tmp->Material = buf->getMaterial();
+				// preallocate
+				tmp->Vertices->Data.reserve(total_vtx);
+				tmp->Indices->Data.reserve(total_idx);
+			}
+
+			appendToMeshBuffer(tmp, buf, translate);
+			merged_count++;
+		}
+		finish_buf();
+	}
 
 	// first call needs to set the material
 	if (draw_order.size() > draw_order_pre)
@@ -914,7 +943,6 @@ void ClientMap::renderMap(video::IVideoDriver* driver, s32 pass)
 	TimeTaker tt_collect("");
 
 	MeshBufListMaps grouped_buffers;
-	std::vector<scene::IMeshBuffer*> buffer_trash;
 	DrawDescriptorList draw_order;
 
 	auto is_frustum_culled = m_client->getCamera()->getFrustumCuller();
@@ -972,7 +1000,7 @@ void ClientMap::renderMap(video::IVideoDriver* driver, s32 pass)
 	for (auto &map : grouped_buffers.maps) {
 		for (auto &list : map) {
 			merge_saved_drawcalls += transformBuffersToDrawOrder(
-				list.second, draw_order, get_block_wpos, buffer_trash);
+				list.second, draw_order, get_block_wpos, m_dynamic_buffers);
 		}
 	}
 
@@ -1029,6 +1057,16 @@ void ClientMap::renderMap(video::IVideoDriver* driver, s32 pass)
 	if (pass == scene::ESNRP_SOLID) {
 		g_profiler->avg("renderMap(): animated meshes [#]", mesh_animate_count);
 		g_profiler->avg(prefix + "saved drawcalls (merge) [#]", merge_saved_drawcalls);
+
+		for (auto it = m_dynamic_buffers.begin(); it != m_dynamic_buffers.end(); ) {
+			if (++it->second.age > 1) {
+				for (auto *buf : it->second.buf)
+					buf->drop();
+				it = m_dynamic_buffers.erase(it);
+			} else {
+				it++;
+			}
+		}
 	}
 
 	if (pass == scene::ESNRP_TRANSPARENT) {
@@ -1038,9 +1076,6 @@ void ClientMap::renderMap(video::IVideoDriver* driver, s32 pass)
 	g_profiler->avg(prefix + "vertices drawn [#]", vertex_count);
 	g_profiler->avg(prefix + "drawcalls [#]", drawcall_count);
 	g_profiler->avg(prefix + "material swaps [#]", material_swaps);
-
-	for (auto &x : buffer_trash)
-		x->drop();
 }
 
 static bool getVisibleBrightness(Map *map, const v3f &p0, v3f dir, float step,
@@ -1256,7 +1291,6 @@ void ClientMap::renderMapShadows(video::IVideoDriver *driver,
 	};
 
 	MeshBufListMaps grouped_buffers;
-	std::vector<scene::IMeshBuffer*> buffer_trash;
 	DrawDescriptorList draw_order;
 
 	std::size_t count = 0;
@@ -1301,7 +1335,7 @@ void ClientMap::renderMapShadows(video::IVideoDriver *driver,
 	for (auto &map : grouped_buffers.maps) {
 		for (auto &list : map) {
 			transformBuffersToDrawOrder(
-				list.second, draw_order, get_block_wpos, buffer_trash);
+				list.second, draw_order, get_block_wpos, m_dynamic_buffers);
 		}
 	}
 
@@ -1366,9 +1400,6 @@ void ClientMap::renderMapShadows(video::IVideoDriver *driver,
 	g_profiler->avg(prefix + "vertices drawn [#]", vertex_count);
 	g_profiler->avg(prefix + "drawcalls [#]", drawcall_count);
 	g_profiler->avg(prefix + "material swaps [#]", material_swaps);
-
-	for (auto &x : buffer_trash)
-		x->drop();
 }
 
 /*
