9d417b39769ee00683979b7225a19e3f65336156
cx384 <cx384@proton.me>
2024-12-15 17:51:09 +0100

    Add group by underscore prefix feature

--- builtin/mainmenu/dlg_server_list_mods.lua
+++ builtin/mainmenu/dlg_server_list_mods.lua
@@ -5,14 +5,41 @@
 local function get_formspec(dialogdata)
 	local TOUCH_GUI = core.settings:get_bool("touch_gui")
 	local server = dialogdata.server
+	local filter_prefix = dialogdata.filter_prefix
 
 	-- A wrongly behaving server may send ill formed mod names
 	local mods = {}
 	table.sort(server.mods)
-	for i, m in ipairs(server.mods) do
-		mods[i] = core.formspec_escape(m)
+
+	if filter_prefix then
+		for _, mod in ipairs(server.mods) do
+			if mod:sub(0, #filter_prefix) == filter_prefix then
+				table.insert(mods, core.formspec_escape(mod))
+			end
+		end
+	else
+		local last_prefix
+		for i, mod in ipairs(server.mods) do
+			local prefix = mod:match("([^_]*_)")
+			if prefix and last_prefix == prefix then
+				mods[#mods] = "#BBBBBB".. core.formspec_escape(prefix) .. "*"
+			else
+				table.insert(mods, core.formspec_escape(mod))
+				last_prefix = prefix
+			end
+		end
 	end
+	dialogdata.mods = mods
 	mods = table.concat(mods, ",")
+
+	local prefix_button_label
+	if not filter_prefix then
+		prefix_button_label = "Expand all prefixes"
+	elseif filter_prefix == "" then
+		prefix_button_label = "Group by prefix"
+	else
+		prefix_button_label = "Show all mods"
+	end
 
 	local formspec = {
 		"formspec_version[8]",
@@ -23,8 +50,9 @@ local function get_formspec(dialogdata)
 					"the following mods:",
 				"<b>" .. core.hypertext_escape(server.name) .. "</b>",
 				"<style font=mono>" .. (server.gameid or "") .. "</style>") .. "]",
-		"textlist[0.5,1.5;5,6.8;;" .. mods .. "]",
-		"button[1.5,8.5;3,0.8;quit;OK]"
+		"textlist[0.5,1.5;5,6.8;mods;" .. mods .. "]",
+		"button[0.5,8.5;3,0.8;prefix;" .. prefix_button_label .. "]",
+		"button[3.5,8.5;2,0.8;quit;Back]"
 	}
 	return table.concat(formspec, "")
 end
@@ -34,6 +62,27 @@ local function buttonhandler(this, fields)
         this:delete()
 		return true
 	end
+
+	if fields.mods then
+		local exploded = core.explode_textlist_event(fields.mods)
+		if exploded.type == "DCL" then
+			local match = this.data.mods[exploded.index]:match("#BBBBBB([^_]*_)%*")
+			if match then
+				this.data.filter_prefix = match
+				return true
+			end
+		end
+	end
+
+	if fields.prefix then
+		if this.data.filter_prefix then
+			this.data.filter_prefix = nil
+		else
+			this.data.filter_prefix = ""
+		end
+		return true
+	end
+
 	return false
 end
 
