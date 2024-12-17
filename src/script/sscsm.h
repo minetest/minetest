93c955252695e7373ca8e8177056a2fc6452530a
Desour <ds.desour@proton.me>
2024-12-17 12:26:22 +0100

    use thread

--- src/script/sscsm.h
+++ src/script/sscsm.h
@@ -1,11 +1,14 @@
 
 #pragma once
 
+#include <mutex>
+#include <condition_variable>
 #include <memory>
 #include <type_traits>
 #include "irrlichttypes.h"
 #include "client/clientenvironment.h"
 #include "map.h"
+#include "threading/thread.h"
 
 struct SSCSMControler;
 
@@ -49,7 +52,7 @@ T deserializeSSCSMAnswer(const SerializedSSCSMAnswer &answer_serialized)
 }
 
 template <typename T>
-SerializedSSCSMAnswer serializedSSCSMAnswer(const T &answer)
+SerializedSSCSMAnswer serializeSSCSMAnswer(const T &answer)
 {
 	static_assert(std::is_base_of_v<ISSCSMAnswer, T>);
 
@@ -64,58 +67,148 @@ std::unique_ptr<ISSCSMRequest> deserializeSSCSMRequest(SerializedSSCSMRequest re
 }
 
 
+// FIXME: replace this with an ipc channel
+class StupidChannel
+{
+	std::mutex m_mutex;
+	std::condition_variable m_condvar;
+	SerializedSSCSMRequest m_request;
+	SerializedSSCSMAnswer m_answer;
+
+public:
+	void sendA(SerializedSSCSMRequest request)
+	{
+		{
+			auto lock = std::lock_guard(m_mutex);
+
+			m_request = std::move(request);
+		}
+
+		m_condvar.notify_one();
+	}
+
+	SerializedSSCSMAnswer recvA()
+	{
+		auto lock = std::unique_lock(m_mutex);
+
+		while (!m_answer) {
+			m_condvar.wait(lock);
+		}
+
+		auto answer = std::move(m_answer);
+		m_answer = nullptr;
+
+		return answer;
+	}
+
+	SerializedSSCSMAnswer exchangeA(SerializedSSCSMRequest request)
+	{
+		sendA(std::move(request));
+
+		return recvA();
+	}
+
+	void sendB(SerializedSSCSMAnswer answer)
+	{
+		{
+			auto lock = std::lock_guard(m_mutex);
+
+			m_answer = std::move(answer);
+		}
+
+		m_condvar.notify_one();
+	}
+
+	SerializedSSCSMRequest recvB()
+	{
+		auto lock = std::unique_lock(m_mutex);
+
+		while (!m_request) {
+			m_condvar.wait(lock);
+		}
+
+		auto request = std::move(m_request);
+		m_request = nullptr;
+
+		return request;
+	}
+
+	SerializedSSCSMRequest exchangeB(SerializedSSCSMAnswer answer)
+	{
+		sendB(std::move(answer));
+
+		return recvB();
+	}
+};
+
+
 struct SSCSMEnvironment;
 
 struct SSCSMControler
 {
-	// FIXME: replace this with an ipc channel
-	SSCSMEnvironment *m_sscsm_env;
-	// Note: Not needed later. Instead of us passing control flow to the SSCSMEnvironment
-	// via run() (see event*() member functions), the other process will already
-	// be running (or rather waiting), and we only need to "return" the next event.
-	std::optional<int> m_next_event;
-
+	std::unique_ptr<SSCSMEnvironment> m_thread;
+	std::shared_ptr<StupidChannel> m_channel;
 	ClientEnvironment *m_clientenv;
+
+	SSCSMControler(std::unique_ptr<SSCSMEnvironment> thread,
+			std::shared_ptr<StupidChannel> channel,
+			ClientEnvironment *clientenv) :
+		m_thread(std::move(thread)), m_channel(std::move(channel)),
+		m_clientenv(clientenv)
+	{
+	}
+
+	static std::unique_ptr<SSCSMControler> create(ClientEnvironment *clientenv);
 
 	SerializedSSCSMAnswer handleRequest(ISSCSMRequest *req)
 	{
 		return req->exec(this);
 	}
 
+	// Handles requests until the next event is polled
+	void runEvent(int event);
+
+	void eventTearDown();
 	void eventOnStep(f32 dtime);
 };
 
-struct SSCSMEnvironment
+struct SSCSMEnvironment : Thread
 {
-	// FIXME: replace this with an ipc channel
-	SSCSMControler *m_channel;
+	std::shared_ptr<StupidChannel> m_channel;
 
-	void run()
+	SSCSMEnvironment(std::shared_ptr<StupidChannel> channel) :
+		Thread("SSCSMEnvironment-thread"),
+		m_channel(std::move(channel))
 	{
-		// Note: This will later run in another process, and only return if it
-		// wants to tear down the process. The next event will then also no
-		// longer be optional.
-
-		while (true) {
-			auto next_event = cmdPollNextEvent();
-			if (!next_event.has_value())
-				break;
-
-			if (*next_event == 42)
-				runEventOnStep();
-		}
 	}
 
-	SerializedSSCSMAnswer exchange(const SerializedSSCSMRequest &req)
+	void *run()
 	{
-		return m_channel->handleRequest(req.get());
+		while (true) {
+			auto next_event = cmdPollNextEvent();
+
+			if (next_event == 0) // tear down
+				break;
+
+			if (next_event == 42)
+				runEventOnStep();
+		}
+
+		return nullptr;
+	}
+
+	SerializedSSCSMAnswer exchange(SerializedSSCSMRequest req)
+	{
+		return m_channel->exchangeA(std::move(req));
 	}
 
 	void runEventOnStep()
 	{
+		cmdGetNode(v3s16(0, 0, 0));
 	}
 
-	std::optional<int> cmdPollNextEvent();
+	int cmdPollNextEventNoRequest();
+	int cmdPollNextEvent();
 	MapNode cmdGetNode(v3s16 pos);
 };
 
@@ -137,39 +230,76 @@ struct SSCSMRequestGetNode : public ISSCSMRequest
 	{
 		MapNode node = cntrl->m_clientenv->getMap().getNode(pos);
 
-		return serializedSSCSMAnswer(SSCSMAnswerGetNode{node});
+		return serializeSSCSMAnswer(SSCSMAnswerGetNode{node});
 	}
 };
 
 struct SSCSMAnswerPollNextEvent : public ISSCSMAnswer
 {
-	std::optional<int> next_event;
+	int next_event;
 
-	SSCSMAnswerPollNextEvent(std::optional<int> next_event_) : next_event(next_event_) {}
+	SSCSMAnswerPollNextEvent(int next_event_) : next_event(next_event_) {}
 };
 
 struct SSCSMRequestPollNextEvent : public ISSCSMRequest
 {
 	virtual SerializedSSCSMAnswer exec(SSCSMControler *cntrl)
 	{
-		auto next_event = cntrl->m_next_event;
-		cntrl->m_next_event = std::nullopt;
-
-		return serializedSSCSMAnswer(SSCSMAnswerPollNextEvent{next_event});
+		FATAL_ERROR("SSCSMRequestPollNextEvent needs to be handled by SSCSMControler::runEvent()");
 	}
 };
 
 
+
+std::unique_ptr<SSCSMControler> SSCSMControler::create(ClientEnvironment *clientenv)
+{
+	auto channel = std::make_shared<StupidChannel>();
+	auto thread = std::make_unique<SSCSMEnvironment>(channel);
+
+	// Wait for thread to finish initializing.
+	auto req0 = deserializeSSCSMRequest(channel->recvB());
+	FATAL_ERROR_IF(!dynamic_cast<SSCSMRequestPollNextEvent *>(req0.get()),
+			"First request must be pollEvent.");
+
+	return std::make_unique<SSCSMControler>(std::move(thread), channel, clientenv);
+}
+
+void SSCSMControler::runEvent(int event)
+{
+	auto answer = serializeSSCSMAnswer(SSCSMAnswerPollNextEvent{event});
+
+	while (true) {
+		auto request = deserializeSSCSMRequest(m_channel->exchangeB(std::move(answer)));
+
+		if (dynamic_cast<SSCSMRequestPollNextEvent *>(request.get()) != nullptr) {
+			break;
+		}
+
+		answer = handleRequest(request.get());
+	}
+}
+
+void SSCSMControler::eventTearDown()
+{
+	runEvent(0);
+}
+
 void SSCSMControler::eventOnStep(f32 dtime)
 {
-	m_next_event = 42;
-
-	m_sscsm_env->run();
+	runEvent(42);
 }
 
 
 
-std::optional<int> SSCSMEnvironment::cmdPollNextEvent()
+int SSCSMEnvironment::cmdPollNextEventNoRequest()
+{
+	auto answer = deserializeSSCSMAnswer<SSCSMAnswerPollNextEvent>(
+			m_channel->recvA()
+		);
+	return answer.next_event;
+}
+
+int SSCSMEnvironment::cmdPollNextEvent()
 {
 	auto request = SSCSMRequestPollNextEvent{};
 	auto answer = deserializeSSCSMAnswer<SSCSMAnswerPollNextEvent>(
