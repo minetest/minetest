4822f8ea8183ded358e81c2117fb61655db96f82
y5nw <37980625+y5nw@users.noreply.github.com>
2025-01-07 15:48:45 +0100

    review CIrrDeviceSDL

--- irr/src/CIrrDeviceSDL.h
+++ irr/src/CIrrDeviceSDL.h
@@ -327,7 +327,7 @@ private:
 
 	core::rect<s32> lastElemPos;
 
-	std::unordered_map<s32, EKEY_CODE> KeyMap;
+	std::unordered_map<SDL_Keycode, EKEY_CODE> KeyMap;
 	SDL_SysWMinfo Info;
 
 	s32 CurrentTouchCount;
