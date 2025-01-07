0b44f5383326a8b6eaa921684316228a1dd68d25
y5nw <37980625+y5nw@users.noreply.github.com>
2025-01-07 17:36:44 +0100

    Use scancode hack for non-stub/SDL devices

--- src/client/keycode.h
+++ src/client/keycode.h
@@ -28,48 +28,32 @@ public:
 	irr::EKEY_CODE getKeycode() const;
 	wchar_t getKeychar() const;
 
-	//TODO: drop this once we fully switch to SDL
 	u32 getScancode() const
 	{
-#if USE_SDL2
 		if (auto pv = std::get_if<u32>(&scancode))
 			return *pv;
-#endif
 		return 0;
 	}
 
 	bool operator==(const KeyPress &o) const
 	{
-#if USE_SDL2
 		return scancode == o.scancode;
-#else
-		return (Char > 0 && Char == o.Char) || (Keycode::isValid(Key) && Key == o.Key);
-#endif
 	}
 
 	operator bool() const
 	{
-#if USE_SDL2
 		return std::holds_alternative<irr::EKEY_CODE>(scancode) ?
 			Keycode::isValid(std::get<irr::EKEY_CODE>(scancode)) :
 			std::get<u32>(scancode) != 0;
-#else
-		return Char > 0 || Keycode::isValid(Key);
-#endif
 	}
 
 	static const KeyPress &getSpecialKey(const std::string &name);
 
 private:
-#if USE_SDL2
 	bool loadFromScancode(std::string_view name);
 	std::string formatScancode() const;
 
 	std::variant<u32, irr::EKEY_CODE> scancode = irr::KEY_UNKNOWN;
-#else
-	irr::EKEY_CODE Key = irr::KEY_KEY_CODES_COUNT;
-	wchar_t Char = L'\0';
-#endif
 
 	static std::unordered_map<std::string, KeyPress> specialKeyCache;
 };
