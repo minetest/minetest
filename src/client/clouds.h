e17eb7007ab7a4c8d0d2aa1a465a580090a1a6fe
wozrer <wozrer@proton.me>
2025-02-03 20:33:03 +0300

    Debloat

--- src/client/clouds.h
+++ src/client/clouds.h
@@ -4,7 +4,9 @@
 
 #pragma once
 
-#include "irrlichttypes_bloated.h"
+#include "irr_v3d.h"
+#include "irr_v2d.h"
+#include "irr_aabb3d.h"
 #include "constants.h"
 #include "irr_ptr.h"
 #include "skyparams.h"
