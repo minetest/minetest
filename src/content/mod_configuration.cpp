4cb930ad0f1ecd539f43c316c2b93a42c02ff87d
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2025-02-05 22:37:01 +0100

    no comment

--- src/content/mod_configuration.cpp
+++ src/content/mod_configuration.cpp
@@ -140,8 +140,6 @@ void ModConfiguration::addModsFromConfig(
 	 *
 	 * Alternative candidates for a modname are stored in `candidates`,
 	 * and used in an error message later.
-	 *
-	 * If not enabled, add `load_mod_modname = false` to world.mt
 	 */
 	for (const auto &modPath : modPaths) {
 		std::vector<ModSpec> addon_mods_in_path = flattenMods(getModsInPath(modPath.second, modPath.first));
