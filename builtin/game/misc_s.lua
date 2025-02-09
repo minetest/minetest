dd0070a6b8a28e2fe2ac591b3ea96813ba9ffcb0
ROllerozxa <rollerozxa@voxelmanip.se>
2025-02-09 18:09:07 +0100

    Expose client version information in non-debug builds (#15708)
    Co-authored-by: SmallJoker <SmallJoker@users.noreply.github.com>
    Co-authored-by: Lars Mueller <appgurulars@gmx.de>
    Co-authored-by: sfan5 <sfan5@live.de>

--- builtin/game/misc_s.lua
+++ builtin/game/misc_s.lua
@@ -112,3 +112,25 @@ if core.set_push_moveresult1 then
 	end)
 	core.set_push_moveresult1 = nil
 end
+
+-- Protocol version table
+-- see also src/network/networkprotocol.cpp
+core.protocol_versions = {
+	["5.0.0"] = 37,
+	["5.1.0"] = 38,
+	["5.2.0"] = 39,
+	["5.3.0"] = 39,
+	["5.4.0"] = 39,
+	["5.5.0"] = 40,
+	["5.6.0"] = 41,
+	["5.7.0"] = 42,
+	["5.8.0"] = 43,
+	["5.9.0"] = 44,
+	["5.9.1"] = 45,
+	["5.10.0"] = 46,
+	["5.11.0"] = 47,
+}
+
+setmetatable(core.protocol_versions, {__newindex = function()
+	error("core.protocol_versions is read-only")
+end})
