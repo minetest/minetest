6a96aaa408122312d4a5dbb9cfd43c7a2be05136
Andrii Nemchenko <62670490+andriyndev@users.noreply.github.com>
2025-02-09 23:04:42 +0200

    Update src/inventory.cpp
    Co-authored-by: sfan5 <sfan5@live.de>

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -697,7 +697,7 @@ ItemStack InventoryList::removeItem(const ItemStack &item, bool match_meta)
 {
 	ItemStack removed;
 	for (auto i = m_items.rbegin(); i != m_items.rend(); ++i) {
-		if (i->name == item.name && (!match_meta || (i->metadata == item.metadata))) {
+		if (i->name == item.name && (!match_meta || i->metadata == item.metadata)) {
 			u32 still_to_remove = item.count - removed.count;
 			ItemStack leftover = removed.addItem(i->takeItem(still_to_remove),
 					m_itemdef);
