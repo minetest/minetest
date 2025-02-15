4eac096698d5e2e9720d791e54bff453d5bb1cc8
Nell <110815737+Nelli0180@users.noreply.github.com>
2025-02-16 01:14:32 +0300

    Make tab_about.lua more robust and readable
    This commit improves the `tab_about.lua` file to make it more robust and easier to work with.
    
    The main changes:
    - If `credits.json` is missing or broken, the menu won't crash. Instead, it just shows empty credits and logs an error. This is way friendlier for modders and players.
    - The code is now cleaner and easier to read:
      - Variables have better names (like `renderer` instead of `s1`).
      - Used `table.insert` instead of manual indexing because it's more obvious what's happening.
      - Simplified the OpenGL version extraction logic.
    
    Why these changes?
    - Nobody likes crashes, especially over something as simple as a missing file.
    - Cleaner code means fewer bugs and less headache for anyone who works on this in the future.
    - It's just nicer to look at now.
    
    What happens if `credits.json` is missing?
    - Nothing bad! The menu just shows empty credits and keeps running like nothing happened. You might see a warning in the log, but that's it.

--- builtin/mainmenu/tab_about.lua
+++ builtin/mainmenu/tab_about.lua
@@ -17,123 +17,126 @@
 
 
 local function prepare_credits(dest, source)
-	local string = table.concat(source, "\n") .. "\n"
-
-	string = core.hypertext_escape(string)
-	string = string:gsub("%[.-%]", "<gray>%1</gray>")
-
-	table.insert(dest, string)
+    local credit_string = table.concat(source, "\n") .. "\n"
+    credit_string = core.hypertext_escape(credit_string)
+    credit_string = credit_string:gsub("%[.-%]", "<gray>%1</gray>")
+    table.insert(dest, credit_string)
 end
 
 local function get_credits()
-	local f = assert(io.open(core.get_mainmenu_path() .. "/credits.json"))
-	local json = core.parse_json(f:read("*all"))
-	f:close()
-	return json
+    local credits_file = core.get_mainmenu_path() .. "/credits.json"
+    local file, err = io.open(credits_file, "r")
+    if not file then
+        core.log("error", "Could not open credits file: " .. err)
+        return {}
+    end
+    local content = file:read("*all")
+    file:close()
+    return core.parse_json(content) or {}
 end
 
 local function get_renderer_info()
-	local ret = {}
+    local ret = {}
 
-	-- OpenGL version, stripped to just the important part
-	local s1 = core.get_active_renderer()
-	if s1:sub(1, 7) == "OpenGL " then
-		s1 = s1:sub(8)
-	end
-	local m = s1:match("^[%d.]+")
-	if not m then
-		m = s1:match("^ES [%d.]+")
-	end
-	ret[#ret+1] = m or s1
-	-- video driver
-	ret[#ret+1] = core.get_active_driver():lower()
-	-- irrlicht device
-	ret[#ret+1] = core.get_active_irrlicht_device():upper()
+    -- OpenGL version, stripped to just the important part
+    local renderer = core.get_active_renderer()
+    if renderer:sub(1, 7) == "OpenGL " then
+        renderer = renderer:sub(8)
+    end
+    local version_match = renderer:match("^[%d.]+") or renderer:match("^ES [%d.]+")
+    table.insert(ret, version_match or renderer)
+    -- video driver
+    table.insert(ret, core.get_active_driver():lower())
+    -- irrlicht device
+    table.insert(ret, core.get_active_irrlicht_device():upper())
 
-	return table.concat(ret, " / ")
+    return table.concat(ret, " / ")
 end
 
 return {
-	name = "about",
-	caption = fgettext("About"),
+    name = "about",
+    caption = fgettext("About"),
 
-	cbf_formspec = function(tabview, name, tabdata)
-		local logofile = defaulttexturedir .. "logo.png"
-		local version = core.get_version()
+    cbf_formspec = function(tabview, name, tabdata)
+        local logofile = defaulttexturedir .. "logo.png"
+        local version = core.get_version()
 
-		local hypertext = {
-			"<tag name=heading color=#ff0>",
-			"<tag name=gray color=#aaa>",
-		}
+        local hypertext = {
+            "<tag name=heading color=#ff0>",
+            "<tag name=gray color=#aaa>",
+        }
 
-		local credits = get_credits()
+        local credits = get_credits()
 
-		table.insert_all(hypertext, {
-			"<heading>", fgettext_ne("Core Developers"), "</heading>\n",
-		})
-		prepare_credits(hypertext, credits.core_developers)
-		table.insert_all(hypertext, {
-			"\n",
-			"<heading>", fgettext_ne("Core Team"), "</heading>\n",
-		})
-		prepare_credits(hypertext, credits.core_team)
-		table.insert_all(hypertext, {
-			"\n",
-			"<heading>", fgettext_ne("Active Contributors"), "</heading>\n",
-		})
-		prepare_credits(hypertext, credits.contributors)
-		table.insert_all(hypertext, {
-			"\n",
-			"<heading>", fgettext_ne("Previous Core Developers"), "</heading>\n",
-		})
-		prepare_credits(hypertext, credits.previous_core_developers)
-		table.insert_all(hypertext, {
-			"\n",
-			"<heading>", fgettext_ne("Previous Contributors"), "</heading>\n",
-		})
-		prepare_credits(hypertext, credits.previous_contributors)
+        table.insert_all(hypertext, {
+            "<heading>", fgettext_ne("Core Developers"), "</heading>\n",
+        })
+        prepare_credits(hypertext, credits.core_developers or {})
+        table.insert_all(hypertext, {
+            "\n",
+            "<heading>", fgettext_ne("Core Team"), "</heading>\n",
+        })
+        prepare_credits(hypertext, credits.core_team or {})
+        table.insert_all(hypertext, {
+            "\n",
+            "<heading>", fgettext_ne("Active Contributors"), "</heading>\n",
+        })
+        prepare_credits(hypertext, credits.contributors or {})
+        table.insert_all(hypertext, {
+            "\n",
+            "<heading>", fgettext_ne("Previous Core Developers"), "</heading>\n",
+        })
+        prepare_credits(hypertext, credits.previous_core_developers or {})
+        table.insert_all(hypertext, {
+            "\n",
+            "<heading>", fgettext_ne("Previous Contributors"), "</heading>\n",
+        })
+        prepare_credits(hypertext, credits.previous_contributors or {})
 
-		hypertext = table.concat(hypertext):sub(1, -2)
+        hypertext = table.concat(hypertext):sub(1, -2)
 
-		local fs = "image[1.5,0.6;2.5,2.5;" .. core.formspec_escape(logofile) .. "]" ..
-			"style[label_button;border=false]" ..
-			"button[0.1,3.4;5.3,0.5;label_button;" ..
-			core.formspec_escape(version.project .. " " .. version.string) .. "]" ..
-			"button_url[1.5,4.1;2.5,0.8;homepage;luanti.org;https://www.luanti.org/]" ..
-			"hypertext[5.5,0.25;9.75,6.6;credits;" .. core.formspec_escape(hypertext) .. "]"
+        local fs = [[
+            image[1.5,0.6;2.5,2.5;]] .. core.formspec_escape(logofile) .. [[]
+            style[label_button;border=false]
+            button[0.1,3.4;5.3,0.5;label_button;]] .. core.formspec_escape(version.project .. " " .. version.string) .. [[]
+            button_url[1.5,4.1;2.5,0.8;homepage;luanti.org;https://www.luanti.org/]
+            hypertext[5.5,0.25;9.75,6.6;credits;]] .. core.formspec_escape(hypertext) .. [[]
+        ]]
 
-		local active_renderer_info = fgettext("Active renderer:") .. "\n" ..
-			core.formspec_escape(get_renderer_info())
-		fs = fs .. "style[label_button2;border=false]" ..
-			"button[0.1,6;5.3,1;label_button2;" .. active_renderer_info .. "]"..
-			"tooltip[label_button2;" .. active_renderer_info .. "]"
+        local active_renderer_info = fgettext("Active renderer:") .. "\n" .. core.formspec_escape(get_renderer_info())
+        fs = fs .. [[
+            style[label_button2;border=false]
+            button[0.1,6;5.3,1;label_button2;]] .. active_renderer_info .. [[]
+            tooltip[label_button2;]] .. active_renderer_info .. [[]
+        ]]
 
-		if PLATFORM == "Android" then
-			fs = fs .. "button[0.5,5.1;4.5,0.8;share_debug;" .. fgettext("Share debug log") .. "]"
-		else
-			fs = fs .. "tooltip[userdata;" ..
-					fgettext("Opens the directory that contains user-provided worlds, games, mods,\n" ..
-							"and texture packs in a file manager / explorer.") .. "]"
-			fs = fs .. "button[0.5,5.1;4.5,0.8;userdata;" .. fgettext("Open User Data Directory") .. "]"
-		end
+        if PLATFORM == "Android" then
+            fs = fs .. "button[0.5,5.1;4.5,0.8;share_debug;" .. fgettext("Share debug log") .. "]"
+        else
+            fs = fs .. [[
+                tooltip[userdata;]] .. fgettext("Opens the directory that contains user-provided worlds, games, mods,\n" ..
+                        "and texture packs in a file manager / explorer.") .. [[]
+                button[0.5,5.1;4.5,0.8;userdata;]] .. fgettext("Open User Data Directory") .. [[]
+            ]]
+        end
 
-		return fs
-	end,
+        return fs
+    end,
 
-	cbf_button_handler = function(this, fields, name, tabdata)
-		if fields.share_debug then
-			local path = core.get_user_path() .. DIR_DELIM .. "debug.txt"
-			core.share_file(path)
-		end
+    cbf_button_handler = function(this, fields, name, tabdata)
+        if fields.share_debug then
+            local path = core.get_user_path() .. DIR_DELIM .. "debug.txt"
+            core.share_file(path)
+        end
 
-		if fields.userdata then
-			core.open_dir(core.get_user_path())
-		end
-	end,
+        if fields.userdata then
+            core.open_dir(core.get_user_path())
+        end
+    end,
 
-	on_change = function(type)
-		if type == "ENTER" then
-			mm_game_theme.set_engine()
-		end
-	end,
+    on_change = function(type)
+        if type == "ENTER" then
+            mm_game_theme.set_engine()
+        end
+    end,
 }
