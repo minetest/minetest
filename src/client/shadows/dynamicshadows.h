minetest/minetest

commit b861f0c5c59546b64e937fdf384634175df4a372

Fix shadow flicker on camera offset update (#15709)

src/client/shadows/dynamicshadows.h
@@ -34,9 +34,9 @@ public:
 			f32 farValue = 100.0f);
 	~DirectionalLight() = default;
 
-	//DISABLE_CLASS_COPY(DirectionalLight)
+	void updateCameraOffset(const Camera *cam);
 
-	void update_frustum(const Camera *cam, Client *client, bool force = false);
+	void updateFrustum(const Camera *cam, Client *client);
 
 	// when set direction is updated to negative normalized(direction)
 	void setDirection(v3f dir);

src/client/shadows/dynamicshadowsrender.cpp
@@ -256,11 +256,14 @@ void ShadowRenderer::updateSMTextures()
 
 		// detect if SM should be regenerated
 		for (DirectionalLight &light : m_light_list) {
-			if (light.should_update_map_shadow || m_force_update_shadow_map) {
-				light.should_update_map_shadow = false;
-				m_current_frame = 0;
-				reset_sm_texture = true;
-			}
+			if (light.should_update_map_shadow)
+				m_force_update_shadow_map = true;
+			light.should_update_map_shadow = false;
+		}
+
+		if (m_force_update_shadow_map) {
+			m_current_frame = 0;
+			reset_sm_texture = true;
 		}
 
 		video::ITexture* shadowMapTargetTexture = shadowMapClientMapFuture;

src/client/game.cpp
@@ -2984,6 +2984,10 @@ void Game::updateCameraOffset()
 		return;
 
 	if (!m_flags.disable_camera_update) {
+		auto *shadow = RenderingEngine::get_shadow_renderer();
+		if (shadow)
+			shadow->getDirectionalLight().updateCameraOffset(camera);
+
 		env.getClientMap().updateCamera(camera->getPosition(),
 			camera->getDirection(), camera->getFovMax(), camera_offset,
 			env.getLocalPlayer()->light_color);
@@ -3978,11 +3982,10 @@ void Game::updateShadows()
 	v3f light = is_day ? sky->getSunDirection() : sky->getMoonDirection();
 
 	v3f sun_pos = light * offset_constant;
-
 	shadow->getDirectionalLight().setDirection(sun_pos);
 	shadow->setTimeOfDay(in_timeofday);
 
-	shadow->getDirectionalLight().update_frustum(camera, client, m_camera_offset_changed);
+	shadow->getDirectionalLight().updateFrustum(camera, client);
 }
 
 void Game::drawScene(ProfilerGraph *graph, RunStats *stats)

src/client/shadows/dynamicshadows.cpp
@@ -36,7 +36,7 @@ void DirectionalLight::createSplitMatrices(const Camera *cam)
 	// adjusted camera positions
 	v3f cam_pos_world = cam->getPosition();
 
-	// if world position is less than 1 block away from the captured
+	// if world position is less than 1 node away from the captured
 	// world position then stick to the captured value, otherwise recapture.
 	if (cam_pos_world.getDistanceFromSQ(last_cam_pos_world) < BS * BS)
 		cam_pos_world = last_cam_pos_world;
@@ -84,9 +84,16 @@ DirectionalLight::DirectionalLight(const u32 shadowMapResolution,
 		farPlane(farValue), mapRes(shadowMapResolution), pos(position)
 {}
 
-void DirectionalLight::update_frustum(const Camera *cam, Client *client, bool force)
+void DirectionalLight::updateCameraOffset(const Camera *cam)
 {
-	if (dirty && !force)
+	createSplitMatrices(cam);
+	should_update_map_shadow = true;
+	dirty = true;
+}
+
+void DirectionalLight::updateFrustum(const Camera *cam, Client *client)
+{
+	if (dirty)
 		return;
 
 	float zNear = cam->getCameraNode()->getNearValue();
@@ -106,16 +113,6 @@ void DirectionalLight::update_frustum(const Camera *cam, Client *client, bool fo
 			getPosition(), getDirection(), future_frustum.radius, future_frustum.length);
 	should_update_map_shadow = true;
 	dirty = true;
-
-	// when camera offset changes, adjust the current frustum view matrix to avoid flicker
-	v3s16 cam_offset = cam->getOffset();
-	if (cam_offset != shadow_frustum.camera_offset) {
-		v3f rotated_offset = shadow_frustum.ViewMat.rotateAndScaleVect(
-				intToFloat(cam_offset - shadow_frustum.camera_offset, BS));
-		shadow_frustum.ViewMat.setTranslation(shadow_frustum.ViewMat.getTranslation() + rotated_offset);
-		shadow_frustum.player += intToFloat(shadow_frustum.camera_offset - cam->getOffset(), BS);
-		shadow_frustum.camera_offset = cam_offset;
-	}
 }
 
 void DirectionalLight::commitFrustum()

commit e8b9fe85e9aa1a4f4a0edbeee4bca28d4e199626

Fix shadow performance regression due to force update
broken by: b861f0c5c59546b64e937fdf384634175df4a372

src/client/shadows/dynamicshadows.h
@@ -85,6 +85,7 @@ public:
 		return mapRes;
 	}
 
+	/// If true, shadow map needs to be invalidated due to frustum change
 	bool should_update_map_shadow{true};
 
 	void commitFrustum();

src/client/shadows/dynamicshadowsrender.cpp
@@ -254,17 +254,14 @@ void ShadowRenderer::updateSMTextures()
 	if (!m_shadow_node_array.empty()) {
 		bool reset_sm_texture = false;
 
-		// detect if SM should be regenerated
+		// clear texture if requested
 		for (DirectionalLight &light : m_light_list) {
-			if (light.should_update_map_shadow)
-				m_force_update_shadow_map = true;
+			reset_sm_texture |= light.should_update_map_shadow;
 			light.should_update_map_shadow = false;
 		}
 
-		if (m_force_update_shadow_map) {
+		if (reset_sm_texture || m_force_update_shadow_map)
 			m_current_frame = 0;
-			reset_sm_texture = true;
-		}
 
 		video::ITexture* shadowMapTargetTexture = shadowMapClientMapFuture;
 		if (shadowMapTargetTexture == nullptr)
@@ -273,7 +270,7 @@ void ShadowRenderer::updateSMTextures()
 		// Update SM incrementally:
 		for (DirectionalLight &light : m_light_list) {
 			// Static shader values.
-			for (auto cb : {m_shadow_depth_cb, m_shadow_depth_entity_cb, m_shadow_depth_trans_cb})
+			for (auto cb : {m_shadow_depth_cb, m_shadow_depth_entity_cb, m_shadow_depth_trans_cb}) {
 				if (cb) {
 					cb->MapRes = (f32)m_shadow_map_texture_size;
 					cb->MaxFar = (f32)m_shadow_map_max_distance * BS;
@@ -281,12 +278,9 @@ void ShadowRenderer::updateSMTextures()
 					cb->PerspectiveBiasZ = getPerspectiveBiasZ();
 					cb->CameraPos = light.getFuturePlayerPos();
 				}
+			}
 
-			// set the Render Target
-			// right now we can only render in usual RTT, not
-			// Depth texture is available in irrlicth maybe we
-			// should put some gl* fn here
-
+			// Note that force_update means we're drawing everything one go.
 
 			if (m_current_frame < m_map_shadow_update_frames || m_force_update_shadow_map) {
 				m_driver->setRenderTarget(shadowMapTargetTexture, reset_sm_texture, true,

src/client/shadows/dynamicshadowsrender.h
@@ -68,6 +68,7 @@ public:
 	void removeNodeFromShadowList(scene::ISceneNode *node);
 
 	void update(video::ITexture *outputTarget = nullptr);
+	/// Force shadow map to be re-drawn in one go next frame
 	void setForceUpdateShadowMap() { m_force_update_shadow_map = true; }
 	void drawDebug();
 