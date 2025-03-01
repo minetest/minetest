9645f258cabc8b8b040939a7592761d783ad1358
sfan5 <sfan5@live.de>
2025-03-01 22:13:33 +0100

    Fix cloud-related bugs
    First, this reverts 56123b2fbe1ad4e7878556f8d51c551d84fb92e7,
    which un-fixes #15031 but fixes #15798 and #15854.
    
    Then we disable culling for the cloud scene node which fixes #15031 again.

--- src/client/clouds.h
+++ src/client/clouds.h
@@ -134,8 +134,11 @@ private:
 	{
 		float height_bs    = m_params.height    * BS;
 		float thickness_bs = m_params.thickness * BS;
-		m_box = aabb3f(-BS * 1000000.0f, height_bs, -BS * 1000000.0f,
-				BS * 1000000.0f, height_bs + thickness_bs, BS * 1000000.0f);
+		float far_bs       = 1000000.0f         * BS;
+		m_box = aabb3f(-far_bs, height_bs, -far_bs,
+			far_bs, height_bs + thickness_bs, far_bs);
+		m_box.MinEdge -= v3f::from(m_camera_offset) * BS;
+		m_box.MaxEdge -= v3f::from(m_camera_offset) * BS;
 	}
 
 	void updateMesh();
