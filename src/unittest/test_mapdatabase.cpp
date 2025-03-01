b184e820817d3cacb6181c5ef87885809a81acf8
Erich Schubert <erich.schubert@tu-dortmund.de>
2025-03-01 18:27:57 +0100

    add unit tests for map block position encoding

--- src/unittest/test_mapdatabase.cpp
+++ src/unittest/test_mapdatabase.cpp
@@ -72,6 +72,7 @@ public:
 	void testLoad();
 	void testList(int expect);
 	void testRemove();
+	void testPositionEncoding();
 
 private:
 	MapDatabaseProvider *provider = nullptr;
@@ -89,6 +90,8 @@ void TestMapDatabase::runTests(IGameDef *gamedef)
 	for (int c = 255; c >= 0; c--)
 		test_data.push_back(static_cast<char>(c));
 	sanity_check(!test_data.empty());
+
+	TEST(testPositionEncoding);
 
 	rawstream << "-------- Dummy" << std::endl;
 
@@ -192,4 +195,32 @@ void TestMapDatabase::testRemove()
 	// failed remove
 	// FIXME: this isn't working consistently, maybe later
 	//UASSERT(!db->deleteBlock({1, 2, 4}));
+}
+
+void TestMapDatabase::testPositionEncoding()
+{
+	auto db = std::make_unique<Database_Dummy>();
+
+	// Unit vectors and extremes
+	UASSERTEQ(s64, db->getBlockAsInteger({0, 0, 0}), 0)
+	UASSERTEQ(s64, db->getBlockAsInteger({1, 0, 0}), 1)
+	UASSERTEQ(s64, db->getBlockAsInteger({0, 1, 0}), 0x1000)
+	UASSERTEQ(s64, db->getBlockAsInteger({0, 0, 1}), 0x1000000)
+	UASSERTEQ(s64, db->getBlockAsInteger({-1, 0, 0}), -1)
+	UASSERTEQ(s64, db->getBlockAsInteger({0, -1, 0}), -0x1000)
+	UASSERTEQ(s64, db->getBlockAsInteger({0, 0, -1}), -0x1000000)
+	UASSERTEQ(s64, db->getBlockAsInteger({2047, 2047, 2047}), 0x7FF7FF7FF)
+	UASSERTEQ(s64, db->getBlockAsInteger({-2048, -2048, -2048}), -0x800800800)
+	UASSERTEQ(s64, db->getBlockAsInteger({-123, 456, -789}), -0x314e3807b)
+
+	UASSERT(db->getIntegerAsBlock(0) == v3s16(0, 0, 0))
+	UASSERT(db->getIntegerAsBlock(1) == v3s16(1, 0, 0))
+	UASSERT(db->getIntegerAsBlock(0x1000) == v3s16(0, 1, 0))
+	UASSERT(db->getIntegerAsBlock(0x1000000) == v3s16(0, 0, 1))
+	UASSERT(db->getIntegerAsBlock(-1) == v3s16(-1, 0, 0))
+	UASSERT(db->getIntegerAsBlock(-0x1000) == v3s16(0, -1, 0))
+	UASSERT(db->getIntegerAsBlock(-0x1000000) == v3s16(0, 0, -1))
+	UASSERT(db->getIntegerAsBlock(0x7FF7FF7FF) == v3s16(2047, 2047, 2047))
+	UASSERT(db->getIntegerAsBlock(-0x800800800) == v3s16(-2048, -2048, -2048))
+	UASSERT(db->getIntegerAsBlock(-0x314e3807b) == v3s16(-123, 456, -789))
 }
