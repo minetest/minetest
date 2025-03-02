60abba1316fab656aa29af32e01d62b0242c6866
Medley <198984680+maplemedley@users.noreply.github.com>
2025-03-02 16:00:03 +0100

    Disable workaround when any debugger is present
    Not just when the NDEBUG flag is set

--- src/gettext.cpp
+++ src/gettext.cpp
@@ -170,12 +170,10 @@ void init_gettext(const char *path, const std::string &configured_language,
 		SetEnvironmentVariableA("LANGUAGE", configured_language.c_str());
 
 #if CHECK_CLIENT_BUILD()
-#if NDEBUG
 		// Hack to force gettext to see the right environment
-		// Disabled during debug as it can break debugging
-		if (current_language != configured_language)
+		// Disabled when debugger is present as it can break debugging 
+		if (current_language != configured_language && !IsDebuggerPresent())
 			MSVC_LocaleWorkaround(argc, argv);
-#endif
 #else
 		errorstream << "*******************************************************" << std::endl;
 		errorstream << "Can't apply locale workaround for server!" << std::endl;
